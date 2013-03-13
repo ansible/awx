14b0a1ed27636e4719dc881d54aeac4b6b667948
Michael DeHaan <michael.dehaan@gmail.com>
2013-03-13 15:32:22 -0400

    Rename acom to lib to avoid confusion with repo name.

--- manage.py
+++ manage.py
@@ -4,6 +4,6 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acom.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lib.settings')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
