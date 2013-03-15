159dd542df8d90a8938394c23d5fe137c5d2df85
Michael DeHaan <michael.dehaan@gmail.com>
2013-03-15 10:21:56 -0400

    Add Django admin stubs for all existing model objects

--- lib/main/admin.py
+++ lib/main/admin.py
@@ -11,7 +11,66 @@ class InventoryAdmin(admin.ModelAdmin):
 
     list_display = ('name', 'description', 'active')
 
+class TagAdmin(admin.ModelAdmin):
+
+    list_display = ('name', )
+
+class AuditTrailAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'description', 'active')
+
+class HostAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'description', 'active')
+
+class GroupAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'description', 'active')
+
+class VariableDataAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'description', 'active')
+
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'description', 'active')
+
+class CredentialAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'description', 'active')
+
+class TeamAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'description', 'active')
+
+class ProjectAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'description', 'active')
+
+class PermissionAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'description', 'active')
+
+class LaunchJobAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'description', 'active')
+
+class LaunchJobStatusAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'description', 'active')
+
 # FIXME: Add the rest of the models...
 
 admin.site.register(Organization, OrganizationAdmin)
 admin.site.register(Inventory, InventoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(AuditTrail, AuditTrailAdmin)
+admin.site.register(Host, HostAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(VariableData, VariableDataAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Credential, CredentialAdmin)
+admin.site.register(LaunchJob, LaunchJobStatusAdmin)
+admin.site.register(LaunchJobStatus, LaunchJobStatusAdmin)
