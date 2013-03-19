23e685bba8f61c4c1bacce4963b075be98bcc833
Michael DeHaan <Michael DeHaan michael@ansibleworks.com>
2013-03-19 10:27:14 -0400

    make test database use sqlite

--- lib/settings/defaults.py
+++ lib/settings/defaults.py
@@ -29,10 +29,10 @@ DATABASES = {
     }
 }
 
-if 'test' in sys.argv:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3'
-    }
+#if 'test' in sys.argv:
+#    DATABASES['default'] = {
+#        'ENGINE': 'django.db.backends.sqlite3'
+#    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
