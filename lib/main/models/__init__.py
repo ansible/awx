a071ff65e402e56faa2acac504b15f8613b31f68
Michael DeHaan <Michael DeHaan michael@ansibleworks.com>
2013-03-23 18:43:11 -0400

    Add copyright headers.

--- lib/main/models/__init__.py
+++ lib/main/models/__init__.py
@@ -1,3 +1,21 @@
+# (c) 2013, AnsibleWorks, Michael DeHaan <michael@ansibleworks.com>
+#
+# This file is part of Ansible Commander
+#
+# Ansible Commander is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible Commander is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible Commander.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from django.db import models
 from django.db.models import CASCADE, SET_NULL, PROTECT
 from django.utils.translation import ugettext_lazy as _
