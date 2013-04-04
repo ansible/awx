97a83c7e0d4803383c92b72b027fec43e8f27bde
Michael DeHaan <michael@ansibleworks.com>
2013-04-04 15:57:58 -0400

    Add license for ansible plugin (not part of commander or ansible)

--- lib/plugins/callback/acom_callback.py
+++ lib/plugins/callback/acom_callback.py
@@ -1,20 +1,34 @@
 # Copyright (c) 2013 AnsibleWorks, Inc.
+# This file is a utility Ansible plugin that is not part of Ansible Commander or Ansible.
+# (it does not import any ansible-commander code, nor does it's license apply to Ansible
+# or Ansible Commander)
+# 
+# Copyright (c) 2013, AnsibleWorks Inc.
+# All rights reserved.
 #
-# This file is part of Ansible Commander
+# Redistribution and use in source and binary forms, with or without modification, 
+# are permitted provided that the following conditions are met:
+# 
+# Redistributions of source code must retain the above copyright notice, this list 
+# of conditions and the following disclaimer.
 #
-# Ansible Commander is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+#    Redistributions in binary form must reproduce the above copyright notice, 
+# this list of conditions and the following disclaimer in the documentation and/or 
+# other materials provided with the distribution.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+#    Neither the name of the <ORGANIZATION> nor the names of its contributors may be 
+# used to endorse or promote products derived from this software without specific 
+# prior written permission.
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+# OF SUCH DAMAGE.
 
 import json
 import os
@@ -103,3 +117,5 @@ class CallbackModule(object):
         for attr in ('changed', 'dark', 'failures', 'ok', 'processed', 'skipped'):
             d[attr] = getattr(stats, attr)
         self._log_event('playbook_on_stats', **d)
+
+
