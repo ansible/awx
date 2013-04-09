09cca99c696fe83e5b1b4ed3f83ab6f858245c25
Michael DeHaan <michael@ansibleworks.com>
2013-04-09 01:05:55 -0400

    Just use plain GPL.

--- lib/main/rbac.py
+++ lib/main/rbac.py
@@ -1,19 +1,19 @@
 # Copyright (c) 2013 AnsibleWorks, Inc.
 #
-# This file is part of Ansible Commander
-#
+# This file is part of Ansible Commander.
+# 
 # Ansible Commander is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License. 
 #
-# This program is distributed in the hope that it will be useful,
+# Ansible Commander is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Ansible Commander. If not, see <http://www.gnu.org/licenses/>.
+
 
 from lib.main.models import *
 from lib.main.serializers import *
--- lib/main/tests/projects.py
+++ lib/main/tests/projects.py
@@ -1,19 +1,18 @@
-# (c) 2013, AnsibleWorks
+# Copyright (c) 2013 AnsibleWorks, Inc.
 #
-# This file is part of Ansible Commander
-#
+# This file is part of Ansible Commander.
+# 
 # Ansible Commander is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# the Free Software Foundation, version 3 of the License. 
 #
 # Ansible Commander is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
-#
+# 
 # You should have received a copy of the GNU General Public License
-# along with Ansible Commander.  If not, see <http://www.gnu.org/licenses/>.
+# along with Ansible Commander. If not, see <http://www.gnu.org/licenses/>.
 
 import datetime
 import json
