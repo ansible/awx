27ac9a206e582c8258995bb4abff0d3f41ca1c1d
Chris Church <chris@ninemoreminutes.com>
2013-04-16 16:18:25 -0400

    Added migration dependency.

--- lib/main/migrations/0004_changes.py
+++ lib/main/migrations/0004_changes.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ('djcelery', '0001_initial'),
+    )
+
     def forwards(self, orm):
         # Adding model 'LaunchJobStatusEvent'
         db.create_table(u'main_launchjobstatusevent', (
