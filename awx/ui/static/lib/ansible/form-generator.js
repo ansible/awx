7246ab267481b0003a755dda0cce7a2d4e74400d
chouseknecht <chouse@ansibleworks.com>
2013-09-10 17:12:36 -0400

    AC-441,442,443

--- awx/ui/static/lib/ansible/form-generator.js
+++ awx/ui/static/lib/ansible/form-generator.js
@@ -90,7 +90,12 @@ angular.module('FormGenerator', ['GeneratorHelpers', 'ngCookies'])
 
        // Prepend an asterisk to required field label
        $('.form-control[required]').each(function() {
-            var txt = $(this).parent().parent().find('label').prepend('* ');
+            var label = $(this).parent().parent().find('label');
+            var rgx = /^\*/;
+            if ( !rgx.test(label.text()) ) {
+               // required field does not have leading *
+               label.prepend('* ');
+            }
             });
 
        if (options.modal) {
