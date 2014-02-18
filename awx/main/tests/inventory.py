11cfacd57ad844ba603a4edf109f6fabc0269e8a
Matthew Jones <bsdmatburt@gmail.com>
2014-02-18 13:54:56 -0500

    Update unit tests to manage zeromq based tasks

--- awx/main/tests/inventory.py
+++ awx/main/tests/inventory.py
@@ -991,7 +991,12 @@ class InventoryUpdatesTest(BaseTransactionTest):
         self.group = self.inventory.groups.create(name='Cloud Group')
         self.inventory2 = self.organization.inventories.create(name='Cloud Inventory 2')
         self.group2 = self.inventory2.groups.create(name='Cloud Group 2')
+        self.start_queue(settings.CALLBACK_CONSUMER_PORT, settings.CALLBACK_QUEUE_PORT)
 
+    def tearDown(self):
+        super(InventoryUpdatesTest, self).tearDown()
+        self.terminate_queue()
+        
     def update_inventory_source(self, group, **kwargs):
         inventory_source = group.inventory_source
         update_fields = []
