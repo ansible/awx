6f54574a941c3a4b41ed15a55a43a8de0ed26bf4
Chris Houseknecht <chouse@ansible.com>
2014-02-19 13:18:44 -0500

    Fixed spelling issue found in job_templates.limit help.

--- awx/ui/static/js/forms/JobTemplates.js
+++ awx/ui/static/js/forms/JobTemplates.js
@@ -157,7 +157,7 @@ angular.module('JobTemplateFormDefinition', [])
                 column: 1,
                 awPopOver: "<p>Provide a host pattern to further constrain the list of hosts that will be managed or affected by the playbook. " +
                     "Multiple patterns can be separated by &#59; &#58; or &#44;</p><p>For more information and examples see " +
-                    "<a href=\"http://docs.ansible.com/intro_patterns.html\" target=\"_blank\">the Patters topic at docs.ansible.com</a>.</p>",
+                    "<a href=\"http://docs.ansible.com/intro_patterns.html\" target=\"_blank\">the Patterns topic at docs.ansible.com</a>.</p>",
                 dataTitle: 'Limit',
                 dataPlacement: 'right',
                 dataContainer: "body"
