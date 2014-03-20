e66fa7cead503c0ff01cd1b62a1229470c2447a9
Matthew Jones <mat@matburt.net>
2014-03-20 19:23:10 -0400

    Fix up some bugs for the UI's dummy endpoints

--- awx/api/views.py
+++ awx/api/views.py
@@ -93,7 +93,7 @@ class ApiV1RootView(APIView):
         data['job_templates'] = reverse('api:job_template_list')
         data['jobs'] = reverse('api:job_list')
         data['schedules'] = reverse('api:schedules_list')
-        data['unified_job_templates'] = reverse('api:unified_job_templates')
+        data['unified_job_templates'] = reverse('api:unified_job_template_list')
         data['activity_stream'] = reverse('api:activity_stream_list')
         return Response(data)
 
@@ -321,7 +321,7 @@ class UnifiedJobTemplateList(APIView):
                  "modified": "2014-03-19T17:44:30.726Z", 
                  "inventory_source": 11, 
                  "status": "successful", 
-                 "failed": false, 
+                 "failed": False, 
                  "type": "inventory_sync",
                  "result_stdout": "there will be stdout here",
                  "result_traceback": "", 
@@ -373,7 +373,7 @@ class UnifiedJobTemplateList(APIView):
                      "CELERY_LOG_LEVEL": "10", 
                      "TMUX_PANE": "%1"
                  }, 
-                 "license_error": false},
+                 "license_error": False},
                  {
                      "id": 1, 
                      "url": "/api/v1/jobs/1/", 
@@ -393,7 +393,7 @@ class UnifiedJobTemplateList(APIView):
                              "name": "testcred", 
                              "description": "", 
                              "kind": "ssh", 
-                             "cloud": false
+                             "cloud": False
                          }, 
                          "job_template": {
                              "name": "ping1", 
@@ -407,12 +407,12 @@ class UnifiedJobTemplateList(APIView):
                     "inventory": {
                         "name": "Test Inventory", 
                         "description": "", 
-                        "has_active_failures": false, 
+                        "has_active_failures": False, 
                         "total_hosts": 1000, 
                         "hosts_with_active_failures": 0, 
                         "total_groups": 1, 
                         "groups_with_active_failures": 0, 
-                        "has_inventory_sources": false, 
+                        "has_inventory_sources": False, 
                         "total_inventory_sources": 0, 
                         "inventory_sources_with_failures": 0
                     }
@@ -426,7 +426,7 @@ class UnifiedJobTemplateList(APIView):
                      "project": 1,
                      "playbook": "ping.yml", 
                      "credential": 1, 
-                     "cloud_credential": null, 
+                     "cloud_credential": None, 
                      "forks": 0, 
                      "limit": "host-1", 
                      "verbosity": 0, 
@@ -434,7 +434,7 @@ class UnifiedJobTemplateList(APIView):
                      "job_tags": "", 
                      "launch_type": "manual", 
                      "status": "successful", 
-                     "failed": false, 
+                     "failed": False, 
                      "result_traceback": "", 
                      "passwords_needed_to_start": [], 
                      "job_args": "[\"ansible-playbook\", \"-i\", \"/home/ubuntu/projects/tower/awx/plugins/inventory/awxrest.py\", \"-u\", \"admin\", \"--ask-pass\", \"-l\", \"host-1\", \"-e\", \"{\\\"ansible_connection\\\": \\\"local\\\"}\", \"ping.yml\"]", 
@@ -488,6 +488,7 @@ class UnifiedJobTemplateList(APIView):
                          "LS_COLORS": "rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lz=01;31:*.xz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.axv=01;35:*.anx=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.axa=00;36:*.oga=00;36:*.spx=00;36:*.xspf=00;36:", 
                          "TMUX_PANE": "%1"
                      }},]}
+        return Response(data)
 
 class AuthTokenView(APIView):
 
