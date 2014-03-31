7e5ac7b3fa9953807d8ab657bdb178db53ffa621
Matthew Jones <mat@matburt.net>
2014-03-31 11:38:36 -0400

    Make sure we shoehorn start args to empty string

--- awx/main/models/unified_jobs.py
+++ awx/main/models/unified_jobs.py
@@ -534,9 +534,9 @@ class UnifiedJob(PolymorphicModel, PasswordFieldsModel, CommonModelNameNotUnique
         try:
             start_args = json.loads(decrypt_field(self, 'start_args'))
         except Exception, e:
-            start_args = None
+            start_args = ''
         if start_args in (None, ''):
-            start_args = kwargs
+            start_args = ''
         opts = dict([(field, start_args.get(field, '')) for field in needed])
         if not all(opts.values()):
             missing_fields = ', '.join([k for k,v in opts.items() if not v])
