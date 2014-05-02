44bb12c5c3345c05f6cd6cb4464ad2167b7d338b
Matthew Jones <mat@matburt.net>
2014-05-02 13:23:45 -0400

    Include host name in the job event serializer

--- awx/api/serializers.py
+++ awx/api/serializers.py
@@ -1329,7 +1329,7 @@ class JobEventSerializer(BaseSerializer):
         model = JobEvent
         fields = ('*', '-name', '-description', 'job', 'event',
                   'event_display', 'event_data', 'event_level', 'failed',
-                  'changed', 'host', 'parent', 'play', 'task', 'role')
+                  'changed', 'host', 'host_name', 'parent', 'play', 'task', 'role')
 
     def get_related(self, obj):
         res = super(JobEventSerializer, self).get_related(obj)
