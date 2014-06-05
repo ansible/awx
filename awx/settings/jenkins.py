38bfbb4b46f38e2418d23b191d6a0b347b46f7f5
James Laska <jlaska@ansible.com>
2014-06-05 14:41:00 -0400

    test_jenkins - Specify .less files, not directory

--- awx/settings/jenkins.py
+++ awx/settings/jenkins.py
@@ -4,8 +4,7 @@
 # Test settings for AWX project.
 
 # Python
-import sys
-import traceback
+import glob
 
 # Django Split Settings
 from split_settings.tools import optional, include
@@ -29,13 +28,13 @@ PROJECT_APPS = ('awx.main', 'awx.api',)
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pylint',
     'django_jenkins.tasks.with_coverage',
-    'django_jenkins.tasks.django_tests',
+    # 'django_jenkins.tasks.django_tests',
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
     'django_jenkins.tasks.run_jshint',
     'django_jenkins.tasks.run_csslint',
     )
 PEP8_RCFILE = "setup.cfg"
-CSSLINT_CHECKED_FILES = [os.path.join(BASE_DIR, 'ui/static/less/*.less')]
+CSSLINT_CHECKED_FILES = glob.glob(os.path.join(BASE_DIR, 'ui/static/less/*.less'))
 JSHINT_CHECKED_FILES = [os.path.join(BASE_DIR, 'ui/static/js'),
                         os.path.join(BASE_DIR, 'ui/static/lib/ansible'),]
