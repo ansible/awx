5ee176e4512221424d9bfaff6bf2f82c6b2c6ceb
Chris Houseknecht <chouse@ansible.com>
2014-06-25 11:16:39 -0400

    2.0 style changes More work on status area of job detail page. It now collapses perfectly as viewport size shrinks.

--- awx/ui/static/js/controllers/JobDetail.js
+++ awx/ui/static/js/controllers/JobDetail.js
@@ -530,7 +530,6 @@ function JobDetailController ($rootScope, $scope, $compile, $routeParams, $log, 
         $('#tasks-table-detail').mCustomScrollbar("update");
         $('#hosts-table-detail').height(150);
         $('#hosts-table-detail').mCustomScrollbar("update");
-        //height = $('#wrap').height() - $('.site-footer').outerHeight() - $('.main-container').height();
         height = $(window).height() - $('#main-menu-container .navbar').outerHeight();
         if (height > 15) {
             // there's a bunch of white space at the bottom, let's use it
