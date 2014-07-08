9c2e4c91df04314af787c0cccbfe8aca8571b989
Chris Houseknecht <chouse@ansibleworks.com>
2014-07-08 13:43:35 -0400

    Job detail page re-refactor Latest host event viewer changes.

--- awx/ui/static/js/helpers/HostEventsViewer.js
+++ awx/ui/static/js/helpers/HostEventsViewer.js
@@ -35,12 +35,58 @@ angular.module('HostEventsViewerHelper', ['ModalDialog', 'Utilities'])
                 scope.removeJobReady();
             }
             scope.removeEventReady = scope.$on('EventsReady', function(e, data) {
-                //var elem;
+                var elem, html;
+
+                //scope.host_events = data.results;
+                //$log.debug(scope.host_events);
 
                 scope.host_events = data.results;
-                
-                //elem = angular.element(document.getElementById('host-events-modal-dialog'));
-                //$compile(elem)(scope);
+                scope.host_events_search_name = host_name;
+                scope.host_events_search_status = 'all';
+                scope.host_events = [];
+                html = "<table class=\"table\">\n";
+                html += "<thead>\n";
+                html += "<tr><th class=\"col-md-3\">Status</th>" +
+                        "<th class=\"col-md-3\">Play</th>" +
+                        "<th class=\"col-md-3\">Task</th>" +
+                        "<th class=\"col-md-3\">Result</th></tr>\n";
+                html += "</thead>\n";
+                html += "<tbody>\n";
+                data.results.forEach(function(result) {
+                    var msg = '',
+                        status = 'ok',
+                        status_text = 'OK';
+                    if (result.event_data.res) {
+                        msg = result.event_data.res.msg;
+                    }
+                    if (result.event === "runner_on_no_hoss") {
+                        msg = "No hosts remaining";
+                    }
+                    if (result.event === 'runner_on_unreachable') {
+                        status = 'unreachable';
+                        status_text = 'Unreachable';
+                    }
+                    else if (result.failed) {
+                        status = 'failed';
+                        status_text = 'Failed';
+                    }
+                    else if (result.changed) {
+                        status = 'changed';
+                        status_text = 'Changed';
+                    }
+                    html += "<tr ng-click=\"showDetails()\" class=\"cursor-pointer\" aw-tool-tip=\"Click to view details\" data-placement=\"top\">\n";
+                    html += "<td><i class=\"fa icon-job-" + status + "\"></i> <a href=\"\">" + status_text + "</a></td>\n";
+                    html += "<td><a href=\"\">" + result.play + "</a></td>\n";
+                    html += "<td><a href=\"\">" + result.task + "</a></td>\n";
+                    html += "<td><a href=\"\">" + msg + "</a></td>";
+                    html += "</tr>"
+                });
+                html += "</tbody>\n";
+                html += "</table>\n";
+                $('#host-events').html(html);
+
+                elem = angular.element(document.getElementById('host-events-modal-dialog'));
+                $compile(elem)(scope);
 
                 CreateDialog({
                     scope: scope,
@@ -50,10 +96,9 @@ angular.module('HostEventsViewerHelper', ['ModalDialog', 'Utilities'])
                     callback: 'ModalReady',
                     id: 'host-events-modal-dialog',
                     // onResizeStop: resizeText,
-                    title: ( (title) ? title : 'Event Details' ),
-                    onOpen: function() {
-
-                    }
+                    title: ( (title) ? title : 'Host Events' )
+                    //onOpen: function() {
+                    //}
                 });
             });
 
