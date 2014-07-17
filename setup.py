7e284e0c4ab99c7c8aee9da6d90809e7b3d45776
Luke Sneeringer <luke@sneeringer.com>
2014-07-17 14:02:56 -0500

    Theoretical move from /etc/awx/ to /etc/tower/.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from awx import __version__
 build_timestamp = os.getenv("BUILD",datetime.datetime.now().strftime('-%Y%m%d%H%M'))
 
 # Paths we'll use later
-etcpath = "/etc/awx"
+etcpath = "/etc/tower"
 homedir = "/var/lib/awx"
 sharedir = "/usr/share/awx"
 if os.path.exists("/etc/debian_version"):
