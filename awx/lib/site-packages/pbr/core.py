e1da8d169f21dcad161179ce32d39af2ec70895e
Matthew Jones <mat@matburt.net>
2014-08-06 15:34:20 -0400

    Upgrade pbr to 0.10.0

--- awx/lib/site-packages/pbr/core.py
+++ awx/lib/site-packages/pbr/core.py
@@ -40,6 +40,7 @@
 
 from distutils import core
 from distutils import errors
+import logging
 import os
 import sys
 import warnings
@@ -49,7 +50,17 @@ from setuptools import dist
 from pbr import util
 
 
-core.Distribution = dist._get_unpatched(core.Distribution)
+_saved_core_distribution = core.Distribution
+
+
+def _monkeypatch_distribution():
+    core.Distribution = dist._get_unpatched(core.Distribution)
+
+
+def _restore_distribution_monkeypatch():
+    core.Distribution = _saved_core_distribution
+
+
 if sys.version_info[0] == 3:
     string_type = str
     integer_types = (int,)
@@ -76,52 +87,63 @@ def pbr(dist, attr, value):
     not work well with distributions that do use a `Distribution` subclass.
     """
 
-    if not value:
-        return
-    if isinstance(value, string_type):
-        path = os.path.abspath(value)
-    else:
-        path = os.path.abspath('setup.cfg')
-    if not os.path.exists(path):
-        raise errors.DistutilsFileError(
-            'The setup.cfg file %s does not exist.' % path)
-
-    # Converts the setup.cfg file to setup() arguments
     try:
-        attrs = util.cfg_to_args(path)
-    except Exception:
-        e = sys.exc_info()[1]
-        raise errors.DistutilsSetupError(
-            'Error parsing %s: %s: %s' % (path, e.__class__.__name__, e))
+        _monkeypatch_distribution()
+        if not value:
+            return
+        if isinstance(value, string_type):
+            path = os.path.abspath(value)
+        else:
+            path = os.path.abspath('setup.cfg')
+        if not os.path.exists(path):
+            raise errors.DistutilsFileError(
+                'The setup.cfg file %s does not exist.' % path)
 
-    # Repeat some of the Distribution initialization code with the newly
-    # provided attrs
-    if attrs:
-        # Skips 'options' and 'licence' support which are rarely used; may add
-        # back in later if demanded
-        for key, val in attrs.items():
-            if hasattr(dist.metadata, 'set_' + key):
-                getattr(dist.metadata, 'set_' + key)(val)
-            elif hasattr(dist.metadata, key):
-                setattr(dist.metadata, key, val)
-            elif hasattr(dist, key):
-                setattr(dist, key, val)
-            else:
-                msg = 'Unknown distribution option: %s' % repr(key)
-                warnings.warn(msg)
+        # Converts the setup.cfg file to setup() arguments
+        try:
+            attrs = util.cfg_to_args(path)
+        except Exception:
+            e = sys.exc_info()[1]
+            # NB: This will output to the console if no explicit logging has
+            # been setup - but thats fine, this is a fatal distutils error, so
+            # being pretty isn't the #1 goal.. being diagnosable is.
+            logging.exception('Error parsing')
+            raise errors.DistutilsSetupError(
+                'Error parsing %s: %s: %s' % (path, e.__class__.__name__, e))
 
-    # Re-finalize the underlying Distribution
-    core.Distribution.finalize_options(dist)
+        # Repeat some of the Distribution initialization code with the newly
+        # provided attrs
+        if attrs:
+            # Skips 'options' and 'licence' support which are rarely used; may
+            # add back in later if demanded
+            for key, val in attrs.items():
+                if hasattr(dist.metadata, 'set_' + key):
+                    getattr(dist.metadata, 'set_' + key)(val)
+                elif hasattr(dist.metadata, key):
+                    setattr(dist.metadata, key, val)
+                elif hasattr(dist, key):
+                    setattr(dist, key, val)
+                else:
+                    msg = 'Unknown distribution option: %s' % repr(key)
+                    warnings.warn(msg)
 
-    # This bit comes out of distribute/setuptools
-    if isinstance(dist.metadata.version, integer_types + (float,)):
-        # Some people apparently take "version number" too literally :)
-        dist.metadata.version = str(dist.metadata.version)
+        # Re-finalize the underlying Distribution
+        core.Distribution.finalize_options(dist)
 
-    # This bit of hackery is necessary so that the Distribution will ignore
-    # normally unsupport command options (namely pre-hooks and post-hooks).
-    # dist.command_options is normally a dict mapping command names to dicts of
-    # their options.  Now it will be a defaultdict that returns IgnoreDicts for
-    # the each command's options so we can pass through the unsupported options
-    ignore = ['pre_hook.*', 'post_hook.*']
-    dist.command_options = util.DefaultGetDict(lambda: util.IgnoreDict(ignore))
+        # This bit comes out of distribute/setuptools
+        if isinstance(dist.metadata.version, integer_types + (float,)):
+            # Some people apparently take "version number" too literally :)
+            dist.metadata.version = str(dist.metadata.version)
+
+        # This bit of hackery is necessary so that the Distribution will ignore
+        # normally unsupport command options (namely pre-hooks and post-hooks).
+        # dist.command_options is normally a dict mapping command names to
+        # dicts of their options.  Now it will be a defaultdict that returns
+        # IgnoreDicts for the each command's options so we can pass through the
+        # unsupported options
+        ignore = ['pre_hook.*', 'post_hook.*']
+        dist.command_options = util.DefaultGetDict(
+            lambda: util.IgnoreDict(ignore)
+        )
+    finally:
+        _restore_distribution_monkeypatch()
