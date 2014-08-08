0e9b1fa9e15f34bf62d48fd6e2ddb2664f8d7834
Jared Tabor <jtabor@ansible.com>
2014-08-08 10:50:51 -0400

    switched to cowsay About dialog, but still need correct logo from Todd

--- awx/ui/static/js/helpers/AboutAnsible.js
+++ awx/ui/static/js/helpers/AboutAnsible.js
@@ -42,17 +42,28 @@ angular.module('AboutAnsibleHelpModal', ['RestServices', 'Utilities','ModalDialo
                     scope.removeBuildAboutDialog();
                 }
                 scope.removeBuildAboutDialog = scope.$on('BuildAboutDialog', function(e, data) {
-                    var str = data.version;
+                    var spaces, i, j,
+                    paddedStr  = "",
+                    str = data.version;
+
                     if(str.search('-')){
                         str = str.substr(0,str.search('-'));
                     }
-                    $('#about-modal-version').html(str);
+                    spaces = Math.floor((16-str.length)/2);
+                    for( i=0; i<=spaces; i++){
+                        paddedStr = paddedStr +" ";
+                    }
+                    paddedStr = paddedStr+str;
+                    for( j = paddedStr.length; j<16; j++){
+                        paddedStr = paddedStr + " ";
+                    }
+                    $('#about-modal-version').html(paddedStr);
                     CreateDialog({
                         id: 'about-modal-dialog',
                         scope: scope,
                         buttons: [],
-                        width: 600,
-                        height: 300,
+                        width: 730,
+                        height: 320,
                         minWidth: 300,
                         // title:  , //'<img src="static/img/tower_login_logo.png">' ,//'About Ansible',
                         callback: 'DialogReady'
