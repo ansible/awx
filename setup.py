7db362352112a88d5af7b45663fb6e93698647e0
Matthew Jones <mat@matburt.net>
2014-08-11 15:31:18 -0400

    Fix up sosfile strings in setup.py

--- setup.py
+++ setup.py
@@ -23,14 +23,14 @@ if os.path.exists("/etc/debian_version"):
     sysinit = "/etc/init.d"
     webconfig  = "/etc/apache2/conf.d"
     sosconfig = "/usr/share/sosreport/sos/plugins"
-    sosfile = "tower-sos3.py
+    sosfile = "tower-sos3.py"
     shutil.copy("config/awx-munin-ubuntu.conf", "config/awx-munin.conf")
 else:
     sysinit = "/etc/rc.d/init.d"
     webconfig  = "/etc/httpd/conf.d"
     if os.path.exists("/usr/lib/python2.7/site-packages"):
         sosconfig = "/usr/lib/python2.7/site-packages/sos/plugins"
-        sosfile = "tower-sos3.py
+        sosfile = "tower-sos3.py"
     else:
         sosconfig = "/usr/lib/python2.6/site-packages/sos/plugins"
         sosfile = "tower-sos2.py"
