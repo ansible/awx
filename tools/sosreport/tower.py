c45147e35fcbe9ca5605ec92ed4a189a92a2049e
James Laska <jlaska@ansible.com>
2014-08-14 13:45:49 -0400

    sosreport plugin docs recommend add_cmd_output

--- tools/sosreport/tower.py
+++ tools/sosreport/tower.py
@@ -38,10 +38,7 @@ if LooseVersion(sos.__version__) >= LooseVersion('3.0'):
                 self.add_copy_spec(path)
 
             for command in commands:
-                if hasattr(self, 'add_cmd_output'):
-                    self.add_cmd_output(command)
-                else:
-                    self.collect_ext_output(command)
+                self.add_cmd_output(command)
 
 else:
     import sos.plugintools
