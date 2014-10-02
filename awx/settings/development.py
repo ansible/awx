a174c7b1683bf83d9c94b320623daa62e63bc890
James Laska <jlaska@ansible.com>
2014-10-02 12:15:16 -0400

    The latest version of django_jenkins no longer requires django_tests

--- awx/settings/development.py
+++ awx/settings/development.py
@@ -35,7 +35,6 @@ if 'django_jenkins' in INSTALLED_APPS:
     JENKINS_TASKS = (
         'django_jenkins.tasks.run_pylint',
         'django_jenkins.tasks.with_coverage',
-        'django_jenkins.tasks.django_tests',
         'django_jenkins.tasks.run_pep8',
         'django_jenkins.tasks.run_pyflakes',
         'django_jenkins.tasks.run_jshint',
