ec7aa1867fc116bb14b76275b21a7b7a70cba3e8
Luke Sneeringer <luke@sneeringer.com>
2014-10-06 11:43:53 -0500

    Adding JobOrigin model and migration.

--- awx/main/models/ha.py
+++ awx/main/models/ha.py
@@ -2,7 +2,9 @@
 # All Rights Reserved.
 
 from django.db import models
+
 from awx.main.managers import InstanceManager
+from awx.main.models import UnifiedJob
 
 
 class Instance(models.Model):
@@ -26,3 +28,21 @@ class Instance(models.Model):
         if self.primary:
             return 'primary'
         return 'secondary'
+
+
+class JobOrigin(models.Model):
+    """A model representing the relationship between a unified job and
+    the instance that was responsible for starting that job.
+
+    It may be possible that a job has no origin (the common reason for this
+    being that the job was started on Tower < 2.1 before origins were a thing).
+    This is fine, and code should be able to handle it. A job with no origin
+    is always assumed to *not* have the current instance as its origin.
+    """
+    unified_job = models.ForeignKey(UnifiedJob)
+    instance = models.ForeignKey(Instance)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        app_label = 'main'
