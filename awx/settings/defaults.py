66f8f88898c6db60132d66f1c986eb8f5c8368c2
Matthew Jones <mat@matburt.net>
2014-10-23 14:52:00 -0400

    Remove the form and multipart parser from the the list of DRF parser classes

--- awx/settings/defaults.py
+++ awx/settings/defaults.py
@@ -171,8 +171,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
-        'rest_framework.parsers.FormParser',
-        'rest_framework.parsers.MultiPartParser',
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
