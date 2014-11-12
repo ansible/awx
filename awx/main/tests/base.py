cb77e319d2fb7f6710d679adbfff01735653071d
Luke Sneeringer <luke@sneeringer.com>
2014-11-12 12:58:45 -0600

    Newline

--- awx/main/tests/base.py
+++ awx/main/tests/base.py
@@ -463,6 +463,7 @@ class BaseTestMixin(object):
         if hasattr(self, 'queue_process'):
             self.queue_process.terminate()
 
+
 @override_settings(SYSTEM_UUID='00000000-0000-0000-0000-000000000000')
 class BaseTest(BaseTestMixin, django.test.TestCase):
     '''
