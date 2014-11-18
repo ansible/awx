3764bd2303df873d93a2d75311b27c9c632409b4
Matthew Jones <mat@matburt.net>
2014-11-18 12:06:25 -0500

    Reword errors when version metadata does not match or is absent

--- awx/wsgi.py
+++ awx/wsgi.py
@@ -22,11 +22,10 @@ logger = logging.getLogger('awx.main.models.jobs')
 try:
     fd = open("/var/lib/awx/.tower_version", "r")
     if fd.read().strip() != tower_version:
-        logger.error("Tower Versions don't match, potential invalid setup detected")
-        raise Exception("Tower Versions don't match, potential invalid setup detected")
+        raise Exception()
 except Exception:
-    logger.error("Missing tower version metadata at /var/lib/awx/.tower_version")
-    raise Exception("Missing tower version metadata at /var/lib/awx/.tower_version")
+    logger.error("Missing or incorrect metadata for Tower version.  Ensure Tower was installed using the setup playbook.")
+    raise Exception("Missing or incorrect metadata for Tower version.  Ensure Tower was installed using the setup playbook.")
 
 # Return the default Django WSGI application.
 from django.core.wsgi import get_wsgi_application
