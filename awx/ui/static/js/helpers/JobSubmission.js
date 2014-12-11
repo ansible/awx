7b9a6a4bb871bbc650f560e08ec1d7c059c7c85c
Jared Tabor <jtabor@ansible.com>
2014-12-11 15:42:26 -0500

    float's weren't required for survey taker
    accidentally goofed up the expression for ng-required for floats on survey maker.

--- awx/ui/static/js/helpers/JobSubmission.js
+++ awx/ui/static/js/helpers/JobSubmission.js
@@ -549,7 +549,7 @@ function($location, Wait, GetBasePath, LookUpInit, JobTemplateForm, CredentialLi
                     min = (!Empty(question.min)) ? question.min : "";
                     max = (!Empty(question.max)) ? question.max : "" ;
                     defaultValue = (!Empty(question.default)) ? question.default : (!Empty(question.default_float)) ? question.default_float : "" ;
-                    html+='<input type="number" id="'+question.variable+'" ng-model="'+question.variable+'"  class=" form-control" name="'+question.variable+'" ng-required="'+question.variable+'" smart-float aw-min="'+min+'"  aw-max="'+max+'"/>'+
+                    html+='<input type="number" id="'+question.variable+'" ng-model="'+question.variable+'"  class=" form-control" name="'+question.variable+'" ng-required="'+question.required+'" smart-float aw-min="'+min+'"  aw-max="'+max+'"/>'+
                         '<div class="error survey_error" ng-show="job_launch_form.'+question.variable+'.$error.number || job_launch_form.'+question.variable+'.$error.float">This is not valid float!</div>'+
                         '<div class="error survey_error" ng-show="job_launch_form.'+question.variable+'.$error.awMin || job_launch_form.'+question.variable+'.$error.awMax"> The value must be in range {{'+min+'}} to {{'+max+'}}!</div>';
                 }
