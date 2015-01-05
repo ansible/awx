f6baacfbe2f9bae9e294d5dd25a65f1523be25d7
Jared Tabor <jtabor@ansible.com>
2015-01-05 11:14:40 -0500

    Socket.js
    adding 'force new connection' line back into socket file

--- awx/ui/static/lib/ansible/Socket.js
+++ awx/ui/static/lib/ansible/Socket.js
@@ -79,6 +79,7 @@ angular.module('SocketIO', ['AuthService', 'Utilities'])
                             'connect timeout': 3000,
                             'try multiple transports': false,
                             'max reconneciton attemps': 3,
+                            'force new connection': true,
                             'reconnection limit': 3000
                         });
 
