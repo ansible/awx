48d211c56b718f8a654b443c2eeef1c8237a6264
Jared Tabor <jtabor@ansible.com>
2015-05-05 17:08:26 -0400

    adjusting the awFeature controller to return true when a awFeature string is not provided

--- awx/ui/static/js/shared/features/features.controller.js
+++ awx/ui/static/js/shared/features/features.controller.js
@@ -1,10 +1,6 @@
 export default ['$rootScope', function ($rootScope) {
 
     this.isFeatureEnabled = function(feature){
-        if($rootScope.features[feature] === false){
-            return false;
-        } else {
-            return true;
-        }
+        return $rootScope.features[feature] || false;
     };
 }];
