1b2cce19cd5d18dc532bf7eb40d08e01141fbf11
Joe Fiorini <joe@joefiorini.com>
2015-05-06 10:55:39 -0400

    jshint fixes

--- awx/ui/static/js/system-tracking/system-tracking.controller.js
+++ awx/ui/static/js/system-tracking/system-tracking.controller.js
@@ -1,3 +1,5 @@
-export default ['$scope', function($scope) {
-    $scope.viewType = 'host2host';
-}]
+export default
+    [   '$scope',
+        function($scope) {
+            $scope.viewType = 'host2host';
+        }];
