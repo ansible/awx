5f902a55febf0f35ac194553a6b1c8d7e944e221
Joe Fiorini <joe@joefiorini.com>
2015-05-28 12:46:08 -0400

    Sort modules by built-in then custom

--- awx/ui/static/js/system-tracking/data-services/get-module-options.factory.js
+++ awx/ui/static/js/system-tracking/data-services/get-module-options.factory.js
@@ -2,31 +2,47 @@ var moduleConfig =
     {   'packages':
             {   compareKey: ['release', 'version'],
                 nameKey: 'name',
-                displayType: 'flat'
+                displayType: 'flat',
+                sortKey: 1
             },
         'services':
             {   compareKey: ['state', 'source'],
                 nameKey: 'name',
-                displayType: 'flat'
+                displayType: 'flat',
+                sortKey: 2
             },
         'files':
             {   compareKey: ['size', 'mode', 'md5', 'mtime', 'gid', 'uid'],
                 nameKey: 'path',
-                displayType: 'flat'
+                displayType: 'flat',
+                sortKey: 3
+            },
+        'ansible':
+            {   displayType: 'nested',
+                sortKey: 4
             },
         'custom':
             {   displayType: 'nested'
             }
     };
 
-function makeModule(option) {
+function makeModule(option, index) {
     var name = option[0];
     var displayName = option[1];
     var config = moduleConfig.hasOwnProperty(name) ?
                     moduleConfig[name] : moduleConfig.custom;
+    var modulesCount = _.keys(moduleConfig).length - 1;
 
     config.name = name;
     config.displayName = displayName;
+
+    // Use index to sort custom modules,
+    // offset by built-in modules since
+    // they have a hardcoded sort key
+    //
+    if (_.isUndefined(config.sortKey)) {
+        config.sortKey = (index - 1) + modulesCount;
+    }
 
     return config;
 }
@@ -39,7 +55,8 @@ function factory(hostId, rest, getBasePath, _) {
     rest.setUrl(url);
     return _(rest.options())
         .then(function(response) {
-            return response.data.actions.GET.module.choices;
+            var choices = response.data.actions.GET.module.choices;
+            return _.sortBy(choices, '1');
         }).thenMap(makeModule);
 }
 
