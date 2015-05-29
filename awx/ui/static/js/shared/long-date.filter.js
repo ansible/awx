c2a5b21b9e08d752e84d5796268a10440f9bdd6f
Jared Tabor <jtabor@ansible.com>
2015-05-29 11:20:17 -0400

    adding the locale files for moment.js
    also adjusted the source of its locale setting to be more comprehensive (use navigator.language and navigator.languages)

--- awx/ui/static/js/shared/long-date.filter.js
+++ awx/ui/static/js/shared/long-date.filter.js
@@ -1,6 +1,13 @@
 angular.module('longDateFilter', []).filter('longDate', function() {
   return function(input) {
-    var date = moment(input).locale(navigator.language);
+
+      // navigator.language is available in all modern browsers.
+      // however navigator.languages is a new technology that
+      // lists the user's preferred languages, the first in the array
+      // being the user's top choice. navigator.languages is currently
+      // comptabile with chrome>v32, ffox>32, but not IE/Safari
+    var lang = navigator.languages ? navigator.languages[0] : (navigator.language || navigator.userLanguage),
+        date = moment(input).locale(lang);
     return date.format('l LTS');
   };
 });
