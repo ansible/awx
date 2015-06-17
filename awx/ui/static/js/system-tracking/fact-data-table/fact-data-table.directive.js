389eddfc8d828d1b541adef65fdee7742335aa22
Joe Fiorini <joe@joefiorini.com>
2015-06-17 17:39:50 -0400

    [system_tracking] Fix display of nested modules

--- awx/ui/static/js/system-tracking/fact-data-table/fact-data-table.directive.js
+++ awx/ui/static/js/system-tracking/fact-data-table/fact-data-table.directive.js
@@ -11,6 +11,7 @@ export default
                                 rightScanDate: '=',
                                 leftDataNoScans: '=',
                                 rightDataNoScans: '=',
+                                isNestedDisplay: '=',
                                 factData: '='
                             }
                     };
