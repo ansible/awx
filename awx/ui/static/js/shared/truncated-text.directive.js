1b36223fa60600970969b057042bf96e5a58cd7e
Joe Fiorini <joe@joefiorini.com>
2015-07-08 11:10:53 -0400

    [system_tracking] Hide right column for files module in single result view

--- awx/ui/static/js/shared/truncated-text.directive.js
+++ awx/ui/static/js/shared/truncated-text.directive.js
@@ -3,6 +3,14 @@
 function link($compile, scope, element, attrs) {
 
     function elementTextWillWrap(element) {
+
+        // If the element is a DOM comment, that means
+        // it's been hidden with `ng-if` so don't try
+        // to process it or we get an error!
+        if (element[0].nodeType === 8) {
+            return false;
+        }
+
         var fullTextWidth = element[0].scrollWidth;
         var elementWidth = element.outerWidth();
 
