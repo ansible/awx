2dc22042ee6db3716575b32d2b0f3c571b0f1cf8
Chris Meyers <chris.meyers.fsu@gmail.com>
2015-07-20 11:20:17 -0400

    flake8 fixes

--- awx/main/models/inventory.py
+++ awx/main/models/inventory.py
@@ -1256,8 +1256,8 @@ class InventoryUpdate(UnifiedJob, InventorySourceOptions):
         if not super(InventoryUpdate, self).can_start:
             return False
 
-        if (self.source != 'custom'
-                and not (self.credential and self.credential.active)):
+        if (self.source != 'custom' and
+                not (self.credential and self.credential.active)):
             return False
         return True
 
