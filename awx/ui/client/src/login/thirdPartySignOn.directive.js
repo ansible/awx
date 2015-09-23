25132b5daaebc61d7d08d8025dff398b18ed26bd
John Mitchell <jlmitch5@ncsu.edu>
2015-09-23 17:00:13 -0400

    pluggable auth ui styling

---
+++ awx/ui/client/src/login/thirdPartySignOn.directive.js
@@ -0,0 +1,40 @@
+/*************************************************
+ * Copyright (c) 2015 Ansible, Inc.
+ *
+ * All Rights Reserved
+ *************************************************/
+
+/* jshint unused: vars */
+export default
+    [   'templateUrl',
+        function(templateUrl) {
+            return {
+                restrict: 'E',
+                scope: true,
+                // controller: authenticationController,
+                templateUrl: templateUrl('login/thirdPartySignOn'),
+                link: function(scope, element, attrs) {
+                    scope.loginItems = [
+                        {
+                            type: "foo",
+                            icon: "fa-github",
+                            link: "https://google.com",
+                            tooltip: "Login in via Google"
+                        },
+                        {
+                            type: "foo",
+                            icon: "fa-github",
+                            link: "https://google.com",
+                            tooltip: "Login in via Github"
+                        },
+                        {
+                            type: "foo",
+                            icon: "fa-github",
+                            link: "https://google.com",
+                            tooltip: "Login in via SAML"
+                        }
+                    ]
+                }
+            };
+        }
+    ];
