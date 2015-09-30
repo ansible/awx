0c7507d3a0e3073d8d281c0eb51f5a0d1aff74d4
Jared Tabor <jtabor@ansible.com>
2015-09-30 10:20:08 -0400

    moving pendo service to login/authentication folder

--- awx/ui/client/src/login/authenticationServices/main.js
+++ awx/ui/client/src/login/authenticationServices/main.js
@@ -8,10 +8,12 @@ import authenticationService from './authentication.service';
 import checkAccess from './checkAccess.factory';
 import isAdmin from './isAdmin.factory';
 import timer from './timer.factory';
+import pendoService from './pendo.service';
 
 export default
     angular.module('authentication', [])
         .factory('Authorization', authenticationService)
         .factory('CheckAccess', checkAccess)
         .factory('IsAdmin', isAdmin)
-        .factory('Timer', timer);
+        .factory('Timer', timer)
+        .service('pendoService', pendoService);
