0051464525da3f0f3bf2eb3492d9b36b2760df37
Akita Noek <anoek@ansible.com>
2016-02-18 11:23:31 -0500

    Added put,head,options,delete, & patch test fixtures for api related tests

--- awx/main/tests/functional/conftest.py
+++ awx/main/tests/functional/conftest.py
@@ -57,6 +57,91 @@ def get():
     return rf
 
 @pytest.fixture
+def put():
+    def rf(url, data, user=None, middleware=None, **kwargs):
+        view, view_args, view_kwargs = resolve(urlparse(url)[2])
+        if 'format' not in kwargs:
+            kwargs['format'] = 'json'
+        request = APIRequestFactory().put(url, data, **kwargs)
+        if middleware:
+            middleware.process_request(request)
+        if user:
+            force_authenticate(request, user=user)
+        response = view(request, *view_args, **view_kwargs)
+        if middleware:
+            middleware.process_response(request, response)
+        return response
+    return rf
+
+@pytest.fixture
+def patch():
+    def rf(url, data, user=None, middleware=None, **kwargs):
+        view, view_args, view_kwargs = resolve(urlparse(url)[2])
+        if 'format' not in kwargs:
+            kwargs['format'] = 'json'
+        request = APIRequestFactory().patch(url, data, **kwargs)
+        if middleware:
+            middleware.process_request(request)
+        if user:
+            force_authenticate(request, user=user)
+        response = view(request, *view_args, **view_kwargs)
+        if middleware:
+            middleware.process_response(request, response)
+        return response
+    return rf
+
+@pytest.fixture
+def delete():
+    def rf(url, user=None, middleware=None, **kwargs):
+        view, view_args, view_kwargs = resolve(urlparse(url)[2])
+        if 'format' not in kwargs:
+            kwargs['format'] = 'json'
+        request = APIRequestFactory().delete(url, **kwargs)
+        if middleware:
+            middleware.process_request(request)
+        if user:
+            force_authenticate(request, user=user)
+        response = view(request, *view_args, **view_kwargs)
+        if middleware:
+            middleware.process_response(request, response)
+        return response
+    return rf
+
+@pytest.fixture
+def head():
+    def rf(url, user=None, middleware=None, **kwargs):
+        view, view_args, view_kwargs = resolve(urlparse(url)[2])
+        if 'format' not in kwargs:
+            kwargs['format'] = 'json'
+        request = APIRequestFactory().head(url, **kwargs)
+        if middleware:
+            middleware.process_request(request)
+        if user:
+            force_authenticate(request, user=user)
+        response = view(request, *view_args, **view_kwargs)
+        if middleware:
+            middleware.process_response(request, response)
+        return response
+    return rf
+
+@pytest.fixture
+def options():
+    def rf(url, data, user=None, middleware=None, **kwargs):
+        view, view_args, view_kwargs = resolve(urlparse(url)[2])
+        if 'format' not in kwargs:
+            kwargs['format'] = 'json'
+        request = APIRequestFactory().options(url, data, **kwargs)
+        if middleware:
+            middleware.process_request(request)
+        if user:
+            force_authenticate(request, user=user)
+        response = view(request, *view_args, **view_kwargs)
+        if middleware:
+            middleware.process_response(request, response)
+        return response
+    return rf
+
+@pytest.fixture
 def instance(settings):
     return Instance.objects.create(uuid=settings.SYSTEM_UUID, primary=True, hostname="instance.example.org")
 
