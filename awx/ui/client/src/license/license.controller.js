211ae9afad6300245de461dd85a7e632cecce9ca
Leigh Johnson <ljohnson@ansible.com>
2016-02-29 15:01:44 -0500

    create 1/3+2/3 LESS layout, normalize site-wide footer positioning, move CheckLicense factory to separate file, upgrade URL, add file picker + form reset behavior,  #1055, #1057, #1007

--- awx/ui/client/src/license/license.controller.js
+++ awx/ui/client/src/license/license.controller.js
@@ -9,35 +9,47 @@ export default
 	 'GetBasePath', 'Rest', 'ProcessErrors', 'CheckLicense', 'moment',
 	 function( Wait, $state, $scope, $location,
 	 	GetBasePath, Rest, ProcessErrors, CheckLicense, moment){
-	 	// codemirror
-	 	var textArea = document.getElementById('License-codemirror');
-		var editor = CodeMirror.fromTextArea(textArea, {
-		   lineNumbers: true,
-		   mode: 'json'
-		});
-		editor.on('blur', function(cm){
-			$scope.newLicense.file = cm.getValue()
-		});
+	 	$scope.getKey = function(event){
+	 		// Mimic HTML5 spec, show filename
+	 		$scope.fileName = event.target.files[0].name;
+	 		// Grab the key from the raw license file
+	 		var raw = new FileReader();
+	 		// readAsFoo runs async
+	 		raw.onload = function(){
+	 			$scope.newLicense.file = JSON.parse(raw.result);
+	 		}
+	 		raw.readAsText(event.target.files[0]);
+	 	};
+	 	// HTML5 spec doesn't provide a way to customize file input css
+	 	// So we hide the default input, show our own, and simulate clicks to the hidden input
+	 	$scope.fakeClick = function(){
+	 		$('#License-file').click();
+	 		//document.getElementById('License-file').click();
+	 	}
 		$scope.newLicense = {};
-		$scope.submit = function(e){
-			Wait('start')
+		$scope.submit = function(event){
+			Wait('start');
 			CheckLicense.post($scope.newLicense.file, $scope.newLicense.eula)
 				.success(function(res){
-					console.log(res)
+					reset();
+					init();
+					$scope.success = true;
 			});
-		}
+		};
 	 	var calcDaysRemaining = function(ms){
 	 		// calculate the number of days remaining on the license
 	 		var duration = moment.duration(ms);
 	 		return duration.days()
-	 	}
+	 	};
 
 	 	var calcExpiresOn = function(days){
 	 		// calculate the expiration date of the license
 	 		return moment().add(days, 'days').calendar()
-	 	}
-	 	Wait('start');
-	 	CheckLicense.get()
+	 	};
+	 	var init = function(){
+	 		$scope.fileName = "Please choose a file..."
+	 		Wait('start');
+	 		CheckLicense.get()
 	 		.then(function(res){
 	 			$scope.license = res.data;
 	 			$scope.time = {};
@@ -46,5 +58,10 @@ export default
 	 			$scope.valid = CheckLicense.valid($scope.license.license_info);	 			
 	 			Wait('stop');
 	 		});
-	}
+	 	};
+	 	var reset = function(){
+	 		document.getElementById('License-form').reset()
+	 	};
+	 	init();	
+	 }
 	];
--- awx/ui/client/src/license/main.js
+++ awx/ui/client/src/license/main.js
@@ -6,66 +6,14 @@
 
 import route from './license.route';
 import controller from './license.controller';
+import CheckLicense from './checkLicense.factory';
+import fileOnChange from './fileOnChange.directive';
 
 export default
 	angular.module('license', [])
 		.controller('licenseController', controller)
-		.factory('CheckLicense', ['$state', '$rootScope', 'Rest', 'GetBasePath', 'ProcessErrors', function($state, $rootScope, Rest, GetBasePath, ProcessErrors){
-			return {
-				get: function() {
-					var defaultUrl = GetBasePath('config');
-					Rest.setUrl(defaultUrl);
-					return Rest.get()
-						.success(function(res){
-							return res
-						})
-	                    .error(function(res, status){
-	                        ProcessErrors($rootScope, res, status, null, {hdr: 'Error!',
-	                        msg: 'Call to '+ defaultUrl + ' failed. Return status: '+ status});
-	                    });
-				},
-				post: function(license, eula){
-					var defaultUrl = GetBasePath('config');
-					Rest.setUrl(defaultUrl);
-					var data = {
-						eula_accepted: eula,
-						license_key: license
-					};
-					console.log(data)
-					return Rest.post(JSON.stringify(data))
-						.success(function(res){
-							return res
-						})
-						.error(function(res, status){
-	                        ProcessErrors($rootScope, res, status, null, {hdr: 'Error!',
-	                        msg: 'Call to '+ defaultUrl + ' failed. Return status: '+ status});
-	                    });
-				},
-				// Checks current license validity
-				// Intended to for runtime or pre-state checks
-				// Returns false if invalid
-				valid: function(license) {
-					 	if (!license.valid_key){
-					 		return false
-					 	}
-					 	else if (license.free_instances <= 0){
-					 		return false
-					 	}
-					 	// notify if less than 15 days remaining 
-					 	else if (license.time_remaining / 1000 / 60 / 60 / 24 > 15){
-					 		return false
-					 	}
-					 	return true
-				},
-				notify: function(){
-					this.get()
-						.then(function(res){
-							this.valid(res.license_info) ? null : $state.go('license');
-						});
-				}
-
-			}
-		}])
+		.directive('fileOnChange', fileOnChange)
+		.factory('CheckLicense', CheckLicense)
 		.run(['$stateExtender', function($stateExtender) {
 			$stateExtender.addState(route);
 		}]);
