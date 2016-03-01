67d1aa6c8688bc5854fe15a3749615c4d41eac36
Chris Meyers <chris.meyers.fsu@gmail.com>
2016-03-01 10:39:44 -0500

    migration with correct help_text

--- awx/main/migrations/0004_v300_changes.py
+++ awx/main/migrations/0004_v300_changes.py
@@ -16,10 +16,10 @@ class Migration(migrations.Migration):
             name='Fact',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('timestamp', models.DateTimeField(default=None, editable=False)),
+                ('timestamp', models.DateTimeField(default=None, help_text='Date and time of the corresponding fact scan gathering time.', editable=False)),
                 ('module', models.CharField(max_length=128)),
-                ('facts', jsonbfield.fields.JSONField(default={}, blank=True)),
-                ('host', models.ForeignKey(related_name='facts', to='main.Host')),
+                ('facts', jsonbfield.fields.JSONField(default={}, help_text='Arbitrary JSON structure of module facts captured at timestamp for a single host.', blank=True)),
+                ('host', models.ForeignKey(related_name='facts', to='main.Host', help_text='Host for the facts that the fact scan captured.')),
             ],
         ),
         migrations.AlterIndexTogether(
