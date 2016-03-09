2e86c58a653d52d1aaf99c50c91a60ccb03e784e
Chris Meyers <chris.meyers.fsu@gmail.com>
2016-03-09 13:59:02 -0500

    remove mongo fact tests

--- awx/fact/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2015 Ansible, Inc.
-# All Rights Reserved
-
-from __future__ import absolute_import
-
-from .models import * # noqa
-from .utils import * # noqa
-from .base import * # noqa
--- awx/fact/tests/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2015 Ansible, Inc.
-# All Rights Reserved
-
-from __future__ import absolute_import
-
-from .dbtransform import * # noqa
