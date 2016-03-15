af9882af97790c920bf3602b8f766f835c42d067
Leigh Johnson <ljohnson@ansible.com>
2016-03-14 21:16:44 -0400

    Yoink REST calls from Job Details controller into modular service, resolves #1239

--- awx/ui/client/src/job-detail/main.js
+++ awx/ui/client/src/job-detail/main.js
@@ -6,10 +6,12 @@
 
 import route from './job-detail.route';
 import controller from './job-detail.controller';
+import service from './job-detail.service';
 
 export default
     angular.module('jobDetail', [])
         .controller('JobDetailController', controller)
+        .service('JobDetailService', service)
         .run(['$stateExtender', function($stateExtender) {
             $stateExtender.addState(route);
         }]);
