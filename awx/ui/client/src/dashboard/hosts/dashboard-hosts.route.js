f7b5772ebafe3b6e85734643cb4db03d6958ebe0
Leigh Johnson <ljohnson@redhat.com>
2016-05-02 23:59:39 -0400

    add /#/home/hosts/?active-failures=true, resolves #1678

--- awx/ui/client/src/dashboard/hosts/dashboard-hosts.route.js
+++ awx/ui/client/src/dashboard/hosts/dashboard-hosts.route.js
@@ -9,14 +9,14 @@ import listController from './dashboard-hosts-list.controller';
 import editController from './dashboard-hosts-edit.controller';
 
 var dashboardHostsList =  {
-	name: 'dashboardHosts',
-	url: '/home/hosts',
-	controller: listController,
-	templateUrl: templateUrl('dashboard/hosts/dashboard-hosts-list'),
-	data: {
-		activityStream: true,
-		activityStreamTarget: 'host'
-	},
+    name: 'dashboardHosts',
+    url: '/home/hosts?:active-failures',
+    controller: listController,
+    templateUrl: templateUrl('dashboard/hosts/dashboard-hosts-list'),
+    data: {
+        activityStream: true,
+        activityStreamTarget: 'host'
+    },
     ncyBreadcrumb: {
         parent: 'dashboard',
         label: "HOSTS"
@@ -25,39 +25,39 @@ var dashboardHostsList =  {
         features: ['FeaturesService', function(FeaturesService) {
             return FeaturesService.get();
         }],
-        hosts: ['Rest', 'GetBasePath', function(Rest, GetBasePath){
-        	var defaultUrl = GetBasePath('hosts') + '?page_size=10';
-        	Rest.setUrl(defaultUrl);
-        	return Rest.get().then(function(res){
-        		var results = _.map(res.data.results, function(value){
-        			value.inventory_name = value.summary_fields.inventory.name;
-        			value.inventory_id = value.summary_fields.inventory.id;
-        			return value;
-        		});
+        hosts: ['Rest', 'GetBasePath', '$stateParams', function(Rest, GetBasePath, $stateParams){
+            var defaultUrl = GetBasePath('hosts') + '?page_size=10' + ($stateParams['active-failures'] ? '&has_active_failures=true' : '' );
+            Rest.setUrl(defaultUrl);
+            return Rest.get().then(function(res){
+                var results = _.map(res.data.results, function(value){
+                    value.inventory_name = value.summary_fields.inventory.name;
+                    value.inventory_id = value.summary_fields.inventory.id;
+                    return value;
+                });
                 res.data.results = results;
                 return res.data;
-        	});
+            });
         }]
     }
 };
 
 var dashboardHostsEdit = {
-	name: 'dashboardHosts.edit',
-	url: '/:id',
-	controller: editController,
-	templateUrl: templateUrl('dashboard/hosts/dashboard-hosts-edit'),
+    name: 'dashboardHosts.edit',
+    url: '/:id',
+    controller: editController,
+    templateUrl: templateUrl('dashboard/hosts/dashboard-hosts-edit'),
     ncyBreadcrumb: {
         parent: 'dashboardHosts',
         label: "{{host.name}}"
     },
     resolve: {
-    	host: ['$stateParams', 'Rest', 'GetBasePath', function($stateParams, Rest, GetBasePath){
-    		var defaultUrl = GetBasePath('hosts') + '?id=' + $stateParams.id;
-    		Rest.setUrl(defaultUrl);
-    		return Rest.get().then(function(res){
-    			return res.data.results[0];
-    		});
-    	}]
+        host: ['$stateParams', 'Rest', 'GetBasePath', function($stateParams, Rest, GetBasePath){
+            var defaultUrl = GetBasePath('hosts') + '?id=' + $stateParams.id;
+            Rest.setUrl(defaultUrl);
+            return Rest.get().then(function(res){
+                return res.data.results[0];
+            });
+        }]
     }
 };
 
