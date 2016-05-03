2d230919f08677f6b5e1c87cf00cdca1e0e0990f
AlanCoding <arominger@ansible.com>
2016-05-03 16:41:43 -0400

    reorganizing migration numbering

--- awx/main/migrations/0022_v300_adhoc_extravars.py
+++ awx/main/migrations/0022_v300_adhoc_extravars.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0020_v300_labels_changes'),
+        ('main', '0021_v300_activity_stream'),
     ]
 
     operations = [
