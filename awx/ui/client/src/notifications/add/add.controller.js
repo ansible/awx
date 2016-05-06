fd40135d9d064497afceb159015b13f594c9aa25
Jared Tabor <jtabor@ansible.com>
2016-05-06 16:11:50 -0400

    jshint errors

--- awx/ui/client/src/notifications/add/add.controller.js
+++ awx/ui/client/src/notifications/add/add.controller.js
@@ -110,7 +110,7 @@ export default
                 Wait('start');
                 Rest.setUrl(url);
                 Rest.post(params)
-                .success(function (data) {
+                .success(function () {
                     $state.go('notifications', {}, {reload: true});
                     Wait('stop');
                 })
