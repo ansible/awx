1bedc5dacc0d95ad4921f7a2867a2518aa1207a2
John Mitchell <jlmitch5@ncsu.edu>
2016-05-13 10:57:36 -0400

    modal to add users and admins to organizations

--- awx/ui/client/src/organizations/linkout/main.js
+++ awx/ui/client/src/organizations/linkout/main.js
@@ -1,6 +1,7 @@
 import routes from './organizations-linkout.route';
+import AddUsers from './addUsers/main';
 
-export default angular.module('organizationsLinkout', [])
+export default angular.module('organizationsLinkout', [AddUsers.name])
     .run(['$stateExtender', function($stateExtender) {
         routes.forEach(function(route) {
             $stateExtender.addState(route);
