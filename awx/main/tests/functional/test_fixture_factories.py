2f09c7fee17805b4cee9e0b19d2fc21bf3753087
Wayne Witzel III <wayne@riotousliving.com>
2016-05-24 12:53:42 -0400

    added test for factory roles

--- awx/main/tests/functional/test_fixture_factories.py
+++ awx/main/tests/functional/test_fixture_factories.py
@@ -1,6 +1,23 @@
 import pytest
 
 @pytest.mark.django_db
+def test_org_factory_roles(organization_factory):
+    objects = organization_factory('org_roles_test',
+                                   teams=['team1', 'team2'],
+                                   users=['team1:foo', 'bar'],
+                                   projects=['baz', 'bang'],
+                                   roles=['team2.member_role:foo',
+                                          'team2.admin_role:bar',
+                                          'team1.admin_role:team2.admin_role',
+                                          'baz.admin_role:foo'])
+
+    assert objects.users.bar in objects.teams.team1.admin_role
+    assert objects.users.foo in objects.projects.baz.admin_role
+    assert objects.users.foo in objects.teams.team1.member_role
+    assert objects.teams.team2.admin_role in objects.teams.team1n.admin_role.parents.all()
+
+
+@pytest.mark.django_db
 def test_org_factory(organization_factory):
     objects = organization_factory('organization1',
                                    teams=['team1'],
@@ -12,6 +29,7 @@ def test_org_factory(organization_factory):
     assert hasattr(objects.superusers, 'superuser')
     assert objects.users.bob in objects.teams.team1.member_role.members.all()
     assert objects.projects.proj1.organization == objects.organization
+
 
 @pytest.mark.django_db
 def test_job_template_factory(job_template_factory):
