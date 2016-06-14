479119f3ce923c4a7585367e8facfd6c48484fb6
Wayne Witzel III <wayne@riotousliving.com>
2016-06-14 13:39:10 -0400

    add more task unit tests

--- awx/main/tests/unit/test_tasks.py
+++ awx/main/tests/unit/test_tasks.py
@@ -1,4 +1,14 @@
-from awx.main.tasks import run_label_cleanup
+import pytest
+
+from awx.main.models import (
+    UnifiedJob,
+    Notification,
+)
+
+from awx.main.tasks import (
+    run_label_cleanup,
+    send_notifications,
+)
 
 def test_run_label_cleanup(mocker):
     qs = mocker.Mock(**{'count.return_value': 3, 'delete.return_value': None})
@@ -9,4 +19,27 @@ def test_run_label_cleanup(mocker):
     mock_label.assert_called_with()
     qs.delete.assert_called_with()
     assert 3 == ret
+
+def test_send_notifications_not_list():
+    with pytest.raises(TypeError):
+        send_notifications(None)
+
+def test_send_notifications_job_id(mocker):
+    with mocker.patch('awx.main.models.UnifiedJob.objects.get'):
+        send_notifications([], job_id=1)
+        assert UnifiedJob.objects.get.called
+        assert UnifiedJob.objects.get.called_with(id=1)
+
+def test_send_notifications_list(mocker):
+    mock_job = mocker.MagicMock(spec=UnifiedJob)
+    with mocker.patch('awx.main.models.UnifiedJob.objects.get', return_value=mock_job):
+        mock_notification = mocker.MagicMock(spec=Notification, subject="test")
+        with mocker.patch('awx.main.models.Notification.objects.get', return_value=mock_notification):
+            send_notifications([1,2], job_id=1)
+            assert Notification.objects.get.call_count == 2
+            assert mock_notification.status == "successful"
+            assert mock_notification.save.called
+
+            assert mock_job.notifications.add.called
+            assert mock_job.notifications.add.called_with(mock_notification)
 
