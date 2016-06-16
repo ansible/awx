af8011cd9133b9e42d7156c874accce22443e409
James Laska <jlaska@redhat.com>
2016-06-16 15:49:08 -0400

    Honor CLOUD_VERIFY_SSL variable in rax import

--- awx/main/tasks.py
+++ awx/main/tasks.py
@@ -820,7 +820,7 @@ class RunJob(BaseTask):
         elif cloud_cred and cloud_cred.kind == 'rax':
             env['RAX_USERNAME'] = cloud_cred.username
             env['RAX_API_KEY'] = decrypt_field(cloud_cred, 'password')
-            env['CLOUD_VERIFY_SSL'] = str(True)
+            env['CLOUD_VERIFY_SSL'] = str(False)
         elif cloud_cred and cloud_cred.kind == 'gce':
             env['GCE_EMAIL'] = cloud_cred.username
             env['GCE_PROJECT'] = cloud_cred.project
