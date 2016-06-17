3115dd4feed1474e2bd18b0d694cac0716ab77b5
Jared Tabor <jtabor@ansible.com>
2016-06-17 13:43:02 -0700

    hiding notifcation templates on setup menu
    for non org admins

--- awx/ui/client/src/setup-menu/setup.route.js
+++ awx/ui/client/src/setup-menu/setup.route.js
@@ -6,5 +6,30 @@ export default {
     ncyBreadcrumb: {
         label: "SETTINGS"
     },
-    templateUrl: templateUrl('setup-menu/setup-menu')
+    templateUrl: templateUrl('setup-menu/setup-menu'),
+    resolve: {
+        org_admin:
+            ['$rootScope', 'ProcessErrors', 'Rest',
+            function($rootScope, ProcessErrors, Rest){
+            $rootScope.loginConfig.promise.then(function () {
+                if($rootScope.current_user.related.admin_of_organizations){
+                $rootScope.orgAdmin = false;
+                if ($rootScope.current_user.is_superuser) {
+                    $rootScope.orgAdmin = true;
+                } else {
+                    Rest.setUrl(`/api/v1/users/${$rootScope.current_user.id}/admin_of_organizations`);
+                    Rest.get()
+                        .success(function(data) {
+                            $rootScope.orgAdmin = (data.count) ? true : false;
+                        }).error(function (data, status) {
+                            ProcessErrors($rootScope, data, status, null, { hdr: 'Error!', msg: 'Failed to find if users is admin of org' + status });
+                        });
+                }
+            }
+            else{
+                return;
+            }
+        });
+        }]
+    }
 };
