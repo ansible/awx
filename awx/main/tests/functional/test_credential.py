bc457ccef6608de7103b1702bd9cd490930c1325
Wayne Witzel III <wayne@riotousliving.com>
2016-06-21 11:36:31 -0400

    assert Credential unique_togehter works

---
+++ awx/main/tests/functional/test_credential.py
@@ -0,0 +1,15 @@
+import pytest
+
+from django.db import IntegrityError
+from awx.main.models import Credential
+
+@pytest.mark.django_db
+def test_cred_unique_org_name_kind(organization_factory):
+    objects = organization_factory("test")
+
+    cred = Credential(name="test", kind="net", organization=objects.organization)
+    cred.save()
+
+    with pytest.raises(IntegrityError):
+        cred = Credential(name="test", kind="net", organization=objects.organization)
+        cred.save()
