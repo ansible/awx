3575b32eab84b9586e91a92c904e8132abf5d282
Wayne Witzel III <wayne@riotousliving.com>
2016-06-21 11:35:30 -0400

    Credential should have a unique org,kind,name

--- awx/main/models/credential.py
+++ awx/main/models/credential.py
@@ -61,6 +61,7 @@ class Credential(PasswordFieldsModel, CommonModelNameNotUnique, ResourceMixin):
     class Meta:
         app_label = 'main'
         ordering = ('kind', 'name')
+        unique_together = (('organization', 'name', 'kind'),)
 
     deprecated_user = models.ForeignKey(
         'auth.User',
