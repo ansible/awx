cec12d61d8948b7d8c5d14a561a3958751e499e1
Michael Abashian <mabashian@ansible.com>
2016-06-22 10:08:41 -0400

    Tweaked some spacing on the inv manage breadcrumbs

--- awx/ui/client/src/inventories/manage/breadcrumbs/breadcrumbs.controller.js
+++ awx/ui/client/src/inventories/manage/breadcrumbs/breadcrumbs.controller.js
@@ -5,23 +5,29 @@
  *************************************************/
 
 export default
-    ['$state', '$stateParams', '$scope', 'inventoryData', 'breadCrumbData', function($state, $stateParams, $scope, inventoryData, breadCrumbData){
+    ['$state', '$stateParams', '$scope', '$rootScope', 'inventoryData', 'breadCrumbData', function($state, $stateParams, $scope, $rootScope, inventoryData, breadCrumbData){
         // process result data into the same order specified in the traversal path
         $scope.groups = _.sortBy(breadCrumbData, function(item){
             var index = _.indexOf($stateParams.group, item.id);
             return (index === -1) ? $stateParams.group.length : index;
         });
         $scope.inventory = inventoryData;
-        $scope.state = $state;
+        $scope.currentState = $state.current.name;
         // slices the group stack at $index to supply new group params to $state.go()
         $scope.goToGroup = function(index){
             var group = $stateParams.group.slice(0, index);
             $state.go('inventoryManage', {group: group}, {reload: true});
         };
-        $scope.isRootState = function(){
-            return $state.current.name === 'inventoryManage';
-        };
         $scope.goToInventory = function(){
             $state.go('inventoryManage', {group: undefined}, {reload: true});
         };
+
+        var cleanUpStateChangeListener = $rootScope.$on('$stateChangeSuccess', function(event, toState){
+            $scope.currentState = toState.name;
+        });
+
+        // Remove the listener when the scope is destroyed to avoid a memory leak
+        $scope.$on('$destroy', function() {
+            cleanUpStateChangeListener();
+        });
     }];
