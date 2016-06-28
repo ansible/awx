096a0a262ac021d406a889c5cb01a03ec64ccc66
Akita Noek <anoek@ansible.com>
2016-06-28 15:42:56 -0400

    Ported old/ha.py test

---
+++ awx/main/tests/unit/test_ha.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2016 Ansible, Inc.
+
+# Python
+import mock
+
+# AWX
+from awx.main.ha import is_ha_environment
+
+@mock.patch('awx.main.models.Instance.objects.count', lambda: 2)
+def test_multiple_instances():
+    assert is_ha_environment()
+
+@mock.patch('awx.main.models.Instance.objects.count', lambda: 1)
+def test_db_localhost():
+    assert is_ha_environment() is False
