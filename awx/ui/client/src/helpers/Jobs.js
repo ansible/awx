145d9bb7583867ea18591e0602c22db781801cd3
Leigh Johnson <ljohnson@redhat.com>
2016-07-08 10:11:43 -0400

    update tooltips in job list on status change, resolves #2811

--- awx/ui/client/src/helpers/Jobs.js
+++ awx/ui/client/src/helpers/Jobs.js
@@ -231,6 +231,12 @@ export default
                 search_params = params.searchParams,
                 spinner = (params.spinner === undefined) ? true : params.spinner, key;
 
+            var buildTooltips = function(data){
+                data.forEach((val) => {
+                    val.status_tip = 'Job ' + val.status + ". Click for details.";
+                });
+            };
+
             GenerateList.inject(list, {
                 mode: 'edit',
                 id: id,
@@ -260,6 +266,7 @@ export default
             scope.$on('PostRefresh', function(){
                 JobsControllerInit({ scope: scope, parent_scope: parent_scope });
                 JobsListUpdate({ scope: scope, parent_scope: parent_scope, list: list });
+                buildTooltips(scope.jobs);
                 parent_scope.$emit('listLoaded');
             });
 
