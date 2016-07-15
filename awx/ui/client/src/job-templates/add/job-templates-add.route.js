dfa42544a6ca347b9873e81083a8d58fdd1b180b
Michael Abashian <mabashian@ansible.com>
2016-07-15 12:00:26 -0400

    Rolled back the onExit solution previously implemented to handle the backspace navigation on the job launch modal.  New solution listens for state changes within the directive and cleans itself up.

--- awx/ui/client/src/job-templates/add/job-templates-add.route.js
+++ awx/ui/client/src/job-templates/add/job-templates-add.route.js
@@ -16,14 +16,8 @@ export default {
         label: "CREATE JOB TEMPLATE"
     },
     onExit: function(){
-        // close the job launch modal
+        // close the survey maker modal
         // using an onExit event to handle cases where the user navs away using the url bar / back and not modal "X"
-        // Destroy the dialog
-        if($("#job-launch-modal").hasClass('ui-dialog-content')) {
-            $('#job-launch-modal').dialog('destroy');
-        }
-        // Remove the directive from the page (if it's there)
-        $('#content-container').find('submit-job').remove();
 
         if($("#survey-modal-dialog").hasClass('ui-dialog-content')) {
             $('#survey-modal-dialog').dialog('destroy');
