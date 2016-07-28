951d8e1555abd0e936d2369120ad5e312328972b
Graham Mainwaring <graham@mhn.org>
2016-07-28 10:31:29 -0400

    Check that venv is activated before loading wsgi app (#3148)
    Check that venv is activated before loading wsgi app

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,1 +1,4 @@
+import sys
+if sys.prefix != '/var/lib/awx/venv/tower':
+    raise RuntimeError('Tower virtualenv not activated. Check WSGIPythonHome in Apache configuration.')
 from awx.wsgi import application  # NOQA
