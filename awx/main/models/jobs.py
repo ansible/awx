8576dcda194e5afee507ba6843a3e88fbe1c69b6
jangsutsr <jangsutsr@gmail.com>
2016-08-24 10:58:53 -0400

    Remove legacy exception handling.

--- awx/main/models/jobs.py
+++ awx/main/models/jobs.py
@@ -746,14 +746,11 @@ class Job(UnifiedJob, JobOptions):
         Hides fields marked as passwords in survey.
         '''
         if self.survey_passwords:
-            try:
-                extra_vars = json.loads(self.extra_vars)
-                for key, value in self.survey_passwords.items():
-                    if key in extra_vars:
-                        extra_vars[key] = value
-                return json.dumps(extra_vars)
-            except ValueError:
-                pass
+            extra_vars = json.loads(self.extra_vars)
+            for key, value in self.survey_passwords.items():
+                if key in extra_vars:
+                    extra_vars[key] = value
+            return json.dumps(extra_vars)
         else:
             return self.extra_vars
 
