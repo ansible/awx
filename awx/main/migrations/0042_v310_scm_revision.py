0f93b875ca7307a34e3d440f5fc4c08feafcc38d
Matthew Jones <matburt@redhat.com>
2016-10-20 13:25:47 -0400

    Add migrations for execution node and project upds

---
+++ awx/main/migrations/0042_v310_scm_revision.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0041_v310_executionnode'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='project',
+            name='scm_revision',
+            field=models.CharField(default=b'', editable=False, max_length=1024, blank=True, help_text='The last revision fetched by a project update', verbose_name='SCM Revision'),
+        ),
+        migrations.AddField(
+            model_name='projectupdate',
+            name='job_type',
+            field=models.CharField(default=b'check', max_length=64, choices=[(b'run', 'Run'), (b'check', 'Check')]),
+        ),
+    ]
