951906408e212ebe2540a258f81ca6b80a27c194
Leigh Johnson <leigh-johnson@users.noreply.github.com>
2016-11-07 15:17:47 -0500

    Fix username links in Users > List view, kickback on #3805 (#3893)

--- awx/ui/client/src/lists/Users.js
+++ awx/ui/client/src/lists/Users.js
@@ -33,6 +33,7 @@ export default
 
         fields: {
             username: {
+                key: true,
                 label: i18n._('Username'),
                 columnClass: 'col-md-3 col-sm-3 col-xs-9'
             },
