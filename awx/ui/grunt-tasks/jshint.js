3706cc93373fa956bc9f3726750443e6071d6ea9
Leigh Johnson <ljohnson@redhat.com>
2016-11-09 11:41:09 -0500

    Switch Grunt jshint to stdout output #3845

--- awx/ui/grunt-tasks/jshint.js
+++ awx/ui/grunt-tasks/jshint.js
@@ -2,8 +2,7 @@ module.exports = {
     source: {
         src: ['client/src/**/*.js', '*conf.js', '*.config.js', 'Gruntfile.js'],
         options: {
-            reporter: 'jslint',
-            reporterOutput: 'coverage/jshint.xml',
+            reporter: require('jshint-stylish'),
             jshintrc: true
         }
     },
