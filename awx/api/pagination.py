08edbb1b723880ac81b63e5d1ca31c331f79b0a8
Matthew Jones <matburt@redhat.com>
2016-12-12 12:01:48 -0500

    Set an upper limit of 200 on the max page size

--- awx/api/pagination.py
+++ awx/api/pagination.py
@@ -9,6 +9,7 @@ from rest_framework.utils.urls import replace_query_param
 class Pagination(pagination.PageNumberPagination):
 
     page_size_query_param = 'page_size'
+    max_page_size = 200
 
     def get_next_link(self):
         if not self.page.has_next():
