384dbb6da2254af3ce4f2387aded5a9fb601ac97
Wayne Witzel III <wayne@riotousliving.com>
2016-12-13 14:41:23 -0500

    renamed squashed migrations to start at 0002

--- awx/main/migrations/0003_squashed_v300_v303_updates.py
+++ awx/main/migrations/0003_squashed_v300_v303_updates.py
@@ -40,7 +40,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('main', '0001_squashed_v300_release'),
+        ('main', '0002_squashed_v300_release'),
     ]
 
     operations = [
