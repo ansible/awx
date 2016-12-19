dbef3b7571e906bae6113cbf55f216b6727bc26a
Chris Meyers <chris.meyers.fsu@gmail.com>
2016-12-19 17:10:56 -0500

    update vmware inv sync unique instance id

--- awx/settings/defaults.py
+++ awx/settings/defaults.py
@@ -667,7 +667,7 @@ VMWARE_ENABLED_VAR = 'guest.gueststate'
 VMWARE_ENABLED_VALUE = 'running'
 
 # Inventory variable name containing the unique instance ID.
-VMWARE_INSTANCE_ID_VAR = 'vmware_uuid'
+VMWARE_INSTANCE_ID_VAR = 'config.uuid'
 
 # Filter for allowed group and host names when importing inventory
 # from VMware.
