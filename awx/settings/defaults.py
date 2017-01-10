efe0f867050d94edad62ba655d26708ff2cdc6ce
Bill Nottingham <notting@splat.cc>
2017-01-10 10:51:23 -0500

    Set TIME_ZONE to None.
    This causes Tower to use the system timezone.

--- awx/settings/defaults.py
+++ awx/settings/defaults.py
@@ -73,7 +73,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/New_York'
+TIME_ZONE = None
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
