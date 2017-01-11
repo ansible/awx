82f16f0551ad70461d12c2f18fbef05bd4ee7747
jaredevantabor <jtabor@ansible.com>
2017-01-11 14:25:08 -0800

    small change based on PR feedback

--- awx/ui/client/src/shared/smart-search/smart-search.controller.js
+++ awx/ui/client/src/shared/smart-search/smart-search.controller.js
@@ -23,11 +23,11 @@ export default ['$stateParams', '$scope', '$state', 'QuerySet', 'GetBasePath', '
 
         // Removes state definition defaults and pagination terms
         function stripDefaultParams(params) {
-            let strippedCopy, stripped =_.pick(params, (value, key) => {
+            let stripped =_.pick(params, (value, key) => {
                 // setting the default value of a term to null in a state definition is a very explicit way to ensure it will NEVER generate a search tag, even with a non-default value
                 return defaults[key] !== value && key !== 'order_by' && key !== 'page' && key !== 'page_size' && defaults[key] !== null;
             });
-            strippedCopy = _.cloneDeep(stripped);
+            let strippedCopy = _.cloneDeep(stripped);
             if(_.keys(_.pick(defaults, _.keys(strippedCopy))).length > 0){
                 for (var key in strippedCopy) {
                     if (strippedCopy.hasOwnProperty(key)) {
