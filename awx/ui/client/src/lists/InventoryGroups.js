70704610c5a7c576bb17541ceda9093919bfdfbe
Michael Abashian <mabashian@ansible.com>
2017-02-02 12:03:54 -0500

    Expand group name column out a bit

--- awx/ui/client/src/lists/InventoryGroups.js
+++ awx/ui/client/src/lists/InventoryGroups.js
@@ -50,7 +50,7 @@ export default
                 label: 'Groups',
                 key: true,
                 ngClick: "groupSelect(group.id)",
-                columnClass: 'col-lg-3 col-md-3 col-sm-3 col-xs-3',
+                columnClass: 'col-lg-6 col-md-6 col-sm-6 col-xs-6',
                 class: 'InventoryManage-breakWord',
             },
             total_groups: {
