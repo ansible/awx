46fdc123f4078c9cad5e93baec4ea9feb0ba1d73
Ryan Petrello <rpetrell@redhat.com>
2017-02-20 16:12:58 -0500

    more i18n string marking for host forms
    see: #5476

--- awx/ui/client/src/forms/Hosts.js
+++ awx/ui/client/src/forms/Hosts.js
@@ -67,15 +67,13 @@ export default
                     rows: 6,
                     class: 'Form-formGroup--fullWidth',
                     "default": "---",
-                    awPopOver: "<p>" +
-                       i18n._("Enter variables using either JSON or YAML syntax. Use the radio button to toggle between the two.") +
-                       "</p>" +
-                       "JSON:<br />\n" +
-                       "<blockquote>{<br />&emsp;\"somevar\": \"somevalue\",<br />&emsp;\"password\": \"magic\"<br /> }</blockquote>\n" +
-                       "YAML:<br />\n" +
-                       "<blockquote>---<br />somevar: somevalue<br />password: magic<br /></blockquote>\n" +
-                       '<p>View JSON examples at <a href="http://www.json.org" target="_blank">www.json.org</a></p>' +
-                       '<p>View YAML examples at <a href="http://docs.ansible.com/YAMLSyntax.html" target="_blank">docs.ansible.com</a></p>',
+                    awPopOver: "<p>" + i18n._("Enter inventory variables using either JSON or YAML syntax. Use the radio button to toggle between the two.") + "</p>" +
+                        "JSON:<br />\n" +
+                        "<blockquote>{<br />&emsp;\"somevar\": \"somevalue\",<br />&emsp;\"password\": \"magic\"<br /> }</blockquote>\n" +
+                        "YAML:<br />\n" +
+                        "<blockquote>---<br />somevar: somevalue<br />password: magic<br /></blockquote>\n" +
+                        '<p>' + i18n.sprintf(i18n._('View JSON examples at %s'), '<a href="http://www.json.org" target="_blank">www.json.org</a>') + '</p>' +
+                        '<p>' + i18n.sprintf(i18n._('View YAML examples at %s'), '<a href="http://docs.ansible.com/YAMLSyntax.html" target="_blank">docs.ansible.com</a>') + '</p>',
                     dataTitle: i18n._('Host Variables'),
                     dataPlacement: 'right',
                     dataContainer: 'body'
