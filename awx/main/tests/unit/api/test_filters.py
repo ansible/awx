0a5b43acaea37b0920b9ebcc1979869d270d5d1c
Matthew Jones <matburt@redhat.com>
2017-02-21 12:50:41 -0500

    Make unit test valid
    Failed after recent changed that exposed the invalidity

--- awx/main/tests/unit/api/test_filters.py
+++ awx/main/tests/unit/api/test_filters.py
@@ -9,14 +9,14 @@ from awx.main.models import Credential, JobTemplate
 def test_empty_in(empty_value):
     field_lookup = FieldLookupBackend()
     with pytest.raises(ValueError) as excinfo:
-        field_lookup.value_to_python(JobTemplate, 'project__in', empty_value)
+        field_lookup.value_to_python(JobTemplate, 'project__name__in', empty_value)
     assert 'empty value for __in' in str(excinfo.value)
 
 
 @pytest.mark.parametrize(u"valid_value", [u'foo', u'foo,'])
 def test_valid_in(valid_value):
     field_lookup = FieldLookupBackend()
-    value, new_lookup = field_lookup.value_to_python(JobTemplate, 'project__in', valid_value)
+    value, new_lookup = field_lookup.value_to_python(JobTemplate, 'project__name__in', valid_value)
     assert 'foo' in value
 
 
