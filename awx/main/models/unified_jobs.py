dd513621f0cde836827cad608c87645996689527
Chris Meyers <chris.meyers.fsu@gmail.com>
2017-02-27 09:38:45 -0500

    Revert "Merge pull request #5553 from chrismeyersfsu/fix-waiting_blocked"
    This reverts commit 9ba2122f4f85eecaeb6fa53ac92ea2811b05e83f, reversing
    changes made to c3a5f2c96fd85dd1405a8f5c875ffc988dee16a4.

--- awx/main/models/unified_jobs.py
+++ awx/main/models/unified_jobs.py
@@ -930,8 +930,7 @@ class UnifiedJob(PolymorphicModel, PasswordFieldsModel, CommonModelNameNotUnique
 
     def start_celery_task(self, opts, error_callback, success_callback):
         task_class = self._get_task_class()
-        async_result = task_class().apply_async((self.pk,), opts, link_error=error_callback, link=success_callback)
-        return async_result.id
+        task_class().apply_async((self.pk,), opts, link_error=error_callback, link=success_callback)
 
     def start(self, error_callback, success_callback, **kwargs):
         '''
