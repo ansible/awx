455d91fa0ef70428a5a6954f31f609b9c7eec5ad
John Mitchell <jlmitch5@ncsu.edu>
2017-03-21 10:53:32 -0400

    fix workflow edit controller di

--- awx/ui/client/src/templates/workflows/edit-workflow/workflow-edit.controller.js
+++ awx/ui/client/src/templates/workflows/edit-workflow/workflow-edit.controller.js
@@ -9,10 +9,11 @@ export default [
     'ProcessErrors', 'ClearScope', 'GetBasePath', '$q', 'ParseTypeChange',
     'Wait', 'Empty', 'ToJSON', 'initSurvey', '$state', 'CreateSelect2',
     'ParseVariableString', 'TemplatesService', 'Rest', 'ToggleNotification',
+    'OrgAdminLookup',
     function($scope, $stateParams, WorkflowForm, GenerateForm, Alert,
     ProcessErrors, ClearScope, GetBasePath, $q, ParseTypeChange, Wait, Empty,
     ToJSON, SurveyControllerInit, $state, CreateSelect2, ParseVariableString,
-    TemplatesService, Rest, ToggleNotification) {
+    TemplatesService, Rest, ToggleNotification, OrgAdminLookup) {
         ClearScope();
 
         $scope.$watch('workflow_job_template_obj.summary_fields.user_capabilities.edit', function(val) {
