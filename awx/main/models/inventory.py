b6519830452d50cde5f139b1491abbc63a3227f0
Wayne Witzel III <wayne@riotousliving.com>
2017-04-28 12:22:49 -0400

    also add inventory_source_id to websocket data

--- awx/main/models/inventory.py
+++ awx/main/models/inventory.py
@@ -1278,6 +1278,7 @@ class InventoryUpdate(UnifiedJob, InventorySourceOptions, JobNotificationMixin):
 
     def websocket_emit_data(self):
         websocket_data = super(InventoryUpdate, self).websocket_emit_data()
+        websocket_data.update(dict(inventory_source_id=self.inventory_source.pk))
 
         if self.inventory_source.inventory is not None:
             websocket_data.update(dict(inventory_id=self.inventory_source.inventory.pk))
