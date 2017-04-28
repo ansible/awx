1248be9b7cd725d3988a57a2298d8f8bd005a166
Wayne Witzel III <wayne@riotousliving.com>
2017-04-28 09:59:13 -0400

    Add inventory_id to websocket data

--- awx/main/models/inventory.py
+++ awx/main/models/inventory.py
@@ -1278,6 +1278,10 @@ class InventoryUpdate(UnifiedJob, InventorySourceOptions, JobNotificationMixin):
 
     def websocket_emit_data(self):
         websocket_data = super(InventoryUpdate, self).websocket_emit_data()
+
+        if self.inventory_source.inventory is not None:
+            websocket_data.update(dict(inventory_id=self.inventory_source.inventory.pk))
+
         if self.inventory_source.deprecated_group is not None:  # TODO: remove in 3.3
             websocket_data.update(dict(group_id=self.inventory_source.deprecated_group.id))
         return websocket_data
