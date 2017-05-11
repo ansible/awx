40cd2fb8ac6210118c2ff9f523286c38f517d0b7
Ben Thomasson <bthomass@redhat.com>
2017-05-11 10:44:18 -0400

    Move ngTouch and hamsterjs to webpack vendor bundle

--- awx/network_ui/static/network_ui/webpack.config.js
+++ awx/network_ui/static/network_ui/webpack.config.js
@@ -2,13 +2,22 @@ var webpack = require('webpack');
 module.exports = {
     entry: {
         app: "./src/main.js",
-        vendor: ["reconnectingwebsocket"],
+        vendor: ["hamsterjs",
+                 "angular-mousewheel",
+                 "ngTouch",
+                 "reconnectingwebsocket"],
     },
     output: {
         path: __dirname + "/js",
         filename:  "bundle.js",
     },
     plugins: [
+         new webpack.ProvidePlugin({Hamster: 'hamsterjs'}),
          new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' })
-    ]
+    ],
+    resolve: {
+        alias: {
+            ngTouch: __dirname + "/vendor/ngTouch.js"
+        }
+    }
 };
