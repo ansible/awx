2100510334156fe64bda66631df0d688cbb59ce0
Ben Thomasson <bthomass@redhat.com>
2017-05-17 16:05:20 -0400

    Adds JSON and YAML export of the network topology.
    Adds views that export the entire network topology as JSON and YAML.

--- awx/network_ui/views.py
+++ awx/network_ui/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
+from django import forms
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponse
+import yaml
+
 
 # Create your views here.
 from .models import Topology
+from .serializers import topology_data
 
 
 def index(request):
     return render(request, "network_ui/index.html", dict(topologies=Topology.objects.all().order_by('-pk')))
+
+
+class TopologyForm(forms.Form):
+    topology_id = forms.IntegerField()
+
+
+def json_topology_data(request):
+    form = TopologyForm(request.GET)
+    if form.is_valid():
+        return JsonResponse(topology_data(form.cleaned_data['topology_id']))
+    else:
+        return HttpResponseBadRequest(form.errors)
+
+
+def yaml_topology_data(request):
+    form = TopologyForm(request.GET)
+    if form.is_valid():
+        return HttpResponse(yaml.safe_dump(topology_data(form.cleaned_data['topology_id']),
+                                           default_flow_style=False),
+                            content_type='application/yaml')
+    else:
+        return HttpResponseBadRequest(form.errors)
+
