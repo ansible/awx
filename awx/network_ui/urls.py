640e687f3ea99bfdb5ea77285d058c16a96da4f4
Ben Thomasson <bthomass@redhat.com>
2017-05-17 16:05:20 -0400

    Adds JSON and YAML export of the network topology.
    Adds views that export the entire network topology as JSON and YAML.

--- awx/network_ui/urls.py
+++ awx/network_ui/urls.py
@@ -6,6 +6,8 @@ import awx.network_ui.routing
 
 app_name = 'network_ui'
 urlpatterns = [
-        url(r'^$', views.index, name='index'),
+    url(r'^topology.json$', views.json_topology_data, name='json_topology_data'),
+    url(r'^topology.yaml$', views.yaml_topology_data, name='json_topology_data'),
+    url(r'^$', views.index, name='index'),
 ]
 
