1fcfcf8a936d966abcfa01395f22a9cfbed52a35
Ben Thomasson <bthomass@redhat.com>
2017-05-22 14:32:57 -0400

    Add JSON and YAML network UI data download

--- awx/network_ui/views.py
+++ awx/network_ui/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django import forms
-from django.http import JsonResponse, HttpResponseBadRequest
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponse
+import yaml
 
 
 # Create your views here.
@@ -20,6 +21,14 @@ def json_topology_data(request):
     form = TopologyForm(request.GET)
     if form.is_valid():
         return JsonResponse(topology_data(form.cleaned_data['topology_id']))
+    else:
+        return HttpResponseBadRequest(form.errors)
+
+
+def yaml_topology_data(request):
+    form = TopologyForm(request.GET)
+    if form.is_valid():
+        return HttpResponse(yaml.safe_dump(topology_data(form.cleaned_data['topology_id']), default_flow_style=False))
     else:
         return HttpResponseBadRequest(form.errors)
 
