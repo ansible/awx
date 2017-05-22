a0be24938f153c1f465608a282d4ad3ef9d4c139
Ben Thomasson <bthomass@redhat.com>
2017-05-22 14:32:57 -0400

    Add JSON and YAML network UI data download

--- awx/network_ui/urls.py
+++ awx/network_ui/urls.py
@@ -6,7 +6,8 @@ import awx.network_ui.routing
 
 app_name = 'network_ui'
 urlpatterns = [
-        url(r'^topology$', views.json_topology_data, name='json_topology_data'),
+        url(r'^topology_json$', views.json_topology_data, name='json_topology_data'),
+        url(r'^topology_yaml$', views.yaml_topology_data, name='json_topology_data'),
         url(r'^$', views.index, name='index'),
 ]
 
