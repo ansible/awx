e81c6d3c163da6b934beae476214004c6ad8cd6d
Jared Tabor <jaredevantabor@gmail.com>
2017-06-13 13:56:22 -0700

    committing AWX svg icons from trahman
    and committing the variables.less, I don't think it got picked up
    in my last commit

--- awx/ui/client/src/login/loginModal/loginModal.controller.js
+++ awx/ui/client/src/login/loginModal/loginModal.controller.js
@@ -98,7 +98,7 @@ export default ['$log', '$cookies', '$compile', '$rootScope',
             scope.customLogo = $rootScope.custom_logo;
             scope.customLogoPresent = true;
         } else {
-            scope.customLogo = "default-logo-large.png";
+            scope.customLogo = "tower-logo-login.svg";
             scope.customLogoPresent = false;
         }
         scope.customLoginInfo = $AnsibleConfig.custom_login_info;
