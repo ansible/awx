7a795b86816e816f52ae25d1cb539f88fe72d898
Chris Meyers <chris.meyers.fsu@gmail.com>
2017-06-29 13:27:17 -0400

    fix job launch deadlock
    * This both fixes the deadlock problem and a logic problem. We shouldn't
    set the job's job_template current_job to pending jobs.

--- awx/main/models/unified_jobs.py
+++ awx/main/models/unified_jobs.py
@@ -713,7 +713,7 @@ class UnifiedJob(PolymorphicModel, PasswordFieldsModel, CommonModelNameNotUnique
         result = super(UnifiedJob, self).save(*args, **kwargs)
 
         # If status changed, update the parent instance.
-        if self.status != status_before:
+        if self.status != status_before and self.status != 'pending':
             self._update_parent_instance()
 
         # Done.
