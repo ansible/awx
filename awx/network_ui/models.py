8c1a1222ae0157c972826be8f80bfedc7390ddae
Ben Thomasson <bthomass@redhat.com>
2017-07-06 11:46:04 -0400

    Add device_id_seq and link_id_seq to Topology and interface_id_seq to Device

--- awx/network_ui/models.py
+++ awx/network_ui/models.py
@@ -5,11 +5,12 @@ class Device(models.Model):
 
     device_id = models.AutoField(primary_key=True,)
     topology = models.ForeignKey('Topology',)
-    name = models.CharField(max_length=200, )
+    name = models.CharField(max_length=200,)
     x = models.IntegerField()
     y = models.IntegerField()
     id = models.IntegerField()
-    type = models.CharField(max_length=200, )
+    type = models.CharField(max_length=200,)
+    interface_id_seq = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.name
@@ -18,21 +19,23 @@ class Device(models.Model):
 class Link(models.Model):
 
     link_id = models.AutoField(primary_key=True,)
-    from_device = models.ForeignKey('Device',  related_name='from_link', )
-    to_device = models.ForeignKey('Device',  related_name='to_link', )
-    from_interface = models.ForeignKey('Interface',  related_name='from_link', )
-    to_interface = models.ForeignKey('Interface',  related_name='to_link', )
+    from_device = models.ForeignKey('Device', related_name='from_link',)
+    to_device = models.ForeignKey('Device', related_name='to_link',)
+    from_interface = models.ForeignKey('Interface', related_name='from_link',)
+    to_interface = models.ForeignKey('Interface', related_name='to_link',)
     id = models.IntegerField()
-    name = models.CharField(max_length=200, )
+    name = models.CharField(max_length=200,)
 
 
 class Topology(models.Model):
 
     topology_id = models.AutoField(primary_key=True,)
-    name = models.CharField(max_length=200, )
+    name = models.CharField(max_length=200,)
     scale = models.FloatField()
     panX = models.FloatField()
     panY = models.FloatField()
+    device_id_seq = models.IntegerField('Topology', default=0)
+    link_id_seq = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.name
@@ -57,7 +60,7 @@ class TopologyHistory(models.Model):
 class MessageType(models.Model):
 
     message_type_id = models.AutoField(primary_key=True,)
-    name = models.CharField(max_length=200, )
+    name = models.CharField(max_length=200,)
 
     def __unicode__(self):
         return self.name
@@ -67,7 +70,7 @@ class Interface(models.Model):
 
     interface_id = models.AutoField(primary_key=True,)
     device = models.ForeignKey('Device',)
-    name = models.CharField(max_length=200, )
+    name = models.CharField(max_length=200,)
     id = models.IntegerField()
 
     def __unicode__(self):
