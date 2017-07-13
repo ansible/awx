8faf4616719b15107753b23dfafe8813ca05e390
Ben Thomasson <bthomass@redhat.com>
2017-07-13 10:12:44 -0400

    Progress on tables UI

--- awx/network_ui/static/network_ui/src/tables.ui.controller.js
+++ awx/network_ui/static/network_ui/src/tables.ui.controller.js
@@ -1,14 +1,25 @@
 
 
-var TablesUIController = function($scope) {
+var TablesUIController = function($scope, $window) {
 
-    $scope.hello = "world";
+    $window.scope = $scope;
+
+    $scope.user = {
+        name: 'world'
+    };
+
+    $scope.data = [['A', 'B', 'C'],
+    ['1', 1,2,3], ['2', 4,5,6], ['3', 7,8,9]];
 
     console.log("Tables UI started");
 
     $scope.$on('$destroy', function () {
         console.log("Tables UI stopping");
     });
+
+    $scope.updateData = function (old_data, new_data, column_index, row_index, column_name, row_name) {
+        console.log(['updateData', old_data, new_data, column_index, row_index, column_name, row_name]);
+    };
 };
 
 exports.TablesUIController = TablesUIController;
