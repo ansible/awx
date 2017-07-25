7077271299b378ac8f166dfaa14d2ce024b28c4e
Bill Nottingham <notting@splat.cc>
2017-07-25 14:19:52 -0400

    Fixup flake8

--- awx/main/constants.py
+++ awx/main/constants.py
@@ -5,10 +5,4 @@ from django.utils.translation import ugettext_lazy as _
 
 CLOUD_PROVIDERS = ('azure', 'azure_rm', 'ec2', 'gce', 'rax', 'vmware', 'openstack', 'satellite6', 'cloudforms')
 SCHEDULEABLE_PROVIDERS = CLOUD_PROVIDERS + ('custom', 'scm',)
-PRIVILEGE_ESCALATION_METHODS = [
-                ('sudo', _('Sudo')),
-                ('su', _('Su')),
-                ('pbrun', _('Pbrun')),
-                ('pfexec', _('Pfexec')),
-                ('dzdo', _('DZDO')),
-                ('pmrun', _('Pmrun'))]
+PRIVILEGE_ESCALATION_METHODS = [ ('sudo', _('Sudo')), ('su', _('Su')), ('pbrun', _('Pbrun')), ('pfexec', _('Pfexec')), ('dzdo', _('DZDO')), ('pmrun', _('Pmrun'))]
