66c9dec7cd37ffe9e211d455503291374d3521d4
Ben Thomasson <bthomass@redhat.com>
2017-07-26 09:42:15 -0400

    Add hotkeys FSM

---
+++ awx/network_ui/static/network_ui/src/hotkeys.fsm.js
@@ -0,0 +1,104 @@
+var inherits = require('inherits');
+var fsm = require('./fsm.js');
+var messages = require('./messages.js');
+
+function _State () {
+}
+inherits(_State, fsm._State);
+
+
+function _Enabled () {
+    this.name = 'Enabled';
+}
+inherits(_Enabled, _State);
+var Enabled = new _Enabled();
+exports.Enabled = Enabled;
+
+function _Start () {
+    this.name = 'Start';
+}
+inherits(_Start, _State);
+var Start = new _Start();
+exports.Start = Start;
+
+function _Disabled () {
+    this.name = 'Disabled';
+}
+inherits(_Disabled, _State);
+var Disabled = new _Disabled();
+exports.Disabled = Disabled;
+
+
+
+
+_Enabled.prototype.onDisable = function (controller) {
+
+    controller.changeState(Disabled);
+
+};
+_Enabled.prototype.onDisable.transitions = ['Disabled'];
+
+
+_Enabled.prototype.onKeyDown = function(controller, msg_type, $event) {
+
+	var scope = controller.scope;
+
+    if ($event.key === 'l') {
+        scope.first_controller.handle_message("NewLink", $event);
+        return;
+    }
+
+    if ($event.key === 'd') {
+        scope.debug.hidden = !scope.debug.hidden;
+        return;
+    }
+    if ($event.key === 'p') {
+        scope.cursor.hidden = !scope.cursor.hidden;
+        return;
+    }
+    if ($event.key === 'b') {
+        scope.hide_buttons = !scope.hide_buttons;
+        return;
+    }
+    if ($event.key === 'i') {
+        scope.hide_interfaces = !scope.hide_interfaces;
+        return;
+    }
+
+	if ($event.key === 'r') {
+        scope.first_controller.handle_message("NewDevice", new messages.NewDevice("router"));
+        return;
+	}
+    else if ($event.key === 's') {
+        scope.first_controller.handle_message("NewDevice", new messages.NewDevice("switch"));
+        return;
+	}
+    else if ($event.key === 'a') {
+        scope.first_controller.handle_message("NewDevice", new messages.NewDevice("rack"));
+        return;
+	}
+    else if ($event.key === 'h') {
+        scope.first_controller.handle_message("NewDevice", new messages.NewDevice("host"));
+        return;
+	}
+
+	controller.next_controller.handle_message(msg_type, $event);
+};
+
+_Start.prototype.start = function (controller) {
+
+    controller.changeState(Enabled);
+
+};
+_Start.prototype.start.transitions = ['Enabled'];
+
+
+
+_Disabled.prototype.onEnable = function (controller) {
+
+    controller.changeState(Enabled);
+
+};
+_Disabled.prototype.onEnable.transitions = ['Enabled'];
+
+
