c8b014292927523013ed2b3e77707f9e022fd6cb
Shane McDonald <me@shanemcd.com>
2017-07-27 11:39:00 -0400

    Fix test failures

--- tools/docker-compose/unit-tests/entrypoint.sh
+++ tools/docker-compose/unit-tests/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Code duplicated from start_development.sh
-cp -R /tmp/ansible_awx.egg-info /awx_devel/ || true
-sed -i "s/placeholder/$(git describe --long | sed 's/\./\\./g')/" /awx_devel/ansible_awx.egg-info/PKG-INFO
-cp /tmp/ansible-awx.egg-link /venv/awx/lib/python2.7/site-packages/ansible-awx.egg-link
+cp -R /tmp/awx.egg-info /awx_devel/ || true
+sed -i "s/placeholder/$(git describe --long | sed 's/\./\\./g')/" /awx_devel/awx.egg-info/PKG-INFO
+cp /tmp/awx.egg-link /venv/awx/lib/python2.7/site-packages/awx.egg-link
 
 cp -f awx/settings/local_settings.py.docker_compose awx/settings/local_settings.py
 
