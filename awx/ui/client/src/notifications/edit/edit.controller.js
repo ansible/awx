c0f01e671b2a80352aff4f3b0874027ef084885c
Marliana Lara <mlara@redhat.com>
2017-07-27 15:31:39 -0400

    Fix UX items related to Notifications

--- awx/ui/client/src/notifications/edit/edit.controller.js
+++ awx/ui/client/src/notifications/edit/edit.controller.js
@@ -205,10 +205,22 @@ export default ['Rest', 'Wait',
 
         $scope.emailOptionsChange = function () {
             if ($scope.email_options === 'use_ssl') {
+                if ($scope.use_ssl) {
+                    $scope.email_options = null;
+                    $scope.use_ssl = false;
+                    return;
+                }
+
                 $scope.use_ssl = true;
                 $scope.use_tls = false;
             }
             else if ($scope.email_options === 'use_tls') {
+                if ($scope.use_tls) {
+                    $scope.email_options = null;
+                    $scope.use_tls = false;
+                    return;
+                }
+
                 $scope.use_ssl = false;
                 $scope.use_tls = true;
             }
@@ -263,7 +275,7 @@ export default ['Rest', 'Wait',
             Rest.setUrl(url + id + '/');
             Rest.put(params)
                 .success(function() {
-                    $state.go($state.current, null, { reload: true });
+                    $state.go('notifications', {}, { reload: true });
                     Wait('stop');
                 })
                 .error(function(data, status) {
