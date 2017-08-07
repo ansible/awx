d41d9a957e4979f0b826f2ea68f81fc6167a2e8a
AlanCoding <arominge@redhat.com>
2017-08-07 12:20:56 -0400

    stubs and docs for management cmd renames

--- awx/main/management/commands/deprovision_instance.py
+++ awx/main/management/commands/deprovision_instance.py
@@ -3,6 +3,7 @@
 
 from optparse import make_option
 import subprocess
+import warnings
 
 from django.db import transaction
 from django.core.management.base import BaseCommand, CommandError
@@ -17,15 +18,23 @@ class Command(BaseCommand):
     """
 
     option_list = BaseCommand.option_list + (
-        make_option('--name', dest='name', type='string',
+        make_option('--hostname', dest='hostname', type='string',
                     help='Hostname used during provisioning'),
+        make_option('--name', dest='name', type='string',
+                    help='(PENDING DEPRECIATION) Hostname used during provisioning'),
     )
 
     @transaction.atomic
     def handle(self, *args, **options):
-        hostname = options.get('name')
+        # TODO: remove in 3.3
+        if options.get('name'):
+            warnings.warn("`--name` is depreciated in favor of `--hostname`, and will be removed in release 3.3.")
+            if options.get('hostname'):
+                raise CommandError("Cannot accept both --name and --hostname.")
+            options['hostname'] = options['name']
+        hostname = options.get('hostname')
         if not hostname:
-            raise CommandError("--name is a required argument")
+            raise CommandError("--hostname is a required argument")
         with advisory_lock('instance_registration_%s' % hostname):
             instance = Instance.objects.filter(hostname=hostname)
             if instance.exists():
@@ -33,7 +42,8 @@ class Command(BaseCommand):
                 print("Instance Removed")
                 result = subprocess.Popen("rabbitmqctl forget_cluster_node rabbitmq@{}".format(hostname), shell=True).wait()
                 if result != 0:
-                    print("Node deprovisioning may have failed when attempting to remove the RabbitMQ instance from the cluster")
+                    print("Node deprovisioning may have failed when attempting to "
+                          "remove the RabbitMQ instance {} from the cluster".format(hostname))
                 else:
                     print('Successfully deprovisioned {}'.format(hostname))
                 print('(changed: True)')
