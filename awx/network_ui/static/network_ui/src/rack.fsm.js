dcd6d660520f360966b311a07cfbc8439f85ea23
Ben Thomasson <bthomass@redhat.com>
2017-08-17 09:28:07 -0400

    Add PasteRack and PasteSite message handlers

--- awx/network_ui/static/network_ui/src/rack.fsm.js
+++ awx/network_ui/static/network_ui/src/rack.fsm.js
@@ -1,5 +1,7 @@
 var inherits = require('inherits');
 var fsm = require('./fsm.js');
+var models = require('./models.js');
+var messages = require('./messages.js');
 
 function _State () {
 }
@@ -42,3 +44,33 @@ _Start.prototype.start = function (controller) {
 _Start.prototype.start.transitions = ['Ready'];
 
 
+_Ready.prototype.onPasteRack = function (controller, msg_type, message) {
+
+	var scope = controller.scope;
+    scope.hide_groups = false;
+
+    scope.pressedX = scope.mouseX;
+    scope.pressedY = scope.mouseY;
+    scope.pressedScaledX = scope.scaledX;
+    scope.pressedScaledY = scope.scaledY;
+
+    var group = new models.Group(controller.scope.group_id_seq(),
+                                 message.group.name,
+                                 message.group.type,
+                                 scope.scaledX,
+                                 scope.scaledY,
+                                 scope.scaledX + message.group.x2,
+                                 scope.scaledY + message.group.y2,
+                                 false);
+
+    scope.send_control_message(new messages.GroupCreate(scope.client_id,
+                                                        group.id,
+                                                        group.x1,
+                                                        group.y1,
+                                                        group.x2,
+                                                        group.y2,
+                                                        group.name,
+                                                        group.type));
+
+    scope.groups.push(group);
+};
