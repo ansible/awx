87eab79f7070034eef7a6367d5a6451fe68d8e8e
gconsidine <greg@gregconsidine.com>
2017-09-12 14:57:07 -0400

    Add support for quoted values containing spaces in search

--- awx/ui/client/src/shared/smart-search/smart-search.service.js
+++ awx/ui/client/src/shared/smart-search/smart-search.service.js
@@ -1,5 +1,34 @@
 export default [function() {
     return {
+        /**
+         * For the Smart Host Filter, values with spaces are wrapped with double quotes on input.
+         * To avoid having these quoted values split up and treated as terms themselves, some
+         * work is done to encode quotes in quoted values and the spaces within those quoted
+         * values before calling to `splitSearchIntoTerms`.
+         */
+        splitHostIntoTerms (searchString) {
+            let groups = [];
+            let quoted;
+
+            searchString.split(' ').forEach(substring => {
+                if (substring.includes(':"')) {
+                    quoted = substring;
+                } else if (quoted) {
+                    quoted += ` ${substring}`;
+
+                    if (substring.includes('"')) {
+                        quoted = quoted.replace(/"/g, encodeURIComponent('"'));
+                        quoted = quoted.replace(/ /g, encodeURIComponent(' '));
+                        groups.push(quoted);
+                        quoted = undefined;
+                    }
+                } else {
+                    groups.push(substring);
+                }
+            });
+
+            return this.splitSearchIntoTerms(groups.join(' '));
+        },
         splitSearchIntoTerms(searchString) {
             return searchString.match(/(?:[^\s"']+|"[^"]*"|'[^']*')+/g);
         },
