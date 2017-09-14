69122a5454c74bbbb37789e6f8310c9e32d6a7d8
Nicholas O. Wilburn <senior.crepe@gmail.com>
2017-09-14 15:43:39 -0700

    Remove trailing whitespace

--- awx/main/notifications/mattermost_backend.py
+++ awx/main/notifications/mattermost_backend.py
@@ -18,7 +18,7 @@ class MattermostBackend(AWXBaseEmailBackend):
     recipient_parameter = "mattermost_url"
     sender_parameter = None
 
-    def __init__(self, mattermost_channel=None, mattermost_username=None, 
+    def __init__(self, mattermost_channel=None, mattermost_username=None,
                  mattermost_icon_url=None, fail_silently=False, **kwargs):
         super(MattermostBackend, self).__init__(fail_silently=fail_silently)
         self.mattermost_channel = mattermost_channel
