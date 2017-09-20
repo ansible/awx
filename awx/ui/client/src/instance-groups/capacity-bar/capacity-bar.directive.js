2306c346893c76df344818986985c43631541eee
John Mitchell <jlmitch5@ncsu.edu>
2017-09-20 17:49:02 -0400

    update instance capacity bar to be grey when instance or group is offline

--- awx/ui/client/src/instance-groups/capacity-bar/capacity-bar.directive.js
+++ awx/ui/client/src/instance-groups/capacity-bar/capacity-bar.directive.js
@@ -2,11 +2,22 @@ export default ['templateUrl',
     function (templateUrl) {
         return {
             scope: {
-                capacity: '='
+                capacity: '=',
+                totalCapacity: '='
             },
             templateUrl: templateUrl('instance-groups/capacity-bar/capacity-bar'),
             restrict: 'E',
             link: function(scope) {
+                scope.isOffline = false;
+
+                scope.$watch('totalCapacity', function(val) {
+                    if (val === 0) {
+                        scope.isOffline = true;
+                    } else {
+                        scope.isOffline = false;
+                    }
+                }, true);
+
                 scope.$watch('capacity', function() {
                     scope.CapacityStyle = {
                         'flex-grow': scope.capacity * 0.01
