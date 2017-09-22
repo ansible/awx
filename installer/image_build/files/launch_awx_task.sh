73a37a281d99f2d5a3e22dcee42c02e7c6cf09af
Matthew Jones <matburt@redhat.com>
2017-09-21 21:31:26 -0400

    Allow pre-populating the default AWX superuser account

--- installer/image_build/files/launch_awx_task.sh
+++ installer/image_build/files/launch_awx_task.sh
@@ -6,8 +6,13 @@ if [ `id -u` -ge 10000 ]; then
 fi
 ANSIBLE_REMOTE_TEMP=/tmp ANSIBLE_LOCAL_TEMP=/tmp ansible -i "127.0.0.1," -c local -v -m postgresql_db -U $DATABASE_USER -a "name=$DATABASE_NAME owner=$DATABASE_USER login_user=$DATABASE_USER login_host=$DATABASE_HOST login_password=$DATABASE_PASSWORD" all
 awx-manage migrate --noinput --fake-initial
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'root@localhost', 'password')" | awx-manage shell
-awx-manage create_preload_data
+if [ ! -z "$AWX_ADMIN_USER" ]&&[ ! -z "$AWX_ADMIN_PASSWORD" ]; then
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('$AWX_ADMIN_USER', 'root@localhost', '$AWX_ADMIN_PASSWORD')" | awx-manage shell
+    awx-manage create_preload_data
+else
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'root@localhost', 'password')" | awx-manage shell
+    awx-manage create_preload_data
+fi
 awx-manage provision_instance --hostname=$(hostname)
 awx-manage register_queue --queuename=tower --hostnames=$(hostname)
 supervisord -c /supervisor_task.conf
