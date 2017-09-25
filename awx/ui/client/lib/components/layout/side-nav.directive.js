bb625264d47dfa086d2818384dabbf34464fa20b
Marliana Lara <mlara@redhat.com>
2017-09-25 11:56:26 -0400

    Move DOM event listener to directive

--- awx/ui/client/lib/components/layout/side-nav.directive.js
+++ awx/ui/client/lib/components/layout/side-nav.directive.js
@@ -2,27 +2,38 @@ const templateUrl = require('~components/layout/side-nav.partial.html');
 
 function atSideNavLink (scope, element, attrs, ctrl) {
     scope.layoutVm = ctrl;
+
+    document.on('click', (e) => {
+        if ($(e.target).parents('.at-Layout-side').length === 0) {
+            scope.$emit('clickOutsideSideNav');
+        }
+    });
 }
 
-function AtSideNavController ($scope) {
+function AtSideNavController ($scope, $window) {
     let vm = this || {};
+    const breakpoint = 700;
 
     vm.isExpanded = false;
 
     vm.toggleExpansion = () => {
         vm.isExpanded = !vm.isExpanded;
-    }
+    };
 
-    document.body.onclick = (e) => {
-        if ($(e.target).parents(".at-Layout-side").length === 0) {
+    $scope.$watch('layoutVm.currentState', () => {
+        if ($window.innerWidth <= breakpoint) {
             vm.isExpanded = false;
         }
-    }
+    });
 
-    $scope.$on('$locationChangeStart', function(event) {
-        vm.isExpanded = false;
+    $scope.$on('clickOutsideSideNav', () => {
+        if ($window.innerWidth <= breakpoint) {
+            vm.isExpanded = false;
+        }
     });
 }
+
+AtSideNavController.$inject = ['$scope', '$window'];
 
 function atSideNav () {
     return {
