da5e6883d4a87a7870293a86dfaf570de86624e2
Matthew Jones <matburt@redhat.com>
2017-10-13 14:27:30 -0400

    Lower the default uid by which we'll rewrite passwd
    This fixes some issues with openshift under certain security policies

--- installer/image_build/files/launch_awx.sh
+++ installer/image_build/files/launch_awx.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ `id -u` -ge 10000 ]; then
+if [ `id -u` -ge 500 ]; then
     echo "awx:x:`id -u`:`id -g`:,,,:/var/lib/awx:/bin/bash" >> /tmp/passwd
     cat /tmp/passwd > /etc/passwd
     rm /tmp/passwd
