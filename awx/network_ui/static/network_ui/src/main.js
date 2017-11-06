f187f5c550b153bd299e1e0144f98e1bdc1a1dab
Ben Thomasson <bthomass@redhat.com>
2017-11-06 18:12:00 -0500

    Fix network_ui dev env

--- awx/network_ui/static/network_ui/src/main.js
+++ awx/network_ui/static/network_ui/src/main.js
@@ -1,6 +1,8 @@
 var networkUI = require('./network.ui.app.js');
 var tablesUI = require('./tables.ui.app.js');
 var tower = require('./tower.app.js');
+var ngTouch = require('./ngTouch.js');
 exports.networkUI = networkUI.networkUI;
-+exports.tablesUI = tablesUI.tablesUI;
+exports.tablesUI = tablesUI.tablesUI;
 exports.tower = tower.tower;
+exports.ngTouch = ngTouch;
