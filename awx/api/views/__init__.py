fd49659fa0feed02b1335ea5ce47176e28d6b7fc
Wayne Witzel III <wayne@riotousliving.com>
2017-11-10 12:00:33 -0500

    refactor root views

---
+++ awx/api/views/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2017 Ansible, Inc.
+# All Rights Reserved.
+
+from __future__ import absolute_import, unicode_literals
+
+from .root import ( #noqa
+    ApiRootView,
+    ApiVersionRootView,
+    ApiV1RootView,
+    ApiV1PingView,
+    ApiV1ConfigView,
+    ApiV2RootView,
+)
