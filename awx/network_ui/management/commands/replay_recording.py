b87a37f4bf8f7f3675efe8fda3ae35e1fef5f545
Ben Thomasson <bthomass@redhat.com>
2017-11-15 14:26:47 -0500

    Enable capture and replay tests on the network ui

--- awx/network_ui/management/commands/replay_recording.py
+++ awx/network_ui/management/commands/replay_recording.py
@@ -16,22 +16,22 @@ class Command(BaseCommand):
         parser.add_argument('topology_id', type=int)
         parser.add_argument('recording')
         parser.add_argument('--time-scale', dest="time_scale", default=1.0, type=float)
-        parser.add_argument('--delete-topology-at-start', dest="delete_topolgy", action="store_true", default=False)
+        parser.add_argument('--delete-topology-at-start', dest="delete_topology", action="store_true", default=False)
 
     def handle(self, *args, **options):
         print options['topology_id']
         print options['recording']
         topology_id = options['topology_id']
-        if options['delete_topolgy'] is True:
+        if options['delete_topology'] is True:
             TopologyHistory.objects.filter(topology_id=topology_id).delete()
             Device.objects.filter(topology_id=topology_id).delete()
         time.scale = options.get('time_scale', 1.0)
-        ui = MessageHandler(create_connection("ws://localhost:8001/network_ui/topology?topology_id={0}".format(options['topology_id'])))
+        ui = MessageHandler(create_connection("ws://localhost:8013/network_ui/topology?topology_id={0}".format(options['topology_id'])))
         ui.recv()
         ui.recv()
         ui.send('StopRecording')
         ui.send('StartReplay')
-        if options['delete_topolgy'] is True:
+        if options['delete_topology'] is True:
             ui.send_message(['History', []])
             ui.send('Snapshot', sender=ui.client_id, devices=[], links=[])
         messages = []
