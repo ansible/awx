2c2525d82064dc2efe34225af4747dc6db277276
Jared Tabor <jtabor@redhat.com>
2017-12-08 09:11:03 -0800

    adding action-icon directive

--- awx/network_ui/static/network_ui/src/chevron.right.directive.js
+++ awx/network_ui/static/network_ui/src/chevron.right.directive.js
@@ -1,6 +1,12 @@
 /* Copyright (c) 2017 Red Hat, Inc. */
 
 function chevronRight () {
-  return { restrict: 'A', templateUrl: '/static/network_ui/widgets/chevron_right.svg' };
+  return {
+      restrict: 'A',
+      templateUrl: '/static/network_ui/widgets/chevron_right.svg',
+      scope: {
+          actionIcon: '='
+      }
+  };
 }
 exports.chevronRight = chevronRight;
