1c1844d8895cb82ed2630a166fe86b469dc9b902
Ben Thomasson <bthomass@redhat.com>
2018-01-03 10:30:25 -0500

    Reorganizes the network-ui code under awx/client/src/network-ui
    * Moves network UI source to awx/client/src/network-ui
    * Moves network ui partials to awx/ui/client/network-ui
    * Renames widgets with suffix partial.svg
    * Updates directives to use bundled partials
    * Uses ~network-ui for loading UI component

--- awx/network_ui/static/network_ui/src/button.directive.js
+++
@@ -1,6 +0,0 @@
-/* Copyright (c) 2017 Red Hat, Inc. */
-
-function button () {
-  return { restrict: 'A', templateUrl: '/static/network_ui/widgets/button.html' };
-}
-exports.button = button;
---
+++ awx/ui/client/src/network-ui/group.directive.js
@@ -0,0 +1,8 @@
+/* Copyright (c) 2017 Red Hat, Inc. */
+
+const templateUrl = require('~network-ui/group.partial.svg');
+
+function group () {
+  return { restrict: 'A', templateUrl};
+}
+exports.group = group;
