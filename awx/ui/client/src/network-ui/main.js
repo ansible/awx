0b8d85cccb37e60d6bc31b03ca4eaea0e2083bd9
Ben Thomasson <bthomass@redhat.com>
2018-01-05 09:16:50 -0500

    Removing unused widgets app

--- awx/ui/client/src/network-ui/main.js
+++ awx/ui/client/src/network-ui/main.js
@@ -1,6 +1,5 @@
 /* Copyright (c) 2017 Red Hat, Inc. */
 var networkUI = require('./network.ui.app.js');
-var networkWidgets = require('./network.widgets.app.js');
 var tablesUI = require('./tables.ui.app.js');
 var tower = require('./tower.app.js');
 var ngTouch = require('./ngTouch.js');
@@ -8,5 +7,3 @@ exports.networkUI = networkUI.networkUI;
 exports.tablesUI = tablesUI.tablesUI;
 exports.tower = tower.tower;
 exports.ngTouch = ngTouch;
-exports.networkWidgets = networkWidgets;
-
