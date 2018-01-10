bf35d98e9da4d8deb3359d1bded5ac8ae2249a4e
Ben Thomasson <bthomass@redhat.com>
2018-01-10 13:06:32 -0500

    Adds models to track tests

--- awx/network_ui/models.py
+++ awx/network_ui/models.py
@@ -189,9 +189,8 @@ class EventTrace(models.Model):
 class Coverage(models.Model):
 
     coverage_id = models.AutoField(primary_key=True,)
-    client = models.ForeignKey('Client',)
-    trace_session_id = models.IntegerField()
     coverage_data = models.TextField()
+    test_result = models.ForeignKey('TestResult',)
 
 
 class TopologySnapshot(models.Model):
@@ -202,3 +201,35 @@ class TopologySnapshot(models.Model):
     trace_session_id = models.IntegerField()
     snapshot_data = models.TextField('TopologySnapshot',)
     order = models.IntegerField()
+
+
+class TestCase(models.Model):
+
+    test_case_id = models.AutoField(primary_key=True,)
+    name = models.CharField('TestCase', max_length=200,)
+    test_case_data = models.TextField()
+
+
+class Result(models.Model):
+
+    result_id = models.AutoField(primary_key=True,)
+    name = models.CharField(max_length=20,)
+
+
+class CodeUnderTest(models.Model):
+
+    code_under_test_id = models.AutoField('CodeUnderTest', primary_key=True,)
+    version_x = models.IntegerField()
+    version_y = models.IntegerField()
+    version_z = models.IntegerField()
+    commits_since = models.IntegerField()
+    commit_hash = models.CharField(max_length=40,)
+
+
+class TestResult(models.Model):
+
+    test_result_id = models.AutoField(primary_key=True,)
+    test_case = models.ForeignKey('TestCase',)
+    result = models.ForeignKey('Result',)
+    code_under_test = models.ForeignKey('CodeUnderTest',)
+    time = models.DateTimeField()
