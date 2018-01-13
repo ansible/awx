a9faa26933301896c773cb6f93c309e1ce9b7d21
Ben Thomasson <bthomass@redhat.com>
2018-01-13 11:00:55 -0500

    Adds APIs for tests and code coverage

--- awx/network_ui/urls.py
+++ awx/network_ui/urls.py
@@ -9,6 +9,7 @@ app_name = 'network_ui'
 urlpatterns = [
     url(r'^tests$', views.tests, name='tests'),
     url(r'^upload_test$', views.upload_test, name='upload_test'),
+    url(r'^download_coverage/(?P<pk>[0-9]+)$', views.download_coverage, name='download_coverage'),
     url(r'^download_trace$', views.download_trace, name='download_trace'),
     url(r'^download_recording$', views.download_recording, name='download_recording'),
     url(r'^topology.json$', views.json_topology_data, name='json_topology_data'),
