f9bd84efd4479586df0ad8c244455ebb38e9d92e
Ben Thomasson <bthomass@redhat.com>
2018-02-20 16:25:55 -0500

    Adds canvas prefix to network UI api URL names

--- awx/network_ui/v2_api_urls.py
+++ awx/network_ui/v2_api_urls.py
@@ -18,58 +18,58 @@ urls = []
 
 
 urls += [
-    url(r'^device/$', DeviceList.as_view(), name='device_list'),
-    url(r'^device/(?P<pk>[0-9]+)/$', DeviceDetail.as_view(), name='device_detail'),
+    url(r'^device/$', DeviceList.as_view(), name='canvas_device_list'),
+    url(r'^device/(?P<pk>[0-9]+)/$', DeviceDetail.as_view(), name='canvas_device_detail'),
 ]
 
 urls += [
-    url(r'^link/$', LinkList.as_view(), name='link_list'),
-    url(r'^link/(?P<pk>[0-9]+)/$', LinkDetail.as_view(), name='link_detail'),
+    url(r'^link/$', LinkList.as_view(), name='canvas_link_list'),
+    url(r'^link/(?P<pk>[0-9]+)/$', LinkDetail.as_view(), name='canvas_link_detail'),
 ]
 
 urls += [
-    url(r'^topology/$', TopologyList.as_view(), name='topology_list'),
-    url(r'^topology/(?P<pk>[0-9]+)/$', TopologyDetail.as_view(), name='topology_detail'),
+    url(r'^topology/$', TopologyList.as_view(), name='canvas_topology_list'),
+    url(r'^topology/(?P<pk>[0-9]+)/$', TopologyDetail.as_view(), name='canvas_topology_detail'),
 ]
 
 urls += [
-    url(r'^interface/$', InterfaceList.as_view(), name='interface_list'),
-    url(r'^interface/(?P<pk>[0-9]+)/$', InterfaceDetail.as_view(), name='interface_detail'),
+    url(r'^interface/$', InterfaceList.as_view(), name='canvas_interface_list'),
+    url(r'^interface/(?P<pk>[0-9]+)/$', InterfaceDetail.as_view(), name='canvas_interface_detail'),
 ]
 
 urls += [
-    url(r'^group/$', GroupList.as_view(), name='group_list'),
-    url(r'^group/(?P<pk>[0-9]+)/$', GroupDetail.as_view(), name='group_detail'),
+    url(r'^group/$', GroupList.as_view(), name='canvas_group_list'),
+    url(r'^group/(?P<pk>[0-9]+)/$', GroupDetail.as_view(), name='canvas_group_detail'),
 ]
 
 urls += [
-    url(r'^groupdevice/$', GroupDeviceList.as_view(), name='groupdevice_list'),
-    url(r'^groupdevice/(?P<pk>[0-9]+)/$', GroupDeviceDetail.as_view(), name='groupdevice_detail'),
+    url(r'^groupdevice/$', GroupDeviceList.as_view(), name='canvas_groupdevice_list'),
+    url(r'^groupdevice/(?P<pk>[0-9]+)/$', GroupDeviceDetail.as_view(), name='canvas_groupdevice_detail'),
 ]
 
 urls += [
-    url(r'^stream/$', StreamList.as_view(), name='stream_list'),
-    url(r'^stream/(?P<pk>[0-9]+)/$', StreamDetail.as_view(), name='stream_detail'),
+    url(r'^stream/$', StreamList.as_view(), name='canvas_stream_list'),
+    url(r'^stream/(?P<pk>[0-9]+)/$', StreamDetail.as_view(), name='canvas_stream_detail'),
 ]
 
 urls += [
-    url(r'^process/$', ProcessList.as_view(), name='process_list'),
-    url(r'^process/(?P<pk>[0-9]+)/$', ProcessDetail.as_view(), name='process_detail'),
+    url(r'^process/$', ProcessList.as_view(), name='canvas_process_list'),
+    url(r'^process/(?P<pk>[0-9]+)/$', ProcessDetail.as_view(), name='canvas_process_detail'),
 ]
 
 urls += [
-    url(r'^toolbox/$', ToolboxList.as_view(), name='toolbox_list'),
-    url(r'^toolbox/(?P<pk>[0-9]+)/$', ToolboxDetail.as_view(), name='toolbox_detail'),
+    url(r'^toolbox/$', ToolboxList.as_view(), name='canvas_toolbox_list'),
+    url(r'^toolbox/(?P<pk>[0-9]+)/$', ToolboxDetail.as_view(), name='canvas_toolbox_detail'),
 ]
 
 urls += [
-    url(r'^toolboxitem/$', ToolboxItemList.as_view(), name='toolboxitem_list'),
-    url(r'^toolboxitem/(?P<pk>[0-9]+)/$', ToolboxItemDetail.as_view(), name='toolboxitem_detail'),
+    url(r'^toolboxitem/$', ToolboxItemList.as_view(), name='canvas_toolboxitem_list'),
+    url(r'^toolboxitem/(?P<pk>[0-9]+)/$', ToolboxItemDetail.as_view(), name='canvas_toolboxitem_detail'),
 ]
 
 urls += [
-    url(r'^topologyinventory/$', TopologyInventoryList.as_view(), name='topologyinventory_list'),
-    url(r'^topologyinventory/(?P<pk>[0-9]+)/$', TopologyInventoryDetail.as_view(), name='topologyinventory_detail'),
+    url(r'^topologyinventory/$', TopologyInventoryList.as_view(), name='canvas_topologyinventory_list'),
+    url(r'^topologyinventory/(?P<pk>[0-9]+)/$', TopologyInventoryDetail.as_view(), name='canvas_topologyinventory_detail'),
 ]
 
 __all__ = ['urls']
