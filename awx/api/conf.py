99989892cd18815e7646b5ccb23b86bde0db62a6
adamscmRH <chadams@redhat.com>
2018-02-23 09:25:23 -0500

    fixs naming

--- awx/api/conf.py
+++ awx/api/conf.py
@@ -6,25 +6,6 @@ from awx.conf import fields, register
 from awx.api.fields import OAuth2ProviderField
 
 
-# register(
-#     'AUTH_TOKEN_EXPIRATION',
-#     field_class=fields.IntegerField,
-#     min_value=60,
-#     label=_('Idle Time Force Log Out'),
-#     help_text=_('Number of seconds that a user is inactive before they will need to login again.'),
-#     category=_('Authentication'),
-#     category_slug='authentication',
-# )
-# 
-# register(
-#     'AUTH_TOKEN_PER_USER',
-#     field_class=fields.IntegerField,
-#     min_value=-1,
-#     label=_('Maximum number of simultaneous logins'),
-#     help_text=_('Maximum number of simultaneous logins a user may have. To disable enter -1.'),
-#     category=_('Authentication'),
-#     category_slug='authentication',
-# )
 register(
     'SESSION_COOKIE_AGE',
     field_class=fields.IntegerField,
