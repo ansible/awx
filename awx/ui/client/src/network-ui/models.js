25be30316016c1419285a9d718c698863baa78f1
Ben Thomasson <bthomass@redhat.com>
2018-02-23 20:27:52 -0500

    Fixes variables initial value

--- awx/ui/client/src/network-ui/models.js
+++ awx/ui/client/src/network-ui/models.js
@@ -29,7 +29,7 @@ function Device(id, name, x, y, type, host_id) {
     this.process_id_seq = util.natural_numbers(0);
     this.processes = [];
     this.in_group = false;
-    this.variables = [];
+    this.variables = {};
 }
 exports.Device = Device;
 
@@ -465,7 +465,7 @@ function Group(id, name, type, x1, y1, x2, y2, selected) {
     this.streams = [];
     this.group_id = 0;
     this.icon_size = type === 'site' ? 500 : 100;
-    this.variables = [];
+    this.variables = {};
 }
 exports.Group = Group;
 
