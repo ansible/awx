746a2c1eea7ebb410b3622a1876ced7154512bd0
chris meyers <chris.meyers.fsu@gmail.com>
2018-03-02 11:04:18 -0500

    short-circuit middleware if migration loading url
    * Had to monkey patch django middleware logic.
    * Left checks to tell coders to use new middleware behavior in favor of
    monkey patching.

--- awx/wsgi.py
+++ awx/wsgi.py
@@ -8,7 +8,13 @@ from awx import __version__ as tower_version
 from awx import prepare_env, MODE
 prepare_env()
 
+
+from django.core.handlers.base import BaseHandler  # NOQA
 from django.core.wsgi import get_wsgi_application  # NOQA
+import django  # NOQA
+from django.conf import settings  # NOQA
+from django.urls import resolve  # NOQA
+
 
 """
 WSGI config for AWX project.
@@ -28,6 +34,34 @@ if MODE == 'production':
     except Exception:
         logger.error("Missing or incorrect metadata for Tower version.  Ensure Tower was installed using the setup playbook.")
         raise Exception("Missing or incorrect metadata for Tower version.  Ensure Tower was installed using the setup playbook.")
+
+
+if django.__version__ != '1.11.7':
+    raise RuntimeError("Django version other than 1.11.7 detected {}. \
+            Monkey Patch to support short-circuit Django Middelware \
+            is known to work for Django 1.11.7 and may not work with other, \
+            even minor, versions.".format(django.__version__))
+
+
+if settings.MIDDLEWARE:
+    raise RuntimeError("MIDDLEWARE setting detected. \
+            The 'migration in progress' view feature short-circuits OLD Django \
+            MIDDLEWARE_CLASSES behavior. With the new Django MIDDLEWARE beahvior \
+            it's possible to short-ciruit the middleware onion through supported \
+            middleware mechanisms. The monkey patch wrapper below should be removed.")
+
+
+def _wrapper_legacy_get_response(self, request):
+    # short-circuit middleware
+    if getattr(resolve(request.path), 'url_name', '') == 'migrations_notran':
+        return self._get_response(request)
+    # fall through to middle-ware
+    else:
+        return self._real_legacy_get_response(request)
+
+
+BaseHandler._real_legacy_get_response = BaseHandler._legacy_get_response
+BaseHandler._legacy_get_response = _wrapper_legacy_get_response
 
 # Return the default Django WSGI application.
 application = get_wsgi_application()
