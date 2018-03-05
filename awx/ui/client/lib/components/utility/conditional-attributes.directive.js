f3f29088a902ae1f51bdbab67ab2a529c7bb62b8
Alexandru Bogdan Pica <alexandru.pica@1and1.ro>
2018-03-05 14:35:20 +0200

    Fix: linting issues

--- awx/ui/client/lib/components/utility/conditional-attributes.directive.js
+++ awx/ui/client/lib/components/utility/conditional-attributes.directive.js
@@ -1,43 +1,29 @@
-function conditionalAttributesLink(scope, el, attrs, controller) {
-    console.log(scope);
-}
-
 let compileLoopControl = 0;
 
-function conditionalAttributes($compile) {
+function conditionalAttributes ($compile) {
     return {
         priority: 100,
         terminal: true,
-        compile: function () {
-            return {
-                pre: function (scope, element, attr) {
-                    let directives = scope.$eval(attr.conditionalAttributes);
-                    compileLoopControl++;
-
-                    if (compileLoopControl >= 10) {
-                        return;
-                    }
-
-                    for (var key in directives) {
-                        if (directives.hasOwnProperty(key)) {
-                            if (directives[key]) {
-                                attr.$set(key, true);
-                            } else {
-                                attr.$set(key, null);
-                            }
-                        }
-                    }
-
-                    attr.$set('conditionalAttributes', null);
-                    $compile(element)(scope);
-                    compileLoopControl = 0;
-
+        compile: () => ({
+            pre: (scope, element, attr) => {
+                const directives = scope.$eval(attr.conditionalAttributes);
+                compileLoopControl++;
+                if (compileLoopControl >= 10) {
+                    return;
                 }
+                Object.keys(directives).forEach((key) => {
+                    if (directives[key]) {
+                        attr.$set(key, true);
+                    } else {
+                        attr.$set(key, null);
+                    }
+                });
+                attr.$set('conditionalAttributes', null);
+                $compile(element)(scope);
+                compileLoopControl = 0;
             }
-        }
+        })
     };
 }
-
-conditionalAttributes.$inject = ['$compile']
-
+conditionalAttributes.$inject = ['$compile'];
 export default conditionalAttributes;
