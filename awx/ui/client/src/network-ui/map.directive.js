cbb16b691d24f8909c833137dbf43343d3d5e0c4
Ben Thomasson <bthomass@redhat.com>
2018-03-06 10:58:38 -0500

    Removes group, site, and rack features for 3.3

--- awx/ui/client/src/network-ui/map.directive.js
+++
@@ -1,8 +0,0 @@
-/* Copyright (c) 2017 Red Hat, Inc. */
-
-const templateUrl = require('~network-ui/map.partial.svg');
-
-function map () {
-  return { restrict: 'A', templateUrl};
-}
-exports.map = map;
--- awx/ui/client/src/network-ui/process.directive.js
+++
@@ -1,8 +0,0 @@
-/* Copyright (c) 2017 Red Hat, Inc. */
-
-const templateUrl = require('~network-ui/process.partial.svg');
-
-function process () {
-  return { restrict: 'A', templateUrl};
-}
-exports.process = process;
