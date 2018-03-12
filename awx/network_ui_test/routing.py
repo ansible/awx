9ea3539a68aea7b7a579756162a9f38eaa5fbd6d
Ben Thomasson <bthomass@redhat.com>
2018-03-12 17:41:15 -0400

    Makes changes suggested by wwitzel3's review

--- awx/network_ui_test/routing.py
+++ awx/network_ui_test/routing.py
@@ -1,12 +1,12 @@
 # Copyright (c) 2017 Red Hat, Inc
 from channels.routing import route
-from awx.network_ui_test.consumers import ws_connect, ws_message, ws_disconnect, test_persistence
+from awx.network_ui_test.consumers import ws_connect, ws_message, ws_disconnect, TestPersistence
 
 
 channel_routing = [
     route("websocket.connect", ws_connect, path=r"^/network_ui/test"),
     route("websocket.receive", ws_message, path=r"^/network_ui/test"),
     route("websocket.disconnect", ws_disconnect, path=r"^/network_ui/test"),
-    route("test_persistence", test_persistence.handle),
+    route("test_persistence", TestPersistence().handle),
 ]
 
