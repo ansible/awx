7a9acdc6f13f2a24aff4ce49f6694ac505491cca
Ben Thomasson <bthomass@redhat.com>
2018-03-12 17:41:15 -0400

    Makes changes suggested by wwitzel3's review

--- awx/network_ui/routing.py
+++ awx/network_ui/routing.py
@@ -1,10 +1,10 @@
 # Copyright (c) 2017 Red Hat, Inc
 from channels.routing import route
-from awx.network_ui.consumers import ws_connect, ws_message, ws_disconnect, persistence
+from awx.network_ui.consumers import ws_connect, ws_message, ws_disconnect, Persistence
 
 channel_routing = [
     route("websocket.connect", ws_connect, path=r"^/network_ui/topology"),
     route("websocket.receive", ws_message, path=r"^/network_ui/topology"),
     route("websocket.disconnect", ws_disconnect, path=r"^/network_ui/topology"),
-    route("persistence", persistence.handle),
+    route("persistence", Persistence().handle),
 ]
