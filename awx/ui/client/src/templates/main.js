9873bab45185926f52d98ef62f239838c4f9065e
mabashian <mabashia@redhat.com>
2018-03-14 16:26:02 -0400

    Removed unused/commented code

--- awx/ui/client/src/templates/main.js
+++ awx/ui/client/src/templates/main.js
@@ -731,116 +731,6 @@ angular.module('templates', [surveyMaker.name, jobTemplates.name, labels.name, p
                     }
                 };
 
-                // inventoryLookup = {
-                //     searchPrefix: 'inventory',
-                //     name: 'templates.editWorkflowJobTemplate.workflowMaker.inventory',
-                //     url: '/inventory',
-                //     data: {
-                //         formChildState: true
-                //     },
-                //     params: {
-                //         inventory_search: {
-                //             value: {
-                //                 page_size: '5'
-                //             },
-                //             squash: true,
-                //             dynamic: true
-                //         }
-                //     },
-                //     ncyBreadcrumb: {
-                //         skip: true
-                //     },
-                //     views: {
-                //         'related': {
-                //             templateProvider: function(ListDefinition, generateList) {
-                //                 let list_html = generateList.build({
-                //                     mode: 'lookup',
-                //                     list: ListDefinition,
-                //                     input_type: 'radio'
-                //                 });
-                //                 return `<lookup-modal>${list_html}</lookup-modal>`;
-                //
-                //             }
-                //         }
-                //     },
-                //     resolve: {
-                //         ListDefinition: ['InventoryList', function(InventoryList) {
-                //             // mutate the provided list definition here
-                //             let list = _.cloneDeep(InventoryList);
-                //             list.lookupConfirmText = 'SELECT';
-                //             return list;
-                //         }],
-                //         Dataset: ['ListDefinition', 'QuerySet', '$stateParams', 'GetBasePath',
-                //             (list, qs, $stateParams, GetBasePath) => {
-                //                 let path = GetBasePath(list.name) || GetBasePath(list.basePath);
-                //                 return qs.search(path, $stateParams[`${list.iterator}_search`]);
-                //             }
-                //         ]
-                //     },
-                //     onExit: function($state) {
-                //         if ($state.transition) {
-                //             $('#form-modal').modal('hide');
-                //             $('.modal-backdrop').remove();
-                //             $('body').removeClass('modal-open');
-                //         }
-                //     },
-                // };
-                //
-                // credentialLookup = {
-                //     searchPrefix: 'credential',
-                //     name: 'templates.editWorkflowJobTemplate.workflowMaker.credential',
-                //     url: '/credential',
-                //     data: {
-                //         formChildState: true
-                //     },
-                //     params: {
-                //         credential_search: {
-                //             value: {
-                //                 page_size: '5'
-                //             },
-                //             squash: true,
-                //             dynamic: true
-                //         }
-                //     },
-                //     ncyBreadcrumb: {
-                //         skip: true
-                //     },
-                //     views: {
-                //         'related': {
-                //             templateProvider: function(ListDefinition, generateList) {
-                //                 let list_html = generateList.build({
-                //                     mode: 'lookup',
-                //                     list: ListDefinition,
-                //                     input_type: 'radio'
-                //                 });
-                //                 return `<lookup-modal>${list_html}</lookup-modal>`;
-                //
-                //             }
-                //         }
-                //     },
-                //     resolve: {
-                //         ListDefinition: ['CredentialList', function(CredentialList) {
-                //             let list = _.cloneDeep(CredentialList);
-                //             list.lookupConfirmText = 'SELECT';
-                //             return list;
-                //         }],
-                //         Dataset: ['ListDefinition', 'QuerySet', '$stateParams', 'GetBasePath',
-                //             (list, qs, $stateParams, GetBasePath) => {
-                //                 let path = GetBasePath(list.name) || GetBasePath(list.basePath);
-                //                 return qs.search(path, $stateParams[`${list.iterator}_search`]);
-                //             }
-                //         ]
-                //     },
-                //     onExit: function($state) {
-                //         if ($state.transition) {
-                //             $('#form-modal').modal('hide');
-                //             $('.modal-backdrop').remove();
-                //             $('body').removeClass('modal-open');
-                //         }
-                //     },
-                // };
-
-
                 return Promise.all([
                     addJobTemplate,
                     editJobTemplate,
@@ -852,9 +742,7 @@ angular.module('templates', [surveyMaker.name, jobTemplates.name, labels.name, p
                             return result.concat(definition.states);
                         }, [
                             stateExtender.buildDefinition(listRoute),
-                            stateExtender.buildDefinition(workflowMaker),
-                            // stateExtender.buildDefinition(inventoryLookup),
-                            // stateExtender.buildDefinition(credentialLookup)
+                            stateExtender.buildDefinition(workflowMaker)
                         ])
                     };
                 });
