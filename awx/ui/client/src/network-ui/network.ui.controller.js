abb95fdad68b845e75c92ab45b7df0f14e815474
Jared Tabor <jtabor@redhat.com>
2018-03-16 13:22:33 -0500

    Cleans up the Network UI after PR feedback and UX feedback
    -removes stale commented-out lines
    -makes "unknown" type devices smaller on canvas
    -moves "unknown" type device title underneath icon
    -removes collapsed inventory toolbox
    -changes "Delete" to "Remove"
    -removes the "Close" button for "Cancel" on details panel
    -changing Remove color to red

--- awx/ui/client/src/network-ui/network.ui.controller.js
+++ awx/ui/client/src/network-ui/network.ui.controller.js
@@ -873,7 +873,7 @@ var NetworkUIController = function($scope,
     // Context Menu Buttons
     $scope.context_menu_buttons = [
         new models.ContextMenuButton("Details", 236, 231, 160, 26, $scope.onDetailsContextButton, $scope),
-        new models.ContextMenuButton("Delete", 256, 231, 160, 26, $scope.onDeleteContextMenu, $scope)
+        new models.ContextMenuButton("Remove", 256, 231, 160, 26, $scope.onDeleteContextMenu, $scope)
     ];
 
     // Context Menus
