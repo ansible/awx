43601be8a76491d68eb16c90f3601017af2596f9
Ben Thomasson <bthomass@redhat.com>
2018-04-02 15:34:39 -0400

    Removes --fake-initial from awx-manage migrate.
    The --fake-initial option is no longer needed and can cause
    application with an initial migration to fail as was seen
    in the network_ui application.

--- installer/roles/image_build/files/launch_awx_task.sh
+++ installer/roles/image_build/files/launch_awx_task.sh
@@ -10,7 +10,7 @@ ANSIBLE_REMOTE_TEMP=/tmp ANSIBLE_LOCAL_TEMP=/tmp ansible -i "127.0.0.1," -c loca
 ANSIBLE_REMOTE_TEMP=/tmp ANSIBLE_LOCAL_TEMP=/tmp ansible -i "127.0.0.1," -c local -v -m wait_for -a "host=$RABBITMQ_HOST port=5672" all
 ANSIBLE_REMOTE_TEMP=/tmp ANSIBLE_LOCAL_TEMP=/tmp ansible -i "127.0.0.1," -c local -v -m postgresql_db --become-user $DATABASE_USER -a "name=$DATABASE_NAME owner=$DATABASE_USER login_user=$DATABASE_USER login_host=$DATABASE_HOST login_password=$DATABASE_PASSWORD port=$DATABASE_PORT" all
 
-awx-manage migrate --noinput --fake-initial
+awx-manage migrate --noinput
 if [ ! -z "$AWX_ADMIN_USER" ]&&[ ! -z "$AWX_ADMIN_PASSWORD" ]; then
     echo "from django.contrib.auth.models import User; User.objects.create_superuser('$AWX_ADMIN_USER', 'root@localhost', '$AWX_ADMIN_PASSWORD')" | awx-manage shell
     awx-manage create_preload_data
