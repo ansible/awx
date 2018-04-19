a23aadf346951799d1af527c2d88fde200301a29
Wayne Witzel III <wayne@riotousliving.com>
2018-04-19 07:51:28 -0400

    Remove unused code, OrderedDictLoader

--- awx/api/parsers.py
+++ awx/api/parsers.py
@@ -1,7 +1,6 @@
 # Python
 from collections import OrderedDict
 import json
-import yaml
 
 # Django
 from django.conf import settings
@@ -11,36 +10,6 @@ from django.utils.translation import ugettext_lazy as _
 # Django REST Framework
 from rest_framework import parsers
 from rest_framework.exceptions import ParseError
-
-
-class OrderedDictLoader(yaml.SafeLoader):
-    """
-    This yaml loader is used to deal with current pyYAML (3.12) not supporting
-    custom object pairs hook. Remove it when new version adds that support.
-    """
-
-    def construct_mapping(self, node, deep=False):
-        if isinstance(node, yaml.nodes.MappingNode):
-            self.flatten_mapping(node)
-        else:
-            raise yaml.constructor.ConstructorError(
-                None, None,
-                "expected a mapping node, but found %s" % node.id,
-                node.start_mark
-            )
-        mapping = OrderedDict()
-        for key_node, value_node in node.value:
-            key = self.construct_object(key_node, deep=deep)
-            try:
-                hash(key)
-            except TypeError as exc:
-                raise yaml.constructor.ConstructorError(
-                    "while constructing a mapping", node.start_mark,
-                    "found unacceptable key (%s)" % exc, key_node.start_mark
-                )
-            value = self.construct_object(value_node, deep=deep)
-            mapping[key] = value
-        return mapping
 
 
 class JSONParser(parsers.JSONParser):
