92cf8d538a1766566128a52e69e38a0ca45b174e
Jared Tabor <jtabor@redhat.com>
2018-07-02 22:09:24 -0700

    Changes UI dev server to operate over https

--- awx/ui/build/webpack.watch.js
+++ awx/ui/build/webpack.watch.js
@@ -51,7 +51,7 @@ const watch = {
         stats: 'minimal',
         publicPath: '/static/',
         host: '127.0.0.1',
-        https: false,
+        https: true,
         port: 3000,
         clientLogLevel: 'none',
         proxy: [{
