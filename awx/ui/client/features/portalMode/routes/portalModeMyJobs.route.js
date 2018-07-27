61d1cb46ac0d93a197e77943e3c0abd759fc2d36
Jake McDermott <yo@jakemcdermott.me>
2018-07-27 00:54:12 -0400

    fix job portal list dependency reference error

--- awx/ui/client/features/portalMode/routes/portalModeMyJobs.route.js
+++ awx/ui/client/features/portalMode/routes/portalModeMyJobs.route.js
@@ -51,6 +51,10 @@ export default {
                 return qs.search(searchPath, searchParam)
                     .finally(() => Wait('stop'));
             }
+        ],
+        SearchBasePath: [
+            'GetBasePath',
+            (GetBasePath) => GetBasePath('unified_jobs')
         ]
     }
 };
