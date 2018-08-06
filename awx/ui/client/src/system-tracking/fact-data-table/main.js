0a09d98fe85e6e42f19afdf0e87b37122bd01f39
mabashian <mabashia@redhat.com>
2018-08-06 12:03:02 -0400

    Removes system tracking code from the UI.  Moves import of shared out to app.js

--- awx/ui/client/src/system-tracking/fact-data-table/main.js
+++
@@ -1,5 +0,0 @@
-import factDataTable from './fact-data-table.directive';
-
-export default
-    angular.module('systemTracking.factDataTable', [])
-        .directive('factDataTable', factDataTable);
