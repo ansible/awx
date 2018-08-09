5bfe9bf228873f7909386ca295cc73ed60175419
chris meyers <chris.meyers.fsu@gmail.com>
2018-08-09 15:44:59 -0400

    vary the pk

--- awx/main/tests/unit/test_tasks.py
+++ awx/main/tests/unit/test_tasks.py
@@ -792,7 +792,7 @@ class TestJobCredentials(TestJobExecution):
         for i in range(3):
             net = CredentialType.defaults['net']()
             credential = Credential(
-                pk=1,
+                pk=i,
                 credential_type=net,
                 inputs = {'ssh_key_unlock': 'secret{}'.format(i)}
             )
@@ -816,7 +816,7 @@ class TestJobCredentials(TestJobExecution):
 
         ssh = CredentialType.defaults['ssh']()
         ssh_credential = Credential(
-            pk=1,
+            pk=2,
             credential_type=ssh,
             inputs = {'ssh_key_unlock': 'ssh_secret'}
         )
