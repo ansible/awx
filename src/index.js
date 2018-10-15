9f2b2b34566602f5a0c9ca5a7115062ddb017ca4
Jake McDermott <yo@jakemcdermott.me>
2018-10-15 12:55:11 -0400

    adding pf assets / styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import App from './App';
 
 import '@patternfly/react-core/dist/styles/base.css';
+import '@patternfly/patternfly-next/patternfly.css';
+
 import './app.scss';
 
 export default App;
