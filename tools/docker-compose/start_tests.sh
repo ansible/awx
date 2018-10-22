a361b5da6eafaaf8fe459ac58bcf043b36a131fa
Shane McDonald <me@shanemcd.com>
2018-10-22 19:02:31 -0400

    Fix permissions when running dev container as non-root user
    I wanted to pass `—user` to `docker-compose` up, but that option doesnt exist. To get around this, I had to record the uid on the host (CURRENT_UID), interpolate the variable in tools/docker-compose.yml, and detect that inside the container. I then piggy-backed on the /etc/passwd hack we use for scenarios with unpredictable uids.

--- tools/docker-compose/start_tests.sh
+++ tools/docker-compose/start_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set +x
 
-if [ `id -u` -ge 500 ]; then
+if [ `id -u` -ge 500 ] || [ -z "${CURRENT_UID}" ]; then
     echo "awx:x:`id -u`:`id -g`:,,,:/tmp:/bin/bash" >> /tmp/passwd
     cat /tmp/passwd > /etc/passwd
     rm /tmp/passwd
