28512e042b4dfe16ef75d39aea79f2d82459b3a9
Ryan Petrello <rpetrell@redhat.com>
2018-10-26 10:02:43 -0400

    remove DeprecatedAuthTokenMiddleware

--- awx/settings/defaults.py
+++ awx/settings/defaults.py
@@ -262,7 +262,6 @@ MIDDLEWARE_CLASSES = (  # NOQA
     'awx.sso.middleware.SocialAuthMiddleware',
     'crum.CurrentRequestUserMiddleware',
     'awx.main.middleware.URLModificationMiddleware',
-    'awx.main.middleware.DeprecatedAuthTokenMiddleware',
     'awx.main.middleware.SessionTimeoutMiddleware',
 )
 
