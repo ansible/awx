691e02655a81d1f59d2024abcc2a041f1341403b
AlanCoding <arominge@redhat.com>
2018-11-01 10:08:57 -0400

    bump migration number

--- awx/main/migrations/0052_v340_workflow_inventory.py
+++ awx/main/migrations/0052_v340_workflow_inventory.py
@@ -10,7 +10,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0049_v330_validate_instance_capacity_adjustment'),
+        ('main', '0051_v340_job_slicing'),
     ]
 
     operations = [
