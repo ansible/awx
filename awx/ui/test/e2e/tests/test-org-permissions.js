43226919355962758c24e14f2cb41ee7fab418fe
Daniel Sami <dsami@redhat.com>
2018-11-06 16:47:48 -0500

    org permission ui tests

--- awx/ui/test/e2e/tests/test-org-permissions.js
+++ awx/ui/test/e2e/tests/test-org-permissions.js
@@ -6,41 +6,47 @@ import {
 
 let data;
 const spinny = "//*[contains(@class, 'spinny')]";
-const searchBar = "//input[contains(@class, 'SmartSearch-input')]";
-
-const teamsNavTab = "//at-side-nav-item[contains(@name, 'TEAMS')]";
-const teamsTab = '//*[@id="teams_tab"]';
-const teamsSelector = "//a[contains(text(), 'test-actions-team')]";
-const teamsText = 'name.iexact:"test-actions-team"';
-const teamsSearchBadgeCount = '//span[contains(@class, "List-titleBadge") and contains(text(), "1")]';
-
-const addUserToTeam = '//*[@aw-tool-tip="Add User"]';
-const userText = 'username.iexact:"test-actions-user"';
-const modalSearchBar = '//*[@ui-view="modal"]//*[contains(@class, "SmartSearch-input")]';
 const checkbox = '//input[@type="checkbox"]';
-const userRoleSearchBar = '//li//input[contains(@type, "search")]';
-const readRoleText = "read";
+
+const searchBar = "//input[contains(@class, 'SmartSearch-input')]";
+const modalSearchBar = '//*[@ui-view="modal"]//*[contains(@class, "SmartSearch-input")]';
 const teamSearchBar = '//*[@django-model="teams"]//input';
-const teamCheckbox = '//*[@item="team"]//input[@type="checkbox"]';
+const userRoleSearchBar = '//li//input[contains(@type, "search")]';
+const modalOrgsSearchBar = '//smart-search[@django-model="organizations"]//input';
 
+const orgsNavTab = "//at-side-nav-item[contains(@name, 'ORGANIZATIONS')]";
+const teamsNavTab = "//at-side-nav-item[contains(@name, 'TEAMS')]";
+const usersNavTab = "//at-side-nav-item[contains(@name, 'USERS')]";
 
+const orgTab = '//div[not(@ng-show="showSection2Container()")]/div[@class="Form-tabHolder"]/div[@ng-click="selectTab(\'organizations\')"]';
+const teamsTab = '//*[@id="teams_tab"]';
 const permissionsTab = '//*[@id="permissions_tab"]';
 const usersTab = '//*[@id="users_tab"]';
-const addPermission = '//*[@aw-tool-tip="Grant Permission"]';
-const addTeamPermission ='//*[@aw-tool-tip="Add a permission"]';
-const orgTab = '//div[not(@ng-show="showSection2Container()")]/div[@class="Form-tabHolder"]/div[@ng-click="selectTab(\'organizations\')"]';
-const modalOrgsSearchBar = '//smart-search[@django-model="organizations"]//input';
+
 const orgsText = 'name.iexact:"test-actions-organization"';
 const orgsCheckbox = '//select-list-item[@item="organization"]//input[@type="checkbox"]';
 const orgDetails = '//*[contains(@class, "OrgCards-label")]';
 const orgRoleSelector = '//*[contains(@aria-labelledby, "select2-organizations")]';
 const readRole = '//*[contains(@id, "organizations-role") and text()="Read"]';
-// const adminRole = '//*[contains(@id, "organizations-role") and text()="Admin"]';
+
+const memberRoleText = 'member';
+const readRoleText = 'read';
+
+const teamsSelector = "//a[contains(text(), 'test-actions-team')]";
+const teamsText = 'name.iexact:"test-actions-team"';
+const teamsSearchBadgeCount = '//span[contains(@class, "List-titleBadge") and contains(text(), "1")]';
+const teamCheckbox = '//*[@item="team"]//input[@type="checkbox"]';
+const addUserToTeam = '//*[@aw-tool-tip="Add User"]';
+const userText = 'username.iexact:"test-actions-user"';
+
+const trashButton = '//i[contains(@class, "fa-trash")]';
+const deleteButton = '//*[text()="DELETE"]';
 const saveButton = '//*[text()="Save"]';
 
+const addPermission = '//*[@aw-tool-tip="Grant Permission"]';
+const addTeamPermission = '//*[@aw-tool-tip="Add a permission"]';
+const verifyTeamPermissions = '//*[contains(@class, "List-tableRow")]//*[text()="Read"]';
 const readOrgPermissionResults = '//*[@id="permissions_table"]//*[text()="test-actions-organization"]/parent::*/parent::*//*[contains(text(), "Read")]';
-
-const orgsNavTab = "//at-side-nav-item[contains(@name, 'ORGANIZATIONS')]";
 
 module.exports = {
     before: (client, done) => {
@@ -96,7 +102,7 @@ module.exports = {
             .waitForElementNotVisible(spinny)
             .findThenClick(checkbox)
             .findThenClick(userRoleSearchBar)
-            .setValue(userRoleSearchBar, [readRoleText, client.Keys.ENTER])
+            .setValue(userRoleSearchBar, [memberRoleText, client.Keys.ENTER])
             .waitForElementNotVisible(spinny)
             .findThenClick(saveButton)
             // add team-wide permissions to an organization
@@ -112,7 +118,6 @@ module.exports = {
             .clearValue(teamSearchBar)
             .setValue(teamSearchBar, [teamsText, client.Keys.ENTER])
             .waitForElementNotVisible(spinny)
-            .pause(4000)
             .findThenClick(teamCheckbox)
             .findThenClick(userRoleSearchBar)
             .setValue(userRoleSearchBar, [readRoleText, client.Keys.ENTER])
@@ -120,9 +125,15 @@ module.exports = {
             .findThenClick(saveButton)
             .clearValue(searchBar)
             .setValue(searchBar, [userText, client.Keys.ENTER])
-            .pause(4000);
+            .waitForElementVisible(verifyTeamPermissions);
     },
     after: client => {
-        client.end();
+        client
+            .findThenClick(usersNavTab)
+            .setValue(searchBar, [userText, client.Keys.ENTER])
+            .waitForElementNotVisible(spinny)
+            .findThenClick(trashButton)
+            .findThenClick(deleteButton)
+            .end();
     }
 };
