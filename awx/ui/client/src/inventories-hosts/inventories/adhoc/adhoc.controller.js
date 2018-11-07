ceb9bfe4863cb6cadbcc628e7939d4f87449292c
Jake McDermott <yo@jakemcdermott.me>
2018-11-06 22:36:14 -0500

    fix adhoc command cred lookup when many credential types exist

--- awx/ui/client/src/inventories-hosts/inventories/adhoc/adhoc.controller.js
+++ awx/ui/client/src/inventories-hosts/inventories/adhoc/adhoc.controller.js
@@ -12,7 +12,7 @@
 function adhocController($q, $scope, $stateParams,
     $state, CheckPasswords, PromptForPasswords, CreateLaunchDialog, CreateSelect2, adhocForm,
     GenerateForm, Rest, ProcessErrors, GetBasePath, GetChoices,
-    KindChange, Wait, ParseTypeChange, credentialTypes) {
+    KindChange, Wait, ParseTypeChange, machineCredentialType) {
 
     // this is done so that we can access private functions for testing, but
     // we don't want to populate the "public" scope with these internal
@@ -302,12 +302,9 @@ function adhocController($q, $scope, $stateParams,
     };
 
     $scope.lookupCredential = function(){
-        let credType = _.filter(credentialTypes, function(credType){
-            return credType.kind === "ssh";
-        });
         $state.go('.credential', {
             credential_search: {
-                credential_type: credType[0].id,
+                credential_type: machineCredentialType,
                 page_size: '5',
                 page: '1'
             }
@@ -319,5 +316,5 @@ function adhocController($q, $scope, $stateParams,
 export default ['$q', '$scope', '$stateParams',
     '$state', 'CheckPasswords', 'PromptForPasswords', 'CreateLaunchDialog', 'CreateSelect2',
      'adhocForm', 'GenerateForm', 'Rest', 'ProcessErrors', 'GetBasePath',
-    'GetChoices', 'KindChange', 'Wait', 'ParseTypeChange', 'credentialTypes',
+    'GetChoices', 'KindChange', 'Wait', 'ParseTypeChange', 'machineCredentialType',
     adhocController];
