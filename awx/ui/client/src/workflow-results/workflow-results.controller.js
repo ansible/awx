e3c8bd26fe0583033af642714be35a2eb6bcb3f1
mabashian <mabashia@redhat.com>
2018-11-09 09:15:42 -0500

    Fixed serveral bugs including credential prompting.  Added logic to bring links/nodes to the forefront when you hover over them in case there's some overlap

--- awx/ui/client/src/workflow-results/workflow-results.controller.js
+++ awx/ui/client/src/workflow-results/workflow-results.controller.js
@@ -177,7 +177,7 @@ export default ['workflowData', 'workflowResultsService', 'workflowDataOptions',
 
             let depthMap = WorkflowChartService.generateDepthMap(arrayOfLinksForChart);
 
-            $scope.treeState = { arrayOfNodesForChart, arrayOfLinksForChart, depthMap };
+            $scope.graphState = { arrayOfNodesForChart, arrayOfLinksForChart, depthMap };
         }
 
         $scope.toggleStdoutFullscreen = function() {
@@ -277,7 +277,7 @@ export default ['workflowData', 'workflowResultsService', 'workflowDataOptions',
                         runTimeElapsedTimer = workflowResultsService.createOneSecondTimer(moment(), updateWorkflowJobElapsedTimer);
                     }
 
-                    $scope.treeState.arrayOfNodesForChart[chartNodeIdToIndexMapping[nodeIdToChartNodeIdMapping[data.workflow_node_id]]].job = {
+                    $scope.graphState.arrayOfNodesForChart[chartNodeIdToIndexMapping[nodeIdToChartNodeIdMapping[data.workflow_node_id]]].job = {
                         id: data.unified_job_id,
                         status: data.status
                     };
