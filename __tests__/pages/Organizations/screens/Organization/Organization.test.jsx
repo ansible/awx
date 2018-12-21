d040f063e9e4f5f95d80537d9ca4bb0b58e08fe0
Marliana Lara <mlara@redhat.com>
2018-12-21 16:15:39 -0500

    Move Organization screens and tests into new folder structure

--- __tests__/pages/Organizations/screens/Organization/Organization.test.jsx
+++ __tests__/pages/Organizations/screens/Organization/Organization.test.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
 import { I18nProvider } from '@lingui/react';
-import OrganizationDetail from '../../../../src/pages/Organizations/components/OrganizationDetail';
+import Organization from '../../../../../src/pages/Organizations/screens/Organization/Organization';
 
-describe('<OrganizationDetail />', () => {
+describe('<OrganizationView />', () => {
   test('initially renders succesfully', () => {
     mount(
       <I18nProvider>
         <MemoryRouter initialEntries={['/organizations/1']} initialIndex={0}>
-          <OrganizationDetail
+          <Organization
             match={{ path: '/organizations/:id', url: '/organizations/1' }}
             location={{ search: '', pathname: '/organizations/1' }}
           />
