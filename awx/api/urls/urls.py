8a14567d5342ef2e3a1489fa6227385783df2080
Ryan Petrello <rpetrell@redhat.com>
2019-01-28 09:28:32 -0500

    add a minimal framework for generating analytics/metrics
    annotate queries & add license analytics

--- awx/api/urls/urls.py
+++ awx/api/urls/urls.py
@@ -32,6 +32,7 @@ from awx.api.views import (
     OAuth2TokenList,
     ApplicationOAuth2TokenList,
     OAuth2ApplicationDetail,
+    MetricsView,
 )
 
 from .organization import urls as organization_urls
@@ -131,6 +132,7 @@ v2_urls = [
     url(r'^applications/(?P<pk>[0-9]+)/tokens/$', ApplicationOAuth2TokenList.as_view(), name='application_o_auth2_token_list'),
     url(r'^tokens/$', OAuth2TokenList.as_view(), name='o_auth2_token_list'),
     url(r'^', include(oauth2_urls)),
+    url(r'^metrics/$', MetricsView.as_view(), name='metrics_view'),
 ]
 
 app_name = 'api'
--- awx/api/views/root.py
+++ awx/api/views/root.py
@@ -103,6 +103,7 @@ class ApiVersionRootView(APIView):
             data['credential_types'] = reverse('api:credential_type_list', request=request)
             data['applications'] = reverse('api:o_auth2_application_list', request=request)
             data['tokens'] = reverse('api:o_auth2_token_list', request=request)
+            data['metrics'] = reverse('api:metrics_view', request=request)
         data['inventory'] = reverse('api:inventory_list', request=request)
         data['inventory_scripts'] = reverse('api:inventory_script_list', request=request)
         data['inventory_sources'] = reverse('api:inventory_source_list', request=request)
@@ -157,6 +158,7 @@ class ApiV1PingView(APIView):
             'ha': is_ha_environment(),
             'version': get_awx_version(),
             'active_node': settings.CLUSTER_HOST_ID,
+            'system_uuid': settings.system_uuid,
         }
 
         response['instances'] = []
@@ -276,6 +278,3 @@ class ApiV1ConfigView(APIView):
         except Exception:
             # FIX: Log
             return Response({"error": _("Failed to remove license.")}, status=status.HTTP_400_BAD_REQUEST)
-
-
-
