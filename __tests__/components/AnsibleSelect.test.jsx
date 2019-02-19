8be86636657ef77da6a2612d560d7d0ad01178ea
Marliana Lara <mlara@redhat.com>
2019-02-19 16:14:28 -0500

    Fix AnsibleSelect tests

--- __tests__/components/AnsibleSelect.test.jsx
+++ __tests__/components/AnsibleSelect.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { I18nProvider } from '@lingui/react';
 import AnsibleSelect from '../../src/components/AnsibleSelect';
 
 const label = 'test select';
@@ -7,26 +8,30 @@ const mockData = ['/venv/baz/', '/venv/ansible/'];
 describe('<AnsibleSelect />', () => {
   test('initially renders succesfully', async () => {
     mount(
-      <AnsibleSelect
-        value="foo"
-        name="bar"
-        onChange={() => { }}
-        label={label}
-        data={mockData}
-      />
+      <I18nProvider>
+        <AnsibleSelect
+          value="foo"
+          name="bar"
+          onChange={() => { }}
+          label={label}
+          data={mockData}
+        />
+      </I18nProvider>
     );
   });
 
   test('calls "onSelectChange" on dropdown select change', () => {
     const spy = jest.spyOn(AnsibleSelect.prototype, 'onSelectChange');
     const wrapper = mount(
-      <AnsibleSelect
-        value="foo"
-        name="bar"
-        onChange={() => { }}
-        label={label}
-        data={mockData}
-      />
+      <I18nProvider>
+        <AnsibleSelect
+          value="foo"
+          name="bar"
+          onChange={() => { }}
+          label={label}
+          data={mockData}
+        />
+      </I18nProvider>
     );
     expect(spy).not.toHaveBeenCalled();
     wrapper.find('select').simulate('change');
@@ -35,14 +40,16 @@ describe('<AnsibleSelect />', () => {
 
   test('Returns correct select options if defaultSelected props is passed', () => {
     const wrapper = mount(
-      <AnsibleSelect
-        value="foo"
-        name="bar"
-        onChange={() => { }}
-        label={label}
-        data={mockData}
-        defaultSelected={mockData[1]}
-      />
+      <I18nProvider>
+        <AnsibleSelect
+          value="foo"
+          name="bar"
+          onChange={() => { }}
+          label={label}
+          data={mockData}
+          defaultSelected={mockData[1]}
+        />
+      </I18nProvider>
     );
     expect(wrapper.find('FormSelect')).toHaveLength(1);
   });
