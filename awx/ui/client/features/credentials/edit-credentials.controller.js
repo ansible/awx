245668ff1ec15987cb410c76437c013029012468
Jake McDermott <yo@jakemcdermott.me>
2019-02-26 15:17:53 -0500

    add input source prompting and plugin testing

--- awx/ui/client/features/credentials/edit-credentials.controller.js
+++ awx/ui/client/features/credentials/edit-credentials.controller.js
@@ -1,3 +1,5 @@
+/* eslint camelcase: 0 */
+/* eslint arrow-body-style: 0 */
 function EditCredentialsController (
     models,
     $state,
@@ -8,10 +10,16 @@ function EditCredentialsController (
     ngToast,
     Wait,
     $filter,
+    CredentialType,
 ) {
     const vm = this || {};
-
-    const { me, credential, credentialType, organization, isOrgCredAdmin } = models;
+    const {
+        me,
+        credential,
+        credentialType,
+        organization,
+        isOrgCredAdmin,
+    } = models;
 
     const omit = ['user', 'team', 'inputs'];
     const isEditable = credential.isEditable();
@@ -88,6 +96,44 @@ function EditCredentialsController (
     vm.form.credential_type._placeholder = strings.get('inputs.CREDENTIAL_TYPE_PLACEHOLDER');
     vm.isTestable = (isEditable && credentialType.get('kind') === 'external');
 
+    vm.inputSources = {
+        field: null,
+        credentialId: null,
+        credentialTypeId: null,
+        credentialTypeName: null,
+        tabs: {
+            credential: {
+                _active: true,
+                _disabled: false,
+            },
+            metadata: {
+                _active: false,
+                _disabled: false,
+            }
+        },
+        metadata: {},
+        form: {
+            inputs: {
+                _get: () => vm.inputSources.metadata,
+                _reference: 'vm.form.inputs',
+                _key: 'inputs',
+                _source: { _value: {} },
+            }
+        },
+        items: credential.get('related.input_sources.results'),
+    };
+    vm.externalTest = {
+        metadata: null,
+        form: {
+            inputs: {
+                _get: () => vm.externalTest.metadata,
+                _reference: 'vm.form.inputs',
+                _key: 'inputs',
+                _source: { _value: {} },
+            }
+        },
+    };
+
     const gceFileInputSchema = {
         id: 'gce_service_account_key',
         type: 'file',
@@ -98,7 +144,7 @@ function EditCredentialsController (
     let gceFileInputPreEditValues;
 
     vm.form.inputs = {
-        _get () {
+        _get ({ getSubmitData }) {
             let fields;
 
             credentialType.mergeInputProperties();
@@ -128,53 +174,198 @@ function EditCredentialsController (
                     }
                 }
             }
+
+            fields = fields.map((field) => {
+                if (isEditable && credentialType.get('kind') !== 'external') {
+                    field.tagMode = true;
+                }
+                return field;
+            });
+
             vm.isTestable = (isEditable && credentialType.get('kind') === 'external');
+            vm.getSubmitData = getSubmitData;
 
             return fields;
         },
+        _onRemoveTag ({ id }) {
+            vm.onInputSourceClear(id);
+        },
+        _onInputLookup ({ id }) {
+            vm.onInputSourceOpen(id);
+        },
         _source: vm.form.credential_type,
         _reference: 'vm.form.inputs',
-        _key: 'inputs'
+        _key: 'inputs',
+        border: true,
+        title: true,
     };
 
-    vm.form.secondary = ({ inputs }) => {
-        const name = $filter('sanitize')(credentialType.get('name'));
-        const endpoint = `${credential.get('id')}/test/`;
+    vm.onInputSourceClear = (field) => {
+        vm.form[field].tagMode = true;
+        vm.form[field].asTag = false;
+    };
 
-        return credential.http.post({ url: endpoint, data: { inputs }, replace: false })
+    vm.setTab = (name) => {
+        const metaIsActive = name === 'metadata';
+        vm.inputSources.tabs.credential._active = !metaIsActive;
+        vm.inputSources.tabs.credential._disabled = false;
+        vm.inputSources.tabs.metadata._active = metaIsActive;
+        vm.inputSources.tabs.metadata._disabled = false;
+    };
+
+    vm.unsetTabs = () => {
+        vm.inputSources.tabs.credential._active = false;
+        vm.inputSources.tabs.credential._disabled = false;
+        vm.inputSources.tabs.metadata._active = false;
+        vm.inputSources.tabs.metadata._disabled = false;
+    };
+
+    vm.onInputSourceOpen = (field) => {
+        vm.inputSources.field = field;
+        vm.setTab('credential');
+        const sourceItem = vm.inputSources.items
+            .find(({ input_field_name }) => input_field_name === field);
+        if (sourceItem) {
+            const { source_credential, summary_fields } = sourceItem;
+            const { source_credential: { credential_type_id } } = summary_fields;
+            vm.inputSources.credentialId = source_credential;
+            vm.inputSources.credentialTypeId = credential_type_id;
+            vm.inputSources._value = credential_type_id;
+        }
+    };
+
+    vm.onInputSourceClose = () => {
+        vm.inputSources.field = null;
+        vm.inputSources.metadata = null;
+        vm.unsetTabs();
+    };
+
+    vm.onInputSourceNext = () => {
+        const { field, credentialId, credentialTypeId } = vm.inputSources;
+        Wait('start');
+        new CredentialType('get', credentialTypeId)
+            .then(model => {
+                model.mergeInputProperties('metadata');
+                vm.inputSources.metadata = model.get('inputs.metadata');
+                vm.inputSources.credentialTypeName = model.get('name');
+                const [metavals] = vm.inputSources.items
+                    .filter(({ input_field_name }) => input_field_name === field)
+                    .filter(({ source_credential }) => source_credential === credentialId)
+                    .map(({ metadata }) => metadata);
+                Object.keys(metavals || {}).forEach(key => {
+                    const obj = vm.inputSources.metadata.find(o => o.id === key);
+                    if (obj) obj._value = metavals[key];
+                });
+                vm.setTab('metadata');
+            })
+            .finally(() => Wait('stop'));
+    };
+
+    vm.onInputSourceSelect = () => {
+        const { field, credentialId } = vm.inputSources;
+        vm.inputSources.items = vm.inputSources.items
+            .filter(({ input_field_name }) => input_field_name !== field)
+            .concat([{
+                input_field_name: field,
+                source_credential: credentialId,
+                target_credential: credential.get('id'),
+            }]);
+        vm.inputSources.field = null;
+        vm.inputSources.metadata = null;
+        vm.unsetTabs();
+    };
+
+    vm.onInputSourceTabSelect = (name) => {
+        if (name === 'metadata') {
+            vm.onInputSourceNext();
+        } else {
+            vm.setTab('credential');
+        }
+    };
+
+    vm.onInputSourceRowClick = ({ id, credential_type }) => {
+        vm.inputSources.credentialId = id;
+        vm.inputSources.credentialTypeId = credential_type;
+        vm.inputSources._value = credential_type;
+    };
+
+    vm.onInputSourceTest = () => {
+        const metadata = Object.assign({}, ...vm.inputSources.form.inputs._group
+            .filter(({ _value }) => _value !== undefined)
+            .map(({ id, _value }) => ({ [id]: _value })));
+        const name = $filter('sanitize')(vm.inputSources.credentialTypeName);
+        const endpoint = `${vm.inputSources.credentialId}/test/`;
+
+        return vm.runTest({ name, model: credential, endpoint, data: { metadata } });
+    };
+
+    vm.onExternalTestClick = () => {
+        credentialType.mergeInputProperties('metadata');
+        vm.externalTest.metadata = credentialType.get('inputs.metadata');
+    };
+
+    vm.onExternalTestClose = () => {
+        vm.externalTest.metadata = null;
+    };
+
+    vm.onExternalTest = () => {
+        const name = $filter('sanitize')(credentialType.get('name'));
+        const { inputs } = vm.getSubmitData();
+        const metadata = Object.assign({}, ...vm.externalTest.form.inputs._group
+            .filter(({ _value }) => _value !== undefined)
+            .map(({ id, _value }) => ({ [id]: _value })));
+
+        let model;
+        if (credential.get('credential_type') !== credentialType.get('id')) {
+            model = credentialType;
+        } else {
+            model = credential;
+        }
+
+        const endpoint = `${model.get('id')}/test/`;
+        return vm.runTest({ name, model, endpoint, data: { inputs, metadata } });
+    };
+    vm.form.secondary = vm.onExternalTestClick;
+
+    vm.runTest = ({ name, model, endpoint, data: { inputs, metadata } }) => {
+        return model.http.post({ url: endpoint, data: { inputs, metadata }, replace: false })
             .then(() => {
                 ngToast.success({
-                    content: `
-                        <div class="Toast-wrapper">
-                            <div class="Toast-icon">
-                                <i class="fa fa-check-circle Toast-successIcon"></i>
-                            </div>
-                            <div>
-                                <b>${name}:</b> ${strings.get('edit.TEST_PASSED')}
-                            </div>
-                        </div>`,
+                    content: vm.buildTestNotificationContent({
+                        name,
+                        icon: 'fa-check-circle',
+                        msg: strings.get('edit.TEST_PASSED'),
+                    }),
                     dismissButton: false,
                     dismissOnTimeout: true
                 });
             })
             .catch(({ data }) => {
-                const msg = data.inputs ? `${$filter('sanitize')(data.inputs)}` : strings.get('edit.TEST_FAILED');
-
+                const msg = data.inputs
+                    ? `${$filter('sanitize')(data.inputs)}`
+                    : strings.get('edit.TEST_FAILED');
                 ngToast.danger({
-                    content: `
-                        <div class="Toast-wrapper">
-                            <div class="Toast-icon">
-                                <i class="fa fa-exclamation-triangle Toast-successIcon"></i>
-                            </div>
-                            <div>
-                                <b>${name}:</b> ${msg}
-                            </div>
-                        </div>`,
+                    content: vm.buildTestNotificationContent({
+                        name,
+                        msg,
+                        icon: 'fa-exclamation-triangle'
+                    }),
                     dismissButton: false,
                     dismissOnTimeout: true
                 });
             });
     };
+
+    vm.buildTestNotificationContent = ({ name, msg, icon }) => (
+        `<div class="Toast-wrapper">
+            <div class="Toast-icon">
+                <i class="fa ${icon} Toast-successIcon"></i>
+            </div>
+            <div>
+                <b>${name}:</b> ${msg}
+            </div>
+        </div>`
+    );
 
     /**
      * If a credential's `credential_type` is changed while editing, the inputs associated with
@@ -258,6 +449,7 @@ EditCredentialsController.$inject = [
     'ngToast',
     'Wait',
     '$filter',
+    'CredentialTypeModel',
 ];
 
 export default EditCredentialsController;
