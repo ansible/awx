8d44ebc17942ec6b4653c16aa934c6f41b92d4eb
Daniel Sami <dsami@redhat.com>
2019-03-04 11:56:48 -0500

    lint fixes

--- awx/ui/test/e2e/tests/test-404-behavior.js
+++ awx/ui/test/e2e/tests/test-404-behavior.js
@@ -4,14 +4,13 @@ import {
 
 module.exports = {
     before: (client) => {
-
         client
             .login()
             .waitForAngular()
             .resizeWindow(1200, 1000);
     },
     'Verify 404 page behavior': client => {
-        client.navigateTo(AWX_E2E_URL + '#/brokenurl', false);
+        client.navigateTo(`${AWX_E2E_URL}'#/brokenurl`, false);
         client.useXpath().waitForElementVisible('//job-status-graph');
         client.assert.urlContains('#/home');
     },
