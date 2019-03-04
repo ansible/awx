006797014cdda84c9ae391e7405230af307bcd83
Daniel Sami <dsami@redhat.com>
2019-03-04 11:58:32 -0500

    fix url

--- awx/ui/test/e2e/tests/test-404-behavior.js
+++ awx/ui/test/e2e/tests/test-404-behavior.js
@@ -10,7 +10,7 @@ module.exports = {
             .resizeWindow(1200, 1000);
     },
     'Verify 404 page behavior': client => {
-        client.navigateTo(`${AWX_E2E_URL}'#/brokenurl`, false);
+        client.navigateTo(`${AWX_E2E_URL}#/brokenurl`, false);
         client.useXpath().waitForElementVisible('//job-status-graph');
         client.assert.urlContains('#/home');
     },
