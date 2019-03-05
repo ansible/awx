f615a73b44b1343f816ae08ffdccaafb90053759
Jake McDermott <yo@jakemcdermott.me>
2019-03-05 10:28:08 -0500

    don't show lookup until data is fetched

--- awx/ui/client/features/credentials/input-source-lookup.component.js
+++ awx/ui/client/features/credentials/input-source-lookup.component.js
@@ -1,14 +1,22 @@
 const templateUrl = require('~features/credentials/input-source-lookup.partial.html');
 
-function InputSourceLookupController (strings) {
+function InputSourceLookupController (strings, wait) {
     const vm = this || {};
 
     vm.strings = strings;
     vm.title = strings.get('inputSources.TITLE');
+
+    vm.$onInit = () => wait('start');
+
+    vm.onReady = () => {
+        vm.isReady = true;
+        wait('stop');
+    };
 }
 
 InputSourceLookupController.$inject = [
     'CredentialsStrings',
+    'Wait',
 ];
 
 export default {
