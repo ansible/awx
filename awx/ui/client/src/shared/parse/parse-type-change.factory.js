8e48a3a52318709cd57599c6a030691de7f71c6a
Keith Grant <kegrant@redhat.com>
2019-03-20 13:51:10 -0400

    interpret empty codemirror json content as empty object

--- awx/ui/client/src/shared/parse/parse-type-change.factory.js
+++ awx/ui/client/src/shared/parse/parse-type-change.factory.js
@@ -94,8 +94,12 @@ export default
                     // convert json to yaml
                     try {
                         removeField(fld);
+                        let jsonString = scope[fld];
+                        if (jsonString.trim() === '') {
+                          jsonString = '{}';
+                        }
 
-                        json_obj = JSON.parse(scope[fld]);
+                        json_obj = JSON.parse(jsonString);
                         if ($.isEmptyObject(json_obj)) {
                             scope[fld] = '---';
                         }
