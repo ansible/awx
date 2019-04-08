7ed2be7d9fa7df7a40ab3af5189ca65fbf05903e
Jeff Bradberry <jeff.bradberry@gmail.com>
2019-04-08 16:36:26 -0400

    Modify filter_insights_api_response to take in the separate remediations
    since it is accumulated via a different API call.

--- awx/main/tests/data/insights.py
+++ awx/main/tests/data/insights.py
@@ -5,5 +5,7 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 with open(os.path.join(dir_path, 'insights.json')) as data_file:
-    TEST_INSIGHTS_PLANS = json.loads(data_file.read())
+    TEST_INSIGHTS_PLANS = json.load(data_file)
 
+with open(os.path.join(dir_path, 'insights_remediations.json')) as data_file:
+    TEST_INSIGHTS_REMEDIATIONS = json.load(data_file)['data']
