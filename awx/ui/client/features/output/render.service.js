4f55ffe4a0b7f524059854d1335bdbbb87c38541
Alex Corey <alex.swansboro@gmail.com>
2019-04-08 14:46:59 -0400

    Add tool tip to Host Event lines.

--- awx/ui/client/features/output/render.service.js
+++ awx/ui/client/features/output/render.service.js
@@ -35,7 +35,7 @@ const hasAnsi = input => re.test(input);
 
 let $scope;
 
-function JobRenderService ($q, $compile, $sce, $window) {
+function JobRenderService ($q, $compile, $sce, $window, strings) {
     this.init = (_$scope_, { toggles }) => {
         $scope = _$scope_;
         this.setScope();
@@ -132,7 +132,7 @@ function JobRenderService ($q, $compile, $sce, $window) {
             return { html: '', count: 0 };
         }
 
-        const html = this.buildRowHTML(this.records[uuid]);
+        const html = this.buildRowHTML(this.records[uuid], null, null, event);
         const count = 1;
 
         return { html, count };
@@ -193,7 +193,7 @@ function JobRenderService ($q, $compile, $sce, $window) {
             return { html: '', count: 0 };
         }
 
-        const html = this.buildRowHTML(this.records[uuid]);
+        const html = this.buildRowHTML(this.records[uuid], null, null, event);
         const count = 1;
 
         return { html, count };
@@ -226,10 +226,10 @@ function JobRenderService ($q, $compile, $sce, $window) {
             const line = lines[i];
             const isLastLine = i === lines.length - 1;
 
-            let row = this.buildRowHTML(record, ln, line);
+            let row = this.buildRowHTML(record, ln, line, event);
 
             if (record && record.isTruncated && isLastLine) {
-                row += this.buildRowHTML(record);
+                row += this.buildRowHTML(record, null, null, event);
                 count++;
             }
 
@@ -340,7 +340,7 @@ function JobRenderService ($q, $compile, $sce, $window) {
         return list;
     };
 
-    this.buildRowHTML = (record, ln, content) => {
+    this.buildRowHTML = (record, ln, content, event) => {
         let id = '';
         let icon = '';
         let timestamp = '';
@@ -407,12 +407,26 @@ function JobRenderService ($q, $compile, $sce, $window) {
         }
 
         return `
-            <div id="${id}" class="at-Stdout-row ${classList}" ${directives}>
+            <div id="${id}" class="at-Stdout-row ${classList}" ${directives} aw-tool-tip= '${this.createToolTip(event, record)}' aw-tip-placement="top">
                 ${tdToggle}
                 <div class="at-Stdout-line">${ln}</div>
-                <div class="at-Stdout-event"><span ng-non-bindable>${content}</span></div>
+                <div class="at-Stdout-event">
+                    <div>Host Status </div>
+                    <div>Next line </div>
+                    <div ng-non-bindable>${content}</div>
+                </div>
                 <div class="at-Stdout-time">${timestamp}</div>
             </div>`;
+    };
+
+    this.createToolTip = (event, record) => {
+        const status = strings.get('tooltips.HOST_STATUS');
+        const eventID = strings.get('tooltips.EVENT_ID');
+        const clickForDetails = strings.get('tooltips.DETAILS');
+
+        return record.isClickable
+            ? `${status} ${event.event_display}<br />${eventID} ${event.id}<br />${clickForDetails}`
+            : `${status} ${event.event_display}<br />${eventID} ${event.id}`;
     };
 
     this.getTimestamp = created => {
@@ -608,6 +622,6 @@ function JobRenderService ($q, $compile, $sce, $window) {
     this.getCapacity = () => OUTPUT_EVENT_LIMIT - (this.getTailCounter() - this.getHeadCounter());
 }
 
-JobRenderService.$inject = ['$q', '$compile', '$sce', '$window'];
+JobRenderService.$inject = ['$q', '$compile', '$sce', '$window', 'OutputStrings'];
 
 export default JobRenderService;
