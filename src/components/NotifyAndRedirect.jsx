83e6255ba45e4892eae8066f9427c4d26a5905a6
John Mitchell <jlmitch5@ncsu.edu>
2019-04-11 17:16:42 -0400

    update NotifyAndRedirect to function component

--- src/components/NotifyAndRedirect.jsx
+++ src/components/NotifyAndRedirect.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { Redirect, withRouter } from 'react-router-dom';
 
@@ -6,39 +6,39 @@ import { Trans } from '@lingui/macro';
 
 import { withRootDialog } from '../contexts/RootDialog';
 
-class NotifyAndRedirect extends Component {
-  constructor (props) {
-    super(props);
+const NotifyAndRedirect = ({
+  to,
+  push,
+  from,
+  exact,
+  strict,
+  sensitive,
+  setRootDialogMessage,
+  location
+}) => {
+  setRootDialogMessage({
+    title: '404',
+    bodyText: (
+      <Trans>
+        Cannot find route
+        <strong>{` ${location.pathname}`}</strong>
+        .
+      </Trans>
+    ),
+    variant: 'warning'
+  });
 
-    const { setRootDialogMessage, location } = this.props;
-    setRootDialogMessage({
-      title: '404',
-      bodyText: (
-        <Trans>
-          Cannot find route
-          <strong>{` ${location.pathname}`}</strong>
-          .
-        </Trans>
-      ),
-      variant: 'warning'
-    });
-  }
-
-  render () {
-    const { to, push, from, exact, strict, sensitive } = this.props;
-
-    return (
-      <Redirect
-        to={to}
-        push={push}
-        from={from}
-        exact={exact}
-        strict={strict}
-        sensitive={sensitive}
-      />
-    );
-  }
-}
+  return (
+    <Redirect
+      to={to}
+      push={push}
+      from={from}
+      exact={exact}
+      strict={strict}
+      sensitive={sensitive}
+    />
+  );
+};
 
 export { NotifyAndRedirect as _NotifyAndRedirect };
 export default withRootDialog(withRouter(NotifyAndRedirect));
