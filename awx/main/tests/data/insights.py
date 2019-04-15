e2861c6c39c5b3b0e9de0776f08a9b207ed8a79c
Jeff Bradberry <jeff.bradberry@gmail.com>
2019-04-15 15:59:40 -0400

    Fix the tests to conform to the new Insights results

--- awx/main/tests/data/insights.py
+++ awx/main/tests/data/insights.py
@@ -4,6 +4,9 @@ import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
+with open(os.path.join(dir_path, 'insights_hosts.json')) as data_file:
+    TEST_INSIGHTS_HOSTS = json.load(data_file)
+
 with open(os.path.join(dir_path, 'insights.json')) as data_file:
     TEST_INSIGHTS_PLANS = json.load(data_file)
 
