5b8fba58e8f3dc2f154c8d38dfacca9aeef315db
zicklam <zicklam@users.noreply.github.com>
2019-05-06 13:12:41 +0200

    Add "Disable SSL Verification" checkbox to webhook notification This commit will add a checkbox which will disable SSL verification on the generic webhook notification type. This is required when using self-signed certificates.

--- awx/ui/client/src/notifications/notificationTemplates.form.js
+++ awx/ui/client/src/notifications/notificationTemplates.form.js
@@ -399,6 +399,13 @@ export default ['i18n', function(i18n) {
                 subForm: 'typeSubForm',
                 ngDisabled: '!(notification_template.summary_fields.user_capabilities.edit || canAdd)'
             },
+            webhook_no_verify_ssl: {
+                label: i18n._('Disable SSL Verification'),
+                type: 'checkbox',
+                ngShow: "notification_type.value == 'webhook' ",
+                subForm: 'typeSubForm',
+                ngDisabled: '!(notification_template.summary_fields.user_capabilities.edit || canAdd)'
+            },
             headers: {
                 label: i18n._('HTTP Headers'),
                 dataTitle: i18n._('HTTP Headers'),
