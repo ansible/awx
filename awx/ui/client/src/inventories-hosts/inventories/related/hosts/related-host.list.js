2a71232dd696052e7395b830d33f945f4d9ccdbb
Jose Ariza <l.jlac001@gmail.com>
2019-05-15 09:05:27 -0500

    #3348 added description to host list on inventories related hosts
    Signed-off-by: Jose Ariza <l.jlac001@gmail.com>

--- awx/ui/client/src/inventories-hosts/inventories/related/hosts/related-host.list.js
+++ awx/ui/client/src/inventories-hosts/inventories/related/hosts/related-host.list.js
@@ -64,7 +64,17 @@ export default ['i18n', function(i18n) {
                 label: i18n._('Hosts'),
                 uiSref: ".edit({inventory_id: host.inventory_id,host_id: host.id})",
                 ngClass: "{ 'host-disabled-label': !host.enabled }",
-                columnClass: 'col-lg-5 col-md-4 col-sm-8 col-xs-7',
+                columnClass: 'col-lg-3 col-md-3 col-sm-8 col-xs-7',
+                dataHostId: "{{ host.id }}",
+                dataType: "host",
+                class: 'InventoryManage-breakWord'
+            },
+            description: {
+                key: true,
+                label: i18n._('Description'),
+                uiSref: ".edit({inventory_id: host.inventory_id,host_id: host.id})",
+                ngClass: "{ 'host-disabled-label': !host.enabled }",
+                columnClass: 'col-lg-3 col-md-3 col-sm-8 col-xs-7',
                 dataHostId: "{{ host.id }}",
                 dataType: "host",
                 class: 'InventoryManage-breakWord'
@@ -74,13 +84,13 @@ export default ['i18n', function(i18n) {
                 type: 'related_groups',
                 nosort: true,
                 showDelete: true,
-                columnClass: 'd-none d-md-flex List-tableCell col-lg-5 col-md-4'
+                columnClass: 'd-none d-md-flex List-tableCell col-lg-3 col-md-3'
             }
         },
 
         fieldActions: {
 
-            columnClass: 'col-lg-2 col-sm-4 col-xs-5 text-right',
+            columnClass: 'col-lg-2 col-sm-3 col-xs-5 text-right',
             edit: {
                 ngClick: "editHost(host)",
                 icon: 'icon-edit',
