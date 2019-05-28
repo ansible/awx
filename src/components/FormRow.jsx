7452b82856e17f0b729bba3c44e2fda08d7ff10f
Alex Corey <alex.swansboro@gmail.com>
2019-05-28 13:13:42 -0400

    addresses PR issues

--- src/components/FormRow.jsx
+++ src/components/FormRow.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default function FormRow ({ children }) {
-  const Row = styled.div`
+const Row = styled.div`
   display: grid;
   grid-gap: 20px;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   `;
+export default function FormRow ({ children }) {
   return (
     <Row>
       {children}
