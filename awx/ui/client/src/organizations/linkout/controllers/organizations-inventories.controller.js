3c96968ee0c6248d3fb8e7d77bb1f3c0bbae642f
Marliana Lara <mlara@redhat.com>
2019-07-29 12:36:30 -0400

    Add link to organization inventory list

--- awx/ui/client/src/organizations/linkout/controllers/organizations-inventories.controller.js
+++ awx/ui/client/src/organizations/linkout/controllers/organizations-inventories.controller.js
@@ -68,6 +68,7 @@ export default ['$scope', '$rootScope', '$location',
             }
 
             item.kind_label = item.kind === '' ? i18n._('Inventory') : (item.kind === 'smart' ? i18n._('Smart Inventory'): i18n._('Inventory'));
+            item.linkToDetails = (item.kind && item.kind === 'smart') ? `inventories.editSmartInventory({smartinventory_id:${item.id}})` : `inventories.edit({inventory_id:${item.id}})`;
 
             return item;
         }
