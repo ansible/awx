61ca4278c87b8901576f4761d2da18f8fa77fb23
Jake McDermott <yo@jakemcdermott.me>
2019-08-12 09:45:08 -0400

    remove extra and trailing slashes from url

--- awx/ui/test/e2e/settings.js
+++ awx/ui/test/e2e/settings.js
@@ -1,14 +1,18 @@
+const normalizeURL = s => s
+    .replace(/([^:]\/)\/+/g, '$1') // remove duplicate slashes
+    .replace(/\/+$/, ''); // remove trailing slash if there is one
+
 const AWX_E2E_CLUSTER_HOST = process.env.AWX_E2E_CLUSTER_HOST || 'localhost';
 const AWX_E2E_CLUSTER_PORT = process.env.AWX_E2E_CLUSTER_PORT || 4444;
 const AWX_E2E_CLUSTER_WORKERS = process.env.AWX_E2E_CLUSTER_WORKERS || 0;
 const AWX_E2E_PASSWORD = process.env.AWX_E2E_PASSWORD || 'password';
-const AWX_E2E_URL = process.env.AWX_E2E_URL || 'https://localhost:8043';
+const AWX_E2E_URL = normalizeURL(process.env.AWX_E2E_URL || 'https://localhost:8043');
 const AWX_E2E_USERNAME = process.env.AWX_E2E_USERNAME || 'awx-e2e';
 const AWX_E2E_TIMEOUT_ASYNC = process.env.AWX_E2E_TIMEOUT_ASYNC || 120000;
 const AWX_E2E_TIMEOUT_LONG = process.env.AWX_E2E_TIMEOUT_LONG || 10000;
 const AWX_E2E_TIMEOUT_MEDIUM = process.env.AWX_E2E_TIMEOUT_MEDIUM || 5000;
 const AWX_E2E_TIMEOUT_SHORT = process.env.AWX_E2E_TIMEOUT_SHORT || 1000;
-const AWX_E2E_LAUNCH_URL = process.env.AWX_E2E_LAUNCH_URL || AWX_E2E_URL;
+const AWX_E2E_LAUNCH_URL = normalizeURL(process.env.AWX_E2E_LAUNCH_URL || AWX_E2E_URL);
 
 // Screenshot capture settings
 const AWX_E2E_SCREENSHOTS_ENABLED = process.env.AWX_E2E_SCREENSHOTS_ENABLED || false;
