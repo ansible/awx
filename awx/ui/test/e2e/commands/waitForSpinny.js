5dbcafc392ade3d296ed30c8e2201e0281f68ed9
Daniel Sami <dsami@redhat.com>
2019-09-11 08:59:20 -0400

    Added a longer timeout for spinny specifically

--- awx/ui/test/e2e/commands/waitForSpinny.js
+++ awx/ui/test/e2e/commands/waitForSpinny.js
@@ -5,6 +5,9 @@ exports.command = function waitForSpinny (useXpath = false) {
         selector = '//*[contains(@class, "spinny")]';
     }
     this.waitForElementVisible(selector);
-    this.waitForElementNotVisible(selector);
+    // if a process is running for an extended period,
+    // spinny might last longer than five seconds.
+    // this gives it a max of 30 secs before failing.
+    this.waitForElementNotVisible(selector, 30000);
     return this;
 };
