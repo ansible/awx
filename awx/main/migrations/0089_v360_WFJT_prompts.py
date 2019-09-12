fdf9dd733bc12d0857d00f8af80d6728406eb1a8
AlanCoding <arominge@redhat.com>
2019-09-12 09:19:09 -0400

    bump migration

--- awx/main/migrations/0089_v360_WFJT_prompts.py
+++ awx/main/migrations/0089_v360_WFJT_prompts.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0087_v360_update_credential_injector_help_text'),
+        ('main', '0088_v360_dashboard_optimizations'),
     ]
 
     operations = [
