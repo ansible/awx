ce6a276e1f8d08f121774f3fcdb3c96e5863cff4
beeankha <beeankha@gmail.com>
2019-09-17 16:38:48 -0400

    Update migration file

--- awx/main/migrations/0091_v360_approval_node_notifications.py
+++ awx/main/migrations/0091_v360_approval_node_notifications.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0089_v360_new_job_event_types'),
+        ('main', '0090_v360_WFJT_prompts'),
     ]
 
     operations = [
