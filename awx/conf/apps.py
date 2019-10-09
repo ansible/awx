2814e48995364ca6c8c18101a1997c8a6382cdce
Seth Foster <fosterbseth@gmail.com>
2019-10-09 17:07:58 -0400

    Add DynamicOAuth2ProviderSettings
    - Subclass Oauth2ProviderSettings and redefine __getattr__ to use
      django.conf settings instead of oauth2_settings
    - Fixes an issue where values set in UI, e.g.
      REFRESH_TOKEN_EXPIRE_SECONDS are not used in oauth2 methods, namely
      the awx-manage commands and requests to /o/ endpoints. Instead the
      methods would use whatever was present in defaults.py

--- awx/conf/apps.py
+++ awx/conf/apps.py
@@ -9,7 +9,37 @@ class ConfConfig(AppConfig):
     name = 'awx.conf'
     verbose_name = _('Configuration')
 
+    # oauth2_settings grabs values from defaults.py, and does not get updated
+    # with values set from tower (i.e. the UI), that is django.conf settings.
+    # E.g. REFRESH_TOKEN_EXPIRE_SECONDS will retain the value in defaults.py, and
+    # not the value set from the UI.
+    # The following code resets the models and oauth2_validators imports to use
+    # DynamicOAuth2ProviderSettings instead of OAuth2ProviderSettings.
+    # This sub-class will check that the attribute first exists in django.conf.settings
+    # and return that before using the oauth2_settings.
+    
+    def configure_oauth2_provider(self, settings):
+        from django.conf import settings
+        from oauth2_provider import settings as o_settings
+        from oauth2_provider import models, oauth2_validators
+
+        class DynamicOAuth2ProviderSettings(o_settings.OAuth2ProviderSettings):
+
+            def __getattr__(self, attr):
+                if attr in settings.OAUTH2_PROVIDER:
+                    return settings.OAUTH2_PROVIDER[attr]
+                return super(DynamicOAuth2ProviderSettings, self).__getattr__(attr)
+
+        models.oauth2_settings = DynamicOAuth2ProviderSettings(
+            models.oauth2_settings.user_settings, models.oauth2_settings.defaults,
+            models.oauth2_settings.import_strings, models.oauth2_settings.mandatory
+        )
+        oauth2_validators.oauth2_settings = models.oauth2_settings
+
+
     def ready(self):
         self.module.autodiscover()
         from .settings import SettingsWrapper
         SettingsWrapper.initialize()
+        from django.conf import settings
+        self.configure_oauth2_provider(settings)
