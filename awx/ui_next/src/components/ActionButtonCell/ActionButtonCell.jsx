d8814b716212326b29bdc25e58b5cfe0406c85e5
mabashian <mabashia@redhat.com>
2019-10-28 15:11:29 -0400

    Add displayName so that ActionButtonCell can be referenced in tests

--- awx/ui_next/src/components/ActionButtonCell/ActionButtonCell.jsx
+++ awx/ui_next/src/components/ActionButtonCell/ActionButtonCell.jsx
@@ -1,6 +1,7 @@
 import DataListCell from '@components/DataListCell';
 import styled from 'styled-components';
 
+DataListCell.displayName = 'ActionButtonCell';
 export default styled(DataListCell)`
   & > :not(:first-child) {
     margin-left: 20px;
