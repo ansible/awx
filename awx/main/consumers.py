36477a3359dd27a91c7cbf2c9e2976a39280d9ed
chris meyers <chris.meyers.fsu@gmail.com>
2019-11-11 10:16:00 -0500

    clean up code

--- awx/main/consumers.py
+++ awx/main/consumers.py
@@ -1,8 +1,6 @@
 import json
 import logging
 
-#from channels.auth import channel_session_user_from_http, channel_session_user
-
 from django.utils.encoding import smart_str
 from django.http.cookie import parse_cookie
 from django.core.serializers.json import DjangoJSONEncoder
@@ -10,14 +8,10 @@ from channels.consumer import SyncConsumer
 from channels.generic.websocket import JsonWebsocketConsumer
 from channels.layers import get_channel_layer
 
-#from awx.main.middleware import XRFMiddlewareStack
-
-
 from asgiref.sync import async_to_sync
 
 
 logger = logging.getLogger('awx.main.consumers')
-#XRF_KEY = XRFMiddlewareStack.XRF_KEY
 XRF_KEY = '_auth_user_xrf'
 
 
@@ -82,52 +76,6 @@ class EventConsumer(JsonWebsocketConsumer):
     def internal_message(self, event):
         self.send(event['text'])
 
-
-'''
-@channel_session_user
-def ws_receive(message):
-    user = message.user
-    raw_data = message.content['text']
-    data = json.loads(raw_data)
-
-    xrftoken = data.get('xrftoken')
-    if (
-        not xrftoken or
-        XRF_KEY not in message.channel_session or
-        xrftoken != message.channel_session[XRF_KEY]
-    ):
-        logger.error(
-            "access denied to channel, XRF mismatch for {}".format(user.username)
-        )
-        message.reply_channel.send({
-            "text": json.dumps({"error": "access denied to channel"})
-        })
-        return
-
-    if 'groups' in data:
-        discard_groups(message)
-        groups = data['groups']
-        for group_name,v in groups.items():
-            if type(v) is list:
-                for oid in v:
-                    name = '{}-{}'.format(group_name, oid)
-                    access_cls = consumer_access(group_name)
-                    if access_cls is not None:
-                        user_access = access_cls(user)
-                        if not user_access.get_queryset().filter(pk=oid).exists():
-                            message.reply_channel.send({"text": json.dumps(
-                                {"error": "access denied to channel {0} for resource id {1}".format(group_name, oid)})})
-                            continue
-                    async_to_sync(self.channel_layer.group_add)(
-                        name,
-                        self.channel_name
-                    )
-            else:
-                async_to_sync(self.channel_layer.group_add)(
-                    group_name,
-                    self.channel_name
-                )
-'''
 
 def emit_channel_notification(group, payload):
     channel_layer = get_channel_layer()
