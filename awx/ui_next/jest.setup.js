8fcf029eeb5fdb7b8b5dbc7ebaee7d8a10514f03
Jake McDermott <yo@jakemcdermott.me>
2019-11-12 12:35:36 -0500

    Increase async test timeout
    When our CI system is overloaded, tests start running slower.

--- awx/ui_next/jest.setup.js
+++ awx/ui_next/jest.setup.js
@@ -6,4 +6,6 @@ export const asyncFlush = () => new Promise((resolve) => setImmediate(resolve));
 const enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
+jest.setTimeout(5000 * 4);
+
 enzyme.configure({ adapter: new Adapter() });
