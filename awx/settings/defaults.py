60ea5bf008a0b4b7918528803aed81e484a8e2a8
chris meyers <chris.meyers.fsu@gmail.com>
2019-11-13 09:58:56 -0500

    channel deps and redis container

--- awx/settings/defaults.py
+++ awx/settings/defaults.py
@@ -970,7 +970,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [("redis", 6379)],
         },
     },
 }
