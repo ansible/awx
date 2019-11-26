695eab1fdd5856c74d71a075ef97b78d0fd6984f
AlanCoding <arominge@redhat.com>
2019-11-26 09:39:05 -0500

    fix duplicate exception sanity error

--- awx_collection/plugins/modules/tower_credential.py
+++ awx_collection/plugins/modules/tower_credential.py
@@ -404,7 +404,7 @@ def main():
                 result = credential.delete(**params)
         except (exc.NotFound) as excinfo:
             module.fail_json(msg='Failed to update credential, organization not found: {0}'.format(excinfo), changed=False)
-        except (exc.ConnectionError, exc.BadRequest, exc.NotFound, exc.AuthError) as excinfo:
+        except (exc.ConnectionError, exc.BadRequest, exc.AuthError) as excinfo:
             module.fail_json(msg='Failed to update credential: {0}'.format(excinfo), changed=False)
 
     json_output['changed'] = result['changed']
