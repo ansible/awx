35be571eedc08d0a7507f0a1527a2769b67f93ee
humzam96 <humzam96@icloud.com>
2019-12-02 17:16:40 -0500

    Don't block the blockquotes

--- awx/ui/client/src/vendor.js
+++ awx/ui/client/src/vendor.js
@@ -32,6 +32,7 @@ const btn = $.fn.button.noConflict();
 $.fn.btn = btn;
 
 // Whitelist table elements so they can be used in popovers
+$.fn.popover.Constructor.Default.whiteList.blockquote = [];
 $.fn.popover.Constructor.Default.whiteList.table = [];
 $.fn.popover.Constructor.Default.whiteList.th = [];
 $.fn.popover.Constructor.Default.whiteList.tr = [];
