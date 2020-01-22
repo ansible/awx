5a1a47b7aa25e5fc972f0f4538e3e3e39750d142
mabashian <mabashia@redhat.com>
2020-01-22 11:24:50 -0500

    Default '---' not needed due to changes to how the prop is passed to CodeMirrorInput

--- awx/ui_next/src/components/CodeMirrorInput/VariablesDetail.jsx
+++ awx/ui_next/src/components/CodeMirrorInput/VariablesDetail.jsx
@@ -21,7 +21,7 @@ function getValueAsMode(value, mode) {
   return mode === YAML_MODE ? jsonToYaml(value) : yamlToJson(value);
 }
 
-function VariablesDetail({ value = '---', label, rows }) {
+function VariablesDetail({ value, label, rows }) {
   const [mode, setMode] = useState(isJson(value) ? JSON_MODE : YAML_MODE);
   const [currentValue, setCurrentValue] = useState(value || '---');
   const [error, setError] = useState(null);
