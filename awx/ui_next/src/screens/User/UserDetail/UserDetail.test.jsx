b906f8d757f8e4a2cf455e7b3b50b2a993029778
Marliana Lara <mlara@redhat.com>
2020-01-23 17:53:34 -0500

    Add delete button to user details

--- awx/ui_next/src/screens/User/UserDetail/UserDetail.test.jsx
+++ awx/ui_next/src/screens/User/UserDetail/UserDetail.test.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { act } from 'react-dom/test-utils';
 import { createMemoryHistory } from 'history';
 import { mountWithContexts, waitForElement } from '@testUtils/enzymeHelpers';
+import { UsersAPI } from '@api';
 import UserDetail from './UserDetail';
 import mockDetails from '../data.user.json';
+
+jest.mock('@api');
 
 describe('<UserDetail />', () => {
   test('initially renders succesfully', () => {
@@ -39,7 +43,36 @@ describe('<UserDetail />', () => {
     assertDetail('Created', `10/28/2019, 3:01:07 PM`);
   });
 
-  test('should show edit button for users with edit permission', async done => {
+  test('User Type Detail should render expected strings', async () => {
+    let wrapper;
+    wrapper = mountWithContexts(
+      <UserDetail
+        user={{
+          ...mockDetails,
+          is_superuser: false,
+          is_system_auditor: true,
+        }}
+      />
+    );
+    expect(wrapper.find(`Detail[label="User Type"] dd`).text()).toBe(
+      'System Auditor'
+    );
+
+    wrapper = mountWithContexts(
+      <UserDetail
+        user={{
+          ...mockDetails,
+          is_superuser: false,
+          is_system_auditor: false,
+        }}
+      />
+    );
+    expect(wrapper.find(`Detail[label="User Type"] dd`).text()).toBe(
+      'Normal User'
+    );
+  });
+
+  test('should show edit button for users with edit permission', async () => {
     const wrapper = mountWithContexts(<UserDetail user={mockDetails} />);
     const editButton = await waitForElement(
       wrapper,
@@ -47,10 +80,9 @@ describe('<UserDetail />', () => {
     );
     expect(editButton.text()).toEqual('Edit');
     expect(editButton.prop('to')).toBe(`/users/${mockDetails.id}/edit`);
-    done();
   });
 
-  test('should hide edit button for users without edit permission', async done => {
+  test('should hide edit button for users without edit permission', async () => {
     const wrapper = mountWithContexts(
       <UserDetail
         user={{
@@ -65,7 +97,6 @@ describe('<UserDetail />', () => {
     );
     await waitForElement(wrapper, 'UserDetail');
     expect(wrapper.find('UserDetail Button[aria-label="edit"]').length).toBe(0);
-    done();
   });
 
   test('edit button should navigate to user edit', () => {
@@ -78,5 +109,36 @@ describe('<UserDetail />', () => {
       .find('Button[aria-label="edit"] Link')
       .simulate('click', { button: 0 });
     expect(history.location.pathname).toEqual('/users/1/edit');
+  });
+
+  test('expected api call is made for delete', async () => {
+    const wrapper = mountWithContexts(<UserDetail user={mockDetails} />);
+    await waitForElement(wrapper, 'UserDetail Button[aria-label="Delete"]');
+    await act(async () => {
+      wrapper.find('DeleteButton').invoke('onConfirm')();
+    });
+    expect(UsersAPI.destroy).toHaveBeenCalledTimes(1);
+  });
+
+  test('Error dialog shown for failed deletion', async () => {
+    UsersAPI.destroy.mockImplementationOnce(() => Promise.reject(new Error()));
+    const wrapper = mountWithContexts(<UserDetail user={mockDetails} />);
+    await waitForElement(wrapper, 'UserDetail Button[aria-label="Delete"]');
+    await act(async () => {
+      wrapper.find('DeleteButton').invoke('onConfirm')();
+    });
+    await waitForElement(
+      wrapper,
+      'Modal[title="Error!"]',
+      el => el.length === 1
+    );
+    await act(async () => {
+      wrapper.find('Modal[title="Error!"]').invoke('onClose')();
+    });
+    await waitForElement(
+      wrapper,
+      'Modal[title="Error!"]',
+      el => el.length === 0
+    );
   });
 });
