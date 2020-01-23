afc1f856684b3584e30589af79ec199045478236
Marliana Lara <mlara@redhat.com>
2020-01-23 13:59:20 -0500

    Update job template detail unit tests

--- awx/ui_next/src/screens/Template/JobTemplateDetail/index.js
+++ awx/ui_next/src/screens/Template/JobTemplateDetail/index.js
@@ -1,4 +1,1 @@
-import JobTemplateDetail from './JobTemplateDetail';
-
-export { JobTemplateDetail as _JobTemplateDetail };
-export default JobTemplateDetail;
+export { default } from './JobTemplateDetail';
