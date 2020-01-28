de75592f2ab558e70d5bda657687929307d99017
Marliana Lara <mlara@redhat.com>
2020-01-28 12:26:40 -0500

    Refactor Teams and Team components

--- awx/ui_next/src/screens/Team/Teams.jsx
+++ awx/ui_next/src/screens/Team/Teams.jsx
@@ -1,79 +1,58 @@
-import React, { Component, Fragment } from 'react';
-import { Route, withRouter, Switch } from 'react-router-dom';
+import React, { useState, useCallback } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { withI18n } from '@lingui/react';
 import { t } from '@lingui/macro';
 
 import { Config } from '@contexts/Config';
-import Breadcrumbs from '@components/Breadcrumbs/Breadcrumbs';
-
-import TeamsList from './TeamList/TeamList';
-import TeamAdd from './TeamAdd/TeamAdd';
+import Breadcrumbs from '@components/Breadcrumbs';
+import TeamList from './TeamList';
+import TeamAdd from './TeamAdd';
 import Team from './Team';
 
-class Teams extends Component {
-  constructor(props) {
-    super(props);
+function Teams({ i18n }) {
+  const [breadcrumbConfig, setBreadcrumbConfig] = useState({
+    '/teams': i18n._(t`Teams`),
+    '/teams/add': i18n._(t`Create New Team`),
+  });
 
-    const { i18n } = props;
+  const buildBreadcrumbConfig = useCallback(
+    team => {
+      if (!team) {
+        return;
+      }
 
-    this.state = {
-      breadcrumbConfig: {
+      setBreadcrumbConfig({
         '/teams': i18n._(t`Teams`),
         '/teams/add': i18n._(t`Create New Team`),
-      },
-    };
-  }
+        [`/teams/${team.id}`]: `${team.name}`,
+        [`/teams/${team.id}/edit`]: i18n._(t`Edit Details`),
+        [`/teams/${team.id}/details`]: i18n._(t`Details`),
+        [`/teams/${team.id}/users`]: i18n._(t`Users`),
+        [`/teams/${team.id}/access`]: i18n._(t`Access`),
+      });
+    },
+    [i18n]
+  );
 
-  setBreadcrumbConfig = team => {
-    const { i18n } = this.props;
-
-    if (!team) {
-      return;
-    }
-
-    const breadcrumbConfig = {
-      '/teams': i18n._(t`Teams`),
-      '/teams/add': i18n._(t`Create New Team`),
-      [`/teams/${team.id}`]: `${team.name}`,
-      [`/teams/${team.id}/edit`]: i18n._(t`Edit Details`),
-      [`/teams/${team.id}/details`]: i18n._(t`Details`),
-      [`/teams/${team.id}/users`]: i18n._(t`Users`),
-      [`/teams/${team.id}/access`]: i18n._(t`Access`),
-    };
-
-    this.setState({ breadcrumbConfig });
-  };
-
-  render() {
-    const { match, history, location } = this.props;
-    const { breadcrumbConfig } = this.state;
-
-    return (
-      <Fragment>
-        <Breadcrumbs breadcrumbConfig={breadcrumbConfig} />
-        <Switch>
-          <Route path={`${match.path}/add`} render={() => <TeamAdd />} />
-          <Route
-            path={`${match.path}/:id`}
-            render={() => (
-              <Config>
-                {({ me }) => (
-                  <Team
-                    history={history}
-                    location={location}
-                    setBreadcrumb={this.setBreadcrumbConfig}
-                    me={me || {}}
-                  />
-                )}
-              </Config>
-            )}
-          />
-          <Route path={`${match.path}`} render={() => <TeamsList />} />
-        </Switch>
-      </Fragment>
-    );
-  }
+  return (
+    <>
+      <Breadcrumbs breadcrumbConfig={breadcrumbConfig} />
+      <Switch>
+        <Route path="/teams/add">
+          <TeamAdd />
+        </Route>
+        <Route path="/teams/:id">
+          <Team setBreadcrumb={buildBreadcrumbConfig} />
+        </Route>
+        <Route path="/teams">
+          <Config>
+            {({ me }) => <TeamList path="/teams" me={me || {}} />}
+          </Config>
+        </Route>
+      </Switch>
+    </>
+  );
 }
 
 export { Teams as _Teams };
-export default withI18n()(withRouter(Teams));
+export default withI18n()(Teams);
