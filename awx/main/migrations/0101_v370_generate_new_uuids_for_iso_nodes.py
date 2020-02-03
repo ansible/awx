0291c476d4ccea338f9a5d6a88616cfac09afd85
chris meyers <chris.meyers.fsu@gmail.com>
2020-02-03 14:25:06 -0500

    use existing version of Instance
    * Without this change, future modifications to the Instance object may
    result in migration fails (i.e. adding a field to the Instance model)

--- awx/main/migrations/0101_v370_generate_new_uuids_for_iso_nodes.py
+++ awx/main/migrations/0101_v370_generate_new_uuids_for_iso_nodes.py
@@ -3,15 +3,14 @@ from uuid import uuid4
 
 from django.db import migrations
 
-from awx.main.models import Instance
-
 
 def _generate_new_uuid_for_iso_nodes(apps, schema_editor):
+    Instance = apps.get_model('main', 'Instance')
     for instance in Instance.objects.all():
         if instance.is_isolated():
             instance.uuid = str(uuid4())
             instance.save()
-    
+
 
 class Migration(migrations.Migration):
 
