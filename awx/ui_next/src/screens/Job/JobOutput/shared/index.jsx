4be7cf66ecb09761dd2f8266a7b48d19ebecdbf5
Marliana Lara <mlara@redhat.com>
2020-02-05 19:57:55 -0500

    Add host status bar

--- awx/ui_next/src/screens/Job/JobOutput/shared/index.jsx
+++ awx/ui_next/src/screens/Job/JobOutput/shared/index.jsx
@@ -2,3 +2,4 @@ export { default as JobEventLine } from './JobEventLine';
 export { default as JobEventLineToggle } from './JobEventLineToggle';
 export { default as JobEventLineNumber } from './JobEventLineNumber';
 export { default as JobEventLineText } from './JobEventLineText';
+export { default as HostStatusBar } from './HostStatusBar';
