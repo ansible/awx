0eef67713f0ef71f0ffb6bc2833ffc5fd1286e9f
John Westcott IV <john.westcott.iv@redhat.com>
2020-02-12 21:09:06 -0500

    Only try an ID lookup if we can convert the field name_or_id to an integer
    Fix linting issues, update tower_project unit test

--- awx_collection/test/awx/test_project.py
+++ awx_collection/test/awx/test_project.py
@@ -13,7 +13,8 @@ def test_create_project(run_module, admin_user, organization):
         organization=organization.name,
         scm_type='git',
         scm_url='https://foo.invalid',
-        wait=False
+        wait=False,
+        scm_update_cache_timeout=5
     ), admin_user)
     warning = ['scm_update_cache_timeout will be ignored since scm_update_on_launch was not set to true']
     assert result.pop('changed', None), result
