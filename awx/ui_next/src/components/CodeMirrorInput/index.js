fe9b03a189778067949d211ff5bfc09f72f92ada
Marliana Lara <mlara@redhat.com>
2020-02-18 10:31:19 -0500

    Move ButtonGroup into CodeMirrorInput directory

--- awx/ui_next/src/components/CodeMirrorInput/index.js
+++ awx/ui_next/src/components/CodeMirrorInput/index.js
@@ -4,3 +4,4 @@ export default CodeMirrorInput;
 export { default as VariablesDetail } from './VariablesDetail';
 export { default as VariablesInput } from './VariablesInput';
 export { default as VariablesField } from './VariablesField';
+export { default as ButtonGroup } from './ButtonGroup';
