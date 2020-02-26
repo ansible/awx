deced917cf6cd628c6f62a84d30b4696f112b050
Marliana Lara <mlara@redhat.com>
2020-02-26 15:27:19 -0500

    Refactor HostListItem into functional component and add test for host toggle

--- awx/ui_next/src/screens/Host/HostList/HostListItem.test.jsx
+++ awx/ui_next/src/screens/Host/HostList/HostListItem.test.jsx
@@ -3,7 +3,7 @@ import { mountWithContexts } from '@testUtils/enzymeHelpers';
 
 import HostsListItem from './HostListItem';
 
-let onToggleHost;
+const onToggleHost = jest.fn();
 
 const mockHost = {
   id: 1,
@@ -23,16 +23,10 @@ const mockHost = {
 };
 
 describe('<HostsListItem />', () => {
+  let wrapper;
+
   beforeEach(() => {
-    onToggleHost = jest.fn();
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  test('edit button shown to users with edit capabilities', () => {
-    const wrapper = mountWithContexts(
+    wrapper = mountWithContexts(
       <HostsListItem
         isSelected={false}
         detailUrl="/host/1"
@@ -41,13 +35,20 @@ describe('<HostsListItem />', () => {
         onToggleHost={onToggleHost}
       />
     );
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('edit button shown to users with edit capabilities', () => {
     expect(wrapper.find('PencilAltIcon').exists()).toBeTruthy();
   });
 
   test('edit button hidden from users without edit capabilities', () => {
     const copyMockHost = Object.assign({}, mockHost);
     copyMockHost.summary_fields.user_capabilities.edit = false;
-    const wrapper = mountWithContexts(
+    wrapper = mountWithContexts(
       <HostsListItem
         isSelected={false}
         detailUrl="/host/1"
@@ -57,5 +58,9 @@ describe('<HostsListItem />', () => {
       />
     );
     expect(wrapper.find('PencilAltIcon').exists()).toBeFalsy();
+  });
+
+  test('should display host toggle', () => {
+    expect(wrapper.find('HostToggle').length).toBe(1);
   });
 });
