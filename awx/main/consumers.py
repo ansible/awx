2b59af3808d8b110c2c430349aad9fe825bb3904
chris meyers <chris.meyers.fsu@gmail.com>
2020-02-26 11:50:22 -0500

    safely operate in async or sync context

--- awx/main/consumers.py
+++ awx/main/consumers.py
@@ -202,13 +202,9 @@ class EventConsumer(AsyncJsonWebsocketConsumer):
 
 
 def run_sync(func):
-    event_loop = None
-    try:
-        event_loop = asyncio.get_event_loop()
-    except RuntimeError:
-        event_loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(event_loop)
-    return event_loop.run_until_complete(func)
+    event_loop = asyncio.new_event_loop()
+    event_loop.run_until_complete(func)
+    event_loop.close()
 
 
 def _dump_payload(payload):
