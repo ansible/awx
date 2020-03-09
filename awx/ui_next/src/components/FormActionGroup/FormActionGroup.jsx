0f5b6945147b924a5629c77ac2a09918ba3ae835
Marliana Lara <mlara@redhat.com>
2020-03-09 14:56:39 -0400

    Fix form action button layout

--- awx/ui_next/src/components/FormActionGroup/FormActionGroup.jsx
+++ awx/ui_next/src/components/FormActionGroup/FormActionGroup.jsx
@@ -3,27 +3,30 @@ import PropTypes from 'prop-types';
 import { withI18n } from '@lingui/react';
 import { t } from '@lingui/macro';
 import { ActionGroup, Button } from '@patternfly/react-core';
+import { FormFullWidthLayout } from '@components/FormLayout';
 
 const FormActionGroup = ({ onSubmit, submitDisabled, onCancel, i18n }) => (
-  <ActionGroup>
-    <Button
-      aria-label={i18n._(t`Save`)}
-      variant="primary"
-      type="submit"
-      onClick={onSubmit}
-      isDisabled={submitDisabled}
-    >
-      {i18n._(t`Save`)}
-    </Button>
-    <Button
-      aria-label={i18n._(t`Cancel`)}
-      variant="secondary"
-      type="button"
-      onClick={onCancel}
-    >
-      {i18n._(t`Cancel`)}
-    </Button>
-  </ActionGroup>
+  <FormFullWidthLayout>
+    <ActionGroup>
+      <Button
+        aria-label={i18n._(t`Save`)}
+        variant="primary"
+        type="submit"
+        onClick={onSubmit}
+        isDisabled={submitDisabled}
+      >
+        {i18n._(t`Save`)}
+      </Button>
+      <Button
+        aria-label={i18n._(t`Cancel`)}
+        variant="secondary"
+        type="button"
+        onClick={onCancel}
+      >
+        {i18n._(t`Cancel`)}
+      </Button>
+    </ActionGroup>
+  </FormFullWidthLayout>
 );
 
 FormActionGroup.propTypes = {
