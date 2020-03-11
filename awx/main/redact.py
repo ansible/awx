c95624e27f3c3516abe337809c90b3d05dc8177f
Ryan Petrello <rpetrell@redhat.com>
2020-03-10 23:36:13 -0400

    optimize the SCM URL sanitizer regex
    \w+ is too greedy for large strings that don't contain URLs

--- awx/main/redact.py
+++ awx/main/redact.py
@@ -8,7 +8,7 @@ REPLACE_STR = '$encrypted$'
 
 class UriCleaner(object):
     REPLACE_STR = REPLACE_STR
-    SENSITIVE_URI_PATTERN = re.compile(r'(\w+:(\/?\/?)[^\s]+)', re.MULTILINE)  # NOQA
+    SENSITIVE_URI_PATTERN = re.compile(r'(\w{1,20}:(\/?\/?)[^\s]+)', re.MULTILINE)  # NOQA
 
     @staticmethod
     def remove_sensitive(cleartext):
