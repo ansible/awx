0eb526919f6925c5d20beb9782ee7113b724c18a
Alex Corey <alex.swansboro@gmail.com>
2020-03-26 15:23:31 -0400

    Fixes HostAdd Form layout issue

--- awx/ui_next/src/screens/Host/HostAdd/HostAdd.jsx
+++ awx/ui_next/src/screens/Host/HostAdd/HostAdd.jsx
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import { CardBody } from '@components/Card';
 import HostForm from '@components/HostForm';
 import { HostsAPI } from '@api';
+import { PageSection, Card } from '@patternfly/react-core';
 
 function HostAdd() {
   const [formError, setFormError] = useState(null);
@@ -22,13 +23,17 @@ function HostAdd() {
   };
 
   return (
-    <CardBody>
-      <HostForm
-        handleSubmit={handleSubmit}
-        handleCancel={handleCancel}
-        submitError={formError}
-      />
-    </CardBody>
+    <PageSection>
+      <Card>
+        <CardBody>
+          <HostForm
+            handleSubmit={handleSubmit}
+            handleCancel={handleCancel}
+            submitError={formError}
+          />
+        </CardBody>
+      </Card>
+    </PageSection>
   );
 }
 
