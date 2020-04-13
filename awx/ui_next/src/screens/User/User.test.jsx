4d7b5adf123e0bfbefcbdc94966f19bf42915d6d
nixocio <nixocio@gmail.com>
2020-04-13 11:20:54 -0400

    Update User component to be function based
    Update User component to be function based. Also update related
    unit-tests.

--- awx/ui_next/src/screens/User/User.test.jsx
+++ awx/ui_next/src/screens/User/User.test.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { act } from 'react-dom/test-utils';
 import { createMemoryHistory } from 'history';
 import { UsersAPI } from '@api';
 import { mountWithContexts, waitForElement } from '@testUtils/enzymeHelpers';
@@ -6,11 +7,6 @@ import mockDetails from './data.user.json';
 import User from './User';
 
 jest.mock('@api');
-
-const mockMe = {
-  is_super_user: true,
-  is_system_auditor: false,
-};
 
 async function getUsers() {
   return {
@@ -24,29 +20,78 @@ async function getUsers() {
 }
 
 describe('<User />', () => {
-  test('initially renders succesfully', () => {
+  test('initially renders successfully', async () => {
     UsersAPI.readDetail.mockResolvedValue({ data: mockDetails });
     UsersAPI.read.mockImplementation(getUsers);
-    mountWithContexts(<User setBreadcrumb={() => {}} me={mockMe} />);
+    const history = createMemoryHistory({
+      initialEntries: ['/users/1'],
+    });
+    await act(async () => {
+      mountWithContexts(<User setBreadcrumb={() => {}} />, {
+        context: {
+          router: {
+            history,
+            route: {
+              location: history.location,
+              match: {
+                params: { id: 1 },
+                url: '/users/1',
+                path: '/users/1',
+              },
+            },
+          },
+        },
+      });
+    });
   });
 
-  test('notifications tab shown for admins', async () => {
+  test('tabs shown for users', async () => {
     UsersAPI.readDetail.mockResolvedValue({ data: mockDetails });
     UsersAPI.read.mockImplementation(getUsers);
-
-    const wrapper = mountWithContexts(
-      <User setBreadcrumb={() => {}} me={mockMe} />
-    );
+    const history = createMemoryHistory({
+      initialEntries: ['/users/1'],
+    });
+    let wrapper;
+    await act(async () => {
+      wrapper = mountWithContexts(<User setBreadcrumb={() => {}} />, {
+        context: {
+          router: {
+            history,
+            route: {
+              location: history.location,
+              match: {
+                params: { id: 1 },
+                url: '/users/1',
+                path: '/users/1',
+              },
+            },
+          },
+        },
+      });
+    });
     await waitForElement(wrapper, '.pf-c-tabs__item', el => el.length === 5);
+
+    /* eslint-disable react/button-has-type */
+    expect(
+      wrapper
+        .find('Tabs')
+        .containsAllMatchingElements([
+          <button aria-label="Details">Details</button>,
+          <button aria-label="Organizations">Organizations</button>,
+          <button aria-label="Teams">Teams</button>,
+          <button aria-label="Access">Access</button>,
+          <button aria-label="Tokens">Tokens</button>,
+        ])
+    ).toEqual(true);
   });
 
   test('should show content error when user attempts to navigate to erroneous route', async () => {
     const history = createMemoryHistory({
       initialEntries: ['/users/1/foobar'],
     });
-    const wrapper = mountWithContexts(
-      <User setBreadcrumb={() => {}} me={mockMe} />,
-      {
+    let wrapper;
+    await act(async () => {
+      wrapper = mountWithContexts(<User setBreadcrumb={() => {}} />, {
         context: {
           router: {
             history,
@@ -60,8 +105,8 @@ describe('<User />', () => {
             },
           },
         },
-      }
-    );
+      });
+    });
     await waitForElement(wrapper, 'ContentError', el => el.length === 1);
   });
 });
--- awx/ui_next/src/screens/User/Users.jsx
+++ awx/ui_next/src/screens/User/Users.jsx
@@ -1,9 +1,8 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useCallback } from 'react';
 import { Route, useRouteMatch, Switch } from 'react-router-dom';
 import { withI18n } from '@lingui/react';
 import { t } from '@lingui/macro';
 
-import { Config } from '@contexts/Config';
 import Breadcrumbs from '@components/Breadcrumbs/Breadcrumbs';
 
 import UsersList from './UserList/UserList';
@@ -17,24 +16,26 @@ function Users({ i18n }) {
   });
   const match = useRouteMatch();
 
-  const addUserBreadcrumb = user => {
-    if (!user) {
-      return;
-    }
+  const addUserBreadcrumb = useCallback(
+    user => {
+      if (!user) {
+        return;
+      }
 
-    setBreadcrumbConfig({
-      '/users': i18n._(t`Users`),
-      '/users/add': i18n._(t`Create New User`),
-      [`/users/${user.id}`]: `${user.username}`,
-      [`/users/${user.id}/edit`]: i18n._(t`Edit Details`),
-      [`/users/${user.id}/details`]: i18n._(t`Details`),
-      [`/users/${user.id}/access`]: i18n._(t`Access`),
-      [`/users/${user.id}/teams`]: i18n._(t`Teams`),
-      [`/users/${user.id}/organizations`]: i18n._(t`Organizations`),
-      [`/users/${user.id}/tokens`]: i18n._(t`Tokens`),
-    });
-  };
-
+      setBreadcrumbConfig({
+        '/users': i18n._(t`Users`),
+        '/users/add': i18n._(t`Create New User`),
+        [`/users/${user.id}`]: `${user.username}`,
+        [`/users/${user.id}/edit`]: i18n._(t`Edit Details`),
+        [`/users/${user.id}/details`]: i18n._(t`Details`),
+        [`/users/${user.id}/access`]: i18n._(t`Access`),
+        [`/users/${user.id}/teams`]: i18n._(t`Teams`),
+        [`/users/${user.id}/organizations`]: i18n._(t`Organizations`),
+        [`/users/${user.id}/tokens`]: i18n._(t`Tokens`),
+      });
+    },
+    [i18n]
+  );
   return (
     <Fragment>
       <Breadcrumbs breadcrumbConfig={breadcrumbConfig} />
@@ -43,11 +44,7 @@ function Users({ i18n }) {
           <UserAdd />
         </Route>
         <Route path={`${match.path}/:id`}>
-          <Config>
-            {({ me }) => (
-              <User setBreadcrumb={addUserBreadcrumb} me={me || {}} />
-            )}
-          </Config>
+          <User setBreadcrumb={addUserBreadcrumb} />
         </Route>
         <Route path={`${match.path}`}>
           <UsersList />
