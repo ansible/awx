b7ab6ba9bbaa52dad7131a449895f5fe8143cffb
John Westcott IV <john.westcott.iv@redhat.com>
2020-04-29 10:05:04 -0400

    Attempting to fix py2 test issues

--- awx_collection/plugins/module_utils/tower_api.py
+++ awx_collection/plugins/module_utils/tower_api.py
@@ -33,7 +33,7 @@ class ItemNotDefined(Exception):
 
 class TowerModule(AnsibleModule):
     # This gets set by the make process so whatever is in here is irrelevant
-    _COLLECTION_VERSION = "11.0.0"
+    _COLLECTION_VERSION = "11.1.0"
     _COLLECTION_TYPE = "awx"
     # This maps the collections type (awx/tower) to the values returned by the API
     # Those values can be found in awx/api/generics.py line 204
