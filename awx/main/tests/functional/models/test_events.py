3cb24753070c01a80941c6bc4abc32d53e4fa1c8
Ryan Petrello <rpetrell@redhat.com>
2020-05-12 09:38:59 -0400

    properly handle host summary bulk updates if hosts go missing

--- awx/main/tests/functional/models/test_events.py
+++ awx/main/tests/functional/models/test_events.py
@@ -67,7 +67,7 @@ def test_parent_failed(emit, event):
 
 @pytest.mark.django_db
 def test_host_summary_generation():
-    hostnames = [f'Host {i}' for i in range(5000)]
+    hostnames = [f'Host {i}' for i in range(500)]
     inv = Inventory()
     inv.save()
     Host.objects.bulk_create([
@@ -107,3 +107,45 @@ def test_host_summary_generation():
         assert s.processed == 0
         assert s.rescued == 0
         assert s.skipped == 0
+
+
+@pytest.mark.django_db
+def test_host_summary_generation_with_deleted_hosts():
+    hostnames = [f'Host {i}' for i in range(10)]
+    inv = Inventory()
+    inv.save()
+    Host.objects.bulk_create([
+        Host(created=now(), modified=now(), name=h, inventory_id=inv.id)
+        for h in hostnames
+    ])
+    j = Job(inventory=inv)
+    j.save()
+    host_map = dict((host.name, host.id) for host in inv.hosts.all())
+
+    # delete half of the hosts during the playbook run
+    for h in inv.hosts.all()[:5]:
+        h.delete()
+
+    JobEvent.create_from_data(
+        job_id=j.pk,
+        parent_uuid='abc123',
+        event='playbook_on_stats',
+        event_data={
+            'ok': dict((hostname, len(hostname)) for hostname in hostnames),
+            'changed': {},
+            'dark': {},
+            'failures': {},
+            'ignored': {},
+            'processed': {},
+            'rescued': {},
+            'skipped': {},
+        },
+        host_map=host_map
+    ).save()
+
+
+    ids = sorted([s.host_id or -1 for s in j.job_host_summaries.order_by('id').all()])
+    names = sorted([s.host_name for s in j.job_host_summaries.all()])
+    assert ids == [-1, -1, -1, -1, -1, 6, 7, 8, 9, 10]
+    assert names == ['Host 0', 'Host 1', 'Host 2', 'Host 3', 'Host 4', 'Host 5',
+                     'Host 6', 'Host 7', 'Host 8', 'Host 9']
