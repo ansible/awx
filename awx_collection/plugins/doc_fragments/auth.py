1dbea4614ba4d29ab7535649fe5cef9704451fa0
John Westcott IV <john.westcott.iv@redhat.com>
2020-05-26 12:30:43 -0400

    Rebasing and fixing oauthtoken docs

--- awx_collection/plugins/doc_fragments/auth.py
+++ awx_collection/plugins/doc_fragments/auth.py
@@ -32,7 +32,8 @@ options:
     description:
     - The Tower OAuth token to use.
     - If value not set, will try environment variable C(TOWER_OAUTH_TOKEN) and then config files
-    type: str
+    type: raw
+    version_added: "3.7"
   validate_certs:
     description:
     - Whether to allow insecure connections to Tower or AWX.
