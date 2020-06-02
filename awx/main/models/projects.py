9ca39313cc7da48f840b7daedb077f1f324f1401
Bill Nottingham <notting@splat.cc>
2020-06-02 11:39:35 -0400

    Also download roles/collections during the sync job
    Since we're writing into a per-revision cache, we can do this easily now.

--- awx/main/models/projects.py
+++ awx/main/models/projects.py
@@ -604,9 +604,8 @@ class ProjectUpdate(UnifiedJob, ProjectOptions, JobNotificationMixin, TaskManage
         added_update_fields = []
         if not self.job_tags:
             job_tags = ['update_{}'.format(self.scm_type)]
-            if self.job_type == 'run':
-                job_tags.append('install_roles')
-                job_tags.append('install_collections')
+            job_tags.append('install_roles')
+            job_tags.append('install_collections')
             self.job_tags = ','.join(job_tags)
             added_update_fields.append('job_tags')
         if self.scm_delete_on_update and 'delete' not in self.job_tags and self.job_type == 'check':
