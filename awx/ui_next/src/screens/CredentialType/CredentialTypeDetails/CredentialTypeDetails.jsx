8eecfeaab43f11e7ec4fe624530eca0c60e8556c
nixocio <nixocio@gmail.com>
2020-06-09 16:49:57 -0400

    Add stub files for Credential Types
    Add stub files for Credential Types. Routing system, and screens layout.
    
    closes: https://github.com/ansible/awx/issues/7301

---
+++ awx/ui_next/src/screens/CredentialType/CredentialTypeDetails/CredentialTypeDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card, PageSection } from '@patternfly/react-core';
+
+function CredentialTypeDetails() {
+  return (
+    <PageSection>
+      <Card>Credential Type Details</Card>
+    </PageSection>
+  );
+}
+
+export default CredentialTypeDetails;
---
+++ awx/ui_next/src/screens/CredentialType/CredentialTypeEdit/CredentialTypeEdit.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card, PageSection } from '@patternfly/react-core';
+
+function CredentialTypeEdit() {
+  return (
+    <PageSection>
+      <Card>Credential Type Edit</Card>
+    </PageSection>
+  );
+}
+
+export default CredentialTypeEdit;
