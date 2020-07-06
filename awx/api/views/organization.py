bdf498d91910a91807713c7a57d24ba72ea86fe7
Jeff Bradberry <jeff.bradberry@gmail.com>
2020-07-06 16:23:01 -0400

    Add a new Swagger topic

--- awx/api/views/organization.py
+++ awx/api/views/organization.py
@@ -145,6 +145,7 @@ class OrganizationExecutionEnvironmentsList(SubListCreateAttachDetachAPIView):
     parent_model = Organization
     relationship = 'executionenvironments'
     parent_key = 'organization'
+    swagger_topic = "Execution Environments"
 
 
 class OrganizationJobTemplatesList(SubListCreateAPIView):
