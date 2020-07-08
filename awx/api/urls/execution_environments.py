c225517b509649b542278d91e2bea56e666a8132
Jeff Bradberry <jeff.bradberry@gmail.com>
2020-07-08 11:49:23 -0400

    Make changes to support capture by the activity stream
    Including exposing a new API view for a particular EE's activity
    stream objects.

--- awx/api/urls/execution_environments.py
+++ awx/api/urls/execution_environments.py
@@ -3,12 +3,14 @@ from django.conf.urls import url
 from awx.api.views import (
     ExecutionEnvironmentList,
     ExecutionEnvironmentDetail,
+    ExecutionEnvironmentActivityStreamList,
 )
 
 
 urls = [
     url(r'^$', ExecutionEnvironmentList.as_view(), name='execution_environment_list'),
     url(r'^(?P<pk>[0-9]+)/$', ExecutionEnvironmentDetail.as_view(), name='execution_environment_detail'),
+    url(r'^(?P<pk>[0-9]+)/activity_stream/$', ExecutionEnvironmentActivityStreamList.as_view(), name='execution_environment_activity_stream_list'),
 ]
 
 __all__ = ['urls']
