76297ea5641b21ee6938eea2967daa98871aa571
Jeff Bradberry <jeff.bradberry@gmail.com>
2020-07-08 14:43:14 -0400

    Add a sublist api view for the UJTs that use a given execution environment

--- awx/api/urls/execution_environments.py
+++ awx/api/urls/execution_environments.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from awx.api.views import (
     ExecutionEnvironmentList,
     ExecutionEnvironmentDetail,
+    ExecutionEnvironmentJobTemplateList,
     ExecutionEnvironmentActivityStreamList,
 )
 
@@ -10,6 +11,7 @@ from awx.api.views import (
 urls = [
     url(r'^$', ExecutionEnvironmentList.as_view(), name='execution_environment_list'),
     url(r'^(?P<pk>[0-9]+)/$', ExecutionEnvironmentDetail.as_view(), name='execution_environment_detail'),
+    url(r'^(?P<pk>[0-9]+)/unified_job_templates/$', ExecutionEnvironmentJobTemplateList.as_view(), name='execution_environment_job_template_list'),
     url(r'^(?P<pk>[0-9]+)/activity_stream/$', ExecutionEnvironmentActivityStreamList.as_view(), name='execution_environment_activity_stream_list'),
 ]
 
