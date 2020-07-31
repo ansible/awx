fef5ff69acc1f6b313ed4b21ff85ee282eca18c8
Shane McDonald <me@shanemcd.com>
2020-07-31 09:43:46 -0400

    Initial EE integration

--- tools/docker-compose/entrypoint.sh
+++ tools/docker-compose/entrypoint.sh
@@ -2,13 +2,23 @@
 
 if [ `id -u` -ge 500 ] || [ -z "${CURRENT_UID}" ]; then
 
-  cat << EOF > /tmp/passwd
+cat << EOF > /etc/passwd
 root:x:0:0:root:/root:/bin/bash
-awx:x:`id -u`:`id -g`:,,,:/tmp:/bin/bash
+awx:x:`id -u`:`id -g`:,,,:/var/lib/awx:/bin/bash
 EOF
 
-  cat /tmp/passwd > /etc/passwd
-  rm /tmp/passwd
+cat <<EOF > /etc/subuid
+awx:100000:50001
+EOF
+
+cat <<EOF > /etc/subgid
+awx:100000:50001
+EOF
+
 fi
+
+# Required to get rootless podman working after
+# writing out the sub*id files above
+podman system migrate
 
 exec $@
