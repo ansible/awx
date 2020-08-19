9bf92ee39da5eb882294f1ae815660c2bc867590
Kersom <9053044+nixocio@users.noreply.github.com>
2020-08-19 11:28:03 -0400

    Add execution environments files (#7909)
    Update navigation bar and routing system to add execution environments.
    Also, add stub files for the remaining related work.
    
    See: https://github.com/ansible/awx/issues/7885
    Also: https://github.com/ansible/awx/issues/7884

---
+++ awx/ui_next/src/screens/ExecutionEnvironment/ExecutionEnvironmentDetails/ExecutionEnvironmentDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Card, PageSection } from '@patternfly/react-core';
+
+function ExecutionEnvironmentDetails() {
+  return (
+    <PageSection>
+      <Card>
+        <div>Execution environments details</div>
+      </Card>
+    </PageSection>
+  );
+}
+
+export default ExecutionEnvironmentDetails;
