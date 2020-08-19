bb2af6b13dd537c8fde0f9db35fcdab8a6f2d28b
Kersom <9053044+nixocio@users.noreply.github.com>
2020-08-19 11:28:03 -0400

    Add execution environments files (#7909)
    Update navigation bar and routing system to add execution environments.
    Also, add stub files for the remaining related work.
    
    See: https://github.com/ansible/awx/issues/7885
    Also: https://github.com/ansible/awx/issues/7884

---
+++ awx/ui_next/src/screens/ExecutionEnvironment/ExecutionEnvironments.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useCallback } from 'react';
+import { withI18n } from '@lingui/react';
+import { t } from '@lingui/macro';
+import { Route, Switch } from 'react-router-dom';
+
+import ExecutionEnvironment from './ExecutionEnvironment';
+import ExecutionEnvironmentAdd from './ExecutionEnvironmentAdd';
+import ExecutionEnvironmentList from './ExecutionEnvironmentList';
+import Breadcrumbs from '../../components/Breadcrumbs';
+
+function ExecutionEnvironments({ i18n }) {
+  const [breadcrumbConfig, setBreadcrumbConfig] = useState({
+    '/execution_environments': i18n._(t`Execution environments`),
+    '/execution_environments/add': i18n._(t`Create Execution environments`),
+  });
+
+  const buildBreadcrumbConfig = useCallback(
+    executionEnvironments => {
+      if (!executionEnvironments) {
+        return;
+      }
+      setBreadcrumbConfig({
+        '/execution_environments': i18n._(t`Execution environments`),
+        '/execution_environments/add': i18n._(t`Create Execution environments`),
+        [`/execution_environments/${executionEnvironments.id}`]: `${executionEnvironments.name}`,
+        [`/execution_environments/${executionEnvironments.id}/edit`]: i18n._(
+          t`Edit details`
+        ),
+        [`/execution_environments/${executionEnvironments.id}/details`]: i18n._(
+          t`Details`
+        ),
+      });
+    },
+    [i18n]
+  );
+  return (
+    <>
+      <Breadcrumbs breadcrumbConfig={breadcrumbConfig} />
+      <Switch>
+        <Route path="/execution_environments/add">
+          <ExecutionEnvironmentAdd />
+        </Route>
+        <Route path="/execution_environments/:id">
+          <ExecutionEnvironment setBreadcrumb={buildBreadcrumbConfig} />
+        </Route>
+        <Route path="/execution_environments">
+          <ExecutionEnvironmentList />
+        </Route>
+      </Switch>
+    </>
+  );
+}
+export default withI18n()(ExecutionEnvironments);
