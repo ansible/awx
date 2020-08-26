10a80f222f5dd885cb6d73685b263abb2e8a3567
chris93111 <christopheferreira@ymail.com>
2020-08-26 23:29:43 +0200

    Update tower_inventory_refresh.py
    add organization to option module

--- awx_collection/plugins/modules/tower_inventory_refresh.py
+++ awx_collection/plugins/modules/tower_inventory_refresh.py
@@ -27,6 +27,15 @@ options:
         - The name to use for the inventory.
       required: True
       type: str
+    organization:
+      required: False
+      description:
+        - Organization the job template exists in.
+        - Used to help lookup the object, cannot be modified using this module.
+        - The Organization is inferred from the associated project
+        - If not provided, will lookup by name only, which does not work with duplicates.
+        - Requires Tower Version 3.7.0 or AWX 10.0.0 IS NOT backwards compatible with earlier versions.
+      type: str
 extends_documentation_fragment: awx.awx.auth
 '''
 
@@ -35,6 +44,7 @@ EXAMPLES = '''
 - name: Refresh all source in inventory
   tower_inventory_refresh:
     name: "Foo Inventory"
+    organization: "Default"
     tower_config_file: "~/tower_cli.cfg"
 '''
 
@@ -46,12 +56,26 @@ import json
 def main():
 
     argument_spec = dict(
-        name=dict(required=True)
+        name=dict(required=True),
+        organization=dict()
     )
 
     module = TowerModule(argument_spec=argument_spec)
 
     name = module.params.get('name')
+
+    lookup_data = {
+        'name': name
+    }
+
+    if organization:
+        org_id = module.resolve_name_to_id('organizations', organization)
+        lookup_data['organization'] = org_id
+
+    inventory = module.get_one('inventories', **{'data': lookup_data})
+
+    if inventory == None:
+         module.fail_json(msg="Unable to find inventory by name {0}".format(name))
 
     inventory_id = module.resolve_name_to_id('inventories', name)
 
