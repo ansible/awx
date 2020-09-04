2f1a9a28eafacc5aaaeb0a8816b8b1772e23dd04
Alan Rominger <arominge@redhat.com>
2020-09-04 12:22:03 -0400

    streamline credential test

--- awx_collection/test/awx/test_credential.py
+++ awx_collection/test/awx/test_credential.py
@@ -157,30 +157,22 @@ def test_make_use_of_custom_credential_type(run_module, organization, admin_user
 @pytest.mark.parametrize('update_secrets', [True, False])
 def test_secret_field_write_twice(run_module, organization, admin_user, cred_type, update_secrets):
     val1 = '7rEZK38DJl58A7RxA6EC7lLvUHbBQ1'
-    result = run_module('tower_credential', dict(
-        name='Galaxy Token for Steve',
-        organization=organization.name,
-        credential_type=cred_type.name,
-        inputs={'token': val1}
-    ), admin_user)
-    assert not result.get('failed', False), result.get('msg', result)
-
-    Credential.objects.get(id=result['id']).inputs['token'] == val1
-
     val2 = '7rEZ238DJl5837rxA6xxxlLvUHbBQ1'
+    for val in (val1, val2):
+        result = run_module('tower_credential', dict(
+            name='Galaxy Token for Steve',
+            organization=organization.name,
+            credential_type=cred_type.name,
+            inputs={'token': val},
+            update_secrets=update_secrets
+        ), admin_user)
+        assert not result.get('failed', False), result.get('msg', result)
 
-    result = run_module('tower_credential', dict(
-        name='Galaxy Token for Steve',
-        organization=organization.name,
-        credential_type=cred_type.name,
-        inputs={'token': val2},
-        update_secrets=update_secrets
-    ), admin_user)
-    assert not result.get('failed', False), result.get('msg', result)
+        if update_secrets:
+            assert Credential.objects.get(id=result['id']).get_input('token') == val
 
-    Credential.objects.get(id=result['id']).inputs['token'] == val2
-    print(result)
     if update_secrets:
         assert result.get('changed'), result
     else:
         assert result.get('changed') is False, result
+        assert Credential.objects.get(id=result['id']).get_input('token') == val1
