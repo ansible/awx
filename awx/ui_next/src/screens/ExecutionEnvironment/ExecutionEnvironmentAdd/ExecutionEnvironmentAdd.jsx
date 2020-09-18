e35b2ea9699cfd3e07f255550157975362b82a5b
Kersom <9053044+nixocio@users.noreply.github.com>
2020-09-18 08:40:29 -0400

    Add feature to Add/Edit Execution Environments (#8165)
    * Add feature to Add/Edit Execution Environments
    
    Add feature to Add/Edit Execution Environments.
    
    Also, add key for `ExecutionEnvironmentsList`.
    
    See: https://github.com/ansible/awx/issues/7887
    
    * Update registry credential label

--- awx/ui_next/src/screens/ExecutionEnvironment/ExecutionEnvironmentAdd/ExecutionEnvironmentAdd.jsx
+++ awx/ui_next/src/screens/ExecutionEnvironment/ExecutionEnvironmentAdd/ExecutionEnvironmentAdd.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, PageSection } from '@patternfly/react-core';
+import { useHistory } from 'react-router-dom';
+
+import ExecutionEnvironmentForm from '../shared/ExecutionEnvironmentForm';
+import { CardBody } from '../../../components/Card';
+import { ExecutionEnvironmentsAPI } from '../../../api';
 
 function ExecutionEnvironmentAdd() {
+  const history = useHistory();
+  const [submitError, setSubmitError] = useState(null);
+
+  const handleSubmit = async values => {
+    try {
+      const { data: response } = await ExecutionEnvironmentsAPI.create({
+        ...values,
+        credential: values?.credential?.id,
+      });
+      history.push(`/execution_environments/${response.id}/details`);
+    } catch (error) {
+      setSubmitError(error);
+    }
+  };
+
+  const handleCancel = () => {
+    history.push(`/execution_environments`);
+  };
   return (
     <PageSection>
       <Card>
-        <div>Add Execution Environments</div>
+        <CardBody>
+          <ExecutionEnvironmentForm
+            onSubmit={handleSubmit}
+            submitError={submitError}
+            onCancel={handleCancel}
+          />
+        </CardBody>
       </Card>
     </PageSection>
   );
