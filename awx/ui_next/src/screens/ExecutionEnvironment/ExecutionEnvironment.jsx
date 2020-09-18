02f3ed3910e47db657a0785fdf9bf0822eec4f2d
Kersom <9053044+nixocio@users.noreply.github.com>
2020-09-18 08:40:29 -0400

    Add feature to Add/Edit Execution Environments (#8165)
    * Add feature to Add/Edit Execution Environments
    
    Add feature to Add/Edit Execution Environments.
    
    Also, add key for `ExecutionEnvironmentsList`.
    
    See: https://github.com/ansible/awx/issues/7887
    
    * Update registry credential label

--- awx/ui_next/src/screens/ExecutionEnvironment/ExecutionEnvironment.jsx
+++ awx/ui_next/src/screens/ExecutionEnvironment/ExecutionEnvironment.jsx
@@ -1,25 +1,124 @@
-import React from 'react';
-import { Route, Redirect, Switch } from 'react-router-dom';
+import React, { useEffect, useCallback } from 'react';
+import {
+  Link,
+  Redirect,
+  Route,
+  Switch,
+  useLocation,
+  useParams,
+} from 'react-router-dom';
+import { withI18n } from '@lingui/react';
+import { t } from '@lingui/macro';
+import { Card, PageSection } from '@patternfly/react-core';
+import { CaretLeftIcon } from '@patternfly/react-icons';
+
+import useRequest from '../../util/useRequest';
+import { ExecutionEnvironmentsAPI } from '../../api';
+import RoutedTabs from '../../components/RoutedTabs';
+import ContentError from '../../components/ContentError';
+import ContentLoading from '../../components/ContentLoading';
 
 import ExecutionEnvironmentDetails from './ExecutionEnvironmentDetails';
 import ExecutionEnvironmentEdit from './ExecutionEnvironmentEdit';
 
-function ExecutionEnvironment() {
+function ExecutionEnvironment({ i18n, setBreadcrumb }) {
+  const { id } = useParams();
+  const { pathname } = useLocation();
+
+  const {
+    isLoading,
+    error: contentError,
+    request: fetchExecutionEnvironments,
+    result: executionEnvironment,
+  } = useRequest(
+    useCallback(async () => {
+      const { data } = await ExecutionEnvironmentsAPI.readDetail(id);
+      return data;
+    }, [id]),
+    null
+  );
+
+  useEffect(() => {
+    fetchExecutionEnvironments();
+  }, [fetchExecutionEnvironments, pathname]);
+
+  useEffect(() => {
+    if (executionEnvironment) {
+      setBreadcrumb(executionEnvironment);
+    }
+  }, [executionEnvironment, setBreadcrumb]);
+
+  const tabsArray = [
+    {
+      name: (
+        <>
+          <CaretLeftIcon />
+          {i18n._(t`Back to execution environments`)}
+        </>
+      ),
+      link: '/execution_environments',
+      id: 99,
+    },
+    {
+      name: i18n._(t`Details`),
+      link: `/execution_environments/${id}/details`,
+      id: 0,
+    },
+  ];
+
+  if (!isLoading && contentError) {
+    return (
+      <PageSection>
+        <Card>
+          <ContentError error={contentError}>
+            {contentError.response?.status === 404 && (
+              <span>
+                {i18n._(t`Execution environment not found.`)}{' '}
+                <Link to="/execution_environments">
+                  {i18n._(t`View all execution environments`)}
+                </Link>
+              </span>
+            )}
+          </ContentError>
+        </Card>
+      </PageSection>
+    );
+  }
+
+  let cardHeader = <RoutedTabs tabsArray={tabsArray} />;
+  if (pathname.endsWith('edit')) {
+    cardHeader = null;
+  }
+
   return (
-    <Switch>
-      <Redirect
-        from="/execution_environments/:id"
-        to="/execution_environments/:id/details"
-        exact
-      />
-      <Route path="/execution_environments/:id/edit">
-        <ExecutionEnvironmentEdit />
-      </Route>
-      <Route path="/execution_environments/:id/details">
-        <ExecutionEnvironmentDetails />
-      </Route>
-    </Switch>
+    <PageSection>
+      <Card>
+        {cardHeader}
+        {isLoading && <ContentLoading />}
+        {!isLoading && executionEnvironment && (
+          <Switch>
+            <Redirect
+              from="/execution_environments/:id"
+              to="/execution_environments/:id/details"
+              exact
+            />
+            {executionEnvironment && (
+              <>
+                <Route path="/execution_environments/:id/edit">
+                  <ExecutionEnvironmentEdit
+                    executionEnvironment={executionEnvironment}
+                  />
+                </Route>
+                <Route path="/execution_environments/:id/details">
+                  <ExecutionEnvironmentDetails />
+                </Route>
+              </>
+            )}
+          </Switch>
+        )}
+      </Card>
+    </PageSection>
   );
 }
 
-export default ExecutionEnvironment;
+export default withI18n()(ExecutionEnvironment);
