d0d5eabb3285994e9068668590656034362c815b
Kersom <9053044+nixocio@users.noreply.github.com>
2020-09-18 09:41:09 -0400

    Add details page for Execution Environments (#8172)
    * Add feature to Add/Edit Execution Environments
    
    Add feature to Add/Edit Execution Environments.
    
    Also, add key for `ExecutionEnvironmentsList`.
    
    See: https://github.com/ansible/awx/issues/7887
    
    * Add details page for execution environments
    
    Add details page for execution environments
    
    See: https://github.com/ansible/awx/issues/8171

--- awx/ui_next/src/screens/ExecutionEnvironment/ExecutionEnvironmentDetails/ExecutionEnvironmentDetails.jsx
+++ awx/ui_next/src/screens/ExecutionEnvironment/ExecutionEnvironmentDetails/ExecutionEnvironmentDetails.jsx
@@ -1,14 +1,96 @@
-import React from 'react';
-import { Card, PageSection } from '@patternfly/react-core';
+import React, { useCallback } from 'react';
+import { withI18n } from '@lingui/react';
+import { t } from '@lingui/macro';
+import { Link, useHistory } from 'react-router-dom';
+import { Button, Label } from '@patternfly/react-core';
 
-function ExecutionEnvironmentDetails() {
+import AlertModal from '../../../components/AlertModal';
+import { CardBody, CardActionsRow } from '../../../components/Card';
+import DeleteButton from '../../../components/DeleteButton';
+import {
+  Detail,
+  DetailList,
+  UserDateDetail,
+} from '../../../components/DetailList';
+import useRequest, { useDismissableError } from '../../../util/useRequest';
+import { ExecutionEnvironmentsAPI } from '../../../api';
+
+function ExecutionEnvironmentDetails({ executionEnvironment, i18n }) {
+  const history = useHistory();
+  const { id, image, description } = executionEnvironment;
+
+  const {
+    request: deleteExecutionEnvironment,
+    isLoading,
+    error: deleteError,
+  } = useRequest(
+    useCallback(async () => {
+      await ExecutionEnvironmentsAPI.destroy(id);
+      history.push(`/execution_environments`);
+    }, [id, history])
+  );
+
+  const { error, dismissError } = useDismissableError(deleteError);
+
   return (
-    <PageSection>
-      <Card>
-        <div>Execution environments details</div>
-      </Card>
-    </PageSection>
+    <CardBody>
+      <DetailList>
+        <Detail
+          label={i18n._(t`Image`)}
+          value={image}
+          dataCy="execution-environment-detail-image"
+        />
+        <Detail label={i18n._(t`Description`)} value={description} />
+        {executionEnvironment.summary_fields.credential && (
+          <Detail
+            label={i18n._(t`Credential`)}
+            value={
+              <Label variant="outline" color="blue">
+                {executionEnvironment.summary_fields.credential.name}
+              </Label>
+            }
+            dataCy="execution-environment-credential"
+          />
+        )}
+        <UserDateDetail
+          label={i18n._(t`Created`)}
+          date={executionEnvironment.created}
+          user={executionEnvironment.summary_fields.created_by}
+        />
+        <UserDateDetail
+          label={i18n._(t`Last Modified`)}
+          date={executionEnvironment.modified}
+          user={executionEnvironment.summary_fields.modified_by}
+        />
+      </DetailList>
+      <CardActionsRow>
+        <Button
+          aria-label={i18n._(t`edit`)}
+          component={Link}
+          to={`/execution_environments/${id}/edit`}
+        >
+          {i18n._(t`Edit`)}
+        </Button>
+        <DeleteButton
+          name={image}
+          modalTitle={i18n._(t`Delete Execution Environment`)}
+          onConfirm={deleteExecutionEnvironment}
+          isDisabled={isLoading}
+        >
+          {i18n._(t`Delete`)}
+        </DeleteButton>
+      </CardActionsRow>
+
+      {error && (
+        <AlertModal
+          isOpen={error}
+          onClose={dismissError}
+          title={i18n._(t`Error`)}
+          variant="error"
+        />
+      )}
+    </CardBody>
   );
 }
 
-export default ExecutionEnvironmentDetails;
+export default withI18n()(ExecutionEnvironmentDetails);
