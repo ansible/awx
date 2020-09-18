9aac459c0571031d06d0b2a7ef9d170c649054fc
Kersom <9053044+nixocio@users.noreply.github.com>
2020-09-18 08:40:29 -0400

    Add feature to Add/Edit Execution Environments (#8165)
    * Add feature to Add/Edit Execution Environments
    
    Add feature to Add/Edit Execution Environments.
    
    Also, add key for `ExecutionEnvironmentsList`.
    
    See: https://github.com/ansible/awx/issues/7887
    
    * Update registry credential label

---
+++ awx/ui_next/src/screens/ExecutionEnvironment/ExecutionEnvironmentAdd/ExecutionEnvironmentAdd.test.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { createMemoryHistory } from 'history';
+
+import { mountWithContexts } from '../../../../testUtils/enzymeHelpers';
+import { ExecutionEnvironmentsAPI } from '../../../api';
+import ExecutionEnvironmentAdd from './ExecutionEnvironmentAdd';
+
+jest.mock('../../../api');
+
+const executionEnvironmentData = {
+  credential: 4,
+  description: 'A simple EE',
+  image: 'https://registry.com/image/container',
+};
+
+ExecutionEnvironmentsAPI.create.mockResolvedValue({
+  data: {
+    id: 42,
+  },
+});
+
+describe('<ExecutionEnvironmentAdd/>', () => {
+  let wrapper;
+  let history;
+
+  beforeEach(async () => {
+    history = createMemoryHistory({
+      initialEntries: ['/execution_environments'],
+    });
+    await act(async () => {
+      wrapper = mountWithContexts(<ExecutionEnvironmentAdd />, {
+        context: { router: { history } },
+      });
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    wrapper.unmount();
+  });
+
+  test('handleSubmit should call the api and redirect to details page', async () => {
+    await act(async () => {
+      wrapper.find('ExecutionEnvironmentForm').prop('onSubmit')({
+        executionEnvironmentData,
+      });
+    });
+    wrapper.update();
+    expect(ExecutionEnvironmentsAPI.create).toHaveBeenCalledWith({
+      executionEnvironmentData,
+    });
+    expect(history.location.pathname).toBe(
+      '/execution_environments/42/details'
+    );
+  });
+
+  test('handleCancel should return the user back to the execution environments list', async () => {
+    wrapper.find('Button[aria-label="Cancel"]').simulate('click');
+    expect(history.location.pathname).toEqual('/execution_environments');
+  });
+
+  test('failed form submission should show an error message', async () => {
+    const error = {
+      response: {
+        data: { detail: 'An error occurred' },
+      },
+    };
+    ExecutionEnvironmentsAPI.create.mockImplementationOnce(() =>
+      Promise.reject(error)
+    );
+    await act(async () => {
+      wrapper.find('ExecutionEnvironmentForm').invoke('onSubmit')(
+        executionEnvironmentData
+      );
+    });
+    wrapper.update();
+    expect(wrapper.find('FormSubmitError').length).toBe(1);
+  });
+});
