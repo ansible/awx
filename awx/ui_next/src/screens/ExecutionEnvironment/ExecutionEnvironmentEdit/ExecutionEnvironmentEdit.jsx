4f19e45cf022dea135f1a00de43d468b065757ef
Kersom <9053044+nixocio@users.noreply.github.com>
2020-09-18 08:40:29 -0400

    Add feature to Add/Edit Execution Environments (#8165)
    * Add feature to Add/Edit Execution Environments
    
    Add feature to Add/Edit Execution Environments.
    
    Also, add key for `ExecutionEnvironmentsList`.
    
    See: https://github.com/ansible/awx/issues/7887
    
    * Update registry credential label

--- awx/ui_next/src/screens/ExecutionEnvironment/ExecutionEnvironmentEdit/ExecutionEnvironmentEdit.jsx
+++ awx/ui_next/src/screens/ExecutionEnvironment/ExecutionEnvironmentEdit/ExecutionEnvironmentEdit.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
-import { Card, PageSection } from '@patternfly/react-core';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-function ExecutionEnvironmentEdit() {
+import { CardBody } from '../../../components/Card';
+import { ExecutionEnvironmentsAPI } from '../../../api';
+import ExecutionEnvironmentForm from '../shared/ExecutionEnvironmentForm';
+
+function ExecutionEnvironmentEdit({ executionEnvironment }) {
+  const history = useHistory();
+  const [submitError, setSubmitError] = useState(null);
+  const detailsUrl = `/execution_environments/${executionEnvironment.id}/details`;
+
+  const handleSubmit = async values => {
+    try {
+      await ExecutionEnvironmentsAPI.update(executionEnvironment.id, {
+        ...values,
+        credential: values.credential ? values.credential.id : null,
+      });
+      history.push(detailsUrl);
+    } catch (error) {
+      setSubmitError(error);
+    }
+  };
+
+  const handleCancel = () => {
+    history.push(detailsUrl);
+  };
   return (
-    <PageSection>
-      <Card>
-        <div>Edit Execution environments</div>
-      </Card>
-    </PageSection>
+    <CardBody>
+      <ExecutionEnvironmentForm
+        executionEnvironment={executionEnvironment}
+        onSubmit={handleSubmit}
+        submitError={submitError}
+        onCancel={handleCancel}
+      />
+    </CardBody>
   );
 }
 
---
+++ awx/ui_next/src/screens/ExecutionEnvironment/ExecutionEnvironmentEdit/ExecutionEnvironmentEdit.test.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { createMemoryHistory } from 'history';
+
+import { mountWithContexts } from '../../../../testUtils/enzymeHelpers';
+import { ExecutionEnvironmentsAPI } from '../../../api';
+
+import ExecutionEnvironmentEdit from './ExecutionEnvironmentEdit';
+
+jest.mock('../../../api');
+
+const executionEnvironmentData = {
+  id: 42,
+  credential: { id: 4 },
+  description: 'A simple EE',
+  image: 'https://registry.com/image/container',
+};
+
+const updateExecutionEnvironmentData = {
+  image: 'https://registry.com/image/container2',
+  description: 'Updated new description',
+};
+
+describe('<ExecutionEnvironmentEdit/>', () => {
+  let wrapper;
+  let history;
+
+  beforeAll(async () => {
+    history = createMemoryHistory();
+    await act(async () => {
+      wrapper = mountWithContexts(
+        <ExecutionEnvironmentEdit
+          executionEnvironment={executionEnvironmentData}
+        />,
+        {
+          context: { router: { history } },
+        }
+      );
+    });
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+    wrapper.unmount();
+  });
+
+  test('handleSubmit should call the api and redirect to details page', async () => {
+    await act(async () => {
+      wrapper.find('ExecutionEnvironmentForm').invoke('onSubmit')(
+        updateExecutionEnvironmentData
+      );
+      wrapper.update();
+      expect(ExecutionEnvironmentsAPI.update).toHaveBeenCalledWith(42, {
+        ...updateExecutionEnvironmentData,
+        credential: null,
+      });
+    });
+
+    expect(history.location.pathname).toEqual(
+      '/execution_environments/42/details'
+    );
+  });
+
+  test('should navigate to execution environments details when cancel is clicked', async () => {
+    await act(async () => {
+      wrapper.find('button[aria-label="Cancel"]').prop('onClick')();
+    });
+    expect(history.location.pathname).toEqual(
+      '/execution_environments/42/details'
+    );
+  });
+
+  test('should navigate to execution environments detail after successful submission', async () => {
+    await act(async () => {
+      wrapper.find('ExecutionEnvironmentForm').invoke('onSubmit')({
+        updateExecutionEnvironmentData,
+      });
+    });
+    wrapper.update();
+    expect(wrapper.find('FormSubmitError').length).toBe(0);
+    expect(history.location.pathname).toEqual(
+      '/execution_environments/42/details'
+    );
+  });
+
+  test('failed form submission should show an error message', async () => {
+    const error = {
+      response: {
+        data: { detail: 'An error occurred' },
+      },
+    };
+    ExecutionEnvironmentsAPI.update.mockImplementationOnce(() =>
+      Promise.reject(error)
+    );
+    await act(async () => {
+      wrapper.find('ExecutionEnvironmentForm').invoke('onSubmit')(
+        updateExecutionEnvironmentData
+      );
+    });
+    wrapper.update();
+    expect(wrapper.find('FormSubmitError').length).toBe(1);
+  });
+});
