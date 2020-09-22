1c8f55f9a96fc699deb15f5ac4b9814b9b35ba08
Jeff Bradberry <jeff.bradberry@gmail.com>
2020-09-22 16:40:32 -0400

    Expose the user capabilities dict for EEs (#8208)

--- awx/api/serializers.py
+++ awx/api/serializers.py
@@ -1361,6 +1361,8 @@ class ProjectOptionsSerializer(BaseSerializer):
 
 
 class ExecutionEnvironmentSerializer(BaseSerializer):
+    show_capabilities = ['edit', 'delete']
+
     class Meta:
         model = ExecutionEnvironment
         fields = ('*', '-name', 'organization', 'image', 'managed_by_tower', 'credential')
