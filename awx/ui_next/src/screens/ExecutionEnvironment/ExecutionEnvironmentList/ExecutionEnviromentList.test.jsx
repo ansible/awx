680186cb090509e420b5644ccef28a8a852170b9
Kersom <9053044+nixocio@users.noreply.github.com>
2020-09-23 12:44:08 -0400

    Update usage of summary_fields for execution environments (#8217)
    Update usage of summary_fields for execution environments. Also, update
    unit-tests to cover this change.
    
    See: https://github.com/ansible/awx/issues/8216

--- awx/ui_next/src/screens/ExecutionEnvironment/ExecutionEnvironmentList/ExecutionEnviromentList.test.jsx
+++ awx/ui_next/src/screens/ExecutionEnvironment/ExecutionEnvironmentList/ExecutionEnviromentList.test.jsx
@@ -20,6 +20,7 @@ const executionEnvironments = {
         organization: null,
         credential: null,
         url: '/api/v2/execution_environments/1/',
+        summary_fields: { user_capabilities: { edit: true, delete: true } },
       },
       {
         id: 2,
@@ -27,6 +28,7 @@ const executionEnvironments = {
         organization: null,
         credential: null,
         url: '/api/v2/execution_environments/2/',
+        summary_fields: { user_capabilities: { edit: false, delete: true } },
       },
     ],
     count: 2,
@@ -65,6 +67,81 @@ describe('<ExecutionEnvironmentList/>', () => {
     expect(wrapper.find('ExecutionEnvironmentListItem').length).toBe(2);
     expect(ExecutionEnvironmentsAPI.read).toBeCalled();
     expect(ExecutionEnvironmentsAPI.readOptions).toBeCalled();
+  });
+
+  test('should delete item successfully', async () => {
+    ExecutionEnvironmentsAPI.read.mockResolvedValue(executionEnvironments);
+    ExecutionEnvironmentsAPI.readOptions.mockResolvedValue(options);
+
+    await act(async () => {
+      wrapper = mountWithContexts(<ExecutionEnvironmentList />);
+    });
+    await waitForElement(
+      wrapper,
+      'ExecutionEnvironmentList',
+      el => el.length > 0
+    );
+
+    wrapper
+      .find('input#select-execution-environment-1')
+      .simulate('change', executionEnvironments.data.results[0]);
+    wrapper.update();
+
+    expect(
+      wrapper.find('input#select-execution-environment-1').prop('checked')
+    ).toBe(true);
+
+    await act(async () => {
+      wrapper.find('Button[aria-label="Delete"]').prop('onClick')();
+    });
+    wrapper.update();
+
+    await act(async () => {
+      wrapper.find('Button[aria-label="confirm delete"]').prop('onClick')();
+    });
+
+    expect(ExecutionEnvironmentsAPI.destroy).toBeCalledWith(
+      executionEnvironments.data.results[0].id
+    );
+  });
+
+  test('should render deletion error modal', async () => {
+    ExecutionEnvironmentsAPI.destroy.mockRejectedValue(
+      new Error({
+        response: {
+          config: {
+            method: 'DELETE',
+            url: '/api/v2/execution_environments',
+          },
+          data: 'An error occurred',
+        },
+      })
+    );
+    ExecutionEnvironmentsAPI.read.mockResolvedValue(executionEnvironments);
+    ExecutionEnvironmentsAPI.readOptions.mockResolvedValue(options);
+    await act(async () => {
+      wrapper = mountWithContexts(<ExecutionEnvironmentList />);
+    });
+    waitForElement(wrapper, 'ExecutionEnvironmentList', el => el.length > 0);
+
+    wrapper
+      .find('input#select-execution-environment-1')
+      .simulate('change', 'a');
+    wrapper.update();
+    expect(
+      wrapper.find('input#select-execution-environment-1').prop('checked')
+    ).toBe(true);
+
+    await act(async () =>
+      wrapper.find('Button[aria-label="Delete"]').prop('onClick')()
+    );
+    wrapper.update();
+
+    await act(async () =>
+      wrapper.find('Button[aria-label="confirm delete"]').prop('onClick')()
+    );
+    wrapper.update();
+    expect(wrapper.find('ErrorDetail').length).toBe(1);
   });
 
   test('should thrown content error', async () => {
