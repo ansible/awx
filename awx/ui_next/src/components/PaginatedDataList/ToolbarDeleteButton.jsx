a5c826225bf349174b72cdfd7d4f27ebdf882079
Kersom <9053044+nixocio@users.noreply.github.com>
2020-09-23 12:44:08 -0400

    Update usage of summary_fields for execution environments (#8217)
    Update usage of summary_fields for execution environments. Also, update
    unit-tests to cover this change.
    
    See: https://github.com/ansible/awx/issues/8216

--- awx/ui_next/src/components/PaginatedDataList/ToolbarDeleteButton.jsx
+++ awx/ui_next/src/components/PaginatedDataList/ToolbarDeleteButton.jsx
@@ -20,11 +20,11 @@ const WarningMessage = styled(Alert)`
   margin-top: 10px;
 `;
 
-const requireNameOrUsername = props => {
-  const { name, username } = props;
-  if (!name && !username) {
+const requiredField = props => {
+  const { name, username, image } = props;
+  if (!name && !username && !image) {
     return new Error(
-      `One of 'name' or 'username' is required by ItemToDelete component.`
+      `One of 'name', 'username' or 'image' is required by ItemToDelete component.`
     );
   }
   if (name) {
@@ -47,13 +47,24 @@ const requireNameOrUsername = props => {
       'ItemToDelete'
     );
   }
+  if (image) {
+    checkPropTypes(
+      {
+        image: string,
+      },
+      { image: props.image },
+      'prop',
+      'ItemToDelete'
+    );
+  }
   return null;
 };
 
 const ItemToDelete = shape({
   id: number.isRequired,
-  name: requireNameOrUsername,
-  username: requireNameOrUsername,
+  name: requiredField,
+  username: requiredField,
+  image: requiredField,
   summary_fields: shape({
     user_capabilities: shape({
       delete: bool.isRequired,
@@ -62,7 +73,7 @@ const ItemToDelete = shape({
 });
 
 function cannotDelete(item) {
-  return !item.summary_fields?.user_capabilities?.delete;
+  return !item.summary_fields.user_capabilities.delete;
 }
 
 function ToolbarDeleteButton({
@@ -174,7 +185,7 @@ function ToolbarDeleteButton({
           <div>{i18n._(t`This action will delete the following:`)}</div>
           {itemsToDelete.map(item => (
             <span key={item.id}>
-              <strong>{item.name || item.username}</strong>
+              <strong>{item.name || item.username || item.image}</strong>
               <br />
             </span>
           ))}
