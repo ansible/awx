dd016ab0ecf1123f4d0ebe9fafcd7b68d985f4df
nixocio <nixocio@gmail.com>
2020-10-05 16:45:57 -0400

    Add execution environment list to Organizations
    Add execution environment list to Organizations
    
    See: https://github.com/ansible/awx/issues/8210

---
+++ awx/ui_next/src/screens/Organization/OrganizationExecEnvList/OrganizationExecEnvListItem.test.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+
+import { mountWithContexts } from '../../../../testUtils/enzymeHelpers';
+
+import OrganizationExecEnvListItem from './OrganizationExecEnvListItem';
+
+describe('<OrganizationExecEnvListItem/>', () => {
+  let wrapper;
+  const executionEnvironment = {
+    id: 1,
+    image: 'https://registry.com/r/image/manifest',
+    organization: 1,
+    credential: null,
+  };
+
+  test('should mount successfully', async () => {
+    await act(async () => {
+      wrapper = mountWithContexts(
+        <OrganizationExecEnvListItem
+          executionEnvironment={executionEnvironment}
+          detailUrl="execution_environments/1/details"
+        />
+      );
+    });
+    expect(wrapper.find('OrganizationExecEnvListItem').length).toBe(1);
+  });
+
+  test('should render the proper data', async () => {
+    await act(async () => {
+      wrapper = mountWithContexts(
+        <OrganizationExecEnvListItem
+          executionEnvironment={executionEnvironment}
+          detailUrl="execution_environments/1/details"
+        />
+      );
+    });
+    expect(
+      wrapper
+        .find('DataListCell[aria-label="Execution environment image"]')
+        .text()
+    ).toBe(executionEnvironment.image);
+  });
+});
