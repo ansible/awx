a9c01e891fed78ea5ceaf4440a12478691b10bf6
Keith Grant <barkeep@keithjgrant.com>
2020-10-20 11:18:40 -0700

    remove console log
    Co-authored-by: Jake McDermott <yo@jakemcdermott.me>

--- awx/ui_next/src/util/validators.jsx
+++ awx/ui_next/src/util/validators.jsx
@@ -68,7 +68,6 @@ export function noWhiteSpace(i18n) {
 
 export function integer(i18n) {
   return value => {
-    console.log(value);
     const str = String(value);
     if (/[^0-9]/.test(str)) {
       return i18n._(t`This field must be an integer`);
