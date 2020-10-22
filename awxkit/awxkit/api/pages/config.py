784c924d883d72168b9de2d132bc39b5da311339
Christian M. Adams <rooftopcellist@gmail.com>
2020-10-22 14:59:48 -0400

    remove outdated license references

--- awxkit/awxkit/api/pages/config.py
+++ awxkit/awxkit/api/pages/config.py
@@ -12,11 +12,6 @@ class Config(base.Base):
             'instance-id' in self.license_info
 
     @property
-    def is_demo_license(self):
-        return self.license_info.get('demo', False) or \
-            self.license_info.get('key_present', False)
-
-    @property
     def is_valid_license(self):
         return self.license_info.get('valid_key', False) and \
             'license_key' in self.license_info and \
@@ -30,16 +25,6 @@ class Config(base.Base):
     @property
     def is_awx_license(self):
         return self.license_info.get('license_type', None) == 'open'
-
-    @property
-    def is_legacy_license(self):
-        return self.is_valid_license and \
-            self.license_info.get('license_type', None) == 'legacy'
-
-    @property
-    def is_basic_license(self):
-        return self.is_valid_license and \
-            self.license_info.get('license_type', None) == 'basic'
 
     @property
     def is_enterprise_license(self):
