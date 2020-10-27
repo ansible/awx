aae0b290088ba42007d7f3a4673375e05b25e976
Elyézer Rezende <elyezermr@gmail.com>
2020-10-27 14:51:00 -0400

    Separate page object for config/attach endpoint

--- awxkit/awxkit/api/pages/subscriptions.py
+++ awxkit/awxkit/api/pages/subscriptions.py
@@ -7,8 +7,5 @@ class Subscriptions(page.Page):
     def get_possible_licenses(self, **kwargs):
         return self.post(json=kwargs).json
 
-    def attach(self, **kwargs):
-        return self.connection.post(resources.subscriptions_attach, json=kwargs).json
-
 
 page.register_page(resources.subscriptions, Subscriptions)
