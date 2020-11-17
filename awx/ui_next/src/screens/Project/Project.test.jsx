2f47bacb4f99498d07d5ae01f41aac7406141139
Marliana Lara <mlara@redhat.com>
2020-11-17 16:59:49 -0500

    Convert project/* components into functional components

--- awx/ui_next/src/screens/Project/Project.test.jsx
+++ awx/ui_next/src/screens/Project/Project.test.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { act } from 'react-dom/test-utils';
 import { createMemoryHistory } from 'history';
 import { OrganizationsAPI, ProjectsAPI } from '../../api';
 import {
@@ -28,29 +29,34 @@ async function getOrganizations() {
 }
 
 describe('<Project />', () => {
-  test('initially renders succesfully', () => {
+  let wrapper;
+
+  test('initially renders successfully', async () => {
     ProjectsAPI.readDetail.mockResolvedValue({ data: mockDetails });
     OrganizationsAPI.read.mockImplementation(getOrganizations);
-    mountWithContexts(<Project setBreadcrumb={() => {}} me={mockMe} />);
+    await act(async () => {
+      mountWithContexts(<Project setBreadcrumb={() => {}} me={mockMe} />);
+    });
   });
 
-  test('notifications tab shown for admins', async done => {
+  test('notifications tab shown for admins', async () => {
     ProjectsAPI.readDetail.mockResolvedValue({ data: mockDetails });
     OrganizationsAPI.read.mockImplementation(getOrganizations);
 
-    const wrapper = mountWithContexts(
-      <Project setBreadcrumb={() => {}} me={mockMe} />
-    );
+    await act(async () => {
+      wrapper = mountWithContexts(
+        <Project setBreadcrumb={() => {}} me={mockMe} />
+      );
+    });
     const tabs = await waitForElement(
       wrapper,
       '.pf-c-tabs__item',
       el => el.length === 6
     );
     expect(tabs.at(3).text()).toEqual('Notifications');
-    done();
   });
 
-  test('notifications tab hidden with reduced permissions', async done => {
+  test('notifications tab hidden with reduced permissions', async () => {
     ProjectsAPI.readDetail.mockResolvedValue({ data: mockDetails });
     OrganizationsAPI.read.mockResolvedValue({
       count: 0,
@@ -58,20 +64,20 @@ describe('<Project />', () => {
       previous: null,
       data: { results: [] },
     });
-
-    const wrapper = mountWithContexts(
-      <Project setBreadcrumb={() => {}} me={mockMe} />
-    );
+    await act(async () => {
+      wrapper = mountWithContexts(
+        <Project setBreadcrumb={() => {}} me={mockMe} />
+      );
+    });
     const tabs = await waitForElement(
       wrapper,
       '.pf-c-tabs__item',
       el => el.length === 5
     );
     tabs.forEach(tab => expect(tab.text()).not.toEqual('Notifications'));
-    done();
   });
 
-  test('schedules tab shown for scm based projects.', async done => {
+  test('schedules tab shown for scm based projects.', async () => {
     ProjectsAPI.readDetail.mockResolvedValue({ data: mockDetails });
     OrganizationsAPI.read.mockResolvedValue({
       count: 0,
@@ -80,19 +86,21 @@ describe('<Project />', () => {
       data: { results: [] },
     });
 
-    const wrapper = mountWithContexts(
-      <Project setBreadcrumb={() => {}} me={mockMe} />
-    );
+    await act(async () => {
+      wrapper = mountWithContexts(
+        <Project setBreadcrumb={() => {}} me={mockMe} />
+      );
+    });
+
     const tabs = await waitForElement(
       wrapper,
       '.pf-c-tabs__item',
       el => el.length === 5
     );
     expect(tabs.at(4).text()).toEqual('Schedules');
-    done();
   });
 
-  test('schedules tab hidden for manual projects.', async done => {
+  test('schedules tab hidden for manual projects.', async () => {
     const manualDetails = Object.assign(mockDetails, { scm_type: '' });
     ProjectsAPI.readDetail.mockResolvedValue({ data: manualDetails });
     OrganizationsAPI.read.mockResolvedValue({
@@ -102,40 +110,44 @@ describe('<Project />', () => {
       data: { results: [] },
     });
 
-    const wrapper = mountWithContexts(
-      <Project setBreadcrumb={() => {}} me={mockMe} />
-    );
+    await act(async () => {
+      wrapper = mountWithContexts(
+        <Project setBreadcrumb={() => {}} me={mockMe} />
+      );
+    });
+
     const tabs = await waitForElement(
       wrapper,
       '.pf-c-tabs__item',
       el => el.length === 4
     );
     tabs.forEach(tab => expect(tab.text()).not.toEqual('Schedules'));
-    done();
   });
 
   test('should show content error when user attempts to navigate to erroneous route', async () => {
     const history = createMemoryHistory({
       initialEntries: ['/projects/1/foobar'],
     });
-    const wrapper = mountWithContexts(
-      <Project setBreadcrumb={() => {}} me={mockMe} />,
-      {
-        context: {
-          router: {
-            history,
-            route: {
-              location: history.location,
-              match: {
-                params: { id: 1 },
-                url: '/projects/1/foobar',
-                path: '/project/1/foobar',
+    await act(async () => {
+      wrapper = mountWithContexts(
+        <Project setBreadcrumb={() => {}} me={mockMe} />,
+        {
+          context: {
+            router: {
+              history,
+              route: {
+                location: history.location,
+                match: {
+                  params: { id: 1 },
+                  url: '/projects/1/foobar',
+                  path: '/project/1/foobar',
+                },
               },
             },
           },
-        },
-      }
-    );
+        }
+      );
+    });
     await waitForElement(wrapper, 'ContentError', el => el.length === 1);
   });
 });
