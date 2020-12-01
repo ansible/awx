db9fbf149312cb18c3c96e634f078919f60f7406
Alan Rominger <arominge@redhat.com>
2020-12-01 15:53:55 -0500

    Add log for the case that hg projects were deleted

--- awx/main/migrations/_hg_removal.py
+++ awx/main/migrations/_hg_removal.py
@@ -1,5 +1,7 @@
 import logging
 
+from awx.main.utils.common import set_current_apps
+
 logger = logging.getLogger('awx.main.migrations')
 
 
@@ -9,4 +11,9 @@ def delete_hg_scm(apps, schema_editor):
     ProjectUpdate = apps.get_model('main', 'ProjectUpdate')
 
     ProjectUpdate.objects.filter(project__scm_type='hg').delete()
-    Project.objects.filter(scm_type='hg').delete()
+    deleted_ct, deletion_summary = Project.objects.filter(scm_type='hg').delete()
+
+    if deleted_ct:
+        logger.warn('Removed {} mercurial projects, deprecation period ended'.format(
+            deletion_summary.get('main.Project', '')
+        ))
