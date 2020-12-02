99f382582690350f8336133016f83db3ef4afe30
Alan Rominger <arominge@redhat.com>
2020-12-02 08:57:19 -0500

    Change to manual instead of deleting hg projects

--- awx/main/migrations/_hg_removal.py
+++ awx/main/migrations/_hg_removal.py
@@ -10,10 +10,10 @@ def delete_hg_scm(apps, schema_editor):
     Project = apps.get_model('main', 'Project')
     ProjectUpdate = apps.get_model('main', 'ProjectUpdate')
 
-    ProjectUpdate.objects.filter(project__scm_type='hg').delete()
-    deleted_ct, deletion_summary = Project.objects.filter(scm_type='hg').delete()
+    ProjectUpdate.objects.filter(project__scm_type='hg').update(scm_type='')
+    update_ct = Project.objects.filter(scm_type='hg').update(scm_type='')
 
-    if deleted_ct:
-        logger.warn('Removed {} mercurial projects, deprecation period ended'.format(
-            deletion_summary.get('main.Project', '')
+    if update_ct:
+        logger.warn('Changed {} mercurial projects to manual, deprecation period ended'.format(
+            update_ct
         ))
