876cdbc8a375aa22080bbf2dc0b3d4df03cfcc33
Shane McDonald <me@shanemcd.com>
2021-02-08 14:29:32 -0500

    Remove Ansible from control plane
    Execution Environments or bust!

--- tools/docker-compose/bootstrap_development.sh
+++ tools/docker-compose/bootstrap_development.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set +x
 
-# Wait for the databases to come up
-ansible -i "127.0.0.1," -c local -v -m wait_for -a "host=postgres port=5432" all
-ansible -i "127.0.0.1," -c local -v -m wait_for -a "path=/var/run/redis/redis.sock" all
-
-# In case AWX in the container wants to connect to itself, use "docker exec" to attach to the container otherwise
-# TODO: FIX
-#/etc/init.d/ssh start
-
-ansible -i "127.0.0.1," -c local -v -m postgresql_user --become-user postgres -a "name=awx-dev password=AWXsome1 encrypted=yes login_user=postgres login_password=postgrespass login_host=postgres" all
-ansible -i "127.0.0.1," -c local -v -m postgresql_db --become-user postgres -a "name=awx-dev owner=awx-dev login_user=postgres login_password=postgrespass login_host=postgres" all
-
 # Move to the source directory so we can bootstrap
 if [ -f "/awx_devel/manage.py" ]; then
     cd /awx_devel
