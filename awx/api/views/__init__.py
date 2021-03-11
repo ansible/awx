8501a45531134f252fa79ce679687df707c79a88
Jim Ladd <jladd@redhat.com>
2021-03-11 14:20:35 -0800

    lint fixes

--- awx/api/views/__init__.py
+++ awx/api/views/__init__.py
@@ -2,7 +2,6 @@
 # All Rights Reserved.
 
 # Python
-import datetime
 import dateutil
 import functools
 import html
@@ -889,6 +888,7 @@ class ProjectUpdateEventsList(SubListAPIView):
             job_created=self.get_parent_object().created_or_epoch
         )
 
+
 class SystemJobEventsList(SubListAPIView):
 
     model = models.SystemJobEvent
@@ -906,6 +906,7 @@ class SystemJobEventsList(SubListAPIView):
         return super(SystemJobEventsList, self).get_queryset().filter(
             job_created=self.get_parent_object().created_or_epoch
         )
+
 
 class ProjectUpdateCancel(RetrieveAPIView):
 
