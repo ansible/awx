673db08d109ecf852cc9861e21db7dd0bc9141e6
Jim Ladd <jladd@redhat.com>
2021-03-24 11:58:04 -0700

    Minor rebase fallout
    * fix import
    * remove dropped url

--- awx/api/urls/job_event.py
+++ awx/api/urls/job_event.py
@@ -3,13 +3,11 @@
 
 from django.conf.urls import url
 
-from awx.api.views import JobEventDetail, JobEventChildrenList, JobEventHostsList
-
+from awx.api.views import JobEventDetail, JobEventChildrenList
 
 urls = [
     url(r'^(?P<pk>[0-9]+)/$', JobEventDetail.as_view(), name='job_event_detail'),
     url(r'^(?P<pk>[0-9]+)/children/$', JobEventChildrenList.as_view(), name='job_event_children_list'),
-    url(r'^(?P<pk>[0-9]+)/hosts/$', JobEventHostsList.as_view(), name='job_event_hosts_list'),
 ]
 
 __all__ = ['urls']
