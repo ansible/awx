385c4a16db9ee91042e2cbe7c090988a7661f4c1
Alex Corey <alex.swansboro@gmail.com>
2021-04-06 17:32:29 -0400

    updates the method of loading locales

--- awx/ui_next/src/App.jsx
+++ awx/ui_next/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   useRouteMatch,
   useLocation,
@@ -10,20 +10,13 @@ import {
 import { I18nProvider } from '@lingui/react';
 import { i18n } from '@lingui/core';
 
-import { en, es, fr, nl, zh, ja, zu } from 'make-plural/plurals';
 import AppContainer from './components/AppContainer';
 import Background from './components/Background';
 import NotFound from './screens/NotFound';
 import Login from './screens/Login';
 
-import japanese from './locales/ja/messages';
-import english from './locales/en/messages';
-import zulu from './locales/zu/messages';
-import french from './locales/fr/messages';
-import dutch from './locales/nl/messages';
-import chinese from './locales/zh/messages';
-import spanish from './locales/es/messages';
 import { isAuthenticated } from './util/auth';
+import { locales, dynamicActivate } from './i18nLoader';
 
 import { getLanguageWithoutRegionCode } from './util/language';
 
@@ -80,42 +73,19 @@ const ProtectedRoute = ({ children, ...rest }) =>
   );
 
 function App() {
-  const catalogs = {
-    en: english,
-    ja: japanese,
-    zu: zulu,
-    fr: french,
-    es: spanish,
-    zh: chinese,
-    nl: dutch,
-  };
   let language = getLanguageWithoutRegionCode(navigator);
-  if (!Object.keys(catalogs).includes(language)) {
+  if (!Object.keys(locales).includes(language)) {
     // If there isn't a string catalog available for the browser's
     // preferred language, default to one that has strings.
     language = 'en';
   }
   const { hash, search, pathname } = useLocation();
-
-  i18n.loadLocaleData('en', { plurals: en });
-  i18n.loadLocaleData('es', { plurals: es });
-  i18n.loadLocaleData('fr', { plurals: fr });
-  i18n.loadLocaleData('nl', { plurals: nl });
-  i18n.loadLocaleData('zh', { plurals: zh });
-  i18n.loadLocaleData('ja', { plurals: ja });
-  i18n.loadLocaleData('zu', { plurals: zu });
-  i18n.load({
-    en: english.messages,
-    ja: japanese.messages,
-    zu: zulu.messages,
-    fr: french.messages,
-    nl: dutch.messages,
-    zh: chinese.messages,
-    es: spanish.messages,
-  });
+  useEffect(() => {
+    dynamicActivate(language);
+  }, [language]);
   i18n.activate(language);
   return (
-    <I18nProvider i18n={i18n} catalogs={catalogs}>
+    <I18nProvider i18n={i18n}>
       <Background>
         <Switch>
           <Route exact strict path="/*/">
---
+++ awx/ui_next/src/i18nLoader.js
@@ -0,0 +1,31 @@
+import { i18n } from '@lingui/core';
+import { en, fr, es, nl, ja, zh, zu } from 'make-plural/plurals';
+
+export const locales = {
+  en: 'English',
+  ja: 'Japanese',
+  zu: 'Zulu',
+  fr: 'French',
+  es: 'Spanish',
+  zh: 'Chinese',
+  nl: 'Dutch',
+};
+
+i18n.loadLocaleData({
+  en: { plurals: en },
+  fr: { plurals: fr },
+  es: { plurals: es },
+  nl: { plurals: nl },
+  ja: { plurals: ja },
+  zh: { plurals: zh },
+  zu: { plurals: zu },
+});
+
+/**
+ * We do a dynamic import of just the catalog that we need
+ * @param locale any locale string
+ */
+export async function dynamicActivate(locale) {
+  const { messages } = await import(`./locales/${locale}/messages`);
+  i18n.load(locale, messages);
+}
