0f9f3f58e2ae382d697729530731142b46b28214
Jim Ladd <jladd@redhat.com>
2021-04-20 13:27:04 -0700

    bump migration

--- awx/main/utils/common.py
+++ awx/main/utils/common.py
@@ -213,7 +213,7 @@ def memoize_delete(function_name):
 def get_event_partition_epoch():
     from django.db.migrations.recorder import MigrationRecorder
 
-    return MigrationRecorder.Migration.objects.filter(app='main', name='0136_event_partitions').first().applied
+    return MigrationRecorder.Migration.objects.filter(app='main', name='0137_event_partitions').first().applied
 
 
 @memoize()
