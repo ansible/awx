39ce0ade6d146453b9442f8c843c132ed0ad7f7f
Jake McDermott <yo@jakemcdermott.me>
2021-05-12 15:49:03 -0400

    Update image descriptions, add examples

--- awx/ui_next/src/screens/ExecutionEnvironment/shared/ExecutionEnvironmentForm.jsx
+++ awx/ui_next/src/screens/ExecutionEnvironment/shared/ExecutionEnvironmentForm.jsx
@@ -100,7 +100,22 @@ function ExecutionEnvironmentFormFields({
         type="text"
         validate={required(null)}
         isRequired
-        tooltip={t`The registry location where the container is stored.`}
+        tooltip={
+          <span>
+            {t`The full image location, including the container registry, image name, and version tag.`}
+            <br />
+            <br />
+            {t`Examples:`}
+            <ul css="margin: 10px 0 10px 20px">
+              <li>
+                <code>quay.io/ansible/awx-ee:latest</code>
+              </li>
+              <li>
+                <code>repo/project/image-name:tag</code>
+              </li>
+            </ul>
+          </span>
+        }
       />
       <FormGroup
         fieldId="execution-environment-container-options"
