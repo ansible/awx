4c1b0297e7df4ff75def59e52fa412dffdf90e63
mabashian <mabashia@redhat.com>
2021-06-03 09:48:20 -0400

    Fixes bug where checkbox list item was selecting things twice

--- awx/ui_next/src/components/CheckboxListItem/CheckboxListItem.jsx
+++ awx/ui_next/src/components/CheckboxListItem/CheckboxListItem.jsx
@@ -34,7 +34,6 @@ const CheckboxListItem = ({
         select={{
           rowIndex,
           isSelected,
-          onSelect: isSelected ? onDeselect : onSelect,
           variant: isRadio ? 'radio' : 'checkbox',
         }}
         name={name}
@@ -43,7 +42,7 @@ const CheckboxListItem = ({
 
       {columns?.length > 0 ? (
         columns.map(col => (
-          <Td aria-label={col.name} dataLabel={col.key}>
+          <Td aria-label={col.name} dataLabel={col.key} key={col.key}>
             {item[col.key]}
           </Td>
         ))
