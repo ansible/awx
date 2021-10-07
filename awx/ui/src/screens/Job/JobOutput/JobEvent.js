84a6be67ebc1c9f9d2edf13c4f51a14c2af6daea
Keith J. Grant <barkeep@keithjgrant.com>
2021-10-06 17:06:26 -0700

    JobOutput: fix toggle arrow, ellipsis for parent nodes with multple lines output

--- awx/ui/src/screens/Job/JobOutput/JobEvent.js
+++ awx/ui/src/screens/Job/JobOutput/JobEvent.js
@@ -22,35 +22,46 @@ function JobEvent({
     measure();
   }, [event, isCollapsed, measure]);
 
+  let toggleLineIndex = -1;
+  if (hasChildren) {
+    lineTextHtml.forEach(({ html }, index) => {
+      if (html) {
+        toggleLineIndex = index;
+      }
+    });
+  }
   return !event.stdout ? null : (
     <div style={style} type={event.type}>
-      {lineTextHtml.map(
-        ({ lineNumber, html }) =>
-          lineNumber >= 0 && (
-            <JobEventLine
-              onClick={isClickable ? onJobEventClick : undefined}
-              key={`${event.counter}-${lineNumber}`}
-              isFirst={lineNumber === 0}
-              isClickable={isClickable}
-            >
-              <JobEventLineToggle
-                canToggle={hasChildren && html.length}
-                isCollapsed={isCollapsed}
-                onToggle={onToggleCollapsed}
-              />
-              <JobEventLineNumber>
-                {lineNumber}
-                <JobEventEllipsis isCollapsed={isCollapsed && html.length} />
-              </JobEventLineNumber>
-              <JobEventLineText
-                type="job_event_line_text"
-                dangerouslySetInnerHTML={{
-                  __html: html,
-                }}
-              />
-            </JobEventLine>
-          )
-      )}
+      {lineTextHtml.map(({ lineNumber, html }, index) => {
+        if (lineNumber < 0) {
+          return null;
+        }
+        const canToggle = index === toggleLineIndex;
+        return (
+          <JobEventLine
+            onClick={isClickable ? onJobEventClick : undefined}
+            key={`${event.counter}-${lineNumber}`}
+            isFirst={lineNumber === 0}
+            isClickable={isClickable}
+          >
+            <JobEventLineToggle
+              canToggle={canToggle}
+              isCollapsed={isCollapsed}
+              onToggle={onToggleCollapsed}
+            />
+            <JobEventLineNumber>
+              {lineNumber}
+              <JobEventEllipsis isCollapsed={isCollapsed && canToggle} />
+            </JobEventLineNumber>
+            <JobEventLineText
+              type="job_event_line_text"
+              dangerouslySetInnerHTML={{
+                __html: html,
+              }}
+            />
+          </JobEventLine>
+        );
+      })}
     </div>
   );
 }
