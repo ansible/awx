54f37faf6f1213ed1b0e1337c00d4defd2cb34c4
Alan Rominger <arominge@redhat.com>
2021-10-15 15:03:59 -0400

    Gracefully handle receptorctl RuntimeError in health check

--- awx/main/utils/receptor.py
+++ awx/main/utils/receptor.py
@@ -186,7 +186,7 @@ def worker_info(node_name, work_type='ansible-runner'):
         else:
             error_list.append(details)
 
-    except ReceptorNodeNotFound as exc:
+    except (ReceptorNodeNotFound, RuntimeError) as exc:
         error_list.append(str(exc))
 
     # If we have a connection error, missing keys would be trivial consequence of that
