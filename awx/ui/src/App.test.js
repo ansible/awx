3bcbc42fc27b96522e3c76801b4c71b42c3890c7
Kersom <9053044+nixocio@users.noreply.github.com>
2021-10-27 16:37:54 -0400

    Fix login redirect (#5386)
    Allows the user to visit login page when the login redirect url is set.
    
    Also, redirects to login page once logging out and there is session from
    a SAML available.
    
    See: https://github.com/ansible/awx/issues/11012

--- awx/ui/src/App.test.js
+++ awx/ui/src/App.test.js
@@ -3,7 +3,7 @@ import { act } from 'react-dom/test-utils';
 import { RootAPI } from 'api';
 import * as SessionContext from 'contexts/Session';
 import { mountWithContexts } from '../testUtils/enzymeHelpers';
-import App from './App';
+import App, { ProtectedRoute } from './App';
 
 jest.mock('./api');
 
@@ -20,6 +20,8 @@ describe('<App />', () => {
     const contextValues = {
       setAuthRedirectTo: jest.fn(),
       isSessionExpired: false,
+      isUserBeingLoggedOut: false,
+      loginRedirectOverride: null,
     };
     jest
       .spyOn(SessionContext, 'useSession')
@@ -31,5 +33,37 @@ describe('<App />', () => {
     });
     expect(wrapper.length).toBe(1);
     jest.clearAllMocks();
+  });
+
+  test('redirect to login override', async () => {
+    const { location } = window;
+    delete window.location;
+    window.location = {
+      replace: jest.fn(),
+      href: '/',
+    };
+
+    expect(window.location.replace).not.toHaveBeenCalled();
+
+    const contextValues = {
+      setAuthRedirectTo: jest.fn(),
+      isSessionExpired: false,
+      isUserBeingLoggedOut: false,
+      loginRedirectOverride: '/sso/test',
+    };
+    jest
+      .spyOn(SessionContext, 'useSession')
+      .mockImplementation(() => contextValues);
+
+    await act(async () => {
+      mountWithContexts(
+        <ProtectedRoute>
+          <div>foo</div>
+        </ProtectedRoute>
+      );
+    });
+
+    expect(window.location.replace).toHaveBeenCalled();
+    window.location = location;
   });
 });
