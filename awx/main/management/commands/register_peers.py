4449555abe6d1921d80efe7be7593267c3078593
Jeff Bradberry <jeff.bradberry@gmail.com>
2021-12-17 14:28:33 -0500

    Add a new register_peers management command
    and alter provision_instance to accept hop nodes.

---
+++ awx/main/management/commands/register_peers.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from awx.main.models import Instance, InstanceLink
+
+
+class Command(BaseCommand):
+    """
+    Internal tower command.
+    Register the peers of a receptor node.
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument('source', type=str, help="")
+        parser.add_argument('--peers', type=str, nargs='+', required=True, help="")
+
+    def handle(self, **options):
+        nodes = Instance.objects.in_bulk(field_name='hostname')
+        if options['source'] not in nodes:
+            raise CommandError()
+        missing_peers = set(options['peers']) - set(nodes)
+        if missing_peers:
+            raise CommandError()
+
+        results = 0
+        for target in options['peers']:
+            instance, created = InstanceLink.objects.get_or_create(source=nodes[options['source']], target=nodes[target])
+            if created:
+                results += 1
+
+        print(f"{results} new peer links added to the database.")
