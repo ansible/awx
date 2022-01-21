077ed16ef17d63e6f3ea3ca7c989a3c3d573b9dd
John Westcott IV <john.westcott.iv@redhat.com>
2022-01-21 13:25:12 -0500

    Adding comments about testing

--- awx/main/tests/functional/test_api_generics.py
+++ awx/main/tests/functional/test_api_generics.py
@@ -24,3 +24,8 @@ def test_custom_400_error_log(caplog, post, admin_user):
     with override_settings(API_400_ERROR_LOG_FORMAT="{status_code} {error}"):
         post(url=reverse('api:setting_logging_test'), data={}, user=admin_user, expect=409)
         assert '409 Logging not enabled' in caplog.text
+
+
+# The above tests the generation function with a dict/object.
+# The tower-qa test tests.api.inventories.test_inventory_update.TestInventoryUpdate.test_update_all_inventory_sources_with_nonfunctional_sources tests the function with a list
+# Someday it would be nice to test the else condition (not a dict/list) but we need to find an API test which will do this. For now it was added just as a catch all
