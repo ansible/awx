3fa3682d7318a4b3cd8bf6a1d9fe219282e341a4
Brian Coca <bcoca@users.noreply.github.com>
2022-01-24 16:26:23 -0500

    Update awx_collection/plugins/doc_fragments/auth_plugin.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- awx_collection/plugins/doc_fragments/auth_plugin.py
+++ awx_collection/plugins/doc_fragments/auth_plugin.py
@@ -29,7 +29,7 @@ options:
     - name: CONTROLLER_USERNAME
     - name: TOWER_USERNAME
       deprecated:
-        removed_from_collection: 'awx.awx.controller'
+        removed_from_collection: 'awx.awx'
         removed_in: '4.0.0'
         why: Collection name change
         alternative: 'CONTROLLER_USERNAME'
