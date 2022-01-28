efbb1585df8b053dba3181cf499c5e7026bebf11
Elijah DeLee <kdelee@redhat.com>
2022-01-28 14:46:01 -0500

    clean up inefficient debugging in capacity method

--- awx/main/models/ha.py
+++ awx/main/models/ha.py
@@ -146,11 +146,10 @@ class Instance(HasPolicyEditsMixin, BaseModel):
 
     @property
     def consumed_capacity(self):
-        tasks = [
-            x for x in UnifiedJob.objects.filter((Q(controller_node=self.hostname) | Q(execution_node=self.hostname)) & Q(status__in=('running', 'waiting')))
-        ]
-        capacity_consumed = sum(x.task_impact for x in tasks)
-        logger.debug(f"tasks on the host: {[(task.log_format, task.task_impact) for task in tasks]}")
+        capacity_consumed = sum(
+            x.task_impact
+            for x in UnifiedJob.objects.filter((Q(controller_node=self.hostname) | Q(execution_node=self.hostname)) & Q(status__in=('running', 'waiting')))
+        )
         logger.debug(f"{capacity_consumed} capacity consumed on {self.hostname}")
         return capacity_consumed
 
