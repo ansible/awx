d774d519c78e95120a23e9d64404225ac2abba2d
Rebeccah <rhunter@redhat.com>
2022-02-01 10:34:11 -0500

    add test for hybrid node capacity consumption
    assert that the hybrid node is used for both control and execution and
    capacity is deducted correctly

--- awx/main/tests/functional/task_management/test_scheduler.py
+++ awx/main/tests/functional/task_management/test_scheduler.py
@@ -8,6 +8,7 @@ from awx.main.scheduler.dependency_graph import DependencyGraph
 from awx.main.utils import encrypt_field
 from awx.main.models import WorkflowJobTemplate, JobTemplate, Job
 from awx.main.models.ha import Instance
+from django.conf import settings
 
 
 @pytest.mark.django_db
@@ -146,6 +147,29 @@ class TestJobLifeCycle:
             execution_instance.hostname,
             control_instance_low_capacity.hostname,
         ], enough_capacity
+
+    @pytest.mark.django_db
+    def test_hybrid_capacity(self, job_template, hybrid_instance):
+        enough_capacity = job_template.create_unified_job()
+        insufficient_capacity = job_template.create_unified_job()
+        expected_task_impact = enough_capacity.task_impact + settings.AWX_CONTROL_NODE_TASK_IMPACT
+        all_ujs = [enough_capacity, insufficient_capacity]
+        for uj in all_ujs:
+            uj.signal_start()
+
+        # There is only enough control capacity to run one of the jobs so one should end up in pending and the other in waiting
+        tm = TaskManager()
+        self.run_tm(tm)
+
+        for uj in all_ujs:
+            uj.refresh_from_db()
+        assert enough_capacity.status == 'waiting'
+        assert insufficient_capacity.status == 'pending'
+        assert [enough_capacity.execution_node, enough_capacity.controller_node] == [
+            hybrid_instance.hostname,
+            hybrid_instance.hostname,
+        ], enough_capacity
+        assert expected_task_impact == hybrid_instance.consumed_capacity
 
 
 @pytest.mark.django_db
