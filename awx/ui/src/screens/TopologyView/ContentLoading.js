fec81ebb7fdce5a7783933bc75bbc1fddfea39fe
Kia Lam <digitalanime@gmail.com>
2022-02-09 11:18:19 -0800

    Add new loading screen placeholder.

---
+++ awx/ui/src/screens/TopologyView/ContentLoading.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import styled from 'styled-components';
+import {
+  EmptyState as PFEmptyState,
+  EmptyStateIcon,
+  Text,
+  TextContent,
+  TextVariants,
+  Spinner,
+} from '@patternfly/react-core';
+
+import { TopologyIcon as PFTopologyIcon } from '@patternfly/react-icons';
+
+const EmptyState = styled(PFEmptyState)`
+  --pf-c-empty-state--m-lg--MaxWidth: none;
+  min-height: 250px;
+`;
+
+const TopologyIcon = styled(PFTopologyIcon)`
+  font-size: 3em;
+  fill: #6a6e73;
+`;
+
+const ContentLoading = ({ className }) => (
+  <EmptyState variant="full" className={className}>
+    <TopologyIcon />
+    <TextContent>
+      <Text
+        component={TextVariants.small}
+        style={{ fontWeight: 'bold', color: 'black' }}
+      >
+        Please wait until the topology view is populated...
+      </Text>
+    </TextContent>
+    <EmptyStateIcon variant="container" component={Spinner} />
+  </EmptyState>
+);
+
+export { ContentLoading as _ContentLoading };
+export default ContentLoading;
