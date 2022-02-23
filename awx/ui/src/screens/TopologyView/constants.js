7fbab6760e395426e905a3fa6ab4069d14fc8d79
Kia Lam <digitalanime@gmail.com>
2022-02-23 12:37:20 -0800

    Small layout adjustment.

--- awx/ui/src/screens/TopologyView/constants.js
+++ awx/ui/src/screens/TopologyView/constants.js
@@ -1,30 +1,24 @@
 export const SELECTOR = '#chart';
 export const PARENTSELECTOR = '.mesh-svg';
 export const CHILDSELECTOR = '.mesh';
+export const DEFAULT_RADIUS = 16;
 export const MESH_FORCE_LAYOUT = {
-  defaultCollisionFactor: 80,
-  defaultForceStrength: -100,
-  defaultForceBody: 75,
+  defaultCollisionFactor: DEFAULT_RADIUS * 2 + 20,
+  defaultForceStrength: -30,
   defaultForceX: 0,
   defaultForceY: 0,
 };
-
-export const DEFAULT_RADIUS = 16;
 export const DEFAULT_NODE_COLOR = '#0066CC';
 export const DEFAULT_NODE_HIGHLIGHT_COLOR = '#16407C';
 export const DEFAULT_NODE_LABEL_TEXT_COLOR = 'white';
 export const DEFAULT_FONT_SIZE = '12px';
 export const LABEL_TEXT_MAX_LENGTH = 15;
-
 export const MARGIN = 15;
-export const FALLBACK_WIDTH = 700;
-
 export const NODE_STATE_COLOR_KEY = {
   disabled: '#6A6E73',
   healthy: '#3E8635',
   error: '#C9190B',
 };
-
 export const NODE_STATE_HTML_ENTITY_KEY = {
   disabled: '\u25EF',
   healthy: '\u2713',
