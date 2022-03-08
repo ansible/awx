531dd84bcdd349af27c8946e1eee7b55b4f45223
akus062381 <sarah.akus@gmail.com>
2022-03-08 12:54:03 -0500

    remove case sensitivity from active_hosts to give a more accurate host count

--- awx/main/managers.py
+++ awx/main/managers.py
@@ -6,6 +6,7 @@ import logging
 import os
 from django.db import models
 from django.conf import settings
+from django.db.models.functions import Lower
 
 from awx.main.utils.filters import SmartFilter
 from awx.main.utils.pglock import advisory_lock
@@ -35,7 +36,7 @@ class HostManager(models.Manager):
          - Only consider results that are unique
          - Return the count of this query
         """
-        return self.order_by().exclude(inventory_sources__source='controller').values('name').distinct().count()
+        return self.order_by().exclude(inventory_sources__source='controller').values(name_lower=Lower('name')).distinct().count()
 
     def org_active_count(self, org_id):
         """Return count of active, unique hosts used by an organization.
