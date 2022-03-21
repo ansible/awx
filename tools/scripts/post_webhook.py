313f46dc2df9a5e40b0ffedaa662a10bc583ece9
John Westcott IV <john.westcott.iv@redhat.com>
2022-03-21 15:40:49 -0400

    Adding examples and note to post_webhook.py --help and removing --type option, infering from url

--- tools/scripts/post_webhook.py
+++ tools/scripts/post_webhook.py
@@ -1,18 +1,5 @@
 #!/usr/bin/env python
 
-# You can lookup sample webhook events online.
-#    GitLab - https://docs.gitlab.com/ee/user/project/integrations/webhook_events.html
-#    GitHub - https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads
-#
-
-# User modified variables (maybe params?)
-# file = 'webhook_payload.json'
-# webhook_key = 'AvqBR19JDFaLTsbF3p7FmiU9WpuHsJKdHDfTqKXyzv1HtwDGZ8'
-# url = 'https://tower.jowestco.net:8043/api/v2/job_templates/637/github/'
-# verbose = False
-# webhook_type = 'git'
-# event_type =
-
 from hashlib import sha1
 from sys import exit
 import click
@@ -29,14 +16,60 @@ import uuid
 @click.option('--file', required=True, help='File containing the post data.')
 @click.option('--key', "webhook_key", required=True, help='The webhook key for the job template.')
 @click.option('--url', required=True, help='The webhook url for the job template (i.e. https://tower.jowestco.net:8043/api/v2/job_templates/637/github/.')
-@click.option(
-    '--type', "webhook_type", default='git', type=click.Choice(['git', 'gitlab']), help='What type of webhook is being sent (will send appropriate headers)'
-)
 @click.option('--event-type', help='Specific value for Event header, defaults to "issues" for GitHub and "Push Hook" for GitLab')
 @click.option('--verbose', is_flag=True, help='Dump HTTP communication for debugging')
 @click.option('--insecure', is_flag=True, help='Ignore SSL certs if true')
-def post_webhook(file, webhook_key, url, verbose, webhook_type, event_type, insecure):
+def post_webhook(file, webhook_key, url, verbose, event_type, insecure):
+    """
+    Helper command for submitting POST requests to Webhook endpoints.
 
+    We have two sample webhooks in tools/scripts/webhook_examples for gitlab and github.
+    These or any other file can be pointed to with the --file parameter.
+
+    \b
+    Additional example webhook events can be found online.
+        For GitLab see:
+        https://docs.gitlab.com/ee/user/project/integrations/webhook_events.html
+
+    \b
+        For GitHub see:
+        https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads
+
+    \b
+    For settingup webhooks in AWX see:
+        https://docs.ansible.com/ansible-tower/latest/html/userguide/webhooks.html
+
+    \b
+    Example usage for GitHub:
+      ./post_webhook.py \\
+        --file webhook_examples/github_push.json \\
+        --url https://tower.jowestco.net:8043/api/v2/job_templates/637/github/ \\
+        --key AvqBR19JDFaLTsbF3p7FmiU9WpuHsJKdHDfTqKXyzv1HtwDGZ8 \\
+        --insecure \\
+        --type github
+    
+    \b
+    Example usage for GitLab:
+      ./post_webhook.py \\
+        --file webhook_examples/gitlab_push.json \\
+        --url https://tower.jowestco.net:8043/api/v2/job_templates/638/gitlab/ \\
+        --key fZ8vUpfHfb1Dn7zHtyaAsyZC5IHFcZf2a2xiBc2jmrBDptCOL2 \\
+        --insecure \\
+        --type=gitlab 
+
+    \b
+    NOTE: GitLab webhooks are stored in the DB with a UID of the hash of the POST body.
+          After submitting one post GitLab post body a second POST of the same payload 
+          can result in a response like: 
+              Response code: 202
+              Response body:
+              {
+                  "message": "Webhook previously received, aborting."
+              }
+
+          If you need to test multiple GitLab posts simply change your payload slightly
+
+    """
     if insecure:
         # Disable insecure warnings
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -67,7 +100,7 @@ def post_webhook(file, webhook_key, url, verbose, webhook_type, event_type, inse
     # Compute sha1 mac
     mac = hmac.new(key_bytes, msg=data_bytes, digestmod=sha1)
 
-    if webhook_type == 'git':
+    if url.endswith('/github/'):
         headers.update(
             {
                 'X-Hub-Signature': 'sha1={}'.format(mac.hexdigest()),
@@ -75,7 +108,7 @@ def post_webhook(file, webhook_key, url, verbose, webhook_type, event_type, inse
                 'X-GitHub-Delivery': str(uuid.uuid4()),
             }
         )
-    elif webhook_type == 'gitlab':
+    elif url.endswith('/gitlab/'):
         mac = hmac.new(key_bytes, msg=data_bytes, digestmod=sha1)
         headers.update(
             {
@@ -83,6 +116,9 @@ def post_webhook(file, webhook_key, url, verbose, webhook_type, event_type, inse
                 'X-GitLab-Token': webhook_key,
             }
         )
+    else:
+        click.echo("This utility only knows how to support URLs that end in /github/ or /gitlab/.")
+        exit(250)
 
     # Make post
     r = requests.post(url, data=json.dumps(post_data), headers=headers, verify=(not insecure))
