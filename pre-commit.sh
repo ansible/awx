22d2b2d987d01acae9089d8f7c36032b1f7cefa5
John Westcott IV <john.westcott.iv@redhat.com>
2022-03-24 11:17:07 -0400

    Fixing pre-commit.sh for OS X

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,8 +2,10 @@ if [ -z $AWX_IGNORE_BLACK ] ; then
 	python_files_changed=$(git diff --cached --name-only --diff-filter=AM | grep -E '\.py')
 	if [ "x$python_files_changed" != "x" ] ; then
         	black --check $python_files_changed || \
-        	(echo 'To fix this, run `make black` to auto-format your code prior to commit, or set AWX_IGNORE_BLACK=1' && \
-        	exit 1)
+		if [ $? != 0 ] ; then
+        		echo 'To fix this, run `make black` to auto-format your code prior to commit, or set AWX_IGNORE_BLACK=1'
+        		exit 1
+		fi
 	fi
 fi
 
@@ -11,12 +13,14 @@ if [ -z $AWX_IGNORE_USER ] ; then
 	FAIL=0
 	export CHANGED_FILES=$(git diff --cached --name-only --diff-filter=AM)
 	if [ -d ./pre-commit-user ] ; then
-		for SCRIPT in `find ./pre-commit-user -executable -type f` ; do
-			echo "Running user pre-commit hook $SCRIPT"
-			$SCRIPT
-			if [ $? != 0 ] ; then
-				echo "User test $SCRIPT failed"
-			       	FAIL=1
+		for SCRIPT in `find ./pre-commit-user -type f` ; do
+			if [ -x $SCRIPT ] ; then
+				echo "Running user pre-commit hook $SCRIPT"
+				$SCRIPT
+				if [ $? != 0 ] ; then
+					echo "User test $SCRIPT failed"
+			       		FAIL=1
+				fi
 			fi
 		done
 	fi
