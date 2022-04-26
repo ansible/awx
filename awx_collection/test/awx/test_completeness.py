456bb75dcb2c6fc7b8cf964b7f90d93911156ba8
sean-m-sullivan <ssulliva@redhat.com>
2022-04-26 19:40:16 -0400

    update awx collection workflow module

--- awx_collection/test/awx/test_completeness.py
+++ awx_collection/test/awx/test_completeness.py
@@ -61,7 +61,7 @@ no_api_parameter_ok = {
     # lookup_organization is for specifiying the organization for the unified job template lookup
     'workflow_job_template_node': ['organization', 'approval_node', 'lookup_organization'],
     # Survey is how we handle associations
-    'workflow_job_template': ['survey_spec', 'destroy_current_schema'],
+    'workflow_job_template': ['survey_spec', 'destroy_current_nodes'],
     # organization is how we lookup unified job templates
     'schedule': ['organization'],
     # ad hoc commands support interval and timeout since its more like job_launch
