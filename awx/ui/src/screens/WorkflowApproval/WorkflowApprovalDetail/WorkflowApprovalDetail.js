458a1fc035979be6266bc60b093a22bd877c09d0
nixocio <nixocio@gmail.com>
2022-05-05 11:04:53 -0400

    Add details related workflow job on the workflow approval details
    Add details related workflow job on the work flow approval details
    
    Remove not used prop isLoading, fix, and expand unit-tests related to
    workflow approval details.

--- awx/ui/src/screens/WorkflowApproval/WorkflowApprovalDetail/WorkflowApprovalDetail.js
+++ awx/ui/src/screens/WorkflowApproval/WorkflowApprovalDetail/WorkflowApprovalDetail.js
@@ -1,14 +1,28 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { t } from '@lingui/macro';
 import { Link, useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import {
+  Divider as PFDivider,
+  Title as PFTitle,
+  Chip,
+} from '@patternfly/react-core';
 import AlertModal from 'components/AlertModal';
-import { CardBody, CardActionsRow } from 'components/Card';
+import ChipGroup from 'components/ChipGroup';
+import ContentError from 'components/ContentError';
+import ContentLoading from 'components/ContentLoading';
 import DeleteButton from 'components/DeleteButton';
-import { Detail, DetailList, UserDateDetail } from 'components/DetailList';
 import ErrorDetail from 'components/ErrorDetail';
+import { CardBody, CardActionsRow } from 'components/Card';
+import { Detail, DetailList, UserDateDetail } from 'components/DetailList';
+import { VariablesDetail } from 'components/CodeEditor';
 import { formatDateString, secondsToHHMMSS } from 'util/dates';
-import { WorkflowApprovalsAPI, WorkflowJobsAPI } from 'api';
+import {
+  WorkflowApprovalsAPI,
+  WorkflowJobTemplatesAPI,
+  WorkflowJobsAPI,
+} from 'api';
 import useRequest, { useDismissableError } from 'hooks/useRequest';
 import { WorkflowApproval } from 'types';
 import StatusLabel from 'components/StatusLabel';
@@ -16,7 +30,22 @@ import {
   getDetailPendingLabel,
   getStatus,
 } from '../shared/WorkflowApprovalUtils';
+
 import WorkflowApprovalControls from '../shared/WorkflowApprovalControls';
+
+const Divider = styled(PFDivider)`
+  margin-top: var(--pf-global--spacer--lg);
+  margin-bottom: var(--pf-global--spacer--lg);
+`;
+
+const Title = styled(PFTitle)`
+  margin-top: var(--pf-global--spacer--xl);
+  --pf-c-title--m-md--FontWeight: 700;
+`;
+
+const WFDetailList = styled(DetailList)`
+  padding: 0px var(--pf-global--spacer--lg);
+`;
 
 function WorkflowApprovalDetail({ workflowApproval }) {
   const { id: workflowApprovalId } = useParams();
@@ -80,6 +109,43 @@ function WorkflowApprovalDetail({ workflowApproval }) {
     {}
   );
 
+  const workflowJobTemplateId =
+    workflowApproval.summary_fields.workflow_job_template.id;
+
+  const {
+    error: fetchWorkflowJobError,
+    isLoading: isLoadingWorkflowJob,
+    request: fetchWorkflowJob,
+    result: workflowJob,
+  } = useRequest(
+    useCallback(async () => {
+      if (!workflowJobTemplateId) {
+        return {};
+      }
+      const { data: workflowJobTemplate } =
+        await WorkflowJobTemplatesAPI.readDetail(workflowJobTemplateId);
+
+      let jobId = null;
+
+      if (workflowJobTemplate.summary_fields?.current_job) {
+        jobId = workflowJobTemplate.summary_fields.current_job.id;
+      } else if (workflowJobTemplate.summary_fields?.last_job) {
+        jobId = workflowJobTemplate.summary_fields.last_job.id;
+      }
+      const { data } = await WorkflowJobsAPI.readDetail(jobId);
+
+      return data;
+    }, [workflowJobTemplateId]),
+    {
+      workflowJob: null,
+      isLoading: true,
+    }
+  );
+
+  useEffect(() => {
+    fetchWorkflowJob();
+  }, [fetchWorkflowJob]);
+
   const handleCancel = async () => {
     setIsKebabModalOpen(false);
     await cancelWorkflowApprovals();
@@ -95,7 +161,18 @@ function WorkflowApprovalDetail({ workflowApproval }) {
     workflowApproval?.summary_fields?.workflow_job_template;
 
   const isLoading =
-    isDeleteLoading || isApproveLoading || isDenyLoading || isCancelLoading;
+    isApproveLoading ||
+    isCancelLoading ||
+    isDeleteLoading ||
+    isDenyLoading ||
+    isLoadingWorkflowJob;
+
+  if (isLoadingWorkflowJob) {
+    return <ContentLoading />;
+  }
+  if (fetchWorkflowJobError) {
+    return <ContentError error={fetchWorkflowJobError} />;
+  }
 
   return (
     <CardBody>
@@ -147,19 +224,6 @@ function WorkflowApprovalDetail({ workflowApproval }) {
           dataCy="wa-detail-explanation"
         />
         <Detail
-          label={t`Workflow Job`}
-          value={
-            sourceWorkflowJob && sourceWorkflowJob?.id ? (
-              <Link to={`/jobs/workflow/${sourceWorkflowJob?.id}`}>
-                {`${sourceWorkflowJob?.id} - ${sourceWorkflowJob?.name}`}
-              </Link>
-            ) : (
-              t`Deleted`
-            )
-          }
-          dataCy="wa-detail-source-job"
-        />
-        <Detail
           label={t`Workflow Job Template`}
           value={
             sourceWorkflowJobTemplate && (
@@ -194,6 +258,88 @@ function WorkflowApprovalDetail({ workflowApproval }) {
           value={secondsToHHMMSS(workflowApproval.elapsed)}
         />
       </DetailList>
+      <Title headingLevel="h2">{t`Workflow job details`}</Title>
+      <Divider />
+      <WFDetailList gutter="sm">
+        <Detail
+          label={t`Workflow Job`}
+          value={
+            sourceWorkflowJob && sourceWorkflowJob?.id ? (
+              <Link to={`/jobs/workflow/${sourceWorkflowJob?.id}`}>
+                {`${sourceWorkflowJob?.id} - ${sourceWorkflowJob?.name}`}
+              </Link>
+            ) : (
+              t`Deleted`
+            )
+          }
+          dataCy="wa-detail-source-job"
+        />
+        {workflowJob?.limit ? (
+          <Detail
+            label={t`Limit`}
+            value={workflowJob.limit}
+            dataCy="wa-detail-source-job-limit"
+          />
+        ) : null}
+        {workflowJob?.scm_branch ? (
+          <Detail
+            label={t`Source Control Branch`}
+            value={workflowJob.scm_branch}
+            dataCy="wa-detail-source-job-scm"
+          />
+        ) : null}
+        {workflowJob?.summary_fields?.inventory ? (
+          <Detail
+            label={t`Inventory`}
+            value={
+              workflowJob.summary_fields.inventory ? (
+                <Link
+                  to={`/inventories/${
+                    workflowJob.summary_fields.inventory?.kind === 'smart'
+                      ? 'smart_inventory'
+                      : 'inventory'
+                  }/${workflowJob.summary_fields.inventory?.id}/details`}
+                >
+                  {workflowJob.summary_fields.inventory?.name}
+                </Link>
+              ) : (
+                ' '
+              )
+            }
+            dataCy="wa-detail-inventory"
+          />
+        ) : null}
+        {workflowJob?.summary_fields?.labels?.results?.length > 0 && (
+          <Detail
+            fullWidth
+            label={t`Labels`}
+            value={
+              <ChipGroup
+                numChips={5}
+                totalChips={workflowJob.summary_fields.labels.results.length}
+                ouiaId="wa-detail-label-chips"
+              >
+                {workflowJob.summary_fields.labels.results.map((label) => (
+                  <Chip key={label.id} isReadOnly>
+                    {label.name}
+                  </Chip>
+                ))}
+              </ChipGroup>
+            }
+          />
+        )}
+        {workflowJob?.extra_vars ? (
+          <VariablesDetail
+            dataCy="wa-detail-variables"
+            id="wa-detail-extra-vars"
+            label={t`Variables`}
+            name="extra_vars"
+            rows={5}
+            value={workflowJob.extra_vars}
+          />
+        ) : null}
+      </WFDetailList>
+
       <CardActionsRow>
         {workflowApproval.status === 'pending' &&
           workflowApproval.can_approve_or_deny && (
