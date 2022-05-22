233ebd7bb26d3f84c22226d2488effec672ed274
oudomsieng <oudomsieng@gmail.com>
2022-05-22 18:06:42 +0200

    fix cast json to response

--- awx_collection/plugins/module_utils/awx_request.py
+++ awx_collection/plugins/module_utils/awx_request.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_awx_resources(uri, previousPageResults, awx_auth):
-    response = requests.get(url=awx_auth['host']+uri, auth=(awx_auth['username'], awx_auth['password']), verify=awx_auth['verify_ssl'])
+    response = requests.get(url=awx_auth['host']+uri, auth=(awx_auth['username'], awx_auth['password']), verify=awx_auth['verify_ssl']).json()
     results = previousPageResults + response['results']
     if response['next']:
         return results
