389d73e2783ab4322c2e8243e2d10e99726df085
oudomsieng <oudomsieng@gmail.com>
2022-05-22 18:17:49 +0200

    fix response next is none

--- awx_collection/plugins/module_utils/awx_request.py
+++ awx_collection/plugins/module_utils/awx_request.py
@@ -3,6 +3,6 @@ import requests
 def get_awx_resources(uri, previousPageResults, awx_auth):
     response = requests.get(url=awx_auth['host']+uri, auth=(awx_auth['username'], awx_auth['password']), verify=awx_auth['verify_ssl']).json()
     results = previousPageResults + response['results']
-    if response['next']:
+    if response['next'] is None:
         return results
     return get_awx_resources(response['next'], previousPageResults, awx_auth)
