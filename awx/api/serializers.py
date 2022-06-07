fe6d0ce9ccb8ba39ff60e90851e271e7a64b61c7
John Westcott IV <john.westcott.iv@redhat.com>
2022-06-07 10:50:21 -0400

    Adding help text to until and timezone fields

--- awx/api/serializers.py
+++ awx/api/serializers.py
@@ -4681,8 +4681,16 @@ class SchedulePreviewSerializer(BaseSerializer):
 class ScheduleSerializer(LaunchConfigurationBaseSerializer, SchedulePreviewSerializer):
     show_capabilities = ['edit', 'delete']
 
-    timezone = serializers.SerializerMethodField()
-    until = serializers.SerializerMethodField()
+    timezone = serializers.SerializerMethodField(
+        help_text=_(
+            'The timezone this schedule runs in. This field is extracted from the RRULE. If the timezone in the RRULE is a link to another timezone this field will show the links name.'
+        ),
+        read_only=True,
+    )
+    until = serializers.SerializerMethodField(
+        help_text=_('The date this schedule will end. This field is computed from the RRULE. If the schedule does not end an emptry string will be returned'),
+        read_only=True,
+    )
 
     class Meta:
         model = Schedule
