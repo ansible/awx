109f39de75c5126ba7149b3f0c0ac081f95eca60
Seth Foster <fosterbseth@gmail.com>
2022-06-28 16:35:07 -0400

    add workflow manager

--- awx/main/scheduler/__init__.py
+++ awx/main/scheduler/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2017 Ansible, Inc.
 #
 
-from .task_manager import TaskManager, TaskPrepper
+from .task_manager import TaskManager, DependencyManager, WorkflowManager
 
-__all__ = ['TaskManager', 'TaskPrepper']
+__all__ = ['TaskManager', 'DependencyManager', 'WorkflowManager']
