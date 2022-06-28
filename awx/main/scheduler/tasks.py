1ef953691105b81c6757fab16b19b5b094909cb2
Seth Foster <fosterbseth@gmail.com>
2022-06-28 16:35:07 -0400

    add workflow manager

--- awx/main/scheduler/tasks.py
+++ awx/main/scheduler/tasks.py
@@ -2,7 +2,7 @@
 import logging
 
 # AWX
-from awx.main.scheduler import TaskManager, TaskPrepper
+from awx.main.scheduler import TaskManager, DependencyManager, WorkflowManager
 from awx.main.dispatch.publish import task
 from awx.main.dispatch import get_local_queuename
 
@@ -11,16 +11,20 @@ logger = logging.getLogger('awx.main.scheduler')
 
 @task(queue=get_local_queuename)
 def task_manager():
-    logger.debug("=== Running task manager.")
     TaskManager().schedule()
 
 
 @task(queue=get_local_queuename)
-def task_prepper():
-    logger.debug("=== Running task prepper.")
-    TaskPrepper().schedule()
+def dependency_manager():
+    DependencyManager().schedule()
+
+
+@task(queue=get_local_queuename)
+def workflow_manager():
+    WorkflowManager().schedule()
 
 
 def run_task_manager():
     task_manager()
-    task_prepper()
+    dependency_manager()
+    workflow_manager()
