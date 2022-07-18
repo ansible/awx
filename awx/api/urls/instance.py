f241d66791519cd1010a520026cecbcd941fb43b
TheRealHaoLiu <haoli@redhat.com>
2022-07-18 11:10:59 -0400

    add instance install bundle endpoint
    add scaffolding for instance install_bundle endpoint
    
    - add instance_install_bundle view (does not do anything yet)
    - add `instance_install_bundle` related field to serializer
    - add `/install_bundle` to instance URL
    - `/install_bundle` only available for execution and hop node
    - `/install_bundle` endpoint response contain a downloadable tgz with moc data
    
    TODO: add actual data to the install bundle response
    
    Signed-off-by: Hao Liu <haoli@redhat.com>

--- awx/api/urls/instance.py
+++ awx/api/urls/instance.py
@@ -3,7 +3,7 @@
 
 from django.urls import re_path
 
-from awx.api.views import InstanceList, InstanceDetail, InstanceUnifiedJobsList, InstanceInstanceGroupsList, InstanceHealthCheck
+from awx.api.views import InstanceList, InstanceDetail, InstanceUnifiedJobsList, InstanceInstanceGroupsList, InstanceHealthCheck, InstanceInstallBundle
 
 
 urls = [
@@ -12,6 +12,7 @@ urls = [
     re_path(r'^(?P<pk>[0-9]+)/jobs/$', InstanceUnifiedJobsList.as_view(), name='instance_unified_jobs_list'),
     re_path(r'^(?P<pk>[0-9]+)/instance_groups/$', InstanceInstanceGroupsList.as_view(), name='instance_instance_groups_list'),
     re_path(r'^(?P<pk>[0-9]+)/health_check/$', InstanceHealthCheck.as_view(), name='instance_health_check'),
+    re_path(r'^(?P<pk>[0-9]+)/install_bundle/$', InstanceInstallBundle.as_view(), name='instance_install_bundle'),
 ]
 
 __all__ = ['urls']
