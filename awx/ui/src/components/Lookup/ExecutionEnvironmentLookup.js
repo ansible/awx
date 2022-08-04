11b6294465610de08823fa52ccca281fc83e3e04
mabashian <mabashia@redhat.com>
2022-08-04 11:32:17 -0400

    Refactors EE Lookup to support prompting.  Adds prompting for EE to JT form

--- awx/ui/src/components/Lookup/ExecutionEnvironmentLookup.js
+++ awx/ui/src/components/Lookup/ExecutionEnvironmentLookup.js
@@ -10,9 +10,9 @@ import { getQSConfig, parseQueryString, mergeParams } from 'util/qs';
 import useRequest from 'hooks/useRequest';
 import Popover from '../Popover';
 import OptionsList from '../OptionsList';
-
 import Lookup from './Lookup';
 import LookupErrorMessage from './shared/LookupErrorMessage';
+import FieldWithPrompt from '../FieldWithPrompt';
 
 const QS_CONFIG = getQSConfig('execution_environments', {
   page: 1,
@@ -36,6 +36,9 @@ function ExecutionEnvironmentLookup({
   value,
   fieldName,
   overrideLabel,
+  isPromptableField,
+  promptId,
+  promptName,
 }) {
   const location = useLocation();
   const {
@@ -202,7 +205,22 @@ function ExecutionEnvironmentLookup({
     return t`Execution Environment`;
   };
 
-  return (
+  return isPromptableField ? (
+    <FieldWithPrompt
+      fieldId={id}
+      label={renderLabel()}
+      promptId={promptId}
+      promptName={promptName}
+      tooltip={popoverContent}
+    >
+      {tooltip && isDisabled ? (
+        <Tooltip content={tooltip}>{renderLookup()}</Tooltip>
+      ) : (
+        renderLookup()
+      )}
+      <LookupErrorMessage error={error || fetchProjectError} />
+    </FieldWithPrompt>
+  ) : (
     <FormGroup
       fieldId={id}
       label={renderLabel()}
