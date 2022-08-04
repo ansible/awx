b566a65a8726a07a282a1be55e4d0e7ff2f1193e
mabashian <mabashia@redhat.com>
2022-08-04 11:32:17 -0400

    Refactors EE Lookup to support prompting.  Adds prompting for EE to JT form

--- awx/ui/src/screens/Template/shared/JobTemplate.helptext.js
+++ awx/ui/src/screens/Template/shared/JobTemplate.helptext.js
@@ -6,7 +6,7 @@ const jtHelpTextStrings = () => ({
   jobType: t`For job templates, select run to execute the playbook. Select check to only check playbook syntax, test environment setup, and report problems without executing the playbook.`,
   inventory: t`Select the inventory containing the hosts you want this job to manage.`,
   project: t`Select the project containing the playbook you want this job to execute.`,
-  executionEnvironmentForm: t`Select the execution environment for this job template.`,
+  executionEnvironmentForm: t`The container image to be used for execution.`,
   executionEnvironmentDetail: t`The execution environment that will be used when launching this job template. The resolved execution environment can be overridden by explicitly assigning a different one to this job template.`,
   playbook: t`Select the playbook to be executed by this job.`,
   credentials: t`Select credentials for accessing the nodes this job will be ran against. You can only select one credential of each type. For machine credentials (SSH), checking "Prompt on launch" without selecting credentials will require you to select a machine credential at run time. If you select credentials and check "Prompt on launch", the selected credential(s) become the defaults that can be updated at run time.`,
