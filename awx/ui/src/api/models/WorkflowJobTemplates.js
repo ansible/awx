f7665260115e75b091d3115ba97b743666fe5272
mabashian <mabashia@redhat.com>
2022-08-23 16:58:24 -0400

    Adds fetchAllLabels to JT/WFJT data models

--- awx/ui/src/api/models/WorkflowJobTemplates.js
+++ awx/ui/src/api/models/WorkflowJobTemplates.js
@@ -108,6 +108,31 @@ class WorkflowJobTemplates extends SchedulesMixin(NotificationsMixin(Base)) {
       params,
     });
   }
+
+  readAllLabels(id) {
+    const fetchLabels = async (pageNo = 1, labels = []) => {
+      try {
+        const { data } = await this.http.get(`${this.baseUrl}${id}/labels/`, {
+          params: {
+            page: pageNo,
+            page_size: 200,
+          },
+        });
+        if (data?.next) {
+          return fetchLabels(pageNo + 1, labels.concat(data.results));
+        }
+        return Promise.resolve({
+          data: {
+            results: labels.concat(data.results),
+          },
+        });
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    };
+
+    return fetchLabels();
+  }
 }
 
 export default WorkflowJobTemplates;
