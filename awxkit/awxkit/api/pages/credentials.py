6d62aeac55ad9ddbe5857ecffcfb38891ab17afa
Luiz Costa <luizcosta@redhat.com>
2022-09-02 16:06:36 -0300

    WIP adding GPG to awxkit

--- awxkit/awxkit/api/pages/credentials.py
+++ awxkit/awxkit/api/pages/credentials.py
@@ -94,6 +94,7 @@ credential_type_name_to_config_kind_map = {
     'machine': 'ssh',
     'vault': 'vault',
     'vmware vcenter': 'vmware',
+    'gpg public key': 'gpg_public_key',
 }
 
 config_kind_to_credential_type_name_map = {kind: name for name, kind in credential_type_name_to_config_kind_map.items()}
