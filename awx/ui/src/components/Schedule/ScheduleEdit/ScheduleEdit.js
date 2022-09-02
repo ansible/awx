db4f477f0a1241ad021b07c5bd18c26fcd457a82
mabashian <mabashia@redhat.com>
2022-09-02 14:21:16 -0400

    Fix broken UI unit tests

--- awx/ui/src/components/Schedule/ScheduleEdit/ScheduleEdit.js
+++ awx/ui/src/components/Schedule/ScheduleEdit/ScheduleEdit.js
@@ -83,10 +83,6 @@ function ScheduleEdit({
       submitValues.execution_environment = execution_environment.id;
     }
 
-    submitValues.instance_groups = instance_groups
-      ? instance_groups.map((s) => s.id)
-      : [];
-
     try {
       if (launchConfiguration?.ask_labels_on_launch) {
         const labelIds = [];
