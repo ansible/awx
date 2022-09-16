dcab76c5f591995cbc52a93d39279d3d0f1735f2
Alan Rominger <arominge@redhat.com>
2022-09-16 14:38:15 -0400

    Expand unit tests to apply more fields

--- awx/main/tests/functional/models/test_job_launch_config.py
+++ awx/main/tests/functional/models/test_job_launch_config.py
@@ -1,7 +1,7 @@
 import pytest
 
 # AWX
-from awx.main.models.jobs import JobTemplate, JobLaunchConfig, LaunchTimeConfigBase
+from awx.main.models.jobs import JobTemplate, LaunchTimeConfigBase
 from awx.main.models.execution_environments import ExecutionEnvironment
 
 
@@ -10,18 +10,6 @@ def full_jt(inventory, project, machine_credential):
     jt = JobTemplate.objects.create(name='my-jt', inventory=inventory, project=project, playbook='helloworld.yml')
     jt.credentials.add(machine_credential)
     return jt
-
-
-@pytest.fixture
-def config_factory(full_jt):
-    def return_config(data):
-        job = full_jt.create_unified_job(**data)
-        try:
-            return job.launch_config
-        except JobLaunchConfig.DoesNotExist:
-            return None
-
-    return return_config
 
 
 @pytest.mark.django_db
@@ -41,39 +29,51 @@ class TestConfigCreation:
         assert config.limit == 'foobar'
         assert config.char_prompts == {'limit': 'foobar'}
 
-    def test_added_credential(self, full_jt, credential):
-        job = full_jt.create_unified_job(credentials=[credential])
+    def test_added_related(self, full_jt, credential, default_instance_group, label):
+        job = full_jt.create_unified_job(credentials=[credential], instance_groups=[default_instance_group], labels=[label])
         config = job.launch_config
         assert set(config.credentials.all()) == set([credential])
+        assert set(config.labels.all()) == set([label])
+        assert set(config.instance_groups.all()) == set([default_instance_group])
 
     def test_survey_passwords_ignored(self, inventory_source):
         iu = inventory_source.create_unified_job(survey_passwords={'foo': '$encrypted$'})
         assert iu.launch_config.prompts_dict() == {}
 
 
+@pytest.fixture
+def full_prompts_dict(inventory, credential, label, default_instance_group):
+    ee = ExecutionEnvironment.objects.create(name='test-ee', image='quay.io/foo/bar')
+    r = {
+        'limit': 'foobar',
+        'inventory': inventory,
+        'credentials': [credential],
+        'execution_environment': ee,
+        'labels': [label],
+        'instance_groups': [default_instance_group],
+        'verbosity': 3,
+        'scm_branch': 'non_dev',
+        'diff_mode': True,
+        'skip_tags': 'foobar',
+        'job_tags': 'untagged',
+        'forks': 26,
+        'job_slice_count': 2,
+        'timeout': 200,
+        'extra_vars': {'prompted_key': 'prompted_val'},
+        'job_type': 'check',
+    }
+    assert set(JobTemplate.get_ask_mapping().keys()) - set(r.keys()) == set()  # make fixture comprehensive
+    return r
+
+
 @pytest.mark.django_db
-class TestConfigReversibility:
+def test_config_reversibility(full_jt, full_prompts_dict):
     """
     Checks that a blob of saved prompts will be re-created in the
     prompts_dict for launching new jobs
     """
-
-    def test_char_field_only(self, config_factory):
-        config = config_factory({'limit': 'foobar'})
-        assert config.prompts_dict() == {'limit': 'foobar'}
-
-    def test_related_objects(self, config_factory, inventory, credential, label, default_instance_group):
-        ee = ExecutionEnvironment.objects.create(name='test-ee', image='quay.io/foo/bar')
-        prompts = {
-            'limit': 'foobar',
-            'inventory': inventory,
-            'credentials': [credential],
-            'execution_environment': ee,
-            'labels': [label],
-            'instance_groups': [default_instance_group],
-        }
-        config = config_factory(prompts)
-        assert config.prompts_dict() == prompts
+    config = full_jt.create_unified_job(**full_prompts_dict).launch_config
+    assert config.prompts_dict() == full_prompts_dict
 
 
 @pytest.mark.django_db
