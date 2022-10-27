44d1066a11c00ead6e63fc0c989e1afa2d3398be
darshanip <darshancoding@gmail.com>
2022-10-27 10:32:17 +0530

    Variable update
    Signed-off-by: darshanip <darshancoding@gmail.com>

--- awx_collection/plugins/modules/project_update.py
+++ awx_collection/plugins/modules/project_update.py
@@ -115,7 +115,7 @@ def main():
     result = module.post_endpoint(project['related']['update'])
 
     if result['status_code'] == 405:
-        module.fail_json(msg="Unable to trigger a project update because the project scm_type (Manual) does not support it.", response=result)
+        module.fail_json(msg="Unable to trigger a project update because the project scm_type ("+"project['scm_type']"+") does not support it.", response=result)
     elif result['status_code'] != 202:
         module.fail_json(msg="Failed to update project, see response for details", response=result)
 
