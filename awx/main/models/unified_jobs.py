a8fdb22ab37270d5122003b325dad9869ebe910e
Lila <djyasin.ly@gmail.com>
2022-11-29 16:10:02 -0500

    Fixed indentation of 1354-1357.

--- awx/main/models/unified_jobs.py
+++ awx/main/models/unified_jobs.py
@@ -1351,10 +1351,10 @@ class UnifiedJob(
                 if required in defined_fields and not credential.has_input(required):
                     missing_credential_inputs.append(required)
 
-                if missing_credential_inputs:
-                    self.job_explanation = '{} cannot start because Credential {} does not provide one or more required fields ({}).'.format(
-                        self._meta.verbose_name.title(), credential.name, ', '.join(sorted(missing_credential_inputs))
-                    )
+            if missing_credential_inputs:
+                self.job_explanation = '{} cannot start because Credential {} does not provide one or more required fields ({}).'.format(
+                    self._meta.verbose_name.title(), credential.name, ', '.join(sorted(missing_credential_inputs))
+                )
                 self.save(update_fields=['job_explanation'])
                 return (False, None)
 
