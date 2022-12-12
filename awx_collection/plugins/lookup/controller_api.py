7b2938f515ad306a396338c52b34a0b0591e1511
Manas Maiti <mmaiti@redhat.com>
2022-12-12 18:01:15 +0100

    fix typo

--- awx_collection/plugins/lookup/controller_api.py
+++ awx_collection/plugins/lookup/controller_api.py
@@ -74,7 +74,7 @@ EXAMPLES = """
 
 - name: Load the UI settings specifying the connection info
   set_fact:
-    controller_settings: "{{ lookup('awx.awx.controller_api', 'settings/ui' host='controller.example.com',
+    controller_settings: "{{ lookup('awx.awx.controller_api', 'settings/ui', host='controller.example.com',
                              username='admin', password=my_pass_var, verify_ssl=False) }}"
 
 - name: Report the usernames of all users with admin privs
