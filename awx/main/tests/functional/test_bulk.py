a949f9c838243e0f5812a8140e2d6ce29434f928
Elijah DeLee <kdelee@redhat.com>
2023-01-11 22:37:41 -0500

    also test with super user like I meant to

--- awx/main/tests/functional/test_bulk.py
+++ awx/main/tests/functional/test_bulk.py
@@ -34,13 +34,14 @@ def test_bulk_host_create_num_queries(organization, inventory, post, get, user, 
     inventory_admin = user('inventory_admin', False)
     org_admin = user('org_admin', False)
     org_inv_admin = user('org_admin', False)
+    superuser = user('admin', True)
     for u in [org_admin, org_inv_admin, inventory_admin]:
         organization.member_role.members.add(u)
     organization.admin_role.members.add(org_admin)
     organization.inventory_admin_role.members.add(org_inv_admin)
     inventory.admin_role.members.add(inventory_admin)
 
-    for u in [org_admin, inventory_admin, org_inv_admin]:
+    for u in [org_admin, inventory_admin, org_inv_admin, superuser]:
         hosts = [{'name': uuid4()} for i in range(num_hosts)]
         with withAssertNumQueriesLessThan(num_queries):
             bulk_host_create_response = post(reverse('api:bulk_host_create'), {'inventory': inventory.id, 'hosts': hosts}, u, expect=201).data
