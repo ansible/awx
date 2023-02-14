a69d48727d329769d2f74397140601c26059c2bb
Gabe Muniz <gmuniz@redhat.com>
2023-02-14 00:07:49 -0500

    refactored to use is_valid_relation instead of post

--- awx/main/tests/functional/test_inventory_input_constructed.py
+++ awx/main/tests/functional/test_inventory_input_constructed.py
@@ -3,25 +3,17 @@ from awx.main.models import Inventory
 from awx.api.versioning import reverse
 
 
-@pytest.fixture
-def constructed_inventory(organization):
-    """
-    creates a new constructed inventory source
-    """
-    return Inventory.objects.create(name='dummy2', kind='constructed', organization=organization)
-
-
 @pytest.mark.django_db
-def test_constructed_inventory_post(post, organization, admin_user):
-    inventory1 = Inventory.objects.create(name='dummy1', kind='constructed', organization=organization)
-    inventory2 = Inventory.objects.create(name='dummy2', kind='constructed', organization=organization)
+def test_constructed_inventory_post(post, admin_user, organization):
+    inv1 = Inventory.objects.create(name='dummy1', kind='constructed', organization=organization)
+    inv2 = Inventory.objects.create(name='dummy2', kind='constructed', organization=organization)
     resp = post(
-        url=reverse('api:inventory_input_inventories', kwargs={'pk': inventory1.pk}),
-        data={'id': inventory2.pk},
+        url=reverse('api:inventory_input_inventories', kwargs={'pk': inv1.pk}),
+        data={'id': inv2.pk},
         user=admin_user,
-        expect=405,
+        expect=400,
     )
-    assert resp.status_code == 405
+    assert resp.status_code == 400
 
 
 @pytest.mark.django_db
@@ -58,12 +50,11 @@ def test_add_constructed_inventory_group(post, admin_user, constructed_inventory
 
 
 @pytest.mark.django_db
-def test_edit_constructed_inventory_source(patch, admin_user, inventory):
-    inventory.inventory_sources.create(name="dummysrc", source="constructed")
-    inv_id = inventory.inventory_sources.get(name='dummysrc').id
+def test_edit_constructed_inventory_source(patch, admin_user, inventory_source_factory):
+    inv_src = inventory_source_factory(name='dummy1', source='constructed')
     resp = patch(
-        reverse('api:inventory_source_detail', kwargs={'pk': inv_id}),
-        data={'description': inventory.name},
+        reverse('api:inventory_source_detail', kwargs={'pk': inv_src.pk}),
+        data={'description': inv_src.name},
         user=admin_user,
         expect=400,
     )
