ab3a9a0364fd048d320687649ec27bf3ab135a6a
Marliana Lara <mlara@redhat.com>
2023-02-21 17:47:56 -0500

    Update inventory details after inventory source sync

--- awx/ui/src/screens/Inventory/shared/useWsInventorySourcesDetails.js
+++ awx/ui/src/screens/Inventory/shared/useWsInventorySourcesDetails.js
@@ -1,16 +1,17 @@
 import { useState, useEffect } from 'react';
 import useWebsocket from 'hooks/useWebsocket';
+import { InventorySourcesAPI } from 'api';
 
-export default function useWsInventorySourcesDetails(initialSources) {
-  const [sources, setSources] = useState(initialSources);
+export default function useWsInventorySourcesDetails(initialSource) {
+  const [source, setSource] = useState(initialSource);
   const lastMessage = useWebsocket({
     jobs: ['status_changed'],
     control: ['limit_reached_1'],
   });
 
   useEffect(() => {
-    setSources(initialSources);
-  }, [initialSources]);
+    setSource(initialSource);
+  }, [initialSource]);
 
   useEffect(
     () => {
@@ -21,22 +22,37 @@ export default function useWsInventorySourcesDetails(initialSources) {
       ) {
         return;
       }
-      const updateSource = {
-        ...sources,
-        summary_fields: {
-          ...sources.summary_fields,
-          current_job: {
-            id: lastMessage.unified_job_id,
-            status: lastMessage.status,
-            finished: lastMessage.finished,
-          },
-        },
-      };
 
-      setSources(updateSource);
+      if (
+        ['successful', 'failed', 'error', 'cancelled'].includes(
+          lastMessage.status
+        )
+      ) {
+        fetchSource();
+      }
+      setSource(updateSource(source, lastMessage));
     },
     [lastMessage] // eslint-disable-line react-hooks/exhaustive-deps
   );
 
-  return sources;
+  async function fetchSource() {
+    const { data } = await InventorySourcesAPI.readDetail(source.id);
+    setSource(data);
+  }
+
+  return source;
+}
+
+function updateSource(source, message) {
+  return {
+    ...source,
+    summary_fields: {
+      ...source.summary_fields,
+      current_job: {
+        id: message.unified_job_id,
+        status: message.status,
+        finished: message.finished,
+      },
+    },
+  };
 }
