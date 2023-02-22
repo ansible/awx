1b9e6e2a3b8c64fe3f5eb4d34e0604d0d58324a4
Gabe Muniz <gmuniz@redhat.com>
2023-02-21 22:17:11 -0500

    refactored to shorten logic

--- awx/main/migrations/_OrgAdmin_to_use_ig.py
+++ awx/main/migrations/_OrgAdmin_to_use_ig.py
@@ -1,75 +1,12 @@
-from django.contrib.auth.models import User
-from django.contrib.contenttypes.models import ContentType
-from awx.main.models.rbac import Role
 from awx.main.models import Organization
 from django.apps import apps
 
-# def migrate_org_admin_to_use():
-#     user_list = list()
-#     member_list = list()
-#     for user in User.objects.all():
-#         if user.admin_of_organizations:
-#             user_list.append(user)
-#     for entry in user_list:
-#         membership = entry.admin_of_organizations
-#         member_list.append({'user_id': entry.id, 'org_queryset': membership})
-#     for orgs in Organization.objects.filter(name__in=membership.name):
-#         print(orgs)
-# def migrate_org_admin_to_use():
-#     user_list = list()
-#     for user in User.objects.all():
-#         if user.admin_of_organizations:
-#             user_list.append(user)
-#     member_list = list()
-#     for entry in user_list:
-#         membership = entry.admin_of_organizations
-#         member_list.append(membership)
-#     org_list = list()
-#     for orgs in member_list:
-#         for org_name in orgs:
-#             org_list.append(org_name.id)
-#     ins_list = list()
-#     ins = Organization.objects.filter(id__in=org_list)
-#     for org in ins:
-#         igs = org.instance_groups.all()
-#         if not igs:
-#             continue
-#         else:
-#             ins_list.append(igs)
-#     for instance in ins_list:
-#         for inst in instance:
-#             for user in user_list:
-#                 if user.admin_of_organizations in ins_list:
-#                     inst.use_role.members.add(user_list)
-def migrate_org_admin_to_use():
-          user_list = list()
-          for user in User.objects.all():
-              if user.admin_of_organizations:
-                  user_list.append(user)
-          member_list = list()
-          for entry in user_list:
-              membership = entry.admin_of_organizations
-              member_list.append(membership)
-          org_list = list()
-          for orgs in member_list:
-              for org_name in orgs:
-                  org_list.append(org_name.id)
-          ins_list = list()
-          ins = Organization.objects.filter(id__in=org_list)
-          for org in ins:
-              igs = org.instance_groups.all()
-              if not igs:
-                  continue
-              else:
-                  ins_list.append(igs)
-          for instance in ins_list:
-              for inst in instance:
-                  for user in user_list:
-                      for org in ins:
-                          if user in org.admin_role.members.all():
-                              inst.use_role.members.add(user)
-migrate_org_admin_to_use()
 
-
-            
-
+def migrate_org_admin_to_use(apps):
+    for org in Organization.objects.prefetch_related('admin_role__members').iterator():
+        igs = list(org.instance_groups.all())
+        if not igs:
+            continue
+        for admin in org.admin_role.members.filter(is_superuser=False):
+            for ig in igs:
+                ig.use_role.members.add(admin)
--- awx/main/tests/functional/test_org_admin_migration.py
+++ awx/main/tests/functional/test_org_admin_migration.py
@@ -1,19 +1,16 @@
 import pytest
-from unittest import mock
 
 from django.apps import apps
-from django.contrib.auth.models import User
-from django.contrib.contenttypes.models import ContentType
 
-from awx.main.models.rbac import Role
 from awx.main.models import Organization, InstanceGroup
 from awx.main.migrations import _OrgAdmin_to_use_ig as orgadmin
+
 
 @pytest.mark.django_db
 def test_migrate_admin_role(org_admin, organization):
     instance_group = InstanceGroup.objects.create(name='test')
     organization.admin_role.members.add(org_admin)
     organization.instance_groups.add(instance_group)
-    with mock.patch('django.conf.settings.CONSTRUCTED_INSTANCE_ID_VAR'):
-        orgadmin.migrate_org_admin_to_use(apps)
+    orgadmin.migrate_org_admin_to_use(apps)
     assert org_admin in instance_group.use_role.members.all()
+    assert instance_group.use_role.members.count() == 1
