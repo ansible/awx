de5e616e6d6c4c5d201ecf48a32707547b5f5cda
Gabe Muniz <gmuniz@redhat.com>
2023-03-08 10:04:04 -0500

    Added logging to migration as activitystream is disabled

--- awx/main/migrations/_OrgAdmin_to_use_ig.py
+++ awx/main/migrations/_OrgAdmin_to_use_ig.py
@@ -1,7 +1,13 @@
+import logging
+
 from awx.main.models import Organization
+
+logger = logging.getLogger('awx.main.migrations')
 
 
 def migrate_org_admin_to_use(apps, schema_editor):
+    logger.info('Initiated migration from Org admin to use role')
+    roles_added = 0
     for org in Organization.objects.prefetch_related('admin_role__members').iterator():
         igs = list(org.instance_groups.all())
         if not igs:
@@ -9,3 +15,6 @@ def migrate_org_admin_to_use(apps, schema_editor):
         for admin in org.admin_role.members.filter(is_superuser=False):
             for ig in igs:
                 ig.use_role.members.add(admin)
+                roles_added += 1
+    if roles_added:
+        logger.info(f'Migration converted {roles_added} from organization admin to use role')
