ed65bea95cb62a8a3703cc191aaa178989bbb781
Hao Liu <haoli@redhat.com>
2023-03-13 22:14:56 -0400

    update migration (merge conflict)

--- awx/main/migrations/0177_add_hostmetric_fields.py
+++ awx/main/migrations/0177_add_hostmetric_fields.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('main', '0174_ensure_org_ee_admin_roles'),
+        ('main', '0176_inventorysource_scm_branch'),
     ]
 
     operations = [
