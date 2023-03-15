b75b098ee9cf1bc4c11629e3e5ec3d231d75e258
Hao Liu <haoli@redhat.com>
2023-03-14 22:42:43 -0400

    throw 404 when UI_NEXT false

--- awx/ui_next/urls.py
+++ awx/ui_next/urls.py
@@ -1,9 +1,17 @@
+from django.conf import settings
+from django.http import Http404
 from django.urls import re_path
 from django.views.generic.base import TemplateView
 
 
 class IndexView(TemplateView):
     template_name = 'index_awx.html'
+
+    def get_context_data(self, **kwargs):
+        if settings.UI_NEXT is False:
+            raise Http404()
+
+        return super().get_context_data(**kwargs)
 
 
 app_name = 'ui_next'
