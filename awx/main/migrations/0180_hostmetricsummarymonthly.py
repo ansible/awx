1347d76968e3c35347ca2867476522be1fb24e81
Hao Liu <haoli@redhat.com>
2023-03-20 09:05:57 -0400

    fix migration (merge conflict)

--- awx/main/migrations/0180_hostmetricsummarymonthly.py
+++ awx/main/migrations/0180_hostmetricsummarymonthly.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('main', '0177_add_hostmetric_fields'),
+        ('main', '0179_add_hostmetric_fields'),
     ]
 
     operations = [
