d68048a083120bb222cc20164fd963703dc49851
Jordi Casas <jordicasas1987@gmail.com>
2023-04-14 11:05:29 +0000

    fix: test_proxy_ip_allowed (import SetRemoteAddrFromRemoteHostHeader middleware)

--- awx/main/tests/functional/api/test_generic.py
+++ awx/main/tests/functional/api/test_generic.py
@@ -1,6 +1,7 @@
 import pytest
 
 from awx.api.versioning import reverse
+from awx.main.middleware import SetRemoteAddrFromRemoteHostHeader
 
 
 @pytest.mark.django_db
@@ -8,18 +9,9 @@ def test_proxy_ip_allowed(get, patch, admin):
     url = reverse('api:setting_singleton_detail', kwargs={'category_slug': 'system'})
     patch(url, user=admin, data={'REMOTE_HOST_HEADERS': ['HTTP_X_FROM_THE_LOAD_BALANCER', 'REMOTE_ADDR', 'REMOTE_HOST']})
 
-    class HeaderTrackingMiddleware(object):
-        environ = {}
-
-        def process_request(self, request):
-            pass
-
-        def process_response(self, request, response):
-            self.environ = request.environ
-
     # By default, `PROXY_IP_ALLOWED_LIST` is disabled, so custom `REMOTE_HOST_HEADERS`
     # should just pass through
-    middleware = HeaderTrackingMiddleware()
+    middleware = SetRemoteAddrFromRemoteHostHeader()
     get(url, user=admin, middleware=middleware, HTTP_X_FROM_THE_LOAD_BALANCER='some-actual-ip')
     assert middleware.environ['HTTP_X_FROM_THE_LOAD_BALANCER'] == 'some-actual-ip'
 
@@ -27,20 +19,20 @@ def test_proxy_ip_allowed(get, patch, admin):
     # from 8.9.10.11, the custom `HTTP_X_FROM_THE_LOAD_BALANCER` header should
     # be stripped
     patch(url, user=admin, data={'PROXY_IP_ALLOWED_LIST': ['10.0.1.100']})
-    middleware = HeaderTrackingMiddleware()
+    middleware = SetRemoteAddrFromRemoteHostHeader()
     get(url, user=admin, middleware=middleware, REMOTE_ADDR='8.9.10.11', HTTP_X_FROM_THE_LOAD_BALANCER='some-actual-ip')
     assert 'HTTP_X_FROM_THE_LOAD_BALANCER' not in middleware.environ
 
     # If 8.9.10.11 is added to `PROXY_IP_ALLOWED_LIST` the
     # `HTTP_X_FROM_THE_LOAD_BALANCER` header should be passed through again
     patch(url, user=admin, data={'PROXY_IP_ALLOWED_LIST': ['10.0.1.100', '8.9.10.11']})
-    middleware = HeaderTrackingMiddleware()
+    middleware = SetRemoteAddrFromRemoteHostHeader()
     get(url, user=admin, middleware=middleware, REMOTE_ADDR='8.9.10.11', HTTP_X_FROM_THE_LOAD_BALANCER='some-actual-ip')
     assert middleware.environ['HTTP_X_FROM_THE_LOAD_BALANCER'] == 'some-actual-ip'
 
     # Allow allowed list of proxy hostnames in addition to IP addresses
     patch(url, user=admin, data={'PROXY_IP_ALLOWED_LIST': ['my.proxy.example.org']})
-    middleware = HeaderTrackingMiddleware()
+    middleware = SetRemoteAddrFromRemoteHostHeader()
     get(url, user=admin, middleware=middleware, REMOTE_ADDR='8.9.10.11', REMOTE_HOST='my.proxy.example.org', HTTP_X_FROM_THE_LOAD_BALANCER='some-actual-ip')
     assert middleware.environ['HTTP_X_FROM_THE_LOAD_BALANCER'] == 'some-actual-ip'
 
