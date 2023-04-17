c08f1ddcaa6b619373758ff2c5e360de6466176d
Keith J. Grant <keithjgrant@redhat.com>
2023-04-17 15:34:10 -0700

    rework PersistentFilter to avoid double API call

--- awx/ui/src/screens/Credential/Credential.js
+++ awx/ui/src/screens/Credential/Credential.js
@@ -82,7 +82,7 @@ function Credential({ setBreadcrumb }) {
       ),
       link: `/credentials`,
       id: 99,
-      isBackButton: true,
+      persistentFilterKey: 'credentials',
     },
     { name: t`Details`, link: `/credentials/${id}/details`, id: 0 },
     {
