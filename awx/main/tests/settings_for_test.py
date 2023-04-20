d07fe637eeb234ab5449e3f3bd7425c4b49d8974
Alan Rominger <arominge@redhat.com>
2023-04-20 11:32:55 -0400

    Fix ordering problem

--- awx/main/tests/settings_for_test.py
+++ awx/main/tests/settings_for_test.py
@@ -1,7 +1,8 @@
-IS_TESTING_MODE = True
 from unittest import mock
 
 with mock.patch('__main__.__builtins__.dir', return_value=[]):
     import ldap  # NOQA
 
 from awx.settings.development import *  # NOQA
+
+IS_TESTING_MODE = True
