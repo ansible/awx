3f779d831da98ef8c0ba39c95147993c3bb8ee93
Alan Rominger <arominge@redhat.com>
2023-04-25 11:13:50 -0400

    Limit new OPTIONS data to the detail views

--- awxkit/awxkit/cli/options.py
+++ awxkit/awxkit/cli/options.py
@@ -96,15 +96,6 @@ class ResourceOptionsParser(object):
             self.build_list_actions()
             self.build_detail_actions()
 
-            # all normal resources are also considered to have association abilities
-            related_associations = options_json.get('related_associations', {})
-            self.parser.add_parser('associate', help='Associate via a related endpoint')
-            self.associate = AssociationParser(page, resource)
-            self.associate.add_arguments(self.parser, related_associations)
-            self.parser.add_parser('disassociate', help='Disassociate via a related endpoint')
-            self.disassociate = DisAssociationParser(page, resource)
-            self.disassociate.add_arguments(self.parser, related_associations)
-
         self.handle_custom_actions()
 
     def get_allowed_options(self):
@@ -125,6 +116,7 @@ class ResourceOptionsParser(object):
                 self.options['PUT'] = detail_actions['PUT']
             elif 'GET' in detail_actions:
                 self.options['PUT'] = detail_actions['GET']
+        self.related_associations = options.json().get('related_associations', {})
 
     def build_list_actions(self):
         action_map = {
@@ -169,6 +161,15 @@ class ResourceOptionsParser(object):
             )
             if method == 'get':
                 add_output_formatting_arguments(parser, {})
+
+        if self.resource != 'settings':
+            # all normal resources are also considered to have association abilities
+            self.parser.add_parser('associate', help='Associate via a related endpoint')
+            self.associate = AssociationParser(self.page, self.resource)
+            self.associate.add_arguments(self.parser, self.related_associations)
+            self.parser.add_parser('disassociate', help='Disassociate via a related endpoint')
+            self.disassociate = DisAssociationParser(self.page, self.resource)
+            self.disassociate.add_arguments(self.parser, self.related_associations)
 
     def build_query_arguments(self, method, http_method):
         required_group = None
