8a523aba0ee4a8c109cb2d979e7b03ab23db247b
Ricardo Carrillo Cruz <ricarril@redhat.com>
2023-06-01 14:14:36 +0200

    Add feature flag to allow instance API on non K8s environments

--- awx/settings/defaults.py
+++ awx/settings/defaults.py
@@ -1047,3 +1047,5 @@ CLEANUP_HOST_METRICS_LAST_TS = None
 CLEANUP_HOST_METRICS_INTERVAL = 30  # days
 # Host metrics cleanup - soft-delete HostMetric records with last_automation < [threshold] (in months)
 CLEANUP_HOST_METRICS_THRESHOLD = 12  # months
+# Feature flag to allow Instance API regardless if AWX is running on K8s
+FEATURE_API_INSTANCE_MANAGEMENT = False
