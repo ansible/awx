0669968ab15b643a459a9e23752c13c9104d2d7e
Gabe Muniz <gmuniz@redhat.com>
2023-06-02 12:13:57 -0400

    Moved unified_job_class_to_event_table_name to utils.common Imported the function to cleanup_jobs.py and precreate_partitions.py Removed hardcoded table names in favor of looping through the models

--- awx/main/management/commands/precreate_partitions.py
+++ awx/main/management/commands/precreate_partitions.py
@@ -1,7 +1,8 @@
 from django.utils.timezone import now
 from django.core.management.base import BaseCommand, CommandParser
 from datetime import timedelta
-from awx.main.utils.common import create_partition
+from awx.main.utils.common import create_partition, unified_job_class_to_event_table_name
+from awx.main.models import Job, SystemJob, ProjectUpdate, InventoryUpdate, AdHocCommand
 
 
 class Command(BaseCommand):
@@ -11,9 +12,12 @@ class Command(BaseCommand):
         parser.add_argument('--count', dest='count', action='store', help='The amount of hours of partitions to create', type=int, default=1)
 
     def _create_partitioned_tables(self, count):
+        tables = list()
+        for model in (Job, SystemJob, ProjectUpdate, InventoryUpdate, AdHocCommand):
+            tables.append(unified_job_class_to_event_table_name(model))
         start = now()
         while count > 0:
-            for table in ("main_inventoryupdateevent", "main_jobevent", "main_projectupdateevent", "main_systemjobevent"):
+            for table in tables:
                 create_partition(table, start)
                 print(f'Creating partitions for {table} {start}')
             start = start + timedelta(hours=1)
