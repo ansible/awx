49cd7d618db9bd3e9c01c1ab24e801beec83704e
Lila <djyasin.ly@gmail.com>
2023-06-30 15:26:15 -0400

    Added K8s import and override settings.

--- awx_collection/test/awx/test_instance.py
+++ awx_collection/test/awx/test_instance.py
@@ -5,47 +5,49 @@ __metaclass__ = type
 import pytest
 
 from awx.main.models import Instance
+from django.test.utils import override_settings
 
 
 @pytest.mark.django_db
 def test_hop_node_scenario(run_module, admin_user):
-    result = run_module(
-        'instance',
-        {'hostname': 'hopnode1', 'node_type': 'hop', 'peers_from_control_nodes': True, 'node_state': 'installed', 'listener_port': 27199},
-        admin_user,
-    )
-    assert result['changed']
+    with override_settings(IS_K8S=True):
+        result = run_module(
+            'instance',
+            {'hostname': 'hopnode1', 'node_type': 'hop', 'peers_from_control_nodes': True, 'node_state': 'installed', 'listener_port': 27199},
+            admin_user,
+        )
+        assert result['changed']
 
-    hop_node_1 = Instance.objects.get(pk=result.get('id'))
+        hop_node_1 = Instance.objects.get(pk=result.get('id'))
 
-    assert hop_node_1.peers_from_control_nodes == True
-    assert hop_node_1.node_type == 'hop'
+        assert hop_node_1.peers_from_control_nodes == True
+        assert hop_node_1.node_type == 'hop'
 
-    result = run_module(
-        'instance',
-        {'hostname': 'hopnode2', 'node_type': 'hop', 'peers_from_control_nodes': True, 'node_state': 'installed', 'listener_port': 27199},
-        admin_user,
-    )
-    assert result['changed']
+        result = run_module(
+            'instance',
+            {'hostname': 'hopnode2', 'node_type': 'hop', 'peers_from_control_nodes': True, 'node_state': 'installed', 'listener_port': 27199},
+            admin_user,
+        )
+        assert result['changed']
 
-    hop_node_2 = Instance.objects.get(pk=result.get('id'))
+        hop_node_2 = Instance.objects.get(pk=result.get('id'))
 
-    result = run_module(
-        'instance',
-        {'hostname': 'executionnode', 'node_type': 'execution', 'node_state': 'installed', 'listener_port': 27199, 'peers': ['hopnode1', 'hopnode2']},
-        admin_user,
-    )
-    assert result['changed']
+        result = run_module(
+            'instance',
+            {'hostname': 'executionnode', 'node_type': 'execution', 'node_state': 'installed', 'listener_port': 27199, 'peers': ['hopnode1', 'hopnode2']},
+            admin_user,
+        )
+        assert result['changed']
 
-    execution_node = Instance.objects.get(pk=result.get('id'))
+        execution_node = Instance.objects.get(pk=result.get('id'))
 
-    assert set(execution_node.peers.all()) == {hop_node_1, hop_node_2}
+        assert set(execution_node.peers.all()) == {hop_node_1, hop_node_2}
 
-    result = run_module(
-        'instance',
-        {'hostname': 'executionnode', 'node_type': 'execution', 'node_state': 'installed', 'listener_port': 27199, 'peers': []},
-        admin_user,
-    )
+        result = run_module(
+            'instance',
+            {'hostname': 'executionnode', 'node_type': 'execution', 'node_state': 'installed', 'listener_port': 27199, 'peers': []},
+            admin_user,
+        )
 
-    assert result['changed']
-    assert set(execution_node.peers.all()) == set()
+        assert result['changed']
+        assert set(execution_node.peers.all()) == set()
