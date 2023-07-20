056bdb80d1cb137f4fdad264bcfeae71d36320a2
jessicamack <jmack@redhat.com>
2023-07-20 14:19:53 -0400

    change package name and update licenses
    Signed-off-by: jessicamack <jmack@redhat.com>

--- awx/main/credential_plugins/dsv.py
+++ awx/main/credential_plugins/dsv.py
@@ -2,7 +2,7 @@ from .plugin import CredentialPlugin
 
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
-from thycotic.secrets.vault import SecretsVault
+from delinea.secrets.vault import SecretsVault
 
 
 dsv_inputs = {
