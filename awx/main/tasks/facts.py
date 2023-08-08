8b818fb645e79c0a1446bb41f4a821e03fee8f02
Alan Rominger <arominge@redhat.com>
2023-08-08 09:42:40 -0400

    Add a retry to update host facts on deadlocks

--- awx/main/tasks/facts.py
+++ awx/main/tasks/facts.py
@@ -9,6 +9,7 @@ from django.conf import settings
 from django.db.models.query import QuerySet
 from django.utils.encoding import smart_str
 from django.utils.timezone import now
+from django.db import OperationalError
 
 # AWX
 from awx.main.utils.common import log_excess_runtime
@@ -55,6 +56,21 @@ def start_fact_cache(hosts, destination, log_data, timeout=None, inventory_id=No
     if last_filepath_written:
         return os.path.getmtime(last_filepath_written)
     return None
+
+
+def update_hosts(host_list):
+    if not host_list:
+        return
+    for i in range(5):
+        try:
+            Host.objects.bulk_update(host_list, ['ansible_facts', 'ansible_facts_modified'])
+        except OperationalError as exc:
+            if 'deadlock detected' in str(exc):
+                logger.info(f'Deadlock saving host facts retry {i}')
+                continue
+            else:
+                raise
+        break
 
 
 @log_excess_runtime(
@@ -111,7 +127,5 @@ def finish_fact_cache(hosts, destination, facts_write_time, log_data, job_id=Non
             system_tracking_logger.info('Facts cleared for inventory {} host {}'.format(smart_str(host.inventory.name), smart_str(host.name)))
             log_data['cleared_ct'] += 1
         if len(hosts_to_update) > 100:
-            Host.objects.bulk_update(hosts_to_update, ['ansible_facts', 'ansible_facts_modified'])
-            hosts_to_update = []
-    if hosts_to_update:
-        Host.objects.bulk_update(hosts_to_update, ['ansible_facts', 'ansible_facts_modified'])
+            update_hosts(hosts_to_update)
+    update_hosts(hosts_to_update)
