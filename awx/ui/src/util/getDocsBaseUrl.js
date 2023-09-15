d1267e2e19b1735dd6cb0fcd403980c3b02179b0
Michael Abashian <mabashia@redhat.com>
2023-09-15 15:56:50 -0400

    Simplify docs string base generation

--- awx/ui/src/util/getDocsBaseUrl.js
+++ awx/ui/src/util/getDocsBaseUrl.js
@@ -2,16 +2,9 @@ export default function getDocsBaseUrl(config) {
   let version = 'latest';
   const licenseType = config?.license_info?.license_type;
 
-  if (licenseType && licenseType !== 'open') {
-    if (config?.version) {
-      if (parseFloat(config?.version.split('-')[0]) >= 4.3) {
-        version = parseFloat(config?.version.split('-')[0]);
-      } else {
-        version = config?.version.split('-')[0];
-      }
-    }
-  } else {
-    version = 'latest';
+  if (licenseType && licenseType !== 'open' && config?.version) {
+    version = parseFloat(config?.version.split('-')[0]).toFixed(1);
   }
+
   return `https://docs.ansible.com/automation-controller/${version}`;
 }
