283736165ea7fbaa357a10f5fc64554f5abd97f9
Alan Rominger <arominge@redhat.com>
2023-09-21 15:01:08 -0400

    Separate handling of original sitTERM and sigINT

--- awx/main/tests/unit/tasks/test_signals.py
+++ awx/main/tests/unit/tasks/test_signals.py
@@ -1,14 +1,17 @@
 import signal
-from contextlib import contextmanager
+import functools
 
 from awx.main.tasks.signals import signal_state, signal_callback, with_signal_handling
 
 
-def do_nothing():
-    do_nothing.called_count += 1
+def pytest_sigint():
+    pytest_sigint.called_count += 1
 
 
-@contextmanager
+def pytest_sigterm():
+    pytest_sigterm.called_count += 1
+
+
 def tmp_signals_for_test(func):
     """
     When we run our internal signal handlers, it will call the original signal
@@ -19,12 +22,14 @@ def tmp_signals_for_test(func):
     with new signal handlers that do nothing so that tests do not crash.
     """
 
+    @functools.wraps(func)
     def wrapper():
         original_sigterm = signal.getsignal(signal.SIGTERM)
         original_sigint = signal.getsignal(signal.SIGINT)
-        signal.signal(signal.SIGTERM, do_nothing)
-        signal.signal(signal.SIGINT, do_nothing)
-        do_nothing.called_count = 0
+        signal.signal(signal.SIGTERM, pytest_sigterm)
+        signal.signal(signal.SIGINT, pytest_sigint)
+        pytest_sigterm.called_count = 0
+        pytest_sigint.called_count = 0
         func()
         signal.signal(signal.SIGTERM, original_sigterm)
         signal.signal(signal.SIGINT, original_sigint)
@@ -45,17 +50,19 @@ def test_outer_inner_signal_handling():
     @with_signal_handling
     def f1():
         assert signal_callback() is False
-        signal_state.set_flag()
+        signal_state.set_sigterm_flag()
         assert signal_callback()
         f2()
 
     original_sigterm = signal.getsignal(signal.SIGTERM)
     assert signal_callback() is False
-    assert do_nothing.called_count == 0
+    assert pytest_sigterm.called_count == 0
+    assert pytest_sigint.called_count == 0
     f1()
     assert signal_callback() is False
     assert signal.getsignal(signal.SIGTERM) is original_sigterm
-    assert do_nothing.called_count == 1
+    assert pytest_sigterm.called_count == 1
+    assert pytest_sigint.called_count == 0
 
 
 @tmp_signals_for_test
@@ -67,7 +74,7 @@ def test_inner_outer_signal_handling():
     @with_signal_handling
     def f2():
         assert signal_callback() is False
-        signal_state.set_flag()
+        signal_state.set_sigint_flag()
         assert signal_callback()
 
     @with_signal_handling
@@ -78,8 +85,10 @@ def test_inner_outer_signal_handling():
 
     original_sigterm = signal.getsignal(signal.SIGTERM)
     assert signal_callback() is False
-    assert do_nothing.called_count == 0
+    assert pytest_sigterm.called_count == 0
+    assert pytest_sigint.called_count == 0
     f1()
     assert signal_callback() is False
     assert signal.getsignal(signal.SIGTERM) is original_sigterm
-    assert do_nothing.called_count == 1
+    assert pytest_sigterm.called_count == 0
+    assert pytest_sigint.called_count == 1
