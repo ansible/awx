c1697d91b2cc440fb9202830d4377989033d2721
Nico Ohnezat <nico@no-12.net>
2023-11-06 23:57:31 +0100

    add functional tests for cleanup_schedules

--- awx/main/tests/functional/commands/test_commands.py
+++ awx/main/tests/functional/commands/test_commands.py
@@ -1,11 +1,15 @@
 import sys
 import pytest
+from datetime import datetime, timedelta
+from unittest import mock
 
 from io import StringIO
 
 from django.core.management import call_command
 
 from awx.main.management.commands.update_password import UpdatePassword
+from awx.main.models import Schedule
+from awx.main.tests.factories.fixtures import mk_job_template, mk_schedule, mk_system_job_template
 
 
 def run_command(name, *args, **options):
@@ -49,3 +53,49 @@ def test_update_password_command(mocker, username, password, expected, changed):
             assert stdout == expected
         else:
             assert str(result) == expected
+
+
+@pytest.mark.django_db
+class TestCleanupSchedulesCases:
+    def mk_rrule_str(self, only_once=False):
+        rrule_str = "DTSTART:20200101T000000Z RRULE:FREQ=DAILY"
+        if only_once:
+            rrule_str += ";COUNT=1"
+        return rrule_str
+
+    @pytest.mark.parametrize(
+        "enabled,only_once",
+        [
+            (True, True),
+            (True, False),
+            (False, False),
+        ],
+    )
+    def test_never_cleanup_system_job_shedules(self, enabled, only_once):
+        sjt = mk_system_job_template(name="Cleanup Schedules")
+        mk_schedule(name="Cleanup Schedules", unified_job_template=sjt, rrule=self.mk_rrule_str(only_once=only_once), enabled=enabled)
+
+        run_command("cleanup_schedules", days=0)
+
+        assert Schedule.objects.count() == 1
+
+    @pytest.mark.parametrize(
+        "days_since_modified,enabled,only_once,expected",
+        [
+            (3, True, True, 0),
+            (1, True, True, 1),
+            (3, True, False, 1),
+            (1, True, False, 1),
+            (3, False, False, 0),
+            (1, False, False, 1),
+        ],
+    )
+    @mock.patch("django.utils.timezone.now", return_value=datetime.now())
+    def test_cleanup_schedules_of_regular_job_schedule(self, mocked_now, days_since_modified, enabled, only_once, expected):
+        jt = mk_job_template(name="Test")
+        mk_schedule(name="Test", unified_job_template=jt, rrule=self.mk_rrule_str(only_once=only_once), enabled=enabled)
+        mocked_now.return_value = datetime.now() + timedelta(days=days_since_modified)
+
+        run_command("cleanup_schedules", days=2)
+
+        assert Schedule.objects.count() == expected
