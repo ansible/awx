0057c8daf6c0e4077458384cc8ee5c4543d48f8f
Don Naro <dnaro@redhat.com>
2023-11-11 13:33:41 +0000

    Docs: Include REST API reference content from swagger.json (#14607)

--- docs/docsite/conf.py
+++ docs/docsite/conf.py
@@ -5,7 +5,7 @@ import shlex
 from datetime import datetime
 from importlib import import_module
 
-#sys.path.insert(0, os.path.abspath('./rst/rest_api/_swagger'))
+sys.path.insert(0, os.path.abspath('./rst/rest_api/_swagger'))
 
 project = u'Ansible AWX'
 copyright = u'2023, Red Hat'
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.ifconfig',
     'sphinx_ansible_theme',
+    'swagger',
 ]
 
 html_theme = 'sphinx_ansible_theme'
