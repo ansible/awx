4c74de2d78266a66ecee5ba0a22be0d61833e3b3
Seth Foster <fosterbseth@gmail.com>
2023-11-13 11:58:05 -0500

    Add validation when setting peers
    - cannot peer to self
    - cannot peer to instance that is already peered to self
    
    Other changes:
    - ReceptorAddress protocol field restricted to choices: tcp, ws, wss
    - fix awx-manage list_instances when instance.last_seen is None
    - InstanceLink make source and target unique together
    - Add help text to the ReceptorAddress fields
    
    Signed-off-by: Seth Foster <fosterbseth@gmail.com>

--- awx/main/models/receptor_address.py
+++ awx/main/models/receptor_address.py
@@ -18,17 +18,25 @@ class ReceptorAddress(models.Model):
             )
         ]
 
-    address = models.CharField(max_length=255)
-    port = models.IntegerField(default=27199, validators=[MinValueValidator(0), MaxValueValidator(65535)])
-    protocol = models.CharField(max_length=10, default="tcp")
-    websocket_path = models.CharField(max_length=255, default="", blank=True)
-    is_internal = models.BooleanField(default=False)
-    peers_from_control_nodes = models.BooleanField(default=False)
+    class Protocols(models.TextChoices):
+        TCP = 'tcp', _('TCP')
+        WS = 'ws', _('WS')
+        WSS = 'wss', _('WSS')
+
+    address = models.CharField(help_text=_("Routable address for this instance."), max_length=255)
+    port = models.IntegerField(help_text=_("Port for the address."), default=27199, validators=[MinValueValidator(0), MaxValueValidator(65535)])
+    protocol = models.CharField(help_text=_("Protocol to use when connecting, 'tcp' or 'ws'."), max_length=10, default=Protocols.TCP, choices=Protocols.choices)
+    websocket_path = models.CharField(help_text=_("Websocket path."), max_length=255, default="", blank=True)
+    is_internal = models.BooleanField(help_text=_("If True, only routable inside of the Kubernetes cluster."), default=False)
+    peers_from_control_nodes = models.BooleanField(help_text=_("If True, control plane cluster nodes should automatically peer to it."), default=False)
     instance = models.ForeignKey(
         'Instance',
         related_name='receptor_addresses',
         on_delete=models.CASCADE,
     )
+
+    def __str__(self):
+        return self.get_full_address()
 
     def get_full_address(self):
         scheme = ""
