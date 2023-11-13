b06c80d213524e3043f8a19b8246b0a0f0789fb7
Lila <djyasin.ly@gmail.com>
2023-11-13 15:29:27 -0500

    Add username and password to handle_auth and update exception message
    Revise naming of ldap username and password

--- awx/main/credential_plugins/hashivault.py
+++ awx/main/credential_plugins/hashivault.py
@@ -88,6 +88,20 @@ base_inputs = {
             ),
         },
         {
+            'id': 'username',
+            'label': _('Username'),
+            'type': 'string',
+            'secret': False,
+            'help_text': _('Username for authentication.'),
+        },
+        {
+            'id': 'password',
+            'label': _('Password'),
+            'type': 'string',
+            'secret': True,
+            'help_text': _('Password for user authentication.'),
+        },
+        {
             'id': 'default_auth_path',
             'label': _('Path to Auth'),
             'type': 'string',
@@ -185,19 +199,23 @@ hashi_ssh_inputs['required'].extend(['public_key', 'role'])
 
 def handle_auth(**kwargs):
     token = None
-
     if kwargs.get('token'):
         token = kwargs['token']
     elif kwargs.get('role_id') and kwargs.get('secret_id'):
         token = method_auth(**kwargs, auth_param=approle_auth(**kwargs))
     elif kwargs.get('kubernetes_role'):
         token = method_auth(**kwargs, auth_param=kubernetes_auth(**kwargs))
+    elif kwargs.get('username') and kwargs.get('password'):
+        token = method_auth(**kwargs, auth_param=ldap_auth(**kwargs))
     elif kwargs.get('client_cert_public') and kwargs.get('client_cert_private'):
         token = method_auth(**kwargs, auth_param=client_cert_auth(**kwargs))
     else:
-        raise Exception('Either a token or AppRole, Kubernetes, or TLS authentication parameters must be set')
-
+        raise Exception('Token, LDAP, AppRole, Kubernetes, or TLS authentication parameters must be set')
     return token
+
+
+def ldap_auth(**kwargs):
+    return {'username': kwargs['username'], 'password': kwargs['password']}
 
 
 def approle_auth(**kwargs):
@@ -229,20 +247,25 @@ def method_auth(**kwargs):
     sess = requests.Session()
 
     # Namespace support
-    if kwargs.get('namespace'):
-        sess.headers['X-Vault-Namespace'] = kwargs['namespace']
-    request_url = '/'.join([url, 'auth', auth_path, 'login']).rstrip('/')
-    with CertFiles(cacert) as cert:
-        request_kwargs['verify'] = cert
-        # TLS client certificate support
-        if kwargs.get('client_cert_public') and kwargs.get('client_cert_private'):
-            # Add client cert to requests Session before making call
-            with CertFiles(kwargs['client_cert_public'], key=kwargs['client_cert_private']) as client_cert:
-                sess.cert = client_cert
+    if kwargs.get('username'):
+        request_url = '/'.join([url, 'auth', auth_path, 'login', kwargs['username']]).rstrip('/')
+        resp = sess.post(request_url, **request_kwargs)
+        token = resp.json()['auth']['client_token']
+    else:
+        if kwargs.get('namespace'):
+            sess.headers['X-Vault-Namespace'] = kwargs['namespace']
+        request_url = '/'.join([url, 'auth', auth_path, 'login']).rstrip('/')
+        with CertFiles(cacert) as cert:
+            request_kwargs['verify'] = cert
+            # TLS client certificate support
+            if kwargs.get('client_cert_public') and kwargs.get('client_cert_private'):
+                # Add client cert to requests Session before making call
+                with CertFiles(kwargs['client_cert_public'], key=kwargs['client_cert_private']) as client_cert:
+                    sess.cert = client_cert
+                    resp = sess.post(request_url, **request_kwargs)
+            else:
+                # Make call without client certificate
                 resp = sess.post(request_url, **request_kwargs)
-        else:
-            # Make call without client certificate
-            resp = sess.post(request_url, **request_kwargs)
     resp.raise_for_status()
     token = resp.json()['auth']['client_token']
     return token
