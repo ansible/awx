bcd10be745efdae40fe063d44b369854543dd99e
Chris Meyers <chris.meyers.fsu@gmail.com>
2023-11-15 12:58:33 -0500

    allow pytest --migrations to succeed
    * We actually subvert migrations from running in test via pytest.ini
      --no-migrations option. This has led to bit rot for the sqlite
      migrations happy path. This changeset pays off that tech debt and
      allows for an sqlite migration happy path.
    * This paves the way for programatic invocation of individual migrations
      and weaving of the creation of resources (i.e. Instance, Job Template,
      etc). With this, a developer can instantiate various database states,
      trigger a migration, assert the state of the db, and then have pytest
      rollback all of that.
    * I will note that in practice, running these migrations is dog shit
      slow BUT this work also opens up the possibility of saving and
      re-using sqlite3 database files. Normally, caching is not THE answer
      and causes more harm than good. But in this case, our migrations are
      mostly write-once (I say mostly because this change set violates
      that :) so cache invalidation isn't a major issue.

--- awx/main/migrations/0050_v340_drop_celery_tables.py
+++ awx/main/migrations/0050_v340_drop_celery_tables.py
@@ -3,24 +3,27 @@ from __future__ import unicode_literals
 
 from django.db import migrations
 
+from ._sqlite_helper import dbawaremigrations
+
+tables_to_drop = [
+    'celery_taskmeta',
+    'celery_tasksetmeta',
+    'djcelery_crontabschedule',
+    'djcelery_intervalschedule',
+    'djcelery_periodictask',
+    'djcelery_periodictasks',
+    'djcelery_taskstate',
+    'djcelery_workerstate',
+    'djkombu_message',
+    'djkombu_queue',
+]
+postgres_sql = ([("DROP TABLE IF EXISTS {} CASCADE;".format(table))] for table in tables_to_drop)
+sqlite_sql = ([("DROP TABLE IF EXISTS {};".format(table))] for table in tables_to_drop)
+
 
 class Migration(migrations.Migration):
     dependencies = [
         ('main', '0049_v330_validate_instance_capacity_adjustment'),
     ]
 
-    operations = [
-        migrations.RunSQL([("DROP TABLE IF EXISTS {} CASCADE;".format(table))])
-        for table in (
-            'celery_taskmeta',
-            'celery_tasksetmeta',
-            'djcelery_crontabschedule',
-            'djcelery_intervalschedule',
-            'djcelery_periodictask',
-            'djcelery_periodictasks',
-            'djcelery_taskstate',
-            'djcelery_workerstate',
-            'djkombu_message',
-            'djkombu_queue',
-        )
-    ]
+    operations = [dbawaremigrations.RunSQL(p, sqlite_sql=s) for p, s in zip(postgres_sql, sqlite_sql)]
