5c0e152c242afcdc4b944b6908977a8cb423829f
jessicamack <jmack@redhat.com>
2023-12-15 16:09:05 -0500

    change mock import
    Signed-off-by: jessicamack <jmack@redhat.com>

--- awx/main/tests/unit/utils/test_filters.py
+++ awx/main/tests/unit/utils/test_filters.py
@@ -69,7 +69,7 @@ class mockHost:
 @mock.patch('awx.main.utils.filters.get_model', return_value=mockHost())
 class TestSmartFilterQueryFromString:
     @mock.patch(
-        'ansible_base.utils.filters.get_fields_from_path', lambda model, path: ([model], path)
+        'ansible_base.filters.rest_framework.field_lookup_backend.get_fields_from_path', lambda model, path: ([model], path)
     )  # disable field filtering, because a__b isn't a real Host field
     @pytest.mark.parametrize(
         "filter_string,q_expected",
