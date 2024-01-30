9571db5f979e960ce11dae47fe2221acca53fcaf
jessicamack <jmack@redhat.com>
2024-01-30 11:02:43 -0500

    update urls

--- awx/api/urls/urls.py
+++ awx/api/urls/urls.py
@@ -86,6 +86,8 @@ from .workflow_approval_template import urls as workflow_approval_template_urls
 from .workflow_approval import urls as workflow_approval_urls
 from .analytics import urls as analytics_urls
 
+from ansible_base.lib.dynamic_config.dynamic_urls import api_version_urls, api_urls
+
 v2_urls = [
     re_path(r'^$', ApiV2RootView.as_view(), name='api_v2_root_view'),
     re_path(r'^credential_types/', include(credential_type_urls)),
@@ -160,7 +162,9 @@ v2_urls = [
 
 app_name = 'api'
 urlpatterns = [
+    re_path(r'^$', include(api_urls)),
     re_path(r'^$', ApiRootView.as_view(), name='api_root_view'),
+    re_path(r'^(?P<version>(v2))/', include(api_version_urls)),
     re_path(r'^(?P<version>(v2))/', include(v2_urls)),
     re_path(r'^login/$', LoggedLoginView.as_view(template_name='rest_framework/login.html', extra_context={'inside_login_context': True}), name='login'),
     re_path(r'^logout/$', LoggedLogoutView.as_view(next_page='/api/', redirect_field_name='next'), name='logout'),
