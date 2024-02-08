40150a2be840aa75537165237ee398a38e01e96a
Seth Foster <fosterseth@users.noreply.github.com>
2024-02-08 14:13:11 -0500

    Fix UI peers_from_control_nodes (#14858)
    * Fix UI peers_from_control_nodes
    
    Fixes bug where peers_from_control_node was
    greyed out in UI.
    
    Additional changes:
    - Make edit instance button only show for instances
    with managed = False
    - Make remove instance button only show for instances
    with managed = False
    - InstanceList selectable only for instances with
    managed = False
    
    ---------
    
    Signed-off-by: Seth Foster <fosterbseth@gmail.com>

--- awx/ui/src/screens/Instances/InstanceList/InstanceListItem.js
+++ awx/ui/src/screens/Instances/InstanceList/InstanceListItem.js
@@ -114,7 +114,6 @@ function InstanceListItem({
   );
 
   const isHopNode = instance.node_type === 'hop';
-  const isExecutionNode = instance.node_type === 'execution';
   const isManaged = instance.managed;
 
   return (
@@ -140,7 +139,7 @@ function InstanceListItem({
             rowIndex,
             isSelected,
             onSelect,
-            disable: !(isExecutionNode || isHopNode || isManaged),
+            disable: isManaged,
           }}
           dataLabel={t`Selected`}
         />
