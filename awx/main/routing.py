c2e498a5ea7f0a12bad894cb5c4916823a5779fe
Chris Meyers <chris.meyers.fsu@gmail.com>
2024-02-12 15:28:03 -0500

    Allow connecting to websockets via api/websocket/
    * Before, we just allowed websockets on <host>/websocket/. With this
      change, they can now come from <host>/api/websocket/

--- awx/main/routing.py
+++ awx/main/routing.py
@@ -27,6 +27,7 @@ class AWXProtocolTypeRouter(ProtocolTypeRouter):
 
 
 websocket_urlpatterns = [
+    re_path(r'api/websocket/$', consumers.EventConsumer.as_asgi()),
     re_path(r'websocket/$', consumers.EventConsumer.as_asgi()),
     re_path(r'websocket/relay/$', consumers.RelayConsumer.as_asgi()),
 ]
