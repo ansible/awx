5c7c9e5f44bc7507f5ca3ecee9e303e1acd18c49
Alan Rominger <arominge@redhat.com>
2024-02-16 09:50:25 -0500

    Use more code from DAB RBAC migration utils

--- awx/main/migrations/_dab_rbac.py
+++ awx/main/migrations/_dab_rbac.py
@@ -2,14 +2,9 @@ import json
 import logging
 
 from django.apps import apps as global_apps
-from django.conf import settings
 from django.db.models import ForeignKey
 from django.utils.timezone import now
-from django.contrib.contenttypes.management import create_contenttypes
-from django.db import DEFAULT_DB_ALIAS, router
-
-from ansible_base.rbac.migrations._utils import give_permissions
-from ansible_base.rbac import permission_registry
+from ansible_base.rbac.migrations._utils import give_permissions, create_custom_permissions
 
 from awx.main.fields import ImplicitRoleField
 from awx.main.constants import role_name_to_perm_mapping
@@ -18,93 +13,8 @@ from awx.main.constants import role_name_to_perm_mapping
 logger = logging.getLogger('awx.main.migrations._dab_rbac')
 
 
-def create_permissions(
-    app_config,
-    verbosity=2,
-    interactive=True,
-    using=DEFAULT_DB_ALIAS,
-    apps=global_apps,
-    **kwargs,
-):
-    """
-    This is modified from the django auth
-    """
-    if not app_config.models_module:
-        return
-
-    # Ensure that contenttypes are created for this app. Needed if
-    # 'django.contrib.auth' is in INSTALLED_APPS before
-    # 'django.contrib.contenttypes'.
-    create_contenttypes(
-        app_config,
-        verbosity=verbosity,
-        interactive=interactive,
-        using=using,
-        apps=apps,
-        **kwargs,
-    )
-
-    app_label = app_config.label
-    try:
-        app_config = apps.get_app_config(app_label)
-        ContentType = apps.get_model("contenttypes", "ContentType")
-        Permission = apps.get_model("main", "DABPermission")
-    except LookupError:
-        return
-
-    if not router.allow_migrate_model(using, Permission):
-        return
-
-    permission_models = [cls._meta.model_name for cls in permission_registry.all_registered_models]
-
-    # This will hold the permissions we're looking for as (content_type, (codename, name))
-    searched_perms = []
-    # The codenames and ctypes that should exist.
-    ctypes = set()
-    for klass in app_config.get_models():
-        if klass._meta.model_name not in permission_models:
-            continue
-        # Force looking up the content types in the current database
-        # before creating foreign keys to them.
-        ctype = ContentType.objects.db_manager(using).get_for_model(klass, for_concrete_model=False)
-
-        ctypes.add(ctype)
-
-        for action in klass._meta.default_permissions:
-            searched_perms.append(
-                (
-                    ctype,
-                    (
-                        f"{action}_{klass._meta.model_name}",
-                        f"Can {action} {klass._meta.verbose_name_raw}",
-                    ),
-                )
-            )
-        for codename, name in klass._meta.permissions:
-            searched_perms.append((ctype, (codename, name)))
-
-    # Find all the Permissions that have a content_type for a model we're
-    # looking for.  We don't need to check for codenames since we already have
-    # a list of the ones we're going to create.
-    all_perms = set(Permission.objects.using(using).filter(content_type__in=ctypes).values_list("content_type", "codename"))
-
-    perms = []
-    for ct, (codename, name) in searched_perms:
-        if (ct.pk, codename) not in all_perms:
-            permission = Permission()
-            permission._state.db = using
-            permission.codename = codename
-            permission.name = name
-            permission.content_type = ct
-            perms.append(permission)
-
-    Permission.objects.using(using).bulk_create(perms)
-    for perm in perms:
-        logger.debug("Adding permission '%s'" % perm)
-
-
 def create_permissions_as_operation(apps, schema_editor):
-    create_permissions(global_apps.get_app_config("main"))
+    create_custom_permissions(global_apps.get_app_config("main"))
 
 
 """
@@ -234,9 +144,6 @@ def migrate_to_new_rbac(apps, schema_editor):
     """
     Role = apps.get_model('main', 'Role')
     RoleDefinition = apps.get_model('dab_rbac', 'RoleDefinition')
-    ObjectRole = apps.get_model('dab_rbac', 'ObjectRole')
-    RoleUserAssignment = apps.get_model('dab_rbac', 'RoleUserAssignment')
-    RoleTeamAssignment = apps.get_model('dab_rbac', 'RoleTeamAssignment')
     Permission = apps.get_model('auth', 'Permission')
     migration_time = now()
 
