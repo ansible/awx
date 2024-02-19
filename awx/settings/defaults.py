35b666bdeb85c254dda480bbe08344d3ceb7b42c
jessicamack <jmack@redhat.com>
2024-02-19 10:26:32 -0500

    add resources api to controller

--- awx/settings/defaults.py
+++ awx/settings/defaults.py
@@ -353,6 +353,7 @@ INSTALLED_APPS = [
     'awx.sso',
     'solo',
     'ansible_base.rest_filters',
+    'ansible_base.resource_registry',
 ]
 
 INTERNAL_IPS = ('127.0.0.1',)
@@ -1108,6 +1109,7 @@ METRICS_SUBSYSTEM_CONFIG = {
 # django-ansible-base
 ANSIBLE_BASE_TEAM_MODEL = 'main.Team'
 ANSIBLE_BASE_ORGANIZATION_MODEL = 'main.Organization'
+ANSIBLE_BASE_RESOURCE_CONFIG_MODULE = 'awx.resource_api'
 
 from ansible_base.lib import dynamic_config  # noqa: E402
 
