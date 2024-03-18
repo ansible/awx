2b80826a9c1409d8daf08135ba85ac051a00c563
jessicamack <jmack@redhat.com>
2024-03-18 10:04:07 -0400

    restore previous tests

--- awx/sso/tests/unit/test_fields.py
+++ awx/sso/tests/unit/test_fields.py
@@ -1,8 +1,10 @@
 import pytest
+from unittest import mock
 
 from rest_framework.exceptions import ValidationError
 
-from awx.sso.fields import SAMLOrgAttrField, SAMLTeamAttrField, SAMLUserFlagsAttrField
+from awx.sso.fields import SAMLOrgAttrField, SAMLTeamAttrField, SAMLUserFlagsAttrField, LDAPGroupTypeParamsField, LDAPServerURIField
+
 
 from ansible_base.authentication.authenticator_plugins.ldap import LDAPConfiguration
 from ansible_base.lib.serializers.fields import URLField
@@ -194,6 +196,47 @@ class TestSAMLUserFlagsAttrField:
             field.to_internal_value(data)
         print(e.value.detail)
         assert e.value.detail == expected
+
+
+class TestLDAPGroupTypeParamsField:
+    @pytest.mark.parametrize(
+        "group_type, data, expected",
+        [
+            ('LDAPGroupType', {'name_attr': 'user', 'bob': ['a', 'b'], 'scooter': 'hello'}, ['Invalid key(s): "bob", "scooter".']),
+            ('MemberDNGroupType', {'name_attr': 'user', 'member_attr': 'west', 'bob': ['a', 'b'], 'scooter': 'hello'}, ['Invalid key(s): "bob", "scooter".']),
+            (
+                'PosixUIDGroupType',
+                {'name_attr': 'user', 'member_attr': 'west', 'ldap_group_user_attr': 'legacyThing', 'bob': ['a', 'b'], 'scooter': 'hello'},
+                ['Invalid key(s): "bob", "member_attr", "scooter".'],
+            ),
+        ],
+    )
+    def test_internal_value_invalid(self, group_type, data, expected):
+        field = LDAPGroupTypeParamsField()
+        field.get_depends_on = mock.MagicMock(return_value=group_type)
+
+        with pytest.raises(ValidationError) as e:
+            field.to_internal_value(data)
+        assert e.value.detail == expected
+
+
+class TestOldLDAPServerURIField:
+    @pytest.mark.parametrize(
+        "ldap_uri, exception, expected",
+        [
+            (r'ldap://servername.com:444', None, r'ldap://servername.com:444'),
+            (r'ldap://servername.so3:444', None, r'ldap://servername.so3:444'),
+            (r'ldaps://servername3.s300:344', None, r'ldaps://servername3.s300:344'),
+            (r'ldap://servername.-so3:444', ValidationError, None),
+        ],
+    )
+    def test_run_validators_valid(self, ldap_uri, exception, expected):
+        field = LDAPServerURIField()
+        if exception is None:
+            assert field.run_validators(ldap_uri) == expected
+        else:
+            with pytest.raises(exception):
+                field.run_validators(ldap_uri)
 
 
 class TestLDAPGroupTypeParams:
--- awx/sso/tests/unit/test_ldap.py
+++ awx/sso/tests/unit/test_ldap.py
@@ -1,8 +1,30 @@
 import ldap
 
+from awx.sso.backends import LDAPSettings
+from awx.sso.validators import validate_ldap_filter
 from ansible_base.authentication.authenticator_plugins.ldap import LDAPSettings, validate_ldap_filter
 
 from django.core.cache import cache
+
+
+def test_old_ldap_default_settings(mocker):
+    from_db = mocker.Mock(**{'order_by.return_value': []})
+    with mocker.patch('awx.conf.models.Setting.objects.filter', return_value=from_db):
+        settings = LDAPSettings()
+        assert settings.ORGANIZATION_MAP == {}
+        assert settings.TEAM_MAP == {}
+
+
+def test_old_ldap_default_network_timeout(mocker):
+    cache.clear()  # clearing cache avoids picking up stray default for OPT_REFERRALS
+    from_db = mocker.Mock(**{'order_by.return_value': []})
+    with mocker.patch('awx.conf.models.Setting.objects.filter', return_value=from_db):
+        settings = LDAPSettings()
+        assert settings.CONNECTION_OPTIONS[ldap.OPT_NETWORK_TIMEOUT] == 30
+
+
+def test_old_ldap_filter_validator():
+    validate_ldap_filter('(test-uid=%(user)s)', with_user=True)
 
 
 def test_ldap_default_settings(ldap_configuration):
