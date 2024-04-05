de91ef9710d2c1d93e8307d9cfd2bfc86f7196e0
Alan Rominger <arominge@redhat.com>
2024-04-05 09:28:16 -0400

    Changes to live with test writing

--- awx/main/models/rbac.py
+++ awx/main/models/rbac.py
@@ -679,11 +679,10 @@ def sync_parents_to_new_rbac(instance, action, model, pk_set, reverse, **kwargs)
         # To a fault, we want to avoid running this if triggered from implicit_parents management
         # we only want to do anything if we know for sure this is a non-implicit team role
         if parent_role.role_field in 'member_role' and parent_role.content_type.model == 'team':
-            # Team member role is a parent of its read role
-            # same for admin role as parent of member role
-            # so, for the same object, this parenting
-            # will also be implicit_parents management, but teams may still be assigned permissions to other teams
-            if child_role.content_type.model == 'team' and child_role.object_id == parent_role.object_id:
+            # Team internal parents are member_role->read_role and admin_role->member_role
+            # for the same object, this parenting will also be implicit_parents management
+            # do nothing for internal parents, but OTHER teams may still be assigned permissions to a team
+            if (child_role.content_type_id == parent_role.content_type_id) and (child_role.object_id == parent_role.object_id):
                 return
 
             from awx.main.models.organization import Team
