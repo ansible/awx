e0586aae76ccc411eac412f4bdfd1bd3296b05e3
David Newswanger <dnewswan@redhat.com>
2024-04-09 10:29:55 -0600

    Implement additional data endpoint for users

--- awx/resource_api.py
+++ awx/resource_api.py
@@ -3,8 +3,39 @@ from ansible_base.resource_registry.shared_types import OrganizationType, TeamTy
 
 from awx.main import models
 
+from ansible_base.resource_registry.utils.resource_type_processor import ResourceTypeProcessor
+
+
+class UserMapper:
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+
+class AwxUserProcessor(ResourceTypeProcessor):
+    def pre_serialize_additional(self):
+
+        # TODO: is there a better way to do this?
+        user = UserMapper(
+            username=self.instance.username,
+            email=self.instance.email,
+            first_name=self.instance.first_name,
+            last_name=self.instance.last_name,
+            is_superuser=self.instance.is_superuser,
+            external_auth_provider=None,
+            external_auth_uid=None,
+            organizations=models.Organization.objects.filter(member_role__members=self.instance),
+            teams=models.Team.objects.filter(member_role__members=self.instance),
+            organizations_administered=models.Organization.objects.filter(admin_role__members=self.instance),
+            teams_administered=models.Team.objects.filter(admin_role__members=self.instance),
+        )
+
+        return user
+
 
 class APIConfig(ServiceAPIConfig):
+    custom_resource_processors = {"shared.user": AwxUserProcessor}
+
     service_type = "awx"
 
 
