de25408a2310cd054d84d729b263ec3d7c7839f4
Jeff Bradberry <jeff.bradberry@gmail.com>
2024-04-30 09:09:05 -0400

    Print out details of all of the crosslinked roles

--- tools/scripts/ig-hotfix/role_check.py
+++ tools/scripts/ig-hotfix/role_check.py
@@ -3,4 +3,9 @@ from collections import Counter
 from awx.main.models.rbac import Role
 
 
-print(Counter(r.id == getattr(getattr(r.content_object, r.role_field, None), 'id', None) for r in Role.objects.all() if r.content_type))
+for r in Role.objects.all():
+    if not r.content_type:
+        continue
+    if r.id != getattr(getattr(r.content_object, r.role_field, None), 'id', None):
+        rev = getattr(r.content_object, r.role_field, None)
+        print(f"role.id={r.id}   '{r.content_type}' id={r.object_id} field={r.role_field} | obj.roleid={getattr(rev, 'id', None)} {getattr(rev, 'content_type', None)} {getattr(rev, 'object_id', None)} {getattr(rev, 'role_field', None)}")
