6885601509a1900dcf835f40de88d937ed25bbe8
Seth Foster <fosterbseth@gmail.com>
2024-05-21 11:26:36 -0400

    import EXPORTABLE_RESOURCES
    Signed-off-by: Seth Foster <fosterbseth@gmail.com>

--- awx_collection/plugins/modules/import.py
+++ awx_collection/plugins/modules/import.py
@@ -56,6 +56,8 @@ import logging
 
 # In this module we don't use EXPORTABLE_RESOURCES, we just want to validate that our installed awxkit has import/export
 try:
+    from awxkit.api.pages.api import EXPORTABLE_RESOURCES # noqa: F401; pylint: disable=unused-import
+
     HAS_EXPORTABLE_RESOURCES = True
 except ImportError:
     HAS_EXPORTABLE_RESOURCES = False
