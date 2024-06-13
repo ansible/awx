6c1c33e47d9c8337d303b5a433964e88d500c7f7
Neev Geffen <133771232+neevnuv@users.noreply.github.com>
2024-06-13 15:00:46 +0300

    add HasCreate

--- awxkit/awxkit/api/pages/credential_input_sources.py
+++ awxkit/awxkit/api/pages/credential_input_sources.py
@@ -1,10 +1,11 @@
 from awxkit.api.resources import resources
 from awxkit.api.pages import Credential
+from awxkit.api.mixins import HasCreate
 from . import base
 from . import page
 
 
-class CredentialInputSource(base.Base):
+class CredentialInputSource(HasCreate, base.Base):
     dependencies = [Credential]
     NATURAL_KEY=('target_credential', 'input_field_name')
 
