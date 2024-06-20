01227cba253d79910fa595758b80cecc71c47eee
Alan Rominger <arominge@redhat.com>
2024-06-20 10:32:40 -0400

    Mark tests to AAP number

--- awx/main/tests/functional/dab_rbac/test_access_regressions.py
+++ awx/main/tests/functional/dab_rbac/test_access_regressions.py
@@ -7,6 +7,7 @@ from ansible_base.rbac.models import RoleDefinition
 
 @pytest.mark.django_db
 def test_instance_group_object_role_delete(rando, instance_group, setup_managed_roles):
+    """Basic functionality of IG object-level admin role function AAP-25506"""
     rd = RoleDefinition.objects.get(name='InstanceGroup Admin')
     rd.give_permission(rando, instance_group)
     access = InstanceGroupAccess(rando)
@@ -15,6 +16,7 @@ def test_instance_group_object_role_delete(rando, instance_group, setup_managed_
 
 @pytest.mark.django_db
 def test_notification_template_object_role_change(rando, notification_template, setup_managed_roles):
+    """Basic functionality of NT object-level admin role function AAP-25493"""
     rd = RoleDefinition.objects.get(name='NotificationTemplate Admin')
     rd.give_permission(rando, notification_template)
     access = NotificationTemplateAccess(rando)
