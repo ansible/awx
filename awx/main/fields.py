af900c8370ab9cdb8a2e364778df948a7ea3e673
Peter Braun <pbraun@redhat.com>
2024-09-17 13:37:35 +0200

    fix: maintain order of insertions into m2m relationship tables (#15536)

--- awx/main/fields.py
+++ awx/main/fields.py
@@ -1039,7 +1039,7 @@ class OrderedManyToManyField(models.ManyToManyField):
             descriptor = getattr(instance, self.name)
             order_with_respect_to = descriptor.source_field_name
 
-            for i, ig in enumerate(sender.objects.filter(**{order_with_respect_to: instance.pk})):
+            for i, ig in enumerate(sender.objects.filter(**{order_with_respect_to: instance.pk}).order_by('id')):
                 if ig.position != i:
                     ig.position = i
                     ig.save()
