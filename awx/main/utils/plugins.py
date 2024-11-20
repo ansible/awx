55006f68a48e9811556d615c4b02e9bc5dbdef89
Lila <djyasin.ly@gmail.com>
2024-11-20 16:00:24 -0500

    Add function to extract plugin name descriptions

--- awx/main/utils/plugins.py
+++ awx/main/utils/plugins.py
@@ -44,6 +44,24 @@ def compute_cloud_inventory_sources() -> dict[str, str]:
 
 
 @cache
+def discover_available_cloud_provider_descriptions() -> dict[str, str]:
+    """
+    Return a dictionary of cloud provider plugin descriptions
+    available.
+
+    :returns: Dictionary of plugin cloud descriptions.
+    :rtype: dict[str, str]
+    """
+    from awx.main.models.inventory import InventorySourceOptions
+
+    plugin_description_list = [(plugin_name, plugin.plugin_description) for plugin_name, plugin in InventorySourceOptions.injectors.items()]
+
+    plugin_description = dict(plugin_description_list)
+
+    return plugin_description
+
+
+@cache
 def load_combined_inventory_source_options() -> dict[str, str]:
     """
     Return a dictionary of cloud provider plugin names and 'file'.
@@ -56,7 +74,9 @@ def load_combined_inventory_source_options() -> dict[str, str]:
 
     plugins = compute_cloud_inventory_sources()
 
-    result = {plugin: plugin.plugin_description for plugin in plugins}
+    plugin_description = discover_available_cloud_provider_descriptions()
+
+    result = {plugin: plugin_description.get(plugin, plugin) for plugin in plugins}
 
     result['file'] = 'File-based inventory source'
 
