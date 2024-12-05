417181db7991a6dc216bb13f6d0601d5618cf081
AlanCoding <arominge@redhat.com>
2024-12-05 11:46:30 -0500

    lint fix

--- awx/main/tests/live/tests/conftest.py
+++ awx/main/tests/live/tests/conftest.py
@@ -1,5 +1,3 @@
-import pytest
-
 # These tests are invoked from the awx/main/tests/live/ subfolder
 # so any fixtures from higher-up conftest files must be explicitly included
 from awx.main.tests.functional.conftest import *  # noqa
