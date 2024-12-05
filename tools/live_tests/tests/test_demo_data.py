f6266830a7ed3633129899fc98a7c9aa7e6ad92f
AlanCoding <arominge@redhat.com>
2024-12-05 09:25:56 -0500

    wait longer

--- tools/live_tests/tests/test_demo_data.py
+++ tools/live_tests/tests/test_demo_data.py
@@ -13,10 +13,25 @@ def test_launch_demo_jt(post, admin_user):
     r = post(url=url, data={}, user=admin_user, expect=201)
     job = Job.objects.get(pk=r.data['id'])
 
+    # Loop to wait for task manager processing
     start = time.time()
     while time.time() - start < 25:
         job.refresh_from_db()
-        if job.status not in ('pending', 'waiting', 'running'):
+        if job.status != 'pending':
+            break
+
+    # Loop to wait for dispatcher
+    start = time.time()
+    while time.time() - start < 25:
+        job.refresh_from_db()
+        if job.status != 'waiting':
+            break
+
+    # Loop to wait for job to actually finish
+    start = time.time()
+    while time.time() - start < 800:
+        job.refresh_from_db()
+        if job.status != 'running':
             break
 
     assert job.status == 'successful', f'Job was not successful id={job.id} status={job.status}'
