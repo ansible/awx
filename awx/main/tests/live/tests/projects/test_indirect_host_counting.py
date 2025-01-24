c31436a077036cabc887be998a28d53ed7fdb86a
Alan Rominger <arominge@redhat.com>
2025-01-24 13:35:18 -0500

    Successfully test collection of `event_query.yml` data (#15761)
    * Callback plugin method from cmeyers adapted to global collection list
    
    Get tests passing
    
    Mild rebranding
    
    Put behind feature flag, flip true in dev
    
    Add noqa flag
    
    * Add missing wait_for_events

--- awx/main/tests/live/tests/projects/test_indirect_host_counting.py
+++ awx/main/tests/live/tests/projects/test_indirect_host_counting.py
@@ -1,3 +1,7 @@
+import yaml
+
+from awx.main.tests.live.tests.conftest import wait_for_events
+
 from awx.main.tasks.host_indirect import build_indirect_host_data
 from awx.main.models import Job
 
@@ -5,10 +9,12 @@ from awx.main.models import Job
 def test_indirect_host_counting(live_tmp_folder, run_job_from_playbook):
     run_job_from_playbook('test_indirect_host_counting', 'run_task.yml', scm_url=f'file://{live_tmp_folder}/test_host_query')
     job = Job.objects.filter(name__icontains='test_indirect_host_counting').order_by('-created').first()
+    wait_for_events(job)  # We must wait for events because system tasks iterate on job.job_events.filter(...)
 
     # Data matches to awx/main/tests/data/projects/host_query/meta/event_query.yml
     # this just does things in-line to be a more localized test for the immediate testing
-    event_query = {'demo.query.example': '{canonical_facts: {host_name: .direct_host_name}, facts: {device_type: .device_type}}'}
+    module_jq_str = '{canonical_facts: {host_name: .direct_host_name}, facts: {device_type: .device_type}}'
+    event_query = {'demo.query.example': module_jq_str}
 
     # Run the task logic directly with local data
     results = build_indirect_host_data(job, event_query)
@@ -18,3 +24,12 @@ def test_indirect_host_counting(live_tmp_folder, run_job_from_playbook):
     # Asserts on data that will match to the input jq string from above
     assert host_audit_entry.canonical_facts == {'host_name': 'foo_host_default'}
     assert host_audit_entry.facts == {'device_type': 'Fake Host'}
+
+    # Test collection of data
+    assert 'demo.query' in job.installed_collections
+    assert 'host_query' in job.installed_collections['demo.query']
+    hq_text = job.installed_collections['demo.query']['host_query']
+    hq_data = yaml.safe_load(hq_text)
+    assert hq_data == {'demo.query.example': module_jq_str}
+
+    assert job.ansible_version
