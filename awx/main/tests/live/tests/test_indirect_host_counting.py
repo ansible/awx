56cecf62cbfff2cb45c6512b3b46198e70108c2b
AlanCoding <arominge@redhat.com>
2025-01-29 09:53:43 -0500

    Connect host audit model to jq logic via new task

--- awx/main/tests/live/tests/test_indirect_host_counting.py
+++ awx/main/tests/live/tests/test_indirect_host_counting.py
@@ -1,8 +1,9 @@
 import yaml
+import time
 
 from awx.main.tests.live.tests.conftest import wait_for_events
-
 from awx.main.tasks.host_indirect import build_indirect_host_data
+from awx.main.models.indirect_managed_node_audit import IndirectManagedNodeAudit
 from awx.main.models import Job
 
 
@@ -21,9 +22,12 @@ def test_indirect_host_counting(live_tmp_folder, run_job_from_playbook):
     assert len(results) == 1
     host_audit_entry = results[0]
 
+    canonical_facts = {'host_name': 'foo_host_default'}
+    facts = {'device_type': 'Fake Host'}
+
     # Asserts on data that will match to the input jq string from above
-    assert host_audit_entry.canonical_facts == {'host_name': 'foo_host_default'}
-    assert host_audit_entry.facts == {'device_type': 'Fake Host'}
+    assert host_audit_entry.canonical_facts == canonical_facts
+    assert host_audit_entry.facts == facts
 
     # Test collection of data
     assert 'demo.query' in job.installed_collections
@@ -33,3 +37,17 @@ def test_indirect_host_counting(live_tmp_folder, run_job_from_playbook):
     assert hq_data == {'demo.query.example': module_jq_str}
 
     assert job.ansible_version
+
+    # This will poll, because it depends on the background task finishing
+    for _ in range(10):
+        if IndirectManagedNodeAudit.objects.filter(job=job).exists():
+            break
+        time.sleep(0.2)
+    else:
+        raise RuntimeError(f'No IndirectManagedNodeAudit records ever populated for job_id={job.id}')
+
+    assert IndirectManagedNodeAudit.objects.filter(job=job).count() == 1
+    host_audit = IndirectManagedNodeAudit.objects.filter(job=job).first()
+    assert host_audit.canonical_facts == canonical_facts
+    assert host_audit.facts == facts
+    assert host_audit.organization == job.organization
