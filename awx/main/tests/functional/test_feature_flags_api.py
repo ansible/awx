e8fcbd080790b466e5cc71ac719ffc32fcbcedd5
Zack Kayyali <zkayyali812@gmail.com>
2025-01-29 16:28:39 -0500

    Use updated API /feature_flags_state/

--- awx/main/tests/functional/test_feature_flags_api.py
+++ awx/main/tests/functional/test_feature_flags_api.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import pytest
+from django.test import override_settings
 
 from awx.main.models import (  # noqa
     User,
@@ -10,6 +11,28 @@ from awx.main.models import (  # noqa
 def test_feature_flags_list_endpoint(get):
     bob = User.objects.create(username='bob', password='test_user', is_superuser=False)
 
-    url = "/api/v2/feature_flags_definition/"
+    url = "/api/v2/feature_flags_state/"
     response = get(url, user=bob, expect=200)
     assert len(response.data) == 1
+
+
+@override_settings(
+    FLAGS={
+        "FEATURE_SOME_PLATFORM_FLAG_ENABLED": [
+            {"condition": "boolean", "value": False},
+            {"condition": "before date", "value": "2022-06-01T12:00Z"},
+        ],
+        "FEATURE_SOME_PLATFORM_FLAG_FOO_ENABLED": [
+            {"condition": "boolean", "value": True},
+        ],
+    }
+)
+@pytest.mark.django_db
+def test_feature_flags_list_endpoint_override(get):
+    bob = User.objects.create(username='bob', password='test_user', is_superuser=False)
+
+    url = "/api/v2/feature_flags_state/"
+    response = get(url, user=bob, expect=200)
+    assert len(response.data) == 2
+    assert response.data["FEATURE_SOME_PLATFORM_FLAG_ENABLED"] is False
+    assert response.data["FEATURE_SOME_PLATFORM_FLAG_FOO_ENABLED"] is True
