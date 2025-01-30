af2a986fcd033c7a28684bb3f162f5f25beef318
Alan Rominger <arominge@redhat.com>
2025-01-30 07:45:52 -0500

    Document, implement, and test remaining indirect host audit fields (#15796)
    * Document, implement, and test remaining indirect host audit fields
    
    * Fix hashing

--- awx/main/tasks/host_indirect.py
+++ awx/main/tasks/host_indirect.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Tuple, Union
 
 import yaml
 
@@ -13,26 +14,74 @@ from awx.main.models import Job
 logger = logging.getLogger(__name__)
 
 
+class UnhashableFacts(RuntimeError):
+    pass
+
+
+def get_hashable_form(input_data: Union[dict, list, int, float, str, bool]) -> Tuple[Union[Tuple, dict, int, float]]:
+    "Given a dictionary of JSON types, return something that can be hashed and is the same data"
+    if isinstance(input_data, (int, float, str, bool)):
+        return input_data  # return scalars as-is
+    if isinstance(input_data, dict):
+        # Can't hash because we got a dict? Make the dict a tuple of tuples.
+        # Can't hash the data in the tuple in the tuple? We'll make tuples out of them too.
+        return tuple(sorted(((get_hashable_form(k), get_hashable_form(v)) for k, v in input_data.items())))
+    elif isinstance(input_data, (list, tuple)):
+        # Nested list data might not be hashable, and lists were never hashable in the first place
+        return tuple(get_hashable_form(item) for item in input_data)
+    raise UnhashableFacts(f'Cannonical facts contains a {type(input_data)} type which can not be hashed.')
+
+
 def build_indirect_host_data(job, job_event_queries: dict[str, str]) -> list[IndirectManagedNodeAudit]:
-    results = []
+    results = {}
     compiled_jq_expressions = {}  # Cache for compiled jq expressions
     facts_missing_logged = False
+    unhashable_facts_logged = False
     for event in job.job_events.filter(task__in=job_event_queries.keys()).iterator():
         if 'res' not in event.event_data:
             continue
+
+        # Recall from cache, or process the jq expression, and loop over the jq results
         jq_str_for_event = job_event_queries[event.task]
         if jq_str_for_event not in compiled_jq_expressions:
             compiled_jq_expressions[event.task] = jq.compile(jq_str_for_event)
         compiled_jq = compiled_jq_expressions[event.task]
         for data in compiled_jq.input(event.event_data['res']).all():
+
+            # From this jq result (specific to a single Ansible module), get index information about this host record
             if not data.get('canonical_facts'):
                 if not facts_missing_logged:
                     logger.error(f'jq output missing canonical_facts for module {event.task} on event {event.id} using jq:{jq_str_for_event}')
                 continue
             canonical_facts = data['canonical_facts']
+            try:
+                hashable_facts = get_hashable_form(canonical_facts)
+            except UnhashableFacts:
+                if not unhashable_facts_logged:
+                    logger.info(f'Could not hash canonical_facts {canonical_facts}, skipping')
+                    unhashable_facts_logged = True
+                continue
+
+            # Obtain the record based on the hashable canonical_facts now determined
             facts = data.get('facts')
-            results.append(IndirectManagedNodeAudit(canonical_facts=canonical_facts, facts=facts, job=job, organization=job.organization))
-    return results
+            if hashable_facts in results:
+                audit_record = results[hashable_facts]
+            else:
+                audit_record = IndirectManagedNodeAudit(
+                    canonical_facts=canonical_facts,
+                    facts=facts,
+                    job=job,
+                    organization=job.organization,
+                    name=event.host_name,
+                )
+                results[hashable_facts] = audit_record
+
+            # Increment rolling count fields
+            if event.task not in audit_record.events:
+                audit_record.events.append(event.task)
+            audit_record.count += 1
+
+    return list(results.values())
 
 
 def fetch_job_event_query(job) -> dict[str, str]:
@@ -59,3 +108,5 @@ def save_indirect_host_entries(job_id):
     job_event_queries = fetch_job_event_query(job)
     records = build_indirect_host_data(job, job_event_queries)
     IndirectManagedNodeAudit.objects.bulk_create(records)
+    job.event_queries_processed = True
+    job.save(update_fields=['event_queries_processed'])
--- awx/main/tests/functional/tasks/test_host_indirect.py
+++ awx/main/tests/functional/tasks/test_host_indirect.py
@@ -2,7 +2,7 @@ import yaml
 
 import pytest
 
-from awx.main.tasks.host_indirect import build_indirect_host_data, fetch_job_event_query, save_indirect_host_entries
+from awx.main.tasks.host_indirect import build_indirect_host_data, fetch_job_event_query, save_indirect_host_entries, get_hashable_form
 from awx.main.models.event_query import EventQuery
 from awx.main.models.indirect_managed_node_audit import IndirectManagedNodeAudit
 
@@ -16,23 +16,33 @@ TEST_JQ = "{canonical_facts: {host_name: .direct_host_name}, facts: {another_hos
 @pytest.fixture
 def bare_job(job_factory):
     job = job_factory()
-    job.installed_collections = {'demo.query.example': {'version': '1.0.1'}}
+    job.installed_collections = {'demo.query': {'version': '1.0.1'}, 'demo2.query': {'version': '1.0.1'}}
     job.save(update_fields=['installed_collections'])
     return job
 
 
+def create_registered_event(job, task_name='demo.query.example'):
+    return job.job_events.create(task=task_name, event_data={'res': {'direct_host_name': 'foo_host'}})
+
+
 @pytest.fixture
 def job_with_counted_event(bare_job):
-    bare_job.job_events.create(task='demo.query.example', event_data={'res': {'direct_host_name': 'foo_host'}})
+    create_registered_event(bare_job)
     return bare_job
+
+
+def create_event_query(fqcn='demo.query'):
+    module_name = f'{fqcn}.example'
+    return EventQuery.objects.create(fqcn=fqcn, collection_version='1.0.1', event_query=yaml.dump({module_name: TEST_JQ}, default_flow_style=False))
 
 
 @pytest.fixture
 def event_query():
     "This is ordinarily created by the artifacts callback"
-    return EventQuery.objects.create(
-        fqcn='demo.query.example', collection_version='1.0.1', event_query=yaml.dump({'demo.query.example': TEST_JQ}, default_flow_style=False)
-    )
+    return create_event_query()
+
+
+# ---- end fixtures ----
 
 
 @pytest.mark.django_db
@@ -50,15 +60,140 @@ def test_collect_an_event(job_with_counted_event):
 
 
 @pytest.mark.django_db
-def test_fetch_job_event_query(job_with_counted_event, event_query):
-    assert fetch_job_event_query(job_with_counted_event) == {'demo.query.example': TEST_JQ}
+def test_fetch_job_event_query(bare_job, event_query):
+    assert fetch_job_event_query(bare_job) == {'demo.query.example': TEST_JQ}
+
+
+@pytest.mark.django_db
+def test_fetch_multiple_job_event_query(bare_job):
+    create_event_query(fqcn='demo.query')
+    create_event_query(fqcn='demo2.query')
+    assert fetch_job_event_query(bare_job) == {'demo.query.example': TEST_JQ, 'demo2.query.example': TEST_JQ}
 
 
 @pytest.mark.django_db
 def test_save_indirect_host_entries(job_with_counted_event, event_query):
+    assert job_with_counted_event.event_queries_processed is False
     save_indirect_host_entries(job_with_counted_event.id)
+    job_with_counted_event.refresh_from_db()
+    assert job_with_counted_event.event_queries_processed is True
     assert IndirectManagedNodeAudit.objects.filter(job=job_with_counted_event).count() == 1
     host_audit = IndirectManagedNodeAudit.objects.filter(job=job_with_counted_event).first()
+    assert host_audit.count == 1
     assert host_audit.canonical_facts == {'host_name': 'foo_host'}
     assert host_audit.facts == {'another_host_name': 'foo_host'}
     assert host_audit.organization == job_with_counted_event.organization
+
+
+@pytest.mark.django_db
+def test_multiple_events_same_module_same_host(bare_job, event_query):
+    "This tests that the count field gives correct answers"
+    create_registered_event(bare_job)
+    create_registered_event(bare_job)
+    create_registered_event(bare_job)
+
+    save_indirect_host_entries(bare_job.id)
+
+    assert IndirectManagedNodeAudit.objects.filter(job=bare_job).count() == 1
+    host_audit = IndirectManagedNodeAudit.objects.filter(job=bare_job).first()
+
+    assert host_audit.count == 3
+    assert host_audit.events == ['demo.query.example']
+
+
+@pytest.mark.django_db
+def test_multiple_registered_modules(bare_job):
+    "This tests that the events will list multiple modules if more than 1 module from different collections is registered and used"
+    create_registered_event(bare_job, task_name='demo.query.example')
+    create_registered_event(bare_job, task_name='demo2.query.example')
+
+    # These take the place of using the event_query fixture
+    create_event_query(fqcn='demo.query')
+    create_event_query(fqcn='demo2.query')
+
+    save_indirect_host_entries(bare_job.id)
+
+    assert IndirectManagedNodeAudit.objects.filter(job=bare_job).count() == 1
+    host_audit = IndirectManagedNodeAudit.objects.filter(job=bare_job).first()
+
+    assert host_audit.count == 2
+    assert set(host_audit.events) == {'demo.query.example', 'demo2.query.example'}
+
+
+@pytest.mark.django_db
+def test_multiple_registered_modules_same_collection(bare_job):
+    "This tests that the events will list multiple modules if more than 1 module in same collection is registered and used"
+    create_registered_event(bare_job, task_name='demo.query.example')
+    create_registered_event(bare_job, task_name='demo.query.example2')
+
+    # Takes place of event_query fixture, doing manually here
+    EventQuery.objects.create(
+        fqcn='demo.query',
+        collection_version='1.0.1',
+        event_query=yaml.dump(
+            {
+                'demo.query.example': TEST_JQ,
+                'demo.query.example2': TEST_JQ,
+            },
+            default_flow_style=False,
+        ),
+    )
+
+    save_indirect_host_entries(bare_job.id)
+
+    assert IndirectManagedNodeAudit.objects.filter(job=bare_job).count() == 1
+    host_audit = IndirectManagedNodeAudit.objects.filter(job=bare_job).first()
+
+    assert host_audit.count == 2
+    assert set(host_audit.events) == {'demo.query.example', 'demo.query.example2'}
+
+
+class TestHashableForm:
+    def test_same_dict(self):
+        assert get_hashable_form({'a': 'b'}) == get_hashable_form({'a': 'b'})
+
+    def test_same_list(self):
+        assert get_hashable_form(['a', 'b']) == get_hashable_form(['a', 'b'])
+        assert get_hashable_form(('a', 'b')) == get_hashable_form(('a', 'b'))
+
+    def test_different_list(self):
+        assert get_hashable_form(['a', 'b']) != get_hashable_form(['a', 'c'])
+        assert get_hashable_form(('a', 'b')) != get_hashable_form(('a', 'c'))
+
+    def test_values_different(self):
+        assert get_hashable_form({'a': 'b'}) != get_hashable_form({'a': 'c'})
+
+    def test_has_extra_key(self):
+        assert get_hashable_form({'a': 'b'}) != get_hashable_form({'a': 'b', 'c': 'd'})
+
+    def test_nested_dictionaries_different(self):
+        assert get_hashable_form({'a': {'b': 'c'}}) != get_hashable_form({'a': {'b': 'd'}})
+
+    def test_nested_dictionaries_same(self):
+        assert get_hashable_form({'a': {'b': 'c'}}) == get_hashable_form({'a': {'b': 'c'}})
+
+    def test_nested_lists_different(self):
+        assert get_hashable_form({'a': ['b', 'c']}) != get_hashable_form({'a': ['b', 'd']})
+        assert get_hashable_form({'a': ('b', 'c')}) != get_hashable_form({'a': ('b', 'd')})
+
+    def test_nested_lists_same(self):
+        assert get_hashable_form({'a': ['b', 'c']}) == get_hashable_form({'a': ['b', 'c']})
+        assert get_hashable_form({'a': ('b', 'c')}) == get_hashable_form({'a': ('b', 'c')})
+        assert hash(get_hashable_form({'a': ['b', 'c']})) == hash(get_hashable_form({'a': ['b', 'c']}))
+
+    def test_list_nested_lists_different(self):
+        assert get_hashable_form(['a', ['b', 'c']]) != get_hashable_form(['a', ['b', 'd']])
+        assert get_hashable_form(['a', ('b', 'c')]) != get_hashable_form(['a', ('b', 'd')])
+
+    def test_list_nested_lists_same(self):
+        assert get_hashable_form(['a', ['b', 'c']]) == get_hashable_form(['a', ['b', 'c']])
+        assert get_hashable_form(['a', ('b', 'c')]) == get_hashable_form(['a', ('b', 'c')])
+        assert hash(get_hashable_form(['a', ('b', 'c')])) == hash(get_hashable_form(['a', ('b', 'c')]))
+
+    def test_list_nested_dicts_different(self):
+        assert get_hashable_form(['a', {'b': 'c'}]) != get_hashable_form(['a', {'b': 'd'}])
+        assert hash(get_hashable_form(['a', {'b': 'c'}])) != hash(get_hashable_form(['a', {'b': 'd'}]))
+
+    def test_list_nested_dicts_same(self):
+        assert get_hashable_form(['a', {'b': 'c'}]) == get_hashable_form(['a', {'b': 'c'}])
+        assert hash(get_hashable_form(['a', {'b': 'c'}])) == hash(get_hashable_form(['a', {'b': 'c'}]))
