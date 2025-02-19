b4ae05f3d5b5cb8bbd29fa3a84986fd9267b8e6b
Andrea Restle-Lay <arestlel@redhat.com>
2025-02-19 17:55:50 -0500

    fix unit tests

--- awx/main/tests/unit/utils/test_common.py
+++ awx/main/tests/unit/utils/test_common.py
@@ -240,7 +240,15 @@ def test_extract_ansible_vars():
         ('git', 'https://example.com/bar.git', 'user', 'pw', True, False, 'https://user:pw@example.com/bar.git'),
         ('git', 'https://example@example.com/bar.git', False, 'something', True, False, 'https://example.com/bar.git'),
         # Special github/bitbucket cases
-        ('git', 'notgit@github.com:ansible/awx.git', True, True, True, False, ValueError('Username must be "git" or "x-access-token" (for github app) for SSH access to github.com.')),
+        (
+            'git',
+            'notgit@github.com:ansible/awx.git',
+            True,
+            True,
+            True,
+            False,
+            ValueError('Username must be "git" or "x-access-token" (for github app) for SSH access to github.com.'),
+        ),
         (
             'git',
             'notgit@bitbucket.org:does-not-exist/example.git',
