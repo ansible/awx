c7ac564e77835a847d84e85e58e6b68ca6752a8a
Seth Foster <fosterbseth@gmail.com>
2025-02-27 22:12:17 -0500

    Pass in private_data_dir when project update is on K8S
    Signed-off-by: Seth Foster <fosterbseth@gmail.com>

--- awx/main/tasks/jobs.py
+++ awx/main/tasks/jobs.py
@@ -522,9 +522,13 @@ class BaseTask(object):
 
             credentials = self.build_credentials_list(self.instance)
 
+            inject_credential_kwargs = {}
+            if settings.IS_K8S and isinstance(self.instance, ProjectUpdate):
+                inject_credential_kwargs['incontainer_path'] = private_data_dir
+
             for credential in credentials:
                 if credential:
-                    credential.credential_type.inject_credential(credential, env, self.safe_cred_env, args, private_data_dir)
+                    credential.credential_type.inject_credential(credential, env, self.safe_cred_env, args, private_data_dir, inject_credential_kwargs)
 
             self.runner_callback.safe_env.update(self.safe_cred_env)
 
