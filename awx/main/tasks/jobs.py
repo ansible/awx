8104f730a20e95c5020697028e243fdc0e49a2f5
Alan Rominger <arominge@redhat.com>
2025-04-09 15:13:04 -0400

    Fully handle DispatcherCancel

--- awx/main/tasks/jobs.py
+++ awx/main/tasks/jobs.py
@@ -33,6 +33,7 @@ from gitdb.exc import BadName as BadGitName
 
 # AWX
 from dispatcherd.publish import task
+from dispatcherd.worker.task import DispatcherCancel
 from dispatcherd.utils import serialize_task
 from awx.main.dispatch import get_task_queuename
 from awx.main.constants import (
@@ -628,14 +629,14 @@ class BaseTask(object):
             elif status == 'canceled':
                 self.instance = self.update_model(pk)
                 cancel_flag_value = getattr(self.instance, 'cancel_flag', False)
-                if (cancel_flag_value is False) and signal_callback():
+                if cancel_flag_value is False:
                     self.runner_callback.delay_update(skip_if_already_set=True, job_explanation="Task was canceled due to receiving a shutdown signal.")
-                    status = 'failed'
-                elif cancel_flag_value is False:
-                    self.runner_callback.delay_update(skip_if_already_set=True, job_explanation="The running ansible process received a shutdown signal.")
                     status = 'failed'
         except ReceptorNodeNotFound as exc:
             self.runner_callback.delay_update(job_explanation=str(exc))
+        except DispatcherCancel:
+            # dispatcher uses non-sigterm signal, and will raise this exception
+            status = 'canceled'
         except Exception:
             # this could catch programming or file system errors
             self.runner_callback.delay_update(result_traceback=traceback.format_exc())
