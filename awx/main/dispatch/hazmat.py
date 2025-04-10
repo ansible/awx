344324c101d413a71359d1ada7c8a0a48a30ab47
AlanCoding <arominge@redhat.com>
2025-04-10 13:25:23 -0400

    Complete rest of preload import work

--- awx/main/dispatch/hazmat.py
+++ awx/main/dispatch/hazmat.py
@@ -1,5 +1,11 @@
 import django
 
+# dispatcherd publisher logic is likely to be used, but needs manual preload
+from dispatcherd.brokers import pg_notify  # noqa
+
+# Cache may not be initialized until we are in the worker, so preload here
+from channels_redis import core  # noqa
+
 from awx import prepare_env
 
 from dispatcherd.utils import resolve_callable
@@ -16,6 +22,10 @@ from django.conf import settings
 # Preload all periodic tasks so their imports will be in shared memory
 for name, options in settings.CELERYBEAT_SCHEDULE.items():
     resolve_callable(options['task'])
+
+
+# Preload in-line import from tasks
+from awx.main.scheduler.kubernetes import PodManager  # noqa
 
 
 from django.core.cache import cache as django_cache
