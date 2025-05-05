7556887e8d5cfe599327820488de123753c1ef84
Alan Rominger <arominge@redhat.com>
2025-05-05 15:02:45 -0400

    Bump migrations of existing

--- awx/main/migrations/0202_alter_inventorysource_source_and_more.py
+++ awx/main/migrations/0202_alter_inventorysource_source_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0200_remove_sso_app_content'),
+        ('main', '0201_remove_sso_app_content'),
     ]
 
     operations = [
