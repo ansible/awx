fbb2644b756197b70b00dd2e92789d50d2da023b
Alan Rominger <arominge@redhat.com>
2025-05-14 23:23:18 -0400

    Update test

--- awx/main/tests/functional/tasks/test_tasks_jobs.py
+++ awx/main/tests/functional/tasks/test_tasks_jobs.py
@@ -1,3 +1,5 @@
+import logging
+
 import pytest
 
 from awx.main.tasks.jobs import RunJob
@@ -18,16 +20,14 @@ def test_does_not_run_reaped_job(mocker, mock_me):
 
 
 @pytest.mark.django_db
-def test_cancel_flag_on_start(jt_linked):
+def test_cancel_flag_on_start(jt_linked, caplog):
     job = jt_linked.create_unified_job()
     job.status = 'waiting'
     job.cancel_flag = True
     job.save()
 
     task = RunJob()
-    with pytest.raises(RuntimeError) as exc:
-        task.run(job.id)
-    assert 'because its status "canceled" is not expected' in str(exc)
+    task.run(job.id)
 
     job = Job.objects.get(id=job.id)
     assert job.status == 'canceled'
