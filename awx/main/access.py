326002c2d207114c21b8659629b0ea94740cd4c6
Hao Liu <44379968+TheRealHaoLiu@users.noreply.github.com>
2025-06-05 14:27:51 -0400

    Remove RBAC system conditionals from API layer
    - Remove ANSIBLE_BASE_ROLE_SYSTEM_ACTIVATED checks from API serializers
    - Simplify ResourceAccessList to use only new RBAC system
    - Remove conditional logic from access control classes
    - Clean up role permission handling in API generics
    - Remove legacy role ancestor and member management code
    
    This commit removes all conditional logic that would fall back to the
    old RBAC system, ensuring the API layer exclusively uses the new
    ansible-base RBAC system for all permission checks and role management.

--- awx/main/access.py
+++ awx/main/access.py
@@ -703,16 +703,12 @@ class UserAccess(BaseAccess):
             if not allow_orphans:
                 # in these cases only superusers can modify orphan users
                 return False
-            if settings.ANSIBLE_BASE_ROLE_SYSTEM_ACTIVATED:
-                # Permission granted if the user has all permissions that the target user has
-                target_perms = set(
-                    RoleEvaluation.objects.filter(role__in=obj.has_roles.all()).values_list('object_id', 'content_type_id', 'codename').distinct()
-                )
-                user_perms = set(
-                    RoleEvaluation.objects.filter(role__in=self.user.has_roles.all()).values_list('object_id', 'content_type_id', 'codename').distinct()
-                )
-                return not (target_perms - user_perms)
-            return not obj.roles.all().exclude(ancestors__in=self.user.roles.all()).exists()
+            # Permission granted if the user has all permissions that the target user has
+            target_perms = set(RoleEvaluation.objects.filter(role__in=obj.has_roles.all()).values_list('object_id', 'content_type_id', 'codename').distinct())
+            user_perms = set(
+                RoleEvaluation.objects.filter(role__in=self.user.has_roles.all()).values_list('object_id', 'content_type_id', 'codename').distinct()
+            )
+            return not (target_perms - user_perms)
         else:
             return self.is_all_org_admin(obj)
 
@@ -1319,12 +1315,8 @@ class ExecutionEnvironmentAccess(BaseAccess):
     def can_change(self, obj, data):
         if obj and obj.organization_id is None:
             raise PermissionDenied
-        if settings.ANSIBLE_BASE_ROLE_SYSTEM_ACTIVATED:
-            if not self.user.has_obj_perm(obj, 'change'):
-                return False
-        else:
-            if self.user not in obj.organization.execution_environment_admin_role:
-                raise PermissionDenied
+        if not self.user.has_obj_perm(obj, 'change'):
+            return False
         if not self.check_related('organization', Organization, data, obj=obj, role_field='execution_environment_admin_role'):
             return False
         # Special case that check_related does not catch, org users can not remove the organization from the EE
@@ -2531,9 +2523,7 @@ class ScheduleAccess(UnifiedCredentialsMixin, BaseAccess):
         if not JobLaunchConfigAccess(self.user).can_add(data):
             return False
         if not data:
-            if settings.ANSIBLE_BASE_ROLE_SYSTEM_ACTIVATED:
-                return self.user.has_roles.filter(permission_partials__codename__in=['execute_jobtemplate', 'update_project', 'update_inventory']).exists()
-            return Role.objects.filter(role_field__in=['update_role', 'execute_role'], ancestors__in=self.user.roles.all()).exists()
+            return self.user.has_roles.filter(permission_partials__codename__in=['execute_jobtemplate', 'update_project', 'update_inventory']).exists()
 
         return self.check_related('unified_job_template', UnifiedJobTemplate, data, role_field='execute_role', mandatory=True)
 
@@ -2561,11 +2551,7 @@ class NotificationTemplateAccess(BaseAccess):
     prefetch_related = ('created_by', 'modified_by', 'organization')
 
     def filtered_queryset(self):
-        if settings.ANSIBLE_BASE_ROLE_SYSTEM_ACTIVATED:
-            return self.model.access_qs(self.user, 'view')
-        return self.model.objects.filter(
-            Q(organization__in=Organization.access_qs(self.user, 'add_notificationtemplate')) | Q(organization__in=self.user.auditor_of_organizations)
-        ).distinct()
+        return self.model.access_qs(self.user, 'view')
 
     @check_superuser
     def can_add(self, data):
