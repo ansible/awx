fca1aca6e7ef6b6b569ad9982eb0015d8172d436
AlanCoding <arominge@redhat.com>
2025-10-15 10:22:47 -0400

    Fix f-string in log that is broken

--- awx/main/tasks/jobs.py
+++ awx/main/tasks/jobs.py
@@ -1321,7 +1321,7 @@ class RunProjectUpdate(BaseTask):
 
         galaxy_creds_are_defined = project_update.project.organization and project_update.project.organization.galaxy_credentials.exists()
         if not galaxy_creds_are_defined and (settings.AWX_ROLES_ENABLED or settings.AWX_COLLECTIONS_ENABLED):
-            logger.warning('Galaxy role/collection syncing is enabled, but no credentials are configured for {project_update.project.organization}.')
+            logger.warning(f'Galaxy role/collection syncing is enabled, but no credentials are configured for {project_update.project.organization}.')
 
         extra_vars.update(
             {
