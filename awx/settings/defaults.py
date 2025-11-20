503cc5278c4f9d86a35a125d1aadef94c58767b6
AlanCoding <arominge@redhat.com>
2025-11-20 12:40:47 -0500

    Add back in DAB tags

--- awx/settings/defaults.py
+++ awx/settings/defaults.py
@@ -309,6 +309,10 @@ TEMPLATES = [
                 'awx.ui.context_processors.csp',
                 'awx.ui.context_processors.version',
             ],
+            'libraries': {
+                "ansible_base.lib.templatetags.requests": "ansible_base.lib.templatetags.requests",
+                "ansible_base.lib.templatetags.util": "ansible_base.lib.templatetags.util",
+            },
         },
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
