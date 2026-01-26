7ce75541ed3a54670126421486172fa69a7fa836
AlanCoding <arominge@redhat.com>
2026-01-26 15:08:03 -0500

    Move unwanted commands back to run_dispatcher

--- awx/main/management/commands/dispatcherd.py
+++ awx/main/management/commands/dispatcherd.py
@@ -7,47 +7,19 @@ import logging
 import logging.config
 import os
 
-import yaml
-
 from django.conf import settings
 from django.core.cache import cache as django_cache
 from django.core.management.base import BaseCommand, CommandError
 from django.db import connection
 
 from dispatcherd.config import setup as dispatcher_setup
-from dispatcherd.factories import get_control_from_settings
 
 from awx.main.dispatch.config import get_dispatcherd_config
 
 logger = logging.getLogger('awx.main.dispatch')
 
 
-def _dispatcherd_entrypoint():
-    try:
-        from dispatcherd import cli as dispatcherd_cli
-    except Exception:
-        from dispatcherd import run_service
-
-        return run_service
-
-    for name in ('dispatcherd_main', 'main'):
-        entrypoint = getattr(dispatcherd_cli, name, None)
-        if callable(entrypoint):
-            return entrypoint
-
-    try:
-        import dispatcherd.__main__ as dispatcherd_main_module
-    except Exception:
-        dispatcherd_main_module = None
-
-    if dispatcherd_main_module is not None:
-        entrypoint = getattr(dispatcherd_main_module, 'main', None)
-        if callable(entrypoint):
-            return entrypoint
-
-    from dispatcherd import run_service
-
-    return run_service
+from dispatcherd import run_service
 
 
 def _json_default(value):
@@ -64,56 +36,19 @@ def _hash_config(config):
 
 
 class Command(BaseCommand):
-    help = 'Launch the task dispatcher'
+    help = (
+        'Run the background task service, this is the supported entrypoint since the introduction of dispatcherd as a library. '
+        'This replaces the prior awx-manage run_dispatcher service, and control actions are at awx-manage dispatcherctl.'
+    )
 
     def add_arguments(self, parser):
-        parser.add_argument('--status', dest='status', action='store_true', help='print the internal state of any running dispatchers')
-        parser.add_argument('--running', dest='running', action='store_true', help='print the UUIDs of any tasked managed by this dispatcher')
-        parser.add_argument(
-            '--cancel',
-            dest='cancel',
-            help=(
-                'Cancel a particular task id. Takes either a single id string, or a JSON list of multiple ids. '
-                'Can take in output from the --running argument as input to cancel all tasks. '
-                'Only running tasks can be canceled, queued tasks must be started before they can be canceled.'
-            ),
-        )
+        return
 
     def handle(self, *arg, **options):
-        if options.get('status'):
-            ctl = get_control_from_settings()
-            running_data = ctl.control_with_reply('status')
-            if len(running_data) != 1:
-                raise CommandError('Did not receive expected number of replies')
-            print(yaml.dump(running_data[0], default_flow_style=False))
-            return
-        if options.get('running'):
-            ctl = get_control_from_settings()
-            running_data = ctl.control_with_reply('running')
-            print(yaml.dump(running_data, default_flow_style=False))
-            return
-        if options.get('cancel'):
-            cancel_str = options.get('cancel')
-            try:
-                cancel_data = yaml.safe_load(cancel_str)
-            except Exception:
-                cancel_data = [cancel_str]
-            if not isinstance(cancel_data, list):
-                cancel_data = [cancel_str]
-
-            ctl = get_control_from_settings()
-            results = []
-            for task_id in cancel_data:
-                # For each task UUID, send an individual cancel command
-                result = ctl.control_with_reply('cancel', data={'uuid': task_id})
-                results.append(result)
-            print(yaml.dump(results, default_flow_style=False))
-            return
-
-        self.configure_dispatcher_logging()
         if os.getenv('DISPATCHERD_CONFIG_FILE'):
             raise CommandError('DISPATCHERD_CONFIG_FILE is set but awx-manage dispatcherd uses dynamic config from code')
 
+        self.configure_dispatcher_logging()
         config = get_dispatcherd_config(for_service=True)
         config_hash = _hash_config(config)
         logger.info(
@@ -126,7 +61,7 @@ class Command(BaseCommand):
         django_cache.close()
         dispatcher_setup(config)
 
-        _dispatcherd_entrypoint()()
+        run_service()
 
     def configure_dispatcher_logging(self):
         # Apply special log rule for the parent process
--- awx/main/management/commands/run_dispatcher.py
+++ awx/main/management/commands/run_dispatcher.py
@@ -2,6 +2,12 @@
 # All Rights Reserved.
 import logging
 
+import yaml
+
+from django.core.management.base import CommandError
+
+from dispatcherd.factories import get_control_from_settings
+
 from awx.main.management.commands.dispatcherd import Command as DispatcherdCommand
 
 logger = logging.getLogger('awx.main.dispatch')
@@ -10,6 +16,49 @@ logger = logging.getLogger('awx.main.dispatch')
 class Command(DispatcherdCommand):
     help = 'Launch the task dispatcher (deprecated; use awx-manage dispatcherd)'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--status', dest='status', action='store_true', help='print the internal state of any running dispatchers')
+        parser.add_argument('--running', dest='running', action='store_true', help='print the UUIDs of any tasked managed by this dispatcher')
+        parser.add_argument(
+            '--cancel',
+            dest='cancel',
+            help=(
+                'Cancel a particular task id. Takes either a single id string, or a JSON list of multiple ids. '
+                'Can take in output from the --running argument as input to cancel all tasks. '
+                'Only running tasks can be canceled, queued tasks must be started before they can be canceled.'
+            ),
+        )
+        super().add_arguments(parser)
+
     def handle(self, *args, **options):
         logger.warning('awx-manage run_dispatcher is deprecated; use awx-manage dispatcherd')
+        if options.get('status'):
+            ctl = get_control_from_settings()
+            running_data = ctl.control_with_reply('status')
+            if len(running_data) != 1:
+                raise CommandError('Did not receive expected number of replies')
+            print(yaml.dump(running_data[0], default_flow_style=False))
+            return
+        if options.get('running'):
+            ctl = get_control_from_settings()
+            running_data = ctl.control_with_reply('running')
+            print(yaml.dump(running_data, default_flow_style=False))
+            return
+        if options.get('cancel'):
+            cancel_str = options.get('cancel')
+            try:
+                cancel_data = yaml.safe_load(cancel_str)
+            except Exception:
+                cancel_data = [cancel_str]
+            if not isinstance(cancel_data, list):
+                cancel_data = [cancel_str]
+
+            ctl = get_control_from_settings()
+            results = []
+            for task_id in cancel_data:
+                # For each task UUID, send an individual cancel command
+                result = ctl.control_with_reply('cancel', data={'uuid': task_id})
+                results.append(result)
+            print(yaml.dump(results, default_flow_style=False))
+            return
         return super().handle(*args, **options)
