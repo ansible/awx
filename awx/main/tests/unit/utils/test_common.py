31cbe93b85a2141971390c2305efdf1f5e1a7771
Alan Rominger <arominge@redhat.com>
2026-02-12 12:59:43 -0500

    Use raw-strings and regex markers for matching exception pattern
    Co-authored-by: 🇺🇦 Sviatoslav Sydorenko (Святослав Сидоренко) <wk.cvs.github@sydorenko.org.ua>

--- awx/main/tests/unit/utils/test_common.py
+++ awx/main/tests/unit/utils/test_common.py
@@ -330,12 +330,12 @@ class TestHostnameRegexValidator:
 
     def test_bad_call(self, regex_expr, re_flags):
         h = HostnameRegexValidator(regex=regex_expr, flags=re_flags)
-        with pytest.raises(ValidationError, match="illegal characters detected"):
+        with pytest.raises(ValidationError, match=r"^illegal characters detected$"):
             h("@#$%)$#(TUFAS_DG")
 
     def test_good_call_with_inverse(self, regex_expr, re_flags, inverse_match=True):
         h = HostnameRegexValidator(regex=regex_expr, flags=re_flags, inverse_match=inverse_match)
-        with pytest.raises(ValidationError, match="Enter a valid value"):
+        with pytest.raises(ValidationError, match=r"^Enter a valid value$"):
             h("1.2.3.4")
 
     def test_bad_call_with_inverse(self, regex_expr, re_flags, inverse_match=True):
