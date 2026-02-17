d2e51c4124d05f718cb5a37343382265c86b5419
Peter Braun <pbraun@redhat.com>
2026-02-17 10:13:33 +0100

    do not add optional survey fields with empty strings that are not backed by extra_vars

--- awx/main/models/mixins.py
+++ awx/main/models/mixins.py
@@ -188,6 +188,10 @@ class SurveyJobTemplateMixin(models.Model):
                             runtime_extra_vars.pop(variable_key)
 
                 if default is not None:
+                    # do not add variables that contain an empty string, are not required and are nor present in extra_vars
+                    if default == '' and not survey_element.get('required') and variable_key not in runtime_extra_vars:
+                        continue
+
                     decrypted_default = default
                     if survey_element['type'] == "password" and isinstance(decrypted_default, str) and decrypted_default.startswith('$encrypted$'):
                         decrypted_default = decrypt_value(get_encryption_key('value', pk=None), decrypted_default)
