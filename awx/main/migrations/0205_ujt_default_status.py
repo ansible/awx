a17ddb21c2c23d883577ebb08f315d0bfa6811f3
AlanCoding <arominge@redhat.com>
2026-02-19 15:07:25 -0500

    Bump migration again

--- awx/main/migrations/0205_ujt_default_status.py
+++ awx/main/migrations/0205_ujt_default_status.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('main', '0202_delete_token_cleanup_job'),
+        ('main', '0204_squashed_deletions'),
     ]
 
     operations = [
