f4e8d0273a9803cb5253c8212c30942473baeeac
Dirk Julich <djulich@redhat.com>
2026-06-17 18:16:57 +0200

    Use AddIndexConcurrently instead of AddIndex in the migration for host name lower index

--- awx/main/migrations/0206_add_host_name_lower_index.py
+++ awx/main/migrations/0206_add_host_name_lower_index.py
@@ -1,15 +1,17 @@
 from django.db import migrations, models
+from django.contrib.postgres.operations import AddIndexConcurrently
 from django.db.models.functions import Lower
 
 
 class Migration(migrations.Migration):
+    atomic = False
 
     dependencies = [
         ('main', '0205_add_ordering_to_instancegroup_and_workflow_nodes'),
     ]
 
     operations = [
-        migrations.AddIndex(
+        AddIndexConcurrently(
             model_name='host',
             index=models.Index(
                 Lower('name'),
