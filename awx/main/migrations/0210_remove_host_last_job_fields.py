e9e6413cd966545146d64e63ce6f3145e2125857
AlanCoding <arominge@redhat.com>
2026-08-26 08:26:51 -0400

    bump migration

--- awx/main/migrations/0210_remove_host_last_job_fields.py
+++ awx/main/migrations/0210_remove_host_last_job_fields.py
@@ -3,7 +3,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('main', '0208_fix_system_auditor_migration'),
+        ('main', '0209_fix_system_auditor_migration'),
     ]
 
     operations = [
