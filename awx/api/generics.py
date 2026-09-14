1cf84cf55f89a0e1921d39e718d38d7309b3dbfd
Stevenson Michel <iamstevensonmichel@outlook.com>
2026-09-14 16:24:44 -0400

    POC for deprecation headers

--- awx/api/generics.py
+++ awx/api/generics.py
@@ -297,6 +297,20 @@ class APIView(views.APIView):
             response['X-API-Query-Time'] = '%0.3fs' % sum(q_times)
 
         if getattr(self, 'deprecated', False):
+            # New deprecation headers per Controller POC (ANSTRAT-2346)
+            # Boolean signal - no version information
+            response['X-Deprecated'] = 'true'
+
+            # Optional detail message
+            detail = getattr(self, 'deprecation_detail', 'This resource has been deprecated')
+            response['X-Deprecated-Detail'] = detail
+
+            # Link to changelog/migration docs
+            link = getattr(self, 'deprecation_link',
+                          'https://docs.ansible.com/ansible-tower/latest/html/release-notes/deprecations.html')
+            response['Link'] = f'<{link}>; rel="deprecation"'
+
+            # Keep legacy Warning header for backward compatibility during transition
             response['Warning'] = '299 awx "This resource has been deprecated and will be removed in a future release."'
 
         return response
